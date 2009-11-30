6b32ab807391dd0176865074f19e29ddc8b0f940
Matthew Landauer <matthew@openaustralia.org>
2009-11-30 15:21:45 +1100

    Now loads the authorities table using the list of scraper feeds

--- authorities_data.sql
+++
@@ -1,6 +0,0 @@
-# Clear out the table first... I know, I know. Crude first bash
-DELETE FROM authority;
-
-INSERT INTO authority (full_name, short_name, feed_url, external, disabled, planning_email) values ("Blue Mountains City Council", "Blue Mountains", "http://localhost:4567/blue_mountains?year={year}&month={month}&day={day}", 1, 0, "council@bmcc.nsw.gov.au");
-INSERT INTO authority (full_name, short_name, feed_url, external, disabled, planning_email) values ("Brisbane City Council", "Brisbane", "http://localhost:4567/brisbane?year={year}&month={month}&day={day}", 1, 0, "unknown@unknown.org");
-INSERT INTO authority (full_name, short_name, feed_url, external, disabled, planning_email) values ("Gold Coast City Council", "Gold Coast", "http://localhost:4567/gold_coast?year={year}&month={month}&day={day}", 1, 0, "unknown@unknown.org");
---
+++ load_authorities_data.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+# First bash at a script for loading the list of planning authorities into the database.
+# Currently only supporting internal scrapers. Will be very easy to add external ones
+
+# This URL returns a list of scrapers
+internal_scraper_url = "http://localhost:9393/"
+
+require 'rubygems'
+require 'mechanize'
+require 'active_record'
+
+# Get the database connection information from the php configuration file
+File.open("#{File.dirname(__FILE__)}/docs/include/config.php") do |f|
+  unless f.readlines.grep(/DB_CONNECTION_STRING/).first =~ /'mysql:\/\/(\w+):(\w+)@(\w+)\/(\w+)'/
+    raise "Couldn't match structure of DB_CONNECTION_STRING line in php configuration file"
+  end
+end
+username, password, host, database = $~[1..4]
+
+ActiveRecord::Base.establish_connection(
+        :adapter  => "mysql",
+        :host     => host,
+        :username => username,
+        :password => password,
+        :database => database
+)
+
+class Authority < ActiveRecord::Base
+  set_table_name "authority"
+end
+
+# Brutal: blast away all the data in the authority table!
+Authority.delete_all
+
+agent = WWW::Mechanize.new
+page = agent.get(internal_scraper_url)
+Nokogiri::XML(page.body).search('scraper').each do |scraper|
+  authority = Authority.new(:full_name => scraper.at('authority_name').inner_text,
+    :short_name => scraper.at('authority_short_name').inner_text,
+    :feed_url => scraper.at('url').inner_text,
+    # TODO Find a way of setting the planning email address or maybe it's not used at all
+    :planning_email => "unknown@unknown.org",
+    :external => 1,
+    :disabled => 0)
+  authority.save!
+end
+
+puts "All is good."
+
