0061b99b42854a23078ae59dd2bd4adfd66eaa96
Matthew Landauer <matthew@openaustralia.org>
2009-12-07 11:30:50 +1100

    Now store the url of the page that returns a list of all the internal scrapers in the configuration file

--- load_authorities_data.rb
+++ load_authorities_data.rb
@@ -16,14 +16,11 @@ if lines.grep(/DB_CONNECTION_STRING/).first =~ /'mysql:\/\/(\w+):(\w+)@(\w+)\/(\
 else
   raise "Couldn't match structure of DB_CONNECTION_STRING line in php configuration file"
 end
-if lines.grep(/BASE_URL/).first =~ /'http(.+)'/
-  base_url = "http" + $~[1]
+if lines.grep(/INTERNAL_SCRAPERS_INDEX_URL/).first =~ /'http(.+)'/
+  internal_scrapers_index_url = "http" + $~[1]
 else
   raise "Couldn't match structure of DB_CONNECTION_STRING line in php configuration file"
 end
-
-# This URL returns a list of scrapers
-internal_scraper_url = "#{base_url}/scrapers/"
 
 ActiveRecord::Base.establish_connection(
         :adapter  => "mysql",
@@ -42,11 +39,11 @@ Authority.delete_all
 
 agent = WWW::Mechanize.new
 # Quick little hack to get around the fact that the test instance is password protected
-if internal_scraper_url == "http://test.planningalerts.org.au/scrapers/"
+if internal_scrapers_index_url == "http://test.planningalerts.org.au/scrapers/"
   agent.auth("test", "test")
 end
 
-page = agent.get(internal_scraper_url)
+page = agent.get(internal_scrapers_index_url)
 Nokogiri::XML(page.body).search('scraper').each do |scraper|
   authority = Authority.new(:full_name => scraper.at('authority_name').inner_text,
     :short_name => scraper.at('authority_short_name').inner_text,
