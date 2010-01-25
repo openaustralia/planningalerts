e59a726d3b49a87edaf174d3789fe25a26c87e30
Matthew Landauer <matthew@openaustralia.org>
2010-01-25 16:51:38 +1100

    Extracted example values and helper for creating google map from georss

--- app/helpers/api_howto_helper.rb
+++ app/helpers/api_howto_helper.rb
@@ -1,2 +1,6 @@
 module ApiHowtoHelper
+  # Turn a url for an arbitrary georss feed into a Google Map of that data
+  def mapify(url, zoom = 13)
+      "http://maps.google.com/maps?f=q&hl=en&layer=&ie=UTF8&z=#{zoom}&om=1&q=#{CGI.escape(url)}"
+  end
 end
--- spec/helpers/api_howto_helper_spec.rb
+++ spec/helpers/api_howto_helper_spec.rb
@@ -1,4 +1,7 @@
 require 'spec_helper'
 
 describe ApiHowtoHelper do
+  it "should return the url for mapping arbitrary georss feed on Google maps" do
+    helper.mapify("http://foo.com", 4).should == "http://maps.google.com/maps?f=q&hl=en&layer=&ie=UTF8&z=4&om=1&q=http%3A%2F%2Ffoo.com"
+  end
 end
