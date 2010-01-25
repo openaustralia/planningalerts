f0c213e4b04b348af877ebf6642cd0a25c21dc27
Matthew Landauer <matthew@openaustralia.org>
2010-01-25 15:02:25 +1100

    Removing tests that aren't doing anything really

--- spec/controllers/faq_controller_spec.rb
+++ spec/controllers/faq_controller_spec.rb
@@ -1,10 +1,4 @@
 require 'spec_helper'
 
 describe FaqController do
-
-  #Delete this example and add some real ones
-  it "should use FaqController" do
-    controller.should be_an_instance_of(FaqController)
-  end
-
 end
