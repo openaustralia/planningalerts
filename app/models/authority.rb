1809bb1f82bffdc99d4911a1aa788bd471d23282
Matthew Landauer <matthew@openaustralia.org>
2010-01-25 22:49:04 +1100

    Now number of authorities is calculated

--- app/models/authority.rb
+++ app/models/authority.rb
@@ -1,4 +1,6 @@
 class Authority < ActiveRecord::Base
   set_table_name "authority"
   set_primary_key "authority_id"
+  
+  named_scope :active, :conditions => 'disabled = 0 or disabled is null'
 end
