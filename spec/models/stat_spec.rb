6a8d8998b6adbe0d72c677e3eb57c109c4e995d0
Matthew Landauer <matthew@openaustralia.org>
2010-01-29 08:40:26 +1100

    Now using mocks to avoid hitting the db

--- spec/models/stat_spec.rb
+++ spec/models/stat_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
 describe Stat do
-  fixtures :stats
-  
   it "should read in the number of applications that have gone out in emails" do
-    Stat.applications_sent.should == 15
-    Stat.emails_sent.should == 3
+    Stat.should_receive(:find).with(:first, :conditions => {:key => "applications_sent"}).and_return(mock(:value => 14))
+    
+    Stat.applications_sent.should == 14
+  end
+
+  it "should read in the number of emails that have been sent" do
+    Stat.should_receive(:find).with(:first, :conditions => {:key => "emails_sent"}).and_return(mock(:value => 2))
+    
+    Stat.emails_sent.should == 2
   end
 end
