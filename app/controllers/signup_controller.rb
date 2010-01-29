190c47e7a494fccc31086ff7363ff30415120262
Matthew Landauer <matthew@openaustralia.org>
2010-01-29 12:04:06 +1100

    Calculating coordinates from the more basic quantities

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -14,12 +14,13 @@ class SignupController < ApplicationController
   
   def preview
     @google_maps_key = "ABQIAAAAo-lZBjwKTxZxJsD-PJnp8RSar6C2u_L4pWCtZvTKzbAvP1AIvRSM06g5G1CDCy9niXlYd7l_YqMpVg"
-    @center_long = 150.624263
-    @center_lat = -33.772609
-    @bottom_left_long = 150.62309060152
-    @bottom_left_lat = -33.773508234721
-    @top_right_long = 150.62543539848
-    @top_right_lat = -33.771709765279
+    @center = Geokit::Geocoders::GoogleGeocoder.geocode(params[:address])
+    # Very naive conversion between a box represented as a size in metres to the coordinates of the corners
+    # of the box represented as a latitude / longitude
+    width_lat = 0.00000899234721 * params[:area_size].to_i
+    width_lng = 0.0000117239848 * params[:area_size].to_i
+    @bottom_left = Geokit::LatLng.new(@center.lat - width_lat / 2, @center.lng - width_lng / 2)
+    @top_right = Geokit::LatLng.new(@center.lat + width_lat / 2, @center.lng + width_lng / 2)
 
     render :layout => false
   end
