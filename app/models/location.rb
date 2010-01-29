fc62d741e0d9bc85541fc6e787a86113d6322577
Matthew Landauer <matthew@openaustralia.org>
2010-01-29 19:12:19 +1100

    Method in Location for calculating coordinates of box of a certain size

--- app/models/location.rb
+++ app/models/location.rb
@@ -11,6 +11,12 @@ class Location
     Location.new(l.lat, l.lng)
   end
   
+  # Coordinates of bottom-left and top-right corners of a box centred on the current location
+  # with a given size in metres
+  def box_with_size_in_metres(size_in_metres)
+    [Location.new(-33.773508234721, 150.62309060152), Location.new(-33.771709765279, 150.62543539848)]
+  end
+  
   def ==(a)
     a.lat == lat && a.lng == lng
   end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -6,4 +6,9 @@ describe "Location" do
       Geokit::LatLng.new(-33.772609, 150.624263))
     Location.geocode("24 Bruce Road, Glenbrook, NSW 2773").should == Location.new(-33.772609, 150.624263)
   end
+  
+  it "should calculate the coordinates of a square box on the surface of the earth centred on the first point" do
+    Location.new(-33.772609, 150.624263).box_with_size_in_metres(200).should ==
+      [Location.new(-33.773508234721, 150.62309060152), Location.new(-33.771709765279, 150.62543539848)]
+  end
 end
