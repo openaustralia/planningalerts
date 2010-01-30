1970eb13ca97943627c9c57b3382ed996583aa99
Matthew Landauer <matthew@openaustralia.org>
2010-01-31 08:31:09 +1100

    Using standard values in tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,33 @@
 require 'spec_helper'
 
 describe User do
+  before :each do
+    @address = "24 Bruce Road, Glenbrook, NSW"
+    @attributes = {:email => "matthew@openaustralia.org", :address => @address,
+      :area_size_meters => 200}
+  end
+
   it "should have no trouble creating a user with valid attributes" do
-    User.create!(:email => "matthew@openaustralia.org", :address => "24 Bruce Road, Glenbrook, NSW", :area_size_meters => 200)
+    User.create!(@attributes)
   end
   
   it "should automatically geocode the address" do
     loc = Location.new(1.0, 2.0)
-    Location.should_receive(:geocode).with("24 Bruce Road, Glenbrook, NSW").and_return(loc)
-    user = User.create!(:email => "matthew@openaustralia.org", :address => "24 Bruce Road, Glenbrook, NSW", :area_size_meters => 200)
+    Location.should_receive(:geocode).with(@address).and_return(loc)
+    user = User.create!(@attributes)
     user.location.distance_to(loc).should < 1
   end
   
-  it "should have a valid email address" do
-    u = User.new(:address => "24 Bruce Road, Glenbrook, NSW", :area_size_meters => 200)
+  it "should error if there is nothing in the email address" do
+    @attributes.delete(:email)
+    u = User.new(@attributes)
     u.should_not be_valid
     u.errors.on(:email).should == "Please enter a valid email address"
-    
-    u = User.new(:email => "diddle@", :address => "24 Bruce Road, Glenbrook, NSW", :area_size_meters => 200)
+  end
+
+  it "should have a valid email address" do
+    @attributes[:email] = "diddle@"
+    u = User.new(@attributes)
     u.should_not be_valid
     u.errors.on(:email).should == "Please enter a valid email address"    
   end
