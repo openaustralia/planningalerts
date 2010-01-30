508ba368ec15c456b8b0d9e13100255b9588935e
Matthew Landauer <matthew@openaustralia.org>
2010-01-30 11:11:27 +1100

    Automatically geocodes address on creating user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ActiveRecord::Base
   set_table_name :user
+  
+  before_create :geocode
+  
+  def location=(l)
+    self.lat = l.lat
+    self.lng = l.lng
+  end
+  
+  def location
+    Location.new(self.lat, self.lng)
+  end
+  
+  private
+  
+  def geocode
+    # For the time being just set latitude/longitude to zero so that we can save this record
+    self.location = Location.geocode(self.address)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,14 @@
 require 'spec_helper'
 
 describe User do
+  it "should have no trouble creating a user with valid attributes" do
+    User.create!(:email => "matthew@openaustralia.org", :address => "24 Bruce Road, Glenbrook, NSW", :area_size_meters => 200)
+  end
+  
+  it "should automatically geocode the address" do
+    loc = Location.new(1.0, 2.0)
+    Location.should_receive(:geocode).with("24 Bruce Road, Glenbrook, NSW").and_return(loc)
+    user = User.create!(:email => "matthew@openaustralia.org", :address => "24 Bruce Road, Glenbrook, NSW", :area_size_meters => 200)
+    user.location.distance_to(loc).should < 1
+  end
 end
