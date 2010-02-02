34c33181f456a0c7c425e16cc9c40fdc3aa5309c
Matthew Landauer <matthew@openaustralia.org>
2010-02-03 09:10:49 +1100

    Organise the tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,24 +18,64 @@ describe User do
     User.create!(@attributes)
   end
   
-  it "should automatically geocode the address" do
-    user = User.create!(@attributes)
-    user.lat.should == @loc.lat
-    user.lng.should == @loc.lng
-  end
-  
-  it "should error if there is nothing in the email address" do
-    @attributes[:email] = ""
-    u = User.new(@attributes)
-    u.should_not be_valid
-    u.errors.on(:email_address).should == "can't be empty"
+  describe "geocoding" do
+    it "should happen automatically on saving" do
+      user = User.create!(@attributes)
+      user.lat.should == @loc.lat
+      user.lng.should == @loc.lng
+    end
+
+    it "should error if the address is empty" do
+      Location.stub!(:geocode).and_return(mock(:lat => nil, :lng => nil, :full_address => ""))
+      @attributes[:address] = ""
+      u = User.new(@attributes)
+      u.should_not be_valid
+      u.errors.on(:street_address).should == "can't be empty"
+    end
+    
+    it "should error if the street address is not in australia" do
+      @loc.stub!(:country_code).and_return("US")
+      @attributes[:address] = "New York"
+      u = User.new(@attributes)
+      u.should_not be_valid
+      u.errors.on(:street_address).should == "isn't in Australia"
+    end
+
+    it "should error if there are multiple matches from the geocoder" do
+      @loc.stub!(:all).and_return([@loc, nil])
+      @loc.stub!(:full_address).and_return("Bruce Rd, VIC 3885, Australia")
+
+      @attributes[:address] = "Bruce Road"
+      u = User.new(@attributes)
+      u.should_not be_valid
+      u.errors.on(:street_address).should == "isn't complete. Please enter a full street address, including suburb and state, e.g. Bruce Rd, VIC 3885"
+    end
+
+    it "should error if the address is not a full street address but rather a suburb name or similar" do
+      @loc.stub!(:accuracy).and_return(5)
+      @loc.stub!(:full_address).and_return("Glenbrook NSW")
+
+      @attributes[:address] = "Glenbrook, NSW"
+      u = User.new(@attributes)
+      u.should_not be_valid
+      u.errors.on(:street_address).should == "isn't complete. We saw that address as \"Glenbrook NSW\" which we don't recognise as a full street address. Check your spelling and make sure to include suburb and state"
+    end
+    
+    it "should replace the address with the full resolved address obtained by geocoding" do
+      @attributes[:address] = "24 Bruce Road, Glenbrook"
+      u = User.new(@attributes)
+      u.save!
+      u.address.should == "24 Bruce Rd, Glenbrook NSW 2773"
+    end
   end
 
-  it "should have a valid email address" do
-    @attributes[:email] = "diddle@"
-    u = User.new(@attributes)
-    u.should_not be_valid
-    u.errors.on(:email_address).should == "isn't valid"    
+  describe "email address" do
+    it "should be valid" do
+      @attributes[:email] = "diddle@"
+      u = User.new(@attributes)
+      u.should_not be_valid
+      u.errors.on(:email_address).should == "isn't valid"    
+    end
   end
   
   it "should be able to store the attribute location" do
@@ -55,63 +95,22 @@ describe User do
     u.location.should be_nil
   end
   
-  it "should error if the address is empty" do
-    Location.stub!(:geocode).and_return(mock(:lat => nil, :lng => nil, :full_address => ""))
-    @attributes[:address] = ""
-    u = User.new(@attributes)
-    u.should_not be_valid
-    u.errors.on(:street_address).should == "can't be empty"
-  end
+  describe "area_size_meters" do
+    it "should have a number" do
+      @attributes[:area_size_meters] = "a"
+      u = User.new(@attributes)
+      u.should_not be_valid
+      u.errors.on(:area_size_meters).should == "isn't selected"
+    end
   
-  it "should error if the street address is not in australia" do
-    @loc.stub!(:country_code).and_return("US")
-    @attributes[:address] = "New York"
-    u = User.new(@attributes)
-    u.should_not be_valid
-    u.errors.on(:street_address).should == "isn't in Australia"
+    it "should be greater than zero" do
+      @attributes[:area_size_meters] = "0"
+      u = User.new(@attributes)
+      u.should_not be_valid
+      u.errors.on(:area_size_meters).should == "isn't selected"    
+    end
   end
-  
-  it "should error if there are multiple matches from the geocoder" do
-    @loc.stub!(:all).and_return([@loc, nil])
-    @loc.stub!(:full_address).and_return("Bruce Rd, VIC 3885, Australia")
 
-    @attributes[:address] = "Bruce Road"
-    u = User.new(@attributes)
-    u.should_not be_valid
-    u.errors.on(:street_address).should == "isn't complete. Please enter a full street address, including suburb and state, e.g. Bruce Rd, VIC 3885"
-  end
-  
-  it "should error if the address is not a full street address but rather a suburb name or similar" do
-    @loc.stub!(:accuracy).and_return(5)
-    @loc.stub!(:full_address).and_return("Glenbrook NSW")
-    
-    @attributes[:address] = "Glenbrook, NSW"
-    u = User.new(@attributes)
-    u.should_not be_valid
-    u.errors.on(:street_address).should == "isn't complete. We saw that address as \"Glenbrook NSW\" which we don't recognise as a full street address. Check your spelling and make sure to include suburb and state"
-  end
-  
-  it "should have a number for area_size_meters" do
-    @attributes[:area_size_meters] = "a"
-    u = User.new(@attributes)
-    u.should_not be_valid
-    u.errors.on(:area_size_meters).should == "isn't selected"
-  end
-  
-  it "should have area_size_meters which is greater than zero" do
-    @attributes[:area_size_meters] = "0"
-    u = User.new(@attributes)
-    u.should_not be_valid
-    u.errors.on(:area_size_meters).should == "isn't selected"    
-  end
-  
-  it "should put the full resolved address in after geocoding" do
-    @attributes[:address] = "24 Bruce Road, Glenbrook"
-    u = User.new(@attributes)
-    u.save!
-    u.address.should == "24 Bruce Rd, Glenbrook NSW 2773"
-  end
-  
   describe "confirm_id" do
     it "should be a string" do
       u = User.create!(@attributes)
