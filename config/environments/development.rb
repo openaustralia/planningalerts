6762e2fb764fee575567bdfd1b7f4079081399c8
Matthew Landauer <matthew@openaustralia.org>
2010-02-03 00:43:25 +1100

    Added email config for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,5 +13,7 @@ config.action_controller.consider_all_requests_local = true
 config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
-# Don't care if the mailer can't send
-config.action_mailer.raise_delivery_errors = false
+# Send mail via smtp and raise an error if there is a problem
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = { :address => "mail.internode.on.net" }
