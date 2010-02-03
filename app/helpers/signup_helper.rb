c28b00db29b706a174ee5e4ffa10b238a6800858
Matthew Landauer <matthew@openaustralia.org>
2010-02-03 13:22:58 +1100

    Formats distances more neatly when large distances are involved

--- app/helpers/signup_helper.rb
+++ app/helpers/signup_helper.rb
@@ -1,9 +1,18 @@
 module SignupHelper
-  def meters_in_words(meters)
+  def meters_in_words_as_array(meters)
     if meters < 1000
-      "#{meters} m"
+      [meters, "m"]
     else
-      "#{meters / 1000} km"
+      [meters / 1000, "km"]
     end
+  end
+  
+  def meters_in_words(meters)
+    meters_in_words_as_array(meters).join(" ")
+  end
+  
+  # TODO: Get rid of this helper so that we format distances consistently
+  def meters_in_words_no_space(meters)
+    meters_in_words_as_array(meters).join
   end
 end
---
+++ spec/views/signup/confirmed.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe SignupController do
+  describe "distances" do
+    it "should be displayed in meters when under 1000m" do
+      assigns[:user] = mock_model(User, :area_size_meters => 200, :address => "An address")
+      render "signup/confirmed"
+      response.should include_text("200m")
+    end
+    
+    it "should be displaying in kilometers when over 1000m" do
+      assigns[:user] = mock_model(User, :area_size_meters => 3000, :address => "An address")
+      render "signup/confirmed"
+      response.should include_text("3km")
+    end
+  end
+end
