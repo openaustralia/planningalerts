e2e4146ef5a82cedc8f536dc68cd0e09b957a29d
Matthew Landauer <matthew@openaustralia.org>
2010-02-04 14:22:37 +1100

    Can do a basic search by area for development applications

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,4 +3,8 @@ class Application < ActiveRecord::Base
   set_primary_key "application_id"
   
   belongs_to :authority
+  
+  named_scope :within, lambda { |p1, p2|
+    { :conditions => ['lat > ? AND lng > ? AND lat < ? AND lng < ?', p1.lat, p1.lng, p2.lat, p2.lng] }
+  }
 end
---
+++ spec/models/application_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Application do
+  describe "within" do
+    it "should limit the results to those within the given area" do
+      auth = Authority.create!(:planning_email => "foo", :full_name => "Fiddlesticks", :short_name => "Fiddle")
+      a1 = Application.create!(:lat => 2.0, :lng => 3.0, :authority_id => auth.id, :postcode => "", :council_reference => "r1", :date_scraped => Date.today) # Within the box
+      a2 = Application.create!(:lat => 4.0, :lng => 3.0, :authority_id => auth.id, :postcode => "", :council_reference => "r2", :date_scraped => Date.today) # Outside the box
+      a3 = Application.create!(:lat => 2.0, :lng => 1.0, :authority_id => auth.id, :postcode => "", :council_reference => "r3", :date_scraped => Date.today) # Outside the box
+      a4 = Application.create!(:lat => 1.5, :lng => 3.5, :authority_id => auth.id, :postcode => "", :council_reference => "r4", :date_scraped => Date.today) # Within the box
+      p1 = Location.new(1.0, 2.0)
+      p2 = Location.new(3.0, 4.0)
+      r = Application.within(p1, p2)
+      r.count.should == 2
+      r[0].should == a1
+      r[1].should == a4
+    end
+  end
+end
