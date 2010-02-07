b471c2d1cd387214a90d49b3b3f82f02b5a5a1dd
Matthew Landauer <matthew@openaustralia.org>
2010-02-07 12:58:12 +1100

    Added method for loading all the authorities data from the web service

--- app/models/authority.rb
+++ app/models/authority.rb
@@ -1,7 +1,32 @@
+require 'mechanize'
+
 class Authority < ActiveRecord::Base
   set_table_name "authority"
   set_primary_key "authority_id"
   
   has_many :applications
   named_scope :active, :conditions => 'disabled = 0 or disabled is null'
+  
+  def self.load_from_web_service
+    agent = WWW::Mechanize.new
+    page = agent.get(Configuration::INTERNAL_SCRAPERS_INDEX_URL)
+    Nokogiri::XML(page.body).search('scraper').each do |scraper|
+      short_name = scraper.at('authority_short_name').inner_text
+      authority = Authority.find_by_short_name(short_name)
+      if authority.nil?
+        logger.info "New authority: #{short_name}"
+        authority = Authority.new(:short_name => short_name)
+      else
+        logger.info "Updating authority: #{short_name}"
+      end
+      authority.full_name = scraper.at('authority_name').inner_text
+      authority.feed_url = scraper.at('url').inner_text
+      # TODO Find a way of setting the planning email address or maybe it's not used at all
+      authority.planning_email = "unknown@unknown.org"
+      authority.external = 1
+      authority.disabled = 0
+
+      authority.save!
+    end
+  end
 end
---
+++ spec/models/authority_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe Authority do
+  describe "load_from_web_service" do
+    it "should load all the authorities data from the scraper web service index" do
+      agent = mock("Agent")
+      page = mock("Page")
+      WWW::Mechanize.should_receive(:new).and_return(agent)
+      agent.should_receive(:get).and_return(page)
+      page.should_receive(:body).and_return(
+        <<-EOF
+        <scrapers> 
+          <scraper> 
+            <authority_name>Blue Mountains City Council, NSW</authority_name> 
+            <authority_short_name>Blue Mountains</authority_short_name> 
+            <url>http://localhost:4567/blue_mountains?year={year}&amp;month={month}&amp;day={day}</url> 
+          </scraper> 
+          <scraper> 
+            <authority_name>Brisbane City Council, QLD</authority_name> 
+            <authority_short_name>Brisbane</authority_short_name> 
+            <url>http://localhost:4567/brisbane?year={year}&amp;month={month}&amp;day={day}</url> 
+          </scraper> 
+        <scrapers>
+        EOF
+      )
+      logger = mock("Logger")
+      Authority.stub!(:logger).and_return(logger)
+      logger.should_receive(:info).with("New authority: Blue Mountains")
+      logger.should_receive(:info).with("New authority: Brisbane")
+
+      Authority.delete_all
+      Authority.load_from_web_service
+      Authority.count.should == 2
+      r = Authority.find_by_short_name("Blue Mountains")
+      r.full_name.should == "Blue Mountains City Council, NSW"
+      r.feed_url == "http://localhost:4567/blue_mountains?year={year}&month={month}&day={day}"
+      r = Authority.find_by_short_name("Brisbane")
+      r.full_name.should == "Brisbane City Council, QLD"
+      r.feed_url == "http://localhost:4567/brisbane?year={year}&month={month}&day={day}"
+    end
+  end
+end
