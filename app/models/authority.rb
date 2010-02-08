2c2b589978ab6e211e918a347e5a28e6208c4cba
Matthew Landauer <matthew@openaustralia.org>
2010-02-08 12:40:55 +1100

    Now logging is a little nicer for loading authorities data and added include so that things can run from the rake task again

--- app/models/authority.rb
+++ app/models/authority.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class Authority < ActiveRecord::Base
   set_table_name "authority"
   set_primary_key "authority_id"
@@ -5,16 +7,16 @@ class Authority < ActiveRecord::Base
   has_many :applications
   named_scope :active, :conditions => 'disabled = 0 or disabled is null'
   
-  def self.load_from_web_service
+  def self.load_from_web_service(info_logger = logger)
     page = Nokogiri::XML(open(Configuration::INTERNAL_SCRAPERS_INDEX_URL).read)
     page.search('scraper').each do |scraper|
       short_name = scraper.at('authority_short_name').inner_text
       authority = Authority.find_by_short_name(short_name)
       if authority.nil?
-        logger.info "New authority: #{short_name}"
+        info_logger.info "New authority: #{short_name}"
         authority = Authority.new(:short_name => short_name)
       else
-        logger.info "Updating authority: #{short_name}"
+        info_logger.info "Updating authority: #{short_name}"
       end
       authority.full_name = scraper.at('authority_name').inner_text
       authority.feed_url = scraper.at('url').inner_text
--- lib/tasks/planningalerts.rake
+++ lib/tasks/planningalerts.rake
@@ -2,9 +2,7 @@ namespace :planningalerts do
   namespace :authorities do
     desc "Load all the authorities data from the scraper web service index"
     task :load => :environment do
-      # Log to standard out
-      Authority.logger = Logger.new(STDOUT)
-      Authority.load_from_web_service
+      Authority.load_from_web_service(Logger.new(STDOUT))
     end
   end
   
