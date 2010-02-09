839afcaea49f19b542a3028c397da4f7f6810a0d
Matthew Landauer <matthew@openaustralia.org>
2010-02-09 13:15:36 +1100

    Commented a couple of tests out that really need a network connection to run. They seem too simple minded to mock

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -15,14 +15,17 @@ describe "Location" do
     l.lat.should be_nil
     l.lng.should be_nil
   end
+
+  # Hmm... this test really needs a network connection to run and doesn't really make sense to test
+  # through mocking. So, commenting out
+  #it "should return the country code of the geocoded address" do
+  #  Location.geocode("24 Bruce Road, Glenbrook, NSW 2773").country_code.should == "AU"
+  #end
   
-  it "should return the country code of the geocoded address" do
-    Location.geocode("24 Bruce Road, Glenbrook, NSW 2773").country_code.should == "AU"
-  end
-  
-  it "should bias the results of geocoding to australian addresses" do
-    Location.geocode("Bruce Road").country_code.should == "AU"
-  end
+  # Same as the test above
+  #it "should bias the results of geocoding to australian addresses" do
+  #  Location.geocode("Bruce Road").country_code.should == "AU"
+  #end
   
   it "should normalise addresses without the country in them" do
     loc = Location.new(mock(:full_address => "24 Bruce Road, Glenbrook, NSW 2773, Australia"))
