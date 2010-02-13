c1976b4954a1510ba4126c55150477b4c2164888
Matthew Landauer <matthew@openaustralia.org>
2010-02-14 01:42:19 +1100

    Inlined some of the previous helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,31 +1,16 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def menu
-    menu_items = []
-    menu_items << {:url => about_path, :content => 'About',
-      :controller => "static", :action => "about"}
-    menu_items << {:url => api_howto_path, :content => '<acronym title="Application programming interface">API</acronym>',
-      :controller => "api", :action => "howto"}
-    menu_items << {:url => get_involved_path, :content => 'Get Involved',
-      :controller => "static", :action => "get_involved"}
-    menu_items << {:url => faq_path, :content => '<acronym title="Frequently asked questions">FAQ</acronym>',
-      :controller => "static", :action => "faq"}
-    menu_items << {:url => signup_path, :content => 'Sign Up',
-      :controller => "signup", :action => ["index", "check_mail", "confirmed", "unsubscribe"]}
-
-    content_tag(:div, :id => "divMenu") do
-      content_tag(:ul, :class => "collapse") do
-        menu_items.map do |m|
-          if m[:action].is_a?(Array)
-            selected = m[:action].any?{|a| current_page?(:controller => m[:controller], :action => a) }
-          else
-            selected = current_page?(:controller => m[:controller], :action => m[:action])
-          end
-          content_tag(:li, :class => ("selected" if selected)) do
-            link_to(m[:content], m[:url])
-          end
-        end.join
-      end
+  def page_matches?(options)
+    if options[:action].is_a?(Array)
+      options[:action].any?{|a| current_page?(:controller => options[:controller], :action => a) }
+    else
+      current_page?(:controller => options[:controller], :action => options[:action])
+    end
+  end
+  
+  def li_selected(m)
+    content_tag(:li, :class => ("selected" if page_matches?(m))) do
+      yield
     end
   end
 end
