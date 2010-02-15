dc5f2b194f07a099afe6cf30aeb0036cb13c018e
Matthew Landauer <matthew@openaustralia.org>
2010-02-15 11:55:47 +1100

    Removing integration test on api howto page now that I'm confident that things are being properly tested elsewhere

--- spec/integration/api_controller_spec.rb
+++ spec/integration/api_controller_spec.rb
@@ -5,10 +5,6 @@ describe ApiController do
   include HTMLCompareHelper
   fixtures :stats, :authority, :application
   
-  it "should render the howto page exactly the same as the php version" do
-    compare_with_php("/apihowto.php", "apihowto")
-  end
-  
   it "should render one of the api examples the same as php" do
     compare_with_php("/api.php?call=point&lat=-33.772609&lng=150.624263&area_size=4000", "api_4000")
   end
