ff01ffcdabcf4b3be1093d0378d1c77b1a431936
Matthew Landauer <matthew@openaustralia.org>
2010-02-15 13:46:16 +1100

    removing integration test for completely static pages (except for layout) because it's not really necessary anymore

--- spec/integration/static_controller_spec.rb
+++ spec/integration/static_controller_spec.rb
@@ -8,12 +8,4 @@ describe StaticController do
   it "should render the page exactly the same as the php version" do
     compare_with_php("/about.php", "about")
   end
-
-  it "should render the faq" do
-    compare_with_php("/faq.php", "faq")
-  end
-
-  it "should render the page exactly the same as the php version" do
-    compare_with_php("/getinvolved.php", "getinvolved")
-  end
 end
