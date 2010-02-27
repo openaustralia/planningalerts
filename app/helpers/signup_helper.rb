dddedf89e71b81162a125c8e9d6b129fbd5eaf43
Matthew Landauer <matthew@openaustralia.org>
2010-02-28 10:00:45 +1100

    Consistently format distances

--- app/helpers/signup_helper.rb
+++ app/helpers/signup_helper.rb
@@ -10,9 +10,4 @@ module SignupHelper
   def meters_in_words(meters)
     meters_in_words_as_array(meters).join(" ")
   end
-  
-  # TODO: Get rid of this helper so that we format distances consistently
-  def meters_in_words_no_space(meters)
-    meters_in_words_as_array(meters).join
-  end
 end
--- spec/views/signup/confirmed_spec.rb
+++ spec/views/signup/confirmed_spec.rb
@@ -5,13 +5,13 @@ describe SignupController do
     it "should be displayed in meters when under 1000m" do
       assigns[:alert] = mock_model(Alert, :area_size_meters => 200, :address => "An address")
       render "signup/confirmed"
-      response.should include_text("200m")
+      response.should include_text("200 m")
     end
     
     it "should be displaying in kilometers when over 1000m" do
       assigns[:alert] = mock_model(Alert, :area_size_meters => 3000, :address => "An address")
       render "signup/confirmed"
-      response.should include_text("3km")
+      response.should include_text("3 km")
     end
   end
 end
