7fe46698bca09b1441e74dbc924acdefa8efb8fa
Matthew Landauer <matthew@openaustralia.org>
2010-04-10 16:59:07 +1000

    Switch of size now happens on selecting radio buttons

--- app/helpers/signup_helper.rb
+++ app/helpers/signup_helper.rb
@@ -1,6 +1,6 @@
 module SignupHelper
   def open_preview_window(size)
     area = Area.centre_and_size(@alert.location, @zone_sizes[size])    
-    "javascript:preview(#{area.lower_left.lat}, #{area.lower_left.lng}, #{area.upper_right.lat}, #{area.upper_right.lng});"
+    "preview(#{area.lower_left.lat}, #{area.lower_left.lng}, #{area.upper_right.lat}, #{area.upper_right.lng});"
   end
 end
