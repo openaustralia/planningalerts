7dc6a7cf8ba03d4f84dd08d8d97a18da6332673c
Matthew Landauer <matthew@openaustralia.org>
2010-04-10 11:01:38 +1000

    Slight tweak of wording

--- spec/views/alerts/confirmed_spec.rb
+++ spec/views/alerts/confirmed_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe AlertsController do
   describe "distances" do
     it "should be displayed in meters when under 1000m" do
-      assigns[:alert] = mock_model(Alert, :area_size_meters => 200, :address => "An address")
+      assigns[:alert] = mock_model(Alert, :area_size_meters => 200, :address => "An address", :confirm_id => "abcdef")
       render "alerts/confirmed"
       response.should include_text("200 m")
     end
     
     it "should be displaying in kilometers when over 1000m" do
-      assigns[:alert] = mock_model(Alert, :area_size_meters => 3000, :address => "An address")
+      assigns[:alert] = mock_model(Alert, :area_size_meters => 3000, :address => "An address", :confirm_id => "abcdef")
       render "alerts/confirmed"
       response.should include_text("3 km")
     end
