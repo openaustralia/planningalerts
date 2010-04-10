c88d5b549e3331cac5bba0833ed59a50a9445b53
Matthew Landauer <matthew@openaustralia.org>
2010-04-10 13:17:27 +1000

    Use mapstraction to display preview map

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -31,6 +31,17 @@ class AlertsController < ApplicationController
   
   def preview
     @area = Area.centre_and_size(Location.geocode(params[:address]), params[:area_size].to_i)
+    @map = Mapstraction.new("map_div",:google)
+    @map.control_init(:small => true)
+    @map.center_zoom_init([@area.centre.lat, @area.centre.lng], 13)
+    @map.polyline_init(
+      Polyline.new([
+        LatLonPoint.new([@area.lower_left.lat, @area.lower_left.lng]),
+        LatLonPoint.new([@area.lower_left.lat, @area.upper_right.lng]),
+        LatLonPoint.new([@area.upper_right.lat, @area.upper_right.lng]),
+        LatLonPoint.new([@area.upper_right.lat, @area.lower_left.lng]),
+        LatLonPoint.new([@area.lower_left.lat, @area.lower_left.lng]),
+      ], :width => 5, :color => "#EF2C2C", :opacity => 0.7))
 
     render :layout => false
   end
