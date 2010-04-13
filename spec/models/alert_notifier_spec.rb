352246490371cbadc94ae7525e4cfa82e3083c3d
Matthew Landauer <matthew@openaustralia.org>
2010-04-13 12:22:23 +1000

    Added much more explicit test for layout of planning alerts email

--- spec/models/alert_notifier_spec.rb
+++ spec/models/alert_notifier_spec.rb
@@ -38,8 +38,8 @@ describe AlertNotifier do
     before :each do
       @original_emails_sent = Stat.emails_sent
       @original_applications_sent = Stat.applications_sent
-      @a1 = mock_model(Application, :address => "Foo Street, Bar", :council_reference => "a1", :description => "Knock something down")
-      @a2 = mock_model(Application, :address => "Bar Street, Foo", :council_reference => "a2", :description => "Put something up")
+      @a1 = mock_model(Application, :address => "Foo Street, Bar", :council_reference => "a1", :description => "Knock something down", :id => 1)
+      @a2 = mock_model(Application, :address => "Bar Street, Foo", :council_reference => "a2", :description => "Put something up", :id => 2)
       @email = AlertNotifier.create_alert(@alert, [@a1, @a2])
     end
     
@@ -56,30 +56,6 @@ describe AlertNotifier do
       @email.subject.should == "Planning applications near #{@alert.address}"
     end
     
-    it "should include the development application address and council id" do
-      @email.body.should include_text("FOO STREET, BAR (A1)")
-    end
-    
-    it "should include the description of the application" do
-      @email.body.should include_text("Knock something down")
-    end
-    
-    it "should include useful links for the application" do
-      @email.body.should include_text("For more information: http://dev.planningalerts.org.au/applications/#{@a1.id}")
-    end
-    
-    it "should include a link to the georss feed" do
-      @email.body.should include_text("http://dev.planningalerts.org.au/applications.rss?address=24+Bruce+Rd%2C+Glenbrook+NSW+2773&area_size=800")
-    end
-    
-    it "should include a link to the maps version of the georss feed" do
-      @email.body.should include_text("http://maps.google.com.au/maps?q=http%3A%2F%2Fdev.planningalerts.org.au%2Fapplications.rss%3Faddress%3D24%2BBruce%2BRd%252C%2BGlenbrook%2BNSW%2B2773%26area_size%3D800")
-    end
-    
-    it "should include a link to the unsubscribe url" do
-      @email.body.should include_text("http://dev.planningalerts.org.au/alerts/abcdef/unsubscribe")
-    end
-    
     it "should update the statistics" do
       Stat.emails_sent.should == @original_emails_sent + 1
       Stat.applications_sent.should == @original_applications_sent + 2
@@ -87,6 +63,39 @@ describe AlertNotifier do
     
     it "should update last_sent to the current time" do
       (Time.now - @alert.last_sent).abs.should < 1
+    end
+    
+    it "should nicely format a list of multiple planning applications" do
+      @email.body.should == <<-EOF
+The following new planning applications have been found near 24 Bruce Rd, Glenbrook NSW 2773:
+
+FOO STREET, BAR (A1)
+
+Knock something down
+
+For more information: http://dev.planningalerts.org.au/applications/1
+
+=============================
+
+BAR STREET, FOO (A2)
+
+Put something up
+
+For more information: http://dev.planningalerts.org.au/applications/2
+
+=============================
+
+
+------------------------------------------------------------
+
+PlanningAlerts.org.au is a free service run by the charity OpenAustralia Foundation.
+
+You can subscribe to a geoRSS feed of applications for 24 Bruce Rd, Glenbrook NSW 2773 here: http://dev.planningalerts.org.au/applications.rss?address=24+Bruce+Rd%2C+Glenbrook+NSW+2773&area_size=800
+
+GeoRSS can be used to display planning applications on a map. For example, on Google Maps: http://maps.google.com.au/maps?q=http%3A%2F%2Fdev.planningalerts.org.au%2Fapplications.rss%3Faddress%3D24%2BBruce%2BRd%252C%2BGlenbrook%2BNSW%2B2773%26area_size%3D800
+
+To stop receiving these emails click here: http://dev.planningalerts.org.au/alerts/abcdef/unsubscribe
+      EOF
     end
   end
 end
