cb4b1de7183f98ddc3682da9663ba393912cd2c9
Matthew Landauer <matthew@openaustralia.org>
2010-04-14 13:00:52 +1000

    Now link authority names to list of applications in that authority

--- spec/views/static/about_spec.rb
+++ spec/views/static/about_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe StaticController, "about" do
   it "should show a list of the authorities" do
-    a1 = mock_model(Authority, :full_name => "Wombat District Council")
-    a2 = mock_model(Authority, :full_name => "Kangaroo City Council")
+    a1 = mock_model(Authority, :full_name => "Wombat District Council", :short_name_encoded => "wombat")
+    a2 = mock_model(Authority, :full_name => "Kangaroo City Council", :short_name_encoded => "kangaroo")
     assigns[:authorities] = [a1, a2]
     render "static/about"
     response.should include_text(a1.full_name)
