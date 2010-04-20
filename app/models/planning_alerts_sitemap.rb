dd4df4537ec43b2173724345026ebce23bec2a87
Matthew Landauer <matthew@openaustralia.org>
2010-04-20 16:16:29 +1000

    Add the (mostly) static page urls as well

--- app/models/planning_alerts_sitemap.rb
+++ app/models/planning_alerts_sitemap.rb
@@ -4,12 +4,22 @@ class PlanningAlertsSitemap
   def generate_and_notify
     s = Sitemap.new(root_url(:host => Configuration::HOST)[0..-2], Rails.root.join('public'))
 
-    # We're going to say the home page changes daily because we will probably add news on to it
-    s.add_url root_path, :changefreq => :weekly, :lastmod => Time.now
+    # TODO: There is some redundancy between what is going on here and what is listed in the routes.rb
+    # Figure out if there is a way to combine the configuration information entirely in the routing.
+    # Probably only worth doing when Rails 3 comes out as the routing has completely changed and is
+    # apparently much more powerful
+
+    s.add_url root_path, :changefreq => :monthly
+    s.add_url api_howto_path, :changefreq => :monthly
+    s.add_url about_path, :changefreq => :monthly
+    s.add_url faq_path, :changefreq => :monthly
+    s.add_url get_involved_path, :changefreq => :monthly
+    
     # All the applications pages
     Application.find(:all).each do |application|
       s.add_url application_path(application), :changefreq => :monthly, :lastmod => application.date_scraped
     end
+
     s.finish
     s.notify_search_engines(Configuration::PINGMYMAP_API_KEY)
   end
