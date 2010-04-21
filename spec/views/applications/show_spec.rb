ab41ada44288875fb2d8bc1fdd4fc4d3b8753f40
Matthew Landauer <matthew@openaustralia.org>
2010-04-21 11:28:14 +1000

    PA-157 Refactored tests a little

--- spec/views/applications/show_spec.rb
+++ spec/views/applications/show_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
 describe ApplicationsController do
+  before :each do
+    authority = mock_model(Authority, :full_name => "An authority", :short_name => "Blue Mountains")
+    assigns[:application] = mock_model(Application, :map_url => "http://a.map.url",
+      :description => "A planning application", :council_reference => "A1", :authority => authority, :info_url => "http://info.url", :comment_url => "http://comment.url")
+    assigns[:nearby_applications] = []
+  end
+  
   describe "show" do
     before :each do
-      authority = mock_model(Authority, :full_name => "An authority", :short_name => "Blue Mountains")
-      assigns[:application] = mock_model(Application, :location => Location.new(1.0, 2.0), :lat => 1.0, :lng => 2.0, :address => "foo", :map_url => "http://a.map.url",
-        :description => "A planning application", :council_reference => "A1", :authority => authority, :info_url => "http://info.url", :comment_url => "http://comment.url")
-      assigns[:nearby_applications] = []
+      assigns[:application].stub!(:address).and_return("foo")
+      assigns[:application].stub!(:lat).and_return(1.0)
+      assigns[:application].stub!(:lng).and_return(2.0)
+      assigns[:application].stub!(:location).and_return(Location.new(1.0, 2.0))
     end
     
     it "should say when the application was received by the planning authority and when it appeared on PlanningAlerts" do
@@ -22,15 +29,24 @@ describe ApplicationsController do
       render "applications/show"
       response.should have_tag("p.dates", "We found this application for you on the planning authority's website 18 days ago.\n    The date it was received by them was not recorded.")
     end
+    
+    it "should display the map" do
+      assigns[:application].stub!(:date_received).and_return(nil)
+      assigns[:application].stub!(:date_scraped).and_return(Time.now)
+      render "applications/show"
+      response.should have_tag("div#map_div")      
+    end
   end
   
   describe "show with application with no location" do
     it "should not display the map" do
-      authority = mock_model(Authority, :full_name => "An authority", :short_name => "Blue Mountains")
-      assigns[:application] = mock_model(Application, :location => nil, :lat => nil, :lng => nil, :address => "An address that can't be geocoded", :map_url => "http://a.map.url",
-        :description => "A planning application", :council_reference => "A1", :date_scraped => Time.now, :date_received => nil, :authority => authority, :info_url => "http://info.url",
-        :comment_url => "http://comment.url")
-      assigns[:nearby_applications] = []
+      assigns[:application].stub!(:address).and_return("An address that can't be geocoded")
+      assigns[:application].stub!(:lat).and_return(nil)
+      assigns[:application].stub!(:lng).and_return(nil)
+      assigns[:application].stub!(:location).and_return(nil)
+      assigns[:application].stub!(:date_received).and_return(nil)
+      assigns[:application].stub!(:date_scraped).and_return(Time.now)
+        
       render "applications/show"
       response.should_not have_tag("div#map_div")
     end
