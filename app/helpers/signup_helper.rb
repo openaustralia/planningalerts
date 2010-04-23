3271c48716aad3514f23fb817e0a157184e2913e
Matthew Landauer <matthew@openaustralia.org>
2010-04-23 15:38:59 +1000

    Now shows a circle with a radius of the given size

--- app/helpers/signup_helper.rb
+++ app/helpers/signup_helper.rb
@@ -1,6 +1,5 @@
 module SignupHelper
   def draw_box_javascript(size)
-    area = Area.centre_and_size(@alert.location, @zone_sizes[size])    
-    "preview(#{area.lower_left.lat}, #{area.lower_left.lng}, #{area.upper_right.lat}, #{area.upper_right.lng});"
+    "preview(#{@alert.location.lat}, #{@alert.location.lng}, #{@zone_sizes[size]});"
   end
 end
--- public/javascripts/preview.js
+++ public/javascripts/preview.js
@@ -1,13 +1,8 @@
-function preview(lower_left_lat, lower_left_lng, upper_right_lat, upper_right_lng) {
+function preview(centre_lat, centre_lng, radius_in_metres) {
   map.removeAllPolylines();
-    var myPoly = new mxn.Polyline([
-    new mxn.LatLonPoint(lower_left_lat, lower_left_lng),
-    new mxn.LatLonPoint(lower_left_lat, upper_right_lng),
-    new mxn.LatLonPoint(upper_right_lat, upper_right_lng),
-    new mxn.LatLonPoint(upper_right_lat, lower_left_lng)
-  ]);
-  myPoly.setWidth(0);
-  myPoly.setOpacity(0.2);
-  myPoly.setClosed(true);
-  map.addPolyline(myPoly);
+  r = new mxn.Radius(new mxn.LatLonPoint(centre_lat, centre_lng), 10);
+  p = r.getPolyline(radius_in_metres / 1000, "#FF0000");
+  p.setWidth(0);
+  p.setOpacity(0.2);
+  map.addPolyline(p);
 };
