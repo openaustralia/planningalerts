5cf5af0acef98975db5765f83f37ff4254b83ced
Matthew Landauer <matthew@openaustralia.org>
2010-04-27 22:08:30 +1000

    Mock the httparty gem so that tests don't need to call actual web service

--- spec/models/geo2gov_spec.rb
+++ spec/models/geo2gov_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe Geo2gov do
   it "should return the LGA code for the BLue Mountains for a point in the Blue Mountains" do
+    Geo2gov.should_receive(:get).with("http://geo2gov.com.au/json?location=150.624263+-33.772609").and_return("Census" => [{"MSR" => "11", "LGA" => "LGA10900"}])
+
     g = Geo2gov.new(-33.772609, 150.624263)
     g.lga_code.should == "LGA10900"
   end
