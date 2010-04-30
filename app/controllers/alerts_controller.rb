10c7dddc48f013bc6f666cc71f0334395bc14dd6
Henare Degan <henare.degan@gmail.com>
2010-04-30 21:55:02 +1000

    PA-124 Add a title for the statistics page

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -59,6 +59,7 @@ class AlertsController < ApplicationController
   end
   
   def statistics
+    @page_title = "Alert statistics"
     @no_confirmed_alerts = Alert.confirmed.count
     @no_alerts = Alert.count
     @no_alerts_in_active_areas = @no_alerts - Alert.alerts_in_inactive_areas.count    
