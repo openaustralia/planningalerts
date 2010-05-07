d1f8dc71c2547e92d0b57a0f62b9f59aacaa2b1f
Matthew Landauer <matthew@openaustralia.org>
2010-05-07 17:58:53 +1000

    Inlined method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,16 +14,12 @@ module ApplicationHelper
     end
   end
 
-  def meters_in_words_as_array(meters)
-    if meters < 1000
-      [meters, "m"]
-    else
-      [meters / 1000, "km"]
-    end
-  end
-  
   def meters_in_words(meters)
-    meters_in_words_as_array(meters).join(" ")
+    if meters < 1000
+      "#{meters} m"
+    else
+      "#{meters / 1000} km"
+    end
   end
   
   def is_mobile_optimised?
