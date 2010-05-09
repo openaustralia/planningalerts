cbc3efe67654a465e64d5f5a3318c5478ac3bcd0
Matthew Landauer <matthew@openaustralia.org>
2010-05-09 20:35:27 +1000

    PA-182 Combines javascript and css into single files. Also minifies them in the process using the smurf gem

--- config/environment.rb
+++ config/environment.rb
@@ -30,6 +30,8 @@ Rails::Initializer.run do |config|
   config.gem 'jaap3-addthis', :lib => 'addthis', :source => 'http://gems.github.com'
   config.gem 'httparty'
   config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
+  # For minifying javascript and css
+  config.gem 'smurf', :source => 'http://gemcutter.org'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
