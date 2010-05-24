d0e21a1160fbb0556d995a1db4f5f77365ca6a84
Matthew Landauer <matthew@openaustralia.org>
2010-05-24 16:02:54 +1000

    Adding thinking sphinx

--- config/environment.rb
+++ config/environment.rb
@@ -32,6 +32,7 @@ Rails::Initializer.run do |config|
   config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
   # For minifying javascript and css
   config.gem 'smurf', :source => 'http://gemcutter.org'
+  config.gem 'thinking-sphinx', :lib => 'thinking_sphinx', :version => '1.3.11'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
