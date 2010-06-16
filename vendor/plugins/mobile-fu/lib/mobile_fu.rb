9f17965783583d6b4fc56a01ec72be310fb1d34c
Matthew Landauer <matthew@openaustralia.org>
2010-06-16 17:34:25 +1000

    PA-198 When requesting RSS feed from mobile device serve up the RSS feed rather than HTML. This patch should go back to the mobile-fu project. See PA-201.

--- vendor/plugins/mobile-fu/lib/mobile_fu.rb
+++ vendor/plugins/mobile-fu/lib/mobile_fu.rb
@@ -59,15 +59,17 @@ module ActionController
       # Forces the request format to be :mobile
       
       def force_mobile_format
-        request.format = :mobile
-        session[:mobile_view] = true if session[:mobile_view].nil?
+        if request.format == :html
+          request.format = :mobile
+          session[:mobile_view] = true if session[:mobile_view].nil?
+        end
       end
       
       # Determines the request format based on whether the device is mobile or if
       # the user has opted to use either the 'Standard' view or 'Mobile' view.
       
       def set_mobile_format
-        if is_mobile_device? && !request.xhr?
+        if is_mobile_device? && !request.xhr? && request.format == :html
           request.format = session[:mobile_view] == false ? :html : :mobile
           session[:mobile_view] = true if session[:mobile_view].nil?
         end
