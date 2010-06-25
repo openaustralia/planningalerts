651838d9d7751be1e010f77608f8cfe83c382ebc
Matthew Landauer <matthew@openaustralia.org>
2010-06-25 17:14:11 +1000

    Grouping tests

--- spec/controllers/alerts_controller_spec.rb
+++ spec/controllers/alerts_controller_spec.rb
@@ -1,19 +1,21 @@
 require 'spec_helper'
 
-describe AlertsController, "confirming" do
-  it "should set the alert to be confirmed" do
-    alert = mock_model(Alert)
-    Alert.should_receive(:find_by_confirm_id).with("1234").and_return(alert)
-    alert.should_receive(:confirmed=).with(true)
-    alert.should_receive(:save!)
-    get :confirmed, :cid => "1234"
+describe AlertsController do
+  describe "confirming" do
+    it "should set the alert to be confirmed" do
+      alert = mock_model(Alert)
+      Alert.should_receive(:find_by_confirm_id).with("1234").and_return(alert)
+      alert.should_receive(:confirmed=).with(true)
+      alert.should_receive(:save!)
+      get :confirmed, :cid => "1234"
+    end
+    
+    it "should return a 404 when the wrong confirm_id is used" do
+      get :confirmed, :cid => "1111"
+      response.code.should == "404"
+    end
   end
-  
-  it "should return a 404 when the wrong confirm_id is used" do
-    get :confirmed, :cid => "1111"
-    response.code.should == "404"
-  end
-  
+
   describe "search engine optimisation" do
     it "should provide a sensible title and meta description to make search results useful to people" do
       get :signup
