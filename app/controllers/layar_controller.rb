d30b938a4743147fe1ccfec8c639ad08dd038f8b
Matthew Landauer <matthew@openaustralia.org>
2010-07-12 08:48:54 +1000

    Added page key for paging

--- app/controllers/layar_controller.rb
+++ app/controllers/layar_controller.rb
@@ -1,6 +1,6 @@
 class LayarController < ApplicationController
   def getpoi
     @applications = Application.paginate :origin => [params[:lat].to_f, params[:lon].to_f], :within => params[:radius].to_f / 1000,
-      :page => nil, :per_page => 10
+      :page => params[:pageKey], :per_page => 10
   end
 end
--- spec/controllers/layar_controller_spec.rb
+++ spec/controllers/layar_controller_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe LayarController do
   it "should provide a rest api to serve the layar points of interest" do
     result = mock
-    Application.should_receive(:paginate).with(:origin => [1.0, 2.0], :within => 3.0, :page => nil, :per_page => 10).and_return(result)
+    Application.should_receive(:paginate).with(:origin => [1.0, 2.0], :within => 3.0, :page => "2", :per_page => 10).and_return(result)
 
-    get :getpoi, :lat => 1.0, :lon => 2.0, :radius => 3000
+    get :getpoi, :lat => 1.0, :lon => 2.0, :radius => 3000, :pageKey => "2"
     assigns[:applications].should == result
   end
 end
