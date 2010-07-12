5409ba2068edea84fa3eca40cc4b9d534edacac1
Matthew Landauer <matthew@openaustralia.org>
2010-07-12 11:09:54 +1000

    Partial implementation of correct output json format for a POI

--- app/controllers/layar_controller.rb
+++ app/controllers/layar_controller.rb
@@ -2,5 +2,23 @@ class LayarController < ApplicationController
   def getpoi
     @applications = Application.paginate :origin => [params[:lat].to_f, params[:lon].to_f], :within => params[:radius].to_f / 1000,
       :page => params[:pageKey], :per_page => 10
+    layar_applications = @applications.map do |a|
+      {
+        :actions => [],
+        :attribution => nil,
+        :distance => a.distance.to_f * 1000,
+        :id => a.id,
+        :imageURL => nil,
+        :lat => a.lat,
+        :lon => a.lng,
+        :line2 => nil,
+        :line3 => nil,
+        :line4 => nil,
+        :title => nil,
+        :type => 0
+      }
+    end
+    result = {:hotspots => layar_applications}
+    render :json => result
   end
 end
--- spec/controllers/layar_controller_spec.rb
+++ spec/controllers/layar_controller_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 
 describe LayarController do
   it "should provide a rest api to serve the layar points of interest" do
-    result = mock
+    result = [mock(:distance => 2, :lat => 1.0, :lng => 2.0, :id => 1)]
     Application.should_receive(:paginate).with(:origin => [1.0, 2.0], :within => 3.0, :page => "2", :per_page => 10).and_return(result)
 
     get :getpoi, :lat => 1.0, :lon => 2.0, :radius => 3000, :pageKey => "2"
     assigns[:applications].should == result
+    expected_layar = {
+      "id" => 1, "type" => 0,
+      "lat" => 1.0, "lon" => 2.0, "distance" => 2000.0, 
+      "actions" => [], "imageURL" => nil, "attribution" => nil,
+      "title" => nil, "line2" => nil, "line3" => nil, "line4" => nil,
+    }
+    JSON.parse(response.body).should == {"hotspots" => [expected_layar]}
   end
 end
