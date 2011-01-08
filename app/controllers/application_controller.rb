fb91a234c12a0ec1c49bd170447b9a114a15991f
Matthew Landauer <matthew@openaustralia.org>
2011-01-08 14:23:09 +1100

    Add form post forgery protection back in
    Originally this was commented out to make the form post parameters absolutely
    identical to the php code that this rails code is a port of. Was reminded
    to add the forgery protection back in by running ratproxy.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     rescue_from ActiveRecord::RecordNotFound, :with => :render_404
   end
   helper :all # include all helpers, all the time
-  #protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
