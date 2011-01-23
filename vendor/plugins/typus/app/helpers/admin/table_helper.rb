46e8553abb582bd8e4e6e6b23b122d24de9d5a37
Matthew Landauer <matthew@openaustralia.org>
2011-01-23 21:47:19 +1100

    Fix up html_safe display for table header when sorted

--- vendor/plugins/typus/app/helpers/admin/table_helper.rb
+++ vendor/plugins/typus/app/helpers/admin/table_helper.rb
@@ -128,7 +128,7 @@ module Admin::TableHelper
                   order_by = model.reflect_on_association(key.to_sym).primary_key_name rescue key
                   switch = sort_order.last if params[:order_by].eql?(order_by)
                   options = { :order_by => order_by, :sort_order => sort_order.first }
-                  content = link_to "#{content} #{switch}", params.merge(options)
+                  content = link_to "#{content} #{switch}".html_safe, params.merge(options)
                 end
 
                 content
