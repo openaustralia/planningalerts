75e869b1b41e73ecad6747b9ae78a7e592200b04
Matthew Landauer <matthew@openaustralia.org>
2011-01-30 13:32:58 +1100

    A little configuration on the admin interface
    Restrict the tables and fields that can be edited.

---
+++ config/initializers/rails_admin.rb
@@ -0,0 +1,33 @@
+RailsAdmin.config do |config|
+  # Remove the email alerts completely from the admin view
+  # as this contains private information.
+  config.excluded_models << Alert
+  # Exclude these because they are entirely automatically generated
+  config.excluded_models += [Stat, Application]
+  
+  config.model Comment do
+    list do
+      field :text
+    end
+  end
+  
+  config.model Authority do
+    list do    
+      field :full_name
+      field :state
+      field :disabled
+    end
+    
+    edit do
+      field :full_name
+      field :state
+      field :short_name
+      field :feed_url
+      field :disabled
+      field :email
+      # TODO: Temporarily comment out association as it causes a
+      # 'stack level too deep' error
+      #field :applications
+    end
+  end
+end
