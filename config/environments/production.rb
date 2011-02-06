aa353d804eead4c5472515a5a08e3ba74d06a0cb
Matthew Landauer <matthew@openaustralia.org>
2011-02-06 16:27:28 +1100

    Oops. Managed to overwrite production email configuration while upgrading to Rails 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,4 +47,6 @@ PlanningalertsApp::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.delivery_method = :sendmail
 end
