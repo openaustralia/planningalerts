d9094a1d90af06cb6935fd7963e25c25308233a5
Matthew Landauer <matthew@openaustralia.org>
2011-05-05 13:13:17 +1000

    Add colon at end of line in email

--- spec/mailers/comment_notifier_spec.rb
+++ spec/mailers/comment_notifier_spec.rb
@@ -27,7 +27,7 @@ describe CommentNotifier do
     it "should have specific information in the body of the email" do
       notifier = CommentNotifier.notify(@comment)
       notifier.body.to_s.should == <<-EOF
-For the attention of the General Manager / Planning Manager / Planning Department
+For the attention of the General Manager / Planning Manager / Planning Department:
 
 Application:          X/001
 Address:              12 Foo Rd
