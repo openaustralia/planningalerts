b46f193dd88fbb66b7fb596ea77cd32225eaaba9
Matthew Landauer <matthew@openaustralia.org>
2011-05-06 11:06:00 +1000

    Ensure that a report can't be made on a comment that isn't visible

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,11 +1,11 @@
 class ReportsController < ApplicationController
   def new
-    @comment = Comment.find(params[:comment_id])
+    @comment = Comment.visible.find(params[:comment_id])
     @report = Report.new
   end
   
   def create
-    @comment = Comment.find(params[:comment_id])
+    @comment = Comment.visible.find(params[:comment_id])
     @report = @comment.reports.build(:name => params[:report][:name], :email => params[:report][:email], :details => params[:report][:details])
     if @report.save
       ReportNotifier.notify(@report).deliver
