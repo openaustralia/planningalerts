0d2ef625c71d0806ed8f4a744e64346693357cdb
Matthew Landauer <matthew@openaustralia.org>
2012-07-14 18:02:21 +1000

    Added test to show bug PA-346

--- spec/controllers/confirm_controller_spec.rb
+++ spec/controllers/confirm_controller_spec.rb
@@ -8,6 +8,15 @@ describe EmailConfirmable::ConfirmController do
       alert.should_receive(:confirm!)
       get :confirmed, :resource => 'alerts', :id => "1234"
     end
+
+    it "should set the alert to be confirmed when on an iPhone" do
+      request.stub!(:user_agent).and_return('iphone')
+      alert = mock_model(Alert)
+      Alert.should_receive(:find_by_confirm_id).with("1234").and_return(alert)
+      alert.should_receive(:confirm!)
+      get :confirmed, :resource => 'alerts', :id => "1234"
+      response.should be_success
+    end
     
     it "should return a 404 when the wrong confirm_id is used" do
       get :confirmed, :resource => 'alerts', :id => "1111"
