55fadc9a22e2952c529cb33ff174e782058c0f70
Matthew Landauer <matthew@openaustralia.org>
2012-07-25 16:25:55 +1000

    Also store the user_agent of the api request now

--- app/models/api_statistic.rb
+++ app/models/api_statistic.rb
@@ -1,5 +1,5 @@
 class ApiStatistic < ActiveRecord::Base
   def self.log(request)
-    create!(:ip_address => request.remote_ip, :query => request.fullpath, :query_time => Time.now)
+    create!(:ip_address => request.remote_ip, :query => request.fullpath, :user_agent => request.headers["User-Agent"],:query_time => Time.now)
   end
 end
