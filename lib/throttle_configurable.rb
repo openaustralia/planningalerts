0b4bf337520538c23bca31a7885dc743763c4199
Matthew Landauer <matthew@openaustralia.org>
2012-07-28 13:34:30 +1000

    Precompute hash with ip address lookup

--- lib/throttle_configurable.rb
+++ lib/throttle_configurable.rb
@@ -16,23 +16,36 @@ class ThrottleConfigurable < Rack::Throttle::Limiter
     super
   end
 
-  def strategy_config(ip)
+  def strategy_config2
+    r = {}
     options[:strategies].each do |strategy, value|
       if strategy == "hourly" || strategy == "daily"
         value.each do |m, hosts|
-          if hosts == ip || hosts.include?(ip)
-            return [strategy, m]
+          if hosts.respond_to?(:each)
+            hosts.each do |host|
+              r[host] = [strategy, m]
+            end
+          else
+            r[hosts] = [strategy, m]
           end
         end
       elsif strategy == "unlimited" || strategy == "blocked"
-        if value == ip || value.include?(ip)
-          return [strategy, nil]
+        if value.respond_to?(:each)
+          value.each do |host|
+            r[host] = [strategy, nil]
+          end
+        else
+          r[value] = [strategy, nil]
         end
       else
         raise "Unexpected value for strategy: #{strategy}"
       end
     end
-    strategy_config("default")    
+    r
+  end
+
+  def strategy_config(ip)
+    strategy_config2[ip] || strategy_config2["default"]
   end
 
   def strategy(ip)
