6c2395d63a1a446b4946738b848baa0f910b8d5a
Matthew Landauer <matthew@openaustralia.org>
2012-07-28 13:40:48 +1000

    The ip lookup table is computed only once

--- lib/throttle_configurable.rb
+++ lib/throttle_configurable.rb
@@ -14,38 +14,29 @@ class ThrottleConfigurable < Rack::Throttle::Limiter
   #}
   def initialize(app, options = {})
     super
-  end
 
-  def strategy_config2
-    r = {}
+    @ip_lookup = {}
     options[:strategies].each do |strategy, value|
       if strategy == "hourly" || strategy == "daily"
         value.each do |m, hosts|
-          if hosts.respond_to?(:each)
-            hosts.each do |host|
-              r[host] = [strategy, m]
-            end
-          else
-            r[hosts] = [strategy, m]
+          hosts = [hosts] unless hosts.respond_to?(:each)
+          hosts.each do |host|
+            @ip_lookup[host] = [strategy, m]
           end
         end
       elsif strategy == "unlimited" || strategy == "blocked"
-        if value.respond_to?(:each)
-          value.each do |host|
-            r[host] = [strategy, nil]
-          end
-        else
-          r[value] = [strategy, nil]
+        value = [value] unless value.respond_to?(:each)
+        value.each do |host|
+          @ip_lookup[host] = [strategy, nil]
         end
       else
         raise "Unexpected value for strategy: #{strategy}"
       end
-    end
-    r
+    end    
   end
 
   def strategy_config(ip)
-    strategy_config2[ip] || strategy_config2["default"]
+    @ip_lookup[ip] || @ip_lookup["default"]
   end
 
   def strategy(ip)
