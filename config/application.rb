3dad6cfc982ceca6b1d254ad8b6636d650cb1a78
Matthew Landauer <matthew@openaustralia.org>
2012-08-01 17:35:05 +1000

    Make tests run succesfully under ruby 1.9.3 using bundle exec rake

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'rack/throttle'
-require 'lib/throttle_configurable'
+require File.dirname(__FILE__) + "/../lib/throttle_configurable"
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
