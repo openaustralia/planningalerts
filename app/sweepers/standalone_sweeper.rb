82ea7bf18ce1a58c107568faff95c38aad340f4e
Matthew Landauer <matthew@openaustralia.org>
2012-08-02 16:10:35 +1000

    PA-311: Cached pages were not being expired because the url generated was wrong

--- app/sweepers/standalone_sweeper.rb
+++ app/sweepers/standalone_sweeper.rb
@@ -8,10 +8,10 @@ class StandaloneSweeper < ActionController::Caching::Sweeper
   def expire_page(options)
     if options[:action].is_a?(Array)
       options[:action].each do |action|
-        ActionController::Base.expire_page(url_for options.merge(:only_path => true, :skip_relative_url_root => true, :action => action))
+        ActionController::Base.expire_page(url_for options.merge(:only_path => true, :action => action))
       end
     else
-      ActionController::Base.expire_page(url_for options.merge(:only_path => true, :skip_relative_url_root => true))
+      ActionController::Base.expire_page(url_for options.merge(:only_path => true))
     end
   end
 end
