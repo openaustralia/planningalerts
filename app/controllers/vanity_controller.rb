00b35c702b506dd8dae64c213065054a88fab0d8
Matthew Landauer <matthew@openaustralia.org>
2012-08-04 11:48:39 +1000

    Require admin sign in to see the results of the A/B tests

--- app/controllers/vanity_controller.rb
+++ app/controllers/vanity_controller.rb
@@ -1,4 +1,6 @@
 class VanityController < ApplicationController
   include Vanity::Rails::Dashboard
   layout false
+  # Only allow admin users access to these pages
+  before_filter :authenticate_user!
 end
