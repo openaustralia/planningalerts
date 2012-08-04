fb1c71fe4efdc8123f0ac59b939c3017fd6084e3
Matthew Landauer <matthew@openaustralia.org>
2012-08-04 13:59:59 +1000

    Don't require confirmation of the email alert for it to be a conversion

--- experiments/metrics/alert_signup.rb
+++ experiments/metrics/alert_signup.rb
@@ -1,4 +1,4 @@
-metric "Email Alerts Signup" do
-  description "Measures how many people signup for email alerts and confirmed"
-  model Alert, :conditions => { :confirmed => true }
+metric "Alert Signup" do
+  description "Measures how many people signup for email alerts"
+  model Alert
 end
