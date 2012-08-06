850ed50a3d586d6afe78a8d60652a2b9647ef035
Matthew Landauer <matthew@openaustralia.org>
2012-08-06 18:01:51 +1000

    Remove experiment: remove_rss_on_detail

--- experiments/remove_rss_on_detail.rb
+++
@@ -1,4 +0,0 @@
-ab_test "Remove RSS on detail" do
-  description "Testing to see if removing the RSS feed choice makes someone more likely to sign up to an email alert"
-  metrics :alert_signup
-end
