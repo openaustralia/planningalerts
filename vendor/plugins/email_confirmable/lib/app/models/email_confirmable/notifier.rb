86b08e3c75eae1a86ae9fa3cef713a027d8b3913
Matthew Landauer <matthew@openaustralia.org>
2012-08-10 13:39:18 +1000

    Update EmailConfirmable to use new ActionMailer api

--- vendor/plugins/email_confirmable/lib/app/models/email_confirmable/notifier.rb
+++ vendor/plugins/email_confirmable/lib/app/models/email_confirmable/notifier.rb
@@ -1,11 +1,10 @@
 class EmailConfirmable::Notifier < ActionMailer::Base
   def confirm(object)
     class_name = object.class.name.underscore
-    @recipients = object.email
-    @from = EmailConfirmable.from
-    @subject = "Please confirm your #{object.class.model_name.human.downcase}"
     # This seems a bit long-winded. Is there a better way?
     instance_variable_set(("@" + class_name).to_sym, object)
-    template class_name
+
+    mail(:to => object.email, :subject => "Please confirm your #{object.class.model_name.human.downcase}",
+      :from => EmailConfirmable.from, :template_name => class_name)
   end
 end
