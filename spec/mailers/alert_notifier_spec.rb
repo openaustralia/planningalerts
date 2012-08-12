bb8cbf92047486cce28345e30ada718a38a735c3
Matthew Landauer <matthew@openaustralia.org>
2012-08-12 18:44:58 +1000

    PA-375: One comment in html email

--- spec/mailers/alert_notifier_spec.rb
+++ spec/mailers/alert_notifier_spec.rb
@@ -49,6 +49,10 @@ Cillum ethnic single-origin coffee labore, sriracha fixie jean shorts freegan. O
     it "should nicely format (in text) a list of multiple planning applications" do
       get_message_part(email, /plain/).should == Rails.root.join("spec/mailers/regression/email2.txt").read
     end
+
+    it "should nicely format (in text) a list of multiple planning applications" do
+      get_message_part(email, /html/).should == Rails.root.join("spec/mailers/regression/email2.html").read
+    end
   end
 
   describe "when sending a planning alert with one new planning application" do
