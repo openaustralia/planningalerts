a4b60b9b62ab717cb3062c1d030716acd7a3bb58
Matthew Landauer <matthew@openaustralia.org>
2012-08-14 16:04:50 +1000

    New experiment: see if saying how many people have alerts now makes them more likely to sign up

---
+++ experiments/social_proof_signup.rb
@@ -0,0 +1,4 @@
+ab_test "Social proof signup" do
+  description "See if telling people how many people have signed up to email alerts makes them more likely to sign up"
+  metrics :alert_signup
+end
