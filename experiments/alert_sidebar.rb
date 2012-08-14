37abd3802567348469e97011d61485c5c045d938
Matthew Landauer <matthew@openaustralia.org>
2012-08-14 13:45:29 +1000

    Finish experiment alert_sidebar
    After about 10,000 participants the result was not statistically significant which means that we
    can probably safely put the alert signup on the sidebar without impacting conversion

--- experiments/alert_sidebar.rb
+++
@@ -1,4 +0,0 @@
-ab_test "Alert sidebar" do
-  description "See if putting the email alert box on the sidebar (which is a more logical place) stops people from using it"
-  metrics :alert_signup
-end
