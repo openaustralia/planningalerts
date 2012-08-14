f5462193fda1c8fd9cc42863826925cf247a1ca1
Matthew Landauer <matthew@openaustralia.org>
2012-08-14 11:24:27 +1000

    Get rid of rails admin tables in the db

---
+++ db/migrate/20120814012059_remove_rails_admin_tables.rb
@@ -0,0 +1,20 @@
+class RemoveRailsAdminTables < ActiveRecord::Migration
+  def self.up
+    drop_table :rails_admin_histories
+  end
+
+  def self.down
+    create_table "rails_admin_histories" do |t|
+      t.string   "message"
+      t.string   "username"
+      t.integer  "item"
+      t.string   "table"
+      t.integer  "month",      :limit => 2
+      t.integer  "year",       :limit => 8
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
+
+    add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_histories_on_item_and_table_and_month_and_year"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120813162916) do
+ActiveRecord::Schema.define(:version => 20120814012059) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -100,19 +100,6 @@ ActiveRecord::Schema.define(:version => 20120813162916) do
     t.string   "address"
     t.boolean  "hidden",         :default => false, :null => false
   end
-
-  create_table "rails_admin_histories", :force => true do |t|
-    t.string   "message"
-    t.string   "username"
-    t.integer  "item"
-    t.string   "table"
-    t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 8
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_histories_on_item_and_table_and_month_and_year"
 
   create_table "reports", :force => true do |t|
     t.string   "name"
