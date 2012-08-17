7b8bb820a1d3045d1450da6095dff943cda7c741
Matthew Landauer <matthew@openaustralia.org>
2012-08-17 14:02:24 +1000

    After 7500 participants no winner for experiment. So using the simpler version

--- experiments/social_proof_signup.rb
+++
@@ -1,4 +0,0 @@
-ab_test "Social proof signup" do
-  description "See if telling people how many people have signed up to email alerts makes them more likely to sign up"
-  metrics :alert_signup
-end
