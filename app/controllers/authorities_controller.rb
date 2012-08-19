03277a19100aa36d808f99c94bdb873c4a476fe4
Matthew Landauer <matthew@openaustralia.org>
2012-08-20 07:54:09 +1000

    Only show active scrapers in the list of authorities silly

--- app/controllers/authorities_controller.rb
+++ app/controllers/authorities_controller.rb
@@ -3,7 +3,7 @@ class AuthoritiesController < ApplicationController
     # map from state name to authorities in that state
     states = Authority.active.find(:all, :group => "state", :order => "state").map{|a| a.state}
     @authorities = states.map do |state|
-      [state, Authority.find_all_by_state(state, :order => "full_name")]
+      [state, Authority.active.find_all_by_state(state, :order => "full_name")]
     end
   end
 end
