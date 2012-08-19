9461129cd7b7fd6b87096349545f873c61711402
Matthew Landauer <matthew@openaustralia.org>
2012-08-19 12:18:42 +1000

    PA-402: Show on the coverage page which authority scrapers are possibly broken

--- spec/views/authorities/index_spec.rb
+++ spec/views/authorities/index_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe "authorities/index.haml" do
   it "should show a list of the authorities" do
-    a1 = mock_model(Authority, :full_name => "Wombat District Council", :short_name_encoded => "wombat")
-    a2 = mock_model(Authority, :full_name => "Kangaroo City Council", :short_name_encoded => "kangaroo")
+    a1 = mock_model(Authority, :full_name => "Wombat District Council", :short_name_encoded => "wombat", :broken? => false)
+    a2 = mock_model(Authority, :full_name => "Kangaroo City Council", :short_name_encoded => "kangaroo", :broken? => false)
     assign(:authorities, [["NSW", [a1, a2]]]) 
     render
     rendered.should contain(a1.full_name)
