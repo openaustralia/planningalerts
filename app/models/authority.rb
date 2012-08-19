801caaf7e20b3f500fefbb70c296da5bb827328c
Matthew Landauer <matthew@openaustralia.org>
2012-08-19 19:24:34 +1000

    Load the scraperwiki_name data from the scraper index

--- app/models/authority.rb
+++ app/models/authority.rb
@@ -21,6 +21,7 @@ class Authority < ActiveRecord::Base
       end
       authority.full_name = scraper.at('authority_name').inner_text
       authority.state = scraper.at('state').inner_text
+      authority.scraperwiki_name = scraper.at('scraperwiki_name').inner_text
       authority.feed_url = scraper.at('url').inner_text
       authority.disabled = 0
 
