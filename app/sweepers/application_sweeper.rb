b27017a4ea6a81655af44798bb5ea95a2b5e9240
Matthew Landauer <matthew@openaustralia.org>
2012-08-27 06:03:41 +1000

    Cache coverage page

--- app/sweepers/application_sweeper.rb
+++ app/sweepers/application_sweeper.rb
@@ -5,9 +5,11 @@ class ApplicationSweeper < StandaloneSweeper
 
   def after_create(application)
     expire_page(:controller => 'alerts', :action => 'statistics')
+    expire_page(:controller => 'authorities', :action => 'index')
   end
 
   def after_destroy(application)
     expire_page(:controller => 'alerts', :action => 'statistics')
+    expire_page(:controller => 'authorities', :action => 'index')
   end
 end
--- app/sweepers/authority_sweeper.rb
+++ app/sweepers/authority_sweeper.rb
@@ -7,11 +7,17 @@ class AuthoritySweeper < StandaloneSweeper
     expire_page(:controller => 'alerts', :action => 'statistics')
     expire_page(:controller => 'static', :action => %w( about faq get_involved ))
     expire_page(:controller => 'api', :action => 'howto')
+    expire_page(:controller => 'authorities', :action => 'index')
+  end
+
+  def after_update(authority)
+    expire_page(:controller => 'authorities', :action => 'index')
   end
 
   def after_destroy(authority)
     expire_page(:controller => 'alerts', :action => 'statistics')
     expire_page(:controller => 'static', :action => %w( about faq get_involved ))
     expire_page(:controller => 'api', :action => 'howto')
+    expire_page(:controller => 'authorities', :action => 'index')
   end
 end
