05372fb532652c54b720c87f8ec7aa95d979ae57
Matthew Landauer <matthew@openaustralia.org>
2012-08-27 13:03:58 +1000

    Allow list of authorities to be sorted by population. Allows easy prioritising of big councils

--- app/controllers/authorities_controller.rb
+++ app/controllers/authorities_controller.rb
@@ -2,10 +2,18 @@ class AuthoritiesController < ApplicationController
   caches_page :index
   
   def index
-    # map from state name to authorities in that state
-    states = Authority.active.find(:all, :group => "state", :order => "state").map{|a| a.state}
-    @authorities = states.map do |state|
-      [state, Authority.active.find_all_by_state(state, :order => "full_name")]
+    @sort = params[:sort]
+    # Default to sort by name
+    @sort = "name" if @sort.nil?
+
+    if @sort == "name"
+      # map from state name to authorities in that state
+      states = Authority.active.find(:all, :group => "state", :order => "state").map{|a| a.state}
+      @authorities = states.map do |state|
+        [state, Authority.active.find_all_by_state(state, :order => "full_name")]
+      end
+    else
+      @authorities = Authority.active.order("population_2011 DESC")
     end
   end
 
--- spec/views/authorities/index_spec.rb
+++ spec/views/authorities/index_spec.rb
@@ -5,6 +5,7 @@ describe "authorities/index.haml" do
     a1 = mock_model(Authority, :full_name => "Wombat District Council", :short_name_encoded => "wombat", :broken? => false)
     a2 = mock_model(Authority, :full_name => "Kangaroo City Council", :short_name_encoded => "kangaroo", :broken? => false)
     assign(:authorities, [["NSW", [a1, a2]]]) 
+    assign(:sort, "name")
     render
     rendered.should contain(a1.full_name)
     rendered.should contain(a2.full_name)
