de18049cedec00cde6ff65a511f1368bdcc0b53a
Matthew Landauer <matthew@openaustralia.org>
2012-09-06 14:57:40 +1000

    Only redirect if html request to api.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 PlanningalertsApp::Application.routes.draw do
   # Redirect everything from api. to www.
-  constraints :subdomain => "api" do
+  constraints :subdomain => "api", :format => "html" do
     match "(*path)" => redirect {|p,r| "http://www.#{r.domain(2)}/#{p[:path]}"}
   end
 
