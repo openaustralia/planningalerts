f0464b52a5dedfae4869c4cb2133897a57015297
Matthew Landauer <matthew@openaustralia.org>
2012-09-07 10:50:00 +1000

    Switch examples in api docs to json

--- app/helpers/api_howto_helper.rb
+++ app/helpers/api_howto_helper.rb
@@ -8,59 +8,59 @@ module ApiHowtoHelper
     url.gsub(/(\?|&|&amp;)([a-z_]+)=/, '\1<strong>\2</strong>=').gsub('&', '&amp;')
   end
   
-  def api_example_address_url(address = ::Configuration::API_EXAMPLE_ADDRESS,
+  def api_example_address_url(format, address = ::Configuration::API_EXAMPLE_ADDRESS,
     radius = ::Configuration::API_EXAMPLE_SIZE)
-    applications_url(:format => "rss", :address => address, :radius => radius)
+    applications_url(:format => format, :address => address, :radius => radius)
   end
   
-  def api_example_latlong_url(lat = ::Configuration::API_EXAMPLE_LAT, lng = ::Configuration::API_EXAMPLE_LNG, 
+  def api_example_latlong_url(format, lat = ::Configuration::API_EXAMPLE_LAT, lng = ::Configuration::API_EXAMPLE_LNG, 
     radius = ::Configuration::API_EXAMPLE_SIZE)
-    applications_url(:format => "rss", :lat => lat, :lng => lng, :radius => radius)
+    applications_url(:format => format, :lat => lat, :lng => lng, :radius => radius)
   end
   
-  def api_example_area_url(bottom_left_lat = ::Configuration::API_EXAMPLE_BOTTOM_LEFT_LAT,
+  def api_example_area_url(format, bottom_left_lat = ::Configuration::API_EXAMPLE_BOTTOM_LEFT_LAT,
     bottom_left_lng = ::Configuration::API_EXAMPLE_BOTTOM_LEFT_LNG,
     top_right_lat = ::Configuration::API_EXAMPLE_TOP_RIGHT_LAT,
     top_right_lng = ::Configuration::API_EXAMPLE_TOP_RIGHT_LNG)
-    applications_url(:format => "rss",
+    applications_url(:format => format,
       :bottom_left_lat => bottom_left_lat, :bottom_left_lng => bottom_left_lng,
       :top_right_lat => top_right_lat, :top_right_lng => top_right_lng)
   end
   
-  def api_example_authority_url(authority = ::Configuration::API_EXAMPLE_AUTHORITY)
-    authority_applications_url(:format => "rss", :authority_id => authority)
+  def api_example_authority_url(format, authority = ::Configuration::API_EXAMPLE_AUTHORITY)
+    authority_applications_url(:format => format, :authority_id => authority)
   end
   
-  def api_example_postcode_url(postcode = ::Configuration::API_EXAMPLE_POSTCODE)
-    applications_url(:format => "rss", :postcode => postcode)
+  def api_example_postcode_url(format, postcode = ::Configuration::API_EXAMPLE_POSTCODE)
+    applications_url(:format => format, :postcode => postcode)
   end
   
-  def api_example_suburb_and_state_url(suburb = ::Configuration::API_EXAMPLE_SUBURB, state = ::Configuration::API_EXAMPLE_STATE)
-    applications_url(:format => "rss", :suburb => suburb, :state => state)
+  def api_example_suburb_and_state_url(format, suburb = ::Configuration::API_EXAMPLE_SUBURB, state = ::Configuration::API_EXAMPLE_STATE)
+    applications_url(:format => format, :suburb => suburb, :state => state)
   end
   
-  def api_example_address_url_html
+  def api_example_address_url_html(format)
     # Doing this hackery with 11's and 22's so that we don't escape "[" and "]"
-    htmlify(api_example_address_url("11", "22").sub("11", "[address]").sub("22", "[distance_in_metres]"))
+    htmlify(api_example_address_url(format, "11", "22").sub("11", "[address]").sub("22", "[distance_in_metres]"))
   end
   
-  def api_example_latlong_url_html
-    htmlify(api_example_latlong_url("11", "22", "33").sub("11", "[latitude]").sub("22", "[longitude]").sub("33", "[distance_in_metres]"))
+  def api_example_latlong_url_html(format)
+    htmlify(api_example_latlong_url(format, "11", "22", "33").sub("11", "[latitude]").sub("22", "[longitude]").sub("33", "[distance_in_metres]"))
   end
   
-  def api_example_area_url_html
-    htmlify(api_example_area_url("11", "22", "11", "22").gsub("11", "[latitude]").gsub("22", "[longitude]"))
+  def api_example_area_url_html(format)
+    htmlify(api_example_area_url(format, "11", "22", "11", "22").gsub("11", "[latitude]").gsub("22", "[longitude]"))
   end
   
-  def api_example_authority_url_html
-    htmlify(api_example_authority_url("11").sub("11", "[name]"))
+  def api_example_authority_url_html(format)
+    htmlify(api_example_authority_url(format, "11").sub("11", "[name]"))
   end
 
-  def api_example_postcode_url_html
-    htmlify(api_example_postcode_url("11").sub("11", "[postcode]"))
+  def api_example_postcode_url_html(format)
+    htmlify(api_example_postcode_url(format, "11").sub("11", "[postcode]"))
   end
 
-  def api_example_suburb_and_state_url_html
-    htmlify(api_example_suburb_and_state_url("11", "22").sub("11", "[suburb]").sub("22", "[state]"))
+  def api_example_suburb_and_state_url_html(format)
+    htmlify(api_example_suburb_and_state_url(format, "11", "22").sub("11", "[suburb]").sub("22", "[state]"))
   end
 end
--- spec/helpers/api_howto_helper_spec.rb
+++ spec/helpers/api_howto_helper_spec.rb
@@ -6,20 +6,20 @@ describe ApiHowtoHelper do
   end
   
   it "should provide urls of examples of use of the api" do
-    helper.api_example_address_url.should == "http://test.host/applications.rss?address=24+Bruce+Road+Glenbrook%2C+NSW+2773&radius=4000"
-    helper.api_example_latlong_url.should == "http://test.host/applications.rss?lat=-33.772609&lng=150.624263&radius=4000"
-    helper.api_example_area_url.should == "http://test.host/applications.rss?bottom_left_lat=-38.556757&bottom_left_lng=140.83374&top_right_lat=-29.113775&top_right_lng=153.325195"
-    helper.api_example_authority_url.should == "http://test.host/authorities/blue_mountains/applications.rss"
-    helper.api_example_postcode_url.should == "http://test.host/applications.rss?postcode=2780"
-    helper.api_example_suburb_and_state_url.should == "http://test.host/applications.rss?state=NSW&suburb=Katoomba"
+    helper.api_example_address_url("js").should == "http://test.host/applications.js?address=24+Bruce+Road+Glenbrook%2C+NSW+2773&radius=4000"
+    helper.api_example_latlong_url("js").should == "http://test.host/applications.js?lat=-33.772609&lng=150.624263&radius=4000"
+    helper.api_example_area_url("js").should == "http://test.host/applications.js?bottom_left_lat=-38.556757&bottom_left_lng=140.83374&top_right_lat=-29.113775&top_right_lng=153.325195"
+    helper.api_example_authority_url("js").should == "http://test.host/authorities/blue_mountains/applications.js"
+    helper.api_example_postcode_url("js").should == "http://test.host/applications.js?postcode=2780"
+    helper.api_example_suburb_and_state_url("js").should == "http://test.host/applications.js?state=NSW&suburb=Katoomba"
   end
   
   it "should display the example urls nicely" do
-    helper.api_example_address_url_html.should == "http://test.host/applications.rss?<strong>address</strong>=[address]&amp;<strong>radius</strong>=[distance_in_metres]"
-    helper.api_example_latlong_url_html.should == "http://test.host/applications.rss?<strong>lat</strong>=[latitude]&amp;<strong>lng</strong>=[longitude]&amp;<strong>radius</strong>=[distance_in_metres]"
-    helper.api_example_area_url_html.should == "http://test.host/applications.rss?<strong>bottom_left_lat</strong>=[latitude]&amp;<strong>bottom_left_lng</strong>=[longitude]&amp;<strong>top_right_lat</strong>=[latitude]&amp;<strong>top_right_lng</strong>=[longitude]"
-    helper.api_example_authority_url_html.should == "http://test.host/authorities/[name]/applications.rss"
-    helper.api_example_postcode_url_html.should == "http://test.host/applications.rss?<strong>postcode</strong>=[postcode]"
-    helper.api_example_suburb_and_state_url_html.should == "http://test.host/applications.rss?<strong>state</strong>=[state]&amp;<strong>suburb</strong>=[suburb]"
+    helper.api_example_address_url_html("rss").should == "http://test.host/applications.rss?<strong>address</strong>=[address]&amp;<strong>radius</strong>=[distance_in_metres]"
+    helper.api_example_latlong_url_html("rss").should == "http://test.host/applications.rss?<strong>lat</strong>=[latitude]&amp;<strong>lng</strong>=[longitude]&amp;<strong>radius</strong>=[distance_in_metres]"
+    helper.api_example_area_url_html("rss").should == "http://test.host/applications.rss?<strong>bottom_left_lat</strong>=[latitude]&amp;<strong>bottom_left_lng</strong>=[longitude]&amp;<strong>top_right_lat</strong>=[latitude]&amp;<strong>top_right_lng</strong>=[longitude]"
+    helper.api_example_authority_url_html("rss").should == "http://test.host/authorities/[name]/applications.rss"
+    helper.api_example_postcode_url_html("rss").should == "http://test.host/applications.rss?<strong>postcode</strong>=[postcode]"
+    helper.api_example_suburb_and_state_url_html("rss").should == "http://test.host/applications.rss?<strong>state</strong>=[state]&amp;<strong>suburb</strong>=[suburb]"
   end
 end
