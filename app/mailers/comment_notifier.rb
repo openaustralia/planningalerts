a59724b557ac6eeebcb9debe080cffa9bf03fb53
Matthew Landauer <matthew@openaustralia.org>
2012-10-05 13:04:04 +1000

    Setting reply-to on comments to make doubly-sure that we don't get replies

--- app/mailers/comment_notifier.rb
+++ app/mailers/comment_notifier.rb
@@ -4,6 +4,9 @@ class CommentNotifier < ActionMailer::Base
     
     mail(:from => "#{comment.name} <#{comment.email}>",
       :sender => "#{::Configuration::EMAIL_FROM_NAME} <#{::Configuration::EMAIL_FROM_ADDRESS}>",
+      # Setting reply-to to ensure that we don't get the replies for email clients that are not
+      # respecting the from, sender headers that we've set.
+      :reply_to => "#{comment.name} <#{comment.email}>",
       :to => comment.application.authority.email, :subject => "Comment on application #{comment.application.council_reference}")
   end
 end
