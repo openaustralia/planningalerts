518792bc468ea5a0644d210183a139f694952795
Matthew Landauer <matthew@openaustralia.org>
2012-10-22 14:54:41 +1100

    Log api statistics with special handling for varnish

--- app/models/api_statistic.rb
+++ app/models/api_statistic.rb
@@ -1,5 +1,10 @@
 class ApiStatistic < ActiveRecord::Base
   def self.log(request)
-    create!(:ip_address => request.remote_ip, :query => request.fullpath, :user_agent => request.headers["User-Agent"],:query_time => Time.now)
+    create!(:ip_address => ip_address(request), :query => request.fullpath, :user_agent => request.headers["User-Agent"],:query_time => Time.now)
+  end
+
+  # Get the source ip address. Also handle the case where the request is coming from an http proxy or load balancer.
+  def self.ip_address(request)
+    request.headers["X-Forwarded-For"] || request.remote_ip
   end
 end
