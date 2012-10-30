eb653651dd80b0292afe9bfb4623ddedeeeaf042
Matthew Landauer <matthew@openaustralia.org>
2012-10-31 09:42:46 +1100

    Add Google Maps API for Business api details

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -27,6 +27,11 @@ module Configuration
   # This will be the case if you are using pow (http://pow.cx/)
   GOOGLE_MAPS_KEY = 'ABQIAAAAo-lZBjwKTxZxJsD-PJnp8RTctXwaglzRZcFAUhNsPfHMAK74xRSSE3HhjcyVtlJHXKjyRyk_3L4CYA'
 
+  # If you have Google Maps API for Business (OpenAustralia Foundation gets it through the Google Maps API
+  # grants programme for charities) uncomment and fill out the two lines below
+  #GOOGLE_MAPS_CLIENT_ID = "xxxxxxxxxxx"
+  #GOOGLE_MAPS_CRYPTOGRAPHIC_KEY = "xxxxxxxxxxx"
+
   # Google Analytics key
   GOOGLE_ANALYTICS_KEY = "UA-3107958-5"
   
--- config/initializers/geokit_config.rb
+++ config/initializers/geokit_config.rb
@@ -26,8 +26,12 @@ if defined? Geokit
 	# This is your Google Maps geocoder key. 
 	# See http://www.google.com/apis/maps/signup.html
 	# and http://www.google.com/apis/maps/documentation/#Geocoding_Examples
-	Geokit::Geocoders::google = 'REPLACE_WITH_YOUR_GOOGLE_KEY'
-    
+	#Geokit::Geocoders::google = 'REPLACE_WITH_YOUR_GOOGLE_KEY'
+  
+	if defined?(Configuration::GOOGLE_MAPS_CLIENT_ID)
+		Geokit::Geocoders::google_client_id = Configuration::GOOGLE_MAPS_CLIENT_ID
+		Geokit::Geocoders::google_cryptographic_key = Configuration::GOOGLE_MAPS_CRYPTOGRAPHIC_KEY
+	end
 	# This is your username and password for geocoder.us.
 	# To use the free service, the value can be set to nil or false.  For 
 	# usage tied to an account, the value should be set to username:password.
