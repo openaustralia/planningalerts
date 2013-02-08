1fc5d185382e2bd9d84dcecfa48db81bd0189a6e
Matthew Landauer <matthew@openaustralia.org>
2013-02-08 13:47:19 +1100

    Moved bulk of javascripts and stylesheets to asset pipeline

--- config/compass.rb
+++ config/compass.rb
@@ -5,7 +5,5 @@ require 'susy'
 project_type = :rails
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "public/stylesheets/compiled"
-sass_dir = "app/stylesheets"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
