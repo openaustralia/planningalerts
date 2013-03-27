ee5ddd28e8a021cbd1500b74206b37e127606d65
Matthew Landauer <matthew@openaustralia.org>
2013-03-27 16:13:06 +1100

    Add reset_password_sent_at field to users for resetting password on admin interface

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130314234755) do
+ActiveRecord::Schema.define(:version => 20130327051006) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -158,6 +158,7 @@ ActiveRecord::Schema.define(:version => 20130314234755) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "reset_password_sent_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
