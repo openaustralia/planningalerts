a48646be0940acb65fa809aa170b5c0d145cd127
Matthew Landauer <matthew@openaustralia.org>
2013-04-08 17:14:52 +1000

    In production send emails via a locally running instance of Cuttlefish

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,9 @@ PlanningalertsApp::Application.configure do
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
+  # Send mails to the locally running instance of Cuttlefish
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 2525 }
+  
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'www.planningalerts.org.au' }
