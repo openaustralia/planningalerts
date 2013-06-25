631577b6fae37d6b6b58657a1e11ca37341d25f9
Matthew Landauer <matthew@openaustralia.org>
2013-06-25 16:40:13 +1000

    Adds twitter cards meta info to applications pages

--- app/helpers/applications_helper.rb
+++ app/helpers/applications_helper.rb
@@ -62,9 +62,14 @@ module ApplicationsHelper
     image_tag("http://maps.googleapis.com/maps/api/staticmap?zoom=#{zoom}&size=#{size}&maptype=roadmap&markers=color:red%7C#{application.lat},#{application.lng}&sensor=false".html_safe, :size => size, :alt => "Map of #{application.address}")
   end
 
-  def google_static_streetview(application, options)
+  def google_static_streetview_url(application, options)
     size = options[:size] || "350x200"
     fov = options[:fov] || 90
-    image_tag("http://maps.googleapis.com/maps/api/streetview?size=#{size}&location=#{application.lat},#{application.lng}&fov=#{fov}&sensor=false".html_safe, :size => size, :alt => "Streetview of #{application.address}")
+    "http://maps.googleapis.com/maps/api/streetview?size=#{size}&location=#{application.lat},#{application.lng}&fov=#{fov}&sensor=false".html_safe
+  end
+
+  def google_static_streetview(application, options)
+    size = options[:size] || "350x200"
+    image_tag(google_static_streetview_url(application, options), :size => size, :alt => "Streetview of #{application.address}")
   end
 end
