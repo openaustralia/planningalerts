7d46589fa6194713a9792fe15650a43501fe7b63
Matthew Landauer <matthew@openaustralia.org>
2013-09-03 18:14:58 +1000

    Applies the testing to some test data

--- app/controllers/authorities_controller.rb
+++ app/controllers/authorities_controller.rb
@@ -36,51 +36,68 @@ class AuthoritiesController < ApplicationController
   end
 
   def atdis_test_feed
-  end
-
-  # Available at http://localhost:3000/atdis/1.0/applications.json
-  def atdis_test_data
-    # Make some test 
-    render :json => {
-      :response => [
-        {
-          :application => {
-            :info => {
-              :dat_id => "DA2013-0381",
-              :description => "New pool plus deck",
-              :authority => "Example Council Shire Council",
-              :lodgement_date => "2013-04-20T02:01:07Z",
-              :determination_date => "2013-06-20T02:01:07Z",
-              :notification_start_date => "2013-04-20T02:01:07Z",
-              :notification_end_date => "2013-05-20T02:01:07Z",
-              :status => "OPEN"
-            },
-            :reference => {
-              :more_info_url => "http://www.examplecouncil.nsw.gov.au/atdis/1.0/applications/DA2013-0381"
-            },
-            :location => {
-              :address => "123 Fourfivesix Street Neutral Bay NSW 2089",
-              :land_title_ref => {
-                :lot => "10",
-                :section => "ABC",
-                :dpsp_id => "DP2013-0381"
-              }
-            }
-          }
+    @url = params[:url]
+    if @url
+      if @url == "http://localhost:3000"
+        page = ATDIS::Page.read_json(<<-EOF
+{
+  "response": [
+    {
+      "application": {
+        "info": {
+          "dat_id": "DA2013-0381",
+          "description": "New pool plus deck",
+          "authority": "Example Council Shire Council",
+          "lodgement_date": "2013-04-20T02:01:07Z",
+          "determination_date": "2013-06-20T02:01:07Z",
+          "notification_start_date": "2013-04-20T02:01:07Z",
+          "notification_end_date": "2013-05-20T02:01:07Z",
+          "status": "OPEN"
         },
-        {
-          :application => {}
+        "reference": {
+          "more_info_url": "http://www.examplecouncil.nsw.gov.au/atdis/1.0/applications/DA2013-0381"
+        },
+        "location": {
+          "address": "123 Fourfivesix Street Neutral Bay NSW 2089",
+          "land_title_ref": {
+            "lot": "10",
+            "section": "ABC",
+            "dpsp_id": "DP2013-0381"
+          }
         }
-      ],
-      :count => 2,
-      :pagination => {
-        :previous => nil,
-        :next => nil,
-        :current => 1,
-        :per_page => 25,
-        :count => 100,
-        :pages => 1
       }
+    },
+    {
+      "application": {}
     }
+  ],
+  "count": 2,
+  "pagination": {
+    "previous": null,
+    "next": null,
+    "current": 1,
+    "per_page": 25,
+    "count": 100,
+    "pages": 1
+  }
+}
+          EOF
+          )
+      else
+        # Process feed using atdis gem
+        feed = ATDIS::Feed.new(@url)
+        # Just grabs the first page of results
+        page = feed.applications
+      end
+      @applications = page.results
+      # Try validating the applications and return all the errors for the first non-validating application
+      @applications.each do |application|
+        unless application.valid?
+          @errored_application = application
+          @applications = nil
+          break
+        end
+      end
+    end
   end
 end
