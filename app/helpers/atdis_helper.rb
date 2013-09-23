b1a6cedbf0cad35db4ac489d295b4e84e014c5da
Matthew Landauer <matthew@openaustralia.org>
2013-09-23 15:49:29 +1000

    Converted convoluted helper into view partial

--- app/helpers/atdis_helper.rb
+++ app/helpers/atdis_helper.rb
@@ -34,42 +34,4 @@ module AtdisHelper
       end.join.html_safe
     end
   end
-
-  def attribute_error_value(value)
-    if value.kind_of?(Array)
-      first_errored = value.find{|v| !v.valid?}
-      if first_errored
-        attribute_error_value(first_errored)
-      else
-        content_tag(:p, "&hellip;".html_safe)
-      end
-    elsif value.class.respond_to?(:attribute_names)
-      attribute_error_table(value)
-    else
-      h(value)
-    end
-  end
-
-  # TODO Turn this into a partial
-  def attribute_error_table(model)
-    content_tag(:table, :class => "scraper_fields") do
-      content_tag(:tr) do
-        content_tag(:th, "JSON parameters") + content_tag(:th, "Errors", :class => "error")
-      end + 
-      model.json_errors.map do |attr, errors|
-        content_tag(:tr) do
-          error_html = errors.map do |e|
-            t = content_tag(:span, e.message, :class => "highlight")
-            if e.spec_section
-              t += " &mdash; see ".html_safe +
-                link_to("section #{e.spec_section} of specification", atdis_specification_path(:anchor => "section#{e.spec_section}")) + ".".html_safe
-            end
-            t
-          end.join(" ").html_safe
-          content_tag(:td, content_tag(:pre, h(truncate(MultiJson.dump(attr, :pretty => true), :length => 500)))) +
-            content_tag(:td, error_html)
-        end
-      end.join.html_safe
-    end
-  end
 end
