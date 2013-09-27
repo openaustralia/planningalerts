e8c0e785005f44d94533512873a0ae2181c33679
Matthew Landauer <matthew@openaustralia.org>
2013-09-27 12:12:28 +1000

    Don't raise an exception on badly formatted date

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -7,15 +7,49 @@ class Feed
 
   attr_reader :base_url, :page, :postcode, :lodgement_date_start, :lodgement_date_end,
     :last_modified_date_start, :last_modified_date_end
+  attr_reader :lodgement_date_start_before_type_cast, :lodgement_date_end_before_type_cast,
+    :last_modified_date_start_before_type_cast, :last_modified_date_end_before_type_cast
 
   def initialize(options = {})
-    @base_url = options[:base_url]
-    @page = options[:page] ? options[:page].to_i : 1
-    @postcode = options[:postcode] if options[:postcode].present?
-    @lodgement_date_start = cast_to_date(options[:lodgement_date_start])
-    @lodgement_date_end = cast_to_date(options[:lodgement_date_end])
-    @last_modified_date_start = cast_to_date(options[:last_modified_date_start])
-    @last_modified_date_end = cast_to_date(options[:last_modified_date_end])
+    base_url = options[:base_url]
+    page = options[:page]
+    postcode = options[:postcode]
+    lodgement_date_start = options[:lodgement_date_start]
+    lodgement_date_end = options[:lodgement_date_end]
+    last_modified_date_start = options[:last_modified_date_start]
+    last_modified_date_end = options[:last_modified_date_end]
+  end
+
+  def base_url(value)
+    @base_url = value
+  end
+
+  def page=(value)
+    @page = value ? value.to_i : 1
+  end
+
+  def postcode=(value)
+    @postcode = value if value.present?
+  end
+
+  def lodgement_date_start=(value)
+    @lodgement_date_start_before_type_cast = value
+    @lodgement_date_start = cast_to_date(value)
+  end
+
+  def lodgement_date_end=(value)
+    @lodgement_date_end_before_type_cast = value
+    @lodgement_date_end = cast_to_date(value)
+  end
+
+  def last_modified_date_start=(value)
+    @last_modified_date_start_before_type_cast = value
+    @last_modified_date_start = cast_to_date(value)
+  end
+
+  def last_modified_date_end=(value)
+    @last_modified_date_end_before_type_cast = value
+    @last_modified_date_end = cast_to_date(value)
   end
 
   def self.create_from_url(url)
@@ -61,7 +95,11 @@ class Feed
     if value.kind_of?(Date)
       value
     elsif value.present?
-      Date.parse(value)
+      begin
+        Date.parse(value)
+      rescue ArgumentError
+        nil
+      end
     end
   end
 
