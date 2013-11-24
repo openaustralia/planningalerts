f429df778d44eb142ccb88b287a7c0785604b3e6
Dylan Fogarty-MacDonald <dylan@dylanfm.com>
2013-11-24 21:28:32 +1100

    6 items of news rather than 4.

--- app/models/planning_alerts_rss.rb
+++ app/models/planning_alerts_rss.rb
@@ -6,7 +6,7 @@ class PlanningAlertsRSS
     begin
       content = HTTParty.get(URL).body
       feed    = RSS::Parser.parse(content, false)
-      feed.channel.items[0...4] # just use the first five items
+      feed.channel.items[0...6] # just use the first 6 items
     rescue # TODO rescue a particular exception
       # This happens when the DB is empty
       []
