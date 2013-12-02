76a19bddd21cf188f2252ac49e8b53f8bb9e72bc
Matthew Landauer <matthew@openaustralia.org>
2013-12-02 16:00:42 +1100

    Revert "Add stupid typo on purpose to test 500 page"
    This reverts commit aaccff10f7c4c4f7285f0074ec3ba3148d71f945.

--- app/controllers/atdis_controller.rb
+++ app/controllers/atdis_controller.rb
@@ -27,7 +27,6 @@ class AtdisController < ApplicationController
     if File.exists?(file)
       render :file  => file, :content_type => "text/javascript", :layout => false
     else
-      sdf
       render :text => "not available", :status => 404
     end
   end
