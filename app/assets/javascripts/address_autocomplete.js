cf4e59820500144e03ddb1afed7128adf7344fba
Matthew Landauer <matthew@openaustralia.org>
2013-12-16 11:29:49 +1100

    Convert long state names to shorter version

--- app/assets/javascripts/address_autocomplete.js
+++ app/assets/javascripts/address_autocomplete.js
@@ -13,9 +13,23 @@ $("#alert_address,#q").autocomplete({
                 var length = prediction.matched_substrings[0].length;
                 var offset = prediction.matched_substrings[0].offset;
                 // Remove Australia from the list to make a slightly shorter version of the text
-                text = $.map(prediction.terms.slice(0,-1), function(term){
+                textArray = $.map(prediction.terms.slice(0,-1), function(term){
                     return(term.value);
-                }).join(", ");
+                });
+                // Translate name of state to shorter version
+                state = textArray[textArray.length - 1];
+                switch(state) {
+                    case "Australian Capital Territory": state = "ACT"; break;
+                    case "New South Wales": state = "NSW"; break;
+                    case "Northern Territory": state = "NT"; break;
+                    case "Queensland": state = "QLD"; break;
+                    case "South Australia": state = "SA"; break;
+                    case "Tasmania": state = "TAS"; break;
+                    case "Victoria": state = "VIC"; break;
+                    case "Western Australia": state = "WA"; break;
+                }
+                textArray[textArray.length - 1] = state;
+                text = textArray.join(", ");
                 var html = text.slice(0, offset) +
                     "<strong>" + text.slice(offset, offset+length) + "</strong>" +
                     text.slice(offset+length);
