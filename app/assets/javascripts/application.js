661bdeff0bebfe7e27be5c43eca53f558ed49a5c
Matthew Landauer <matthew@openaustralia.org>
2014-03-26 18:09:13 +1100

    Only include the datapicker js where it's needed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,3 @@
-//= require jquery.ui.datepicker
-//= require jquery.ui.datepicker-en-AU.js
 //= require jquery.ui.autocomplete.js
 //= require jquery.ui.autocomplete.html.js
-//= require atdis.js
 //= require address_autocomplete.js
