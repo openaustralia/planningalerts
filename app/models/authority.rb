e3b74ae6a129c5d35be9f9d1b2f241348c3ec8da
Matthew Landauer <matthew@openaustralia.org>
2014-04-10 17:05:52 +1000

    We can now add authorities without a scraper

--- app/models/authority.rb
+++ app/models/authority.rb
@@ -19,7 +19,7 @@ end
 
 class Authority < ActiveRecord::Base
   has_many :applications
-  scope :active, :conditions => 'disabled = 0 or disabled is null'
+  scope :active, :conditions => '(disabled = 0 or disabled is null) AND morph_name != "" AND morph_name IS NOT NULL'
 
   def full_name_and_state
     full_name + ", " + state
