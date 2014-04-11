ce29b36cea19d5eb501eb0651479c917b7144da9
Henare Degan <henare.degan@gmail.com>
2014-04-11 10:54:08 +1000

    Don't link to scraper app - it doesn't exist any more

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,6 @@ namespace :deploy do
             "#{release_path}/config/production.sphinx.conf"     => "#{shared_path}/production.sphinx.conf",
             "#{release_path}/config/environments/production.rb" => "#{shared_path}/production.rb",
             "#{release_path}/config/sphinx.yml"                 => "#{shared_path}/sphinx.yml",
-            "#{release_path}/public/scrapers"                   => "#{deploy_to}/../parsers/current/public",
             "#{release_path}/public/sitemap.xml"                => "#{shared_path}/sitemap.xml",
             "#{release_path}/public/sitemaps"                   => "#{shared_path}/sitemaps",
             "#{release_path}/public/assets"                     => "#{shared_path}/assets",
