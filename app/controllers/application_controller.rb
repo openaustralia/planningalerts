9937bdc34419c94c562ad9e920e593e6877a8fb8
Matthew Landauer <matthew@openaustralia.org>
2014-05-06 13:11:52 +1000

    First template for NSW version of site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,14 +9,20 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
-  
-  before_filter :load_configuration
-  
+
+  before_filter :load_configuration, :set_view_path
+
   private
   
   def load_configuration
     @alert_count = Stat.applications_sent
     @authority_count = Authority.active.count
   end
-  
+
+  def set_view_path
+    if Rails.env.development? && request.domain(6) == "nsw.127.0.0.1.xip.io"
+      self.prepend_view_path "lib/themes/nsw/views"
+    end
+  end
+
 end
