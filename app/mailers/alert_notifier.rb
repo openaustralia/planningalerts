8953334cabf9cec38fff782b8bb5898e5f36416b
Matthew Landauer <matthew@openaustralia.org>
2014-05-13 17:51:05 +1000

    Improve saving of host value

--- app/mailers/alert_notifier.rb
+++ app/mailers/alert_notifier.rb
@@ -4,27 +4,28 @@ class AlertNotifier < ActionMailer::Base
   helper :application, :applications
 
   def alert(theme, alert, applications, comments = [])
-    # TODO Extract this into the theme
-    if theme == "default"
-      template_path = 'alert_notifier'
-      # TODO Don't set this explicitly
-      AlertNotifier.default_url_options[:host] = "dev.planningalerts.org.au"
-    elsif theme == "nsw"
-      template_path = ["../../lib/themes/nsw/views/alert_notifier", "alert_notifier"]
-      self.prepend_view_path "lib/themes/nsw/views"
-      AlertNotifier.default_url_options[:host] = "planningalerts.nsw.gov.au"
-    else
-      raise "Unknown theme #{theme}"
+    save_host = AlertNotifier.default_url_options[:host]
+    begin
+      # TODO Extract this into the theme
+      if theme == "default"
+        template_path = 'alert_notifier'
+      elsif theme == "nsw"
+        template_path = ["../../lib/themes/nsw/views/alert_notifier", "alert_notifier"]
+        self.prepend_view_path "lib/themes/nsw/views"
+        AlertNotifier.default_url_options[:host] = "planningalerts.nsw.gov.au"
+      else
+        raise "Unknown theme #{theme}"
+      end
+
+      @alert, @applications, @comments = alert, applications, comments
+
+      mail(:to => alert.email,
+        :subject => render_to_string(:partial => "subject",
+          :locals => {:applications => applications, :comments => comments, :alert => alert}).strip,
+        "return-path" => ::Configuration::BOUNCE_EMAIL_ADDRESS,
+        template_path: template_path)
+    ensure
+      AlertNotifier.default_url_options[:host] = save_host
     end
-
-    @alert, @applications, @comments = alert, applications, comments
-
-    m = mail(:to => alert.email,
-      :subject => render_to_string(:partial => "subject",
-        :locals => {:applications => applications, :comments => comments, :alert => alert}).strip,
-      "return-path" => ::Configuration::BOUNCE_EMAIL_ADDRESS,
-      template_path: template_path)
-    AlertNotifier.default_url_options[:host] = "dev.planningalerts.org.au"
-    m
   end
 end
