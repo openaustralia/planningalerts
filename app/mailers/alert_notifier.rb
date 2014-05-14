9c0f481684208caad798ac8a4d43f7fdae11ce3c
Matthew Landauer <matthew@openaustralia.org>
2014-05-14 18:17:01 +1000

    Move module to its own file

--- app/mailers/alert_notifier.rb
+++ app/mailers/alert_notifier.rb
@@ -1,28 +1,3 @@
-module ActionMailerThemer
-
-  private
-
-  def themed_mail(params)
-    theme = params.delete(:theme)
-
-    # TODO Extract this into the theme
-    if theme == "default"
-      template_path = 'alert_notifier'
-      @host = ActionMailer::Base.default_url_options[:host]
-      from = "#{::Configuration::EMAIL_FROM_NAME} <#{::Configuration::EMAIL_FROM_ADDRESS}>"
-    elsif theme == "nsw"
-      template_path = ["../../lib/themes/nsw/views/alert_notifier", "alert_notifier"]
-      self.prepend_view_path "lib/themes/nsw/views"
-      @host = "planningalerts.nsw.gov.au"
-      from = "NSW PlanningAlerts <contact@planningalerts.nsw.gov.au>"
-    else
-      raise "Unknown theme #{theme}"
-    end
-
-    mail(params.merge(from: from, template_path: template_path))
-  end
-end
-
 class AlertNotifier < ActionMailer::Base
   include ActionMailerThemer
   helper :application, :applications
