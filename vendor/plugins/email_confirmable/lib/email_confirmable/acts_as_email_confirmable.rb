c733649940b588b184813758759478e7b6ab4c13
Matthew Landauer <matthew@openaustralia.org>
2014-05-20 15:59:35 +1000

    Whitespace changes only

--- vendor/plugins/email_confirmable/lib/email_confirmable/acts_as_email_confirmable.rb
+++ vendor/plugins/email_confirmable/lib/email_confirmable/acts_as_email_confirmable.rb
@@ -5,11 +5,11 @@ module EmailConfirmable
   def self.included(base)
     base.send :extend, ClassMethods
   end
-  
+
   module ClassMethods
     def acts_as_email_confirmable
       send :include, InstanceMethods
-      
+
       validates_presence_of :email
       validates_email_format_of :email, :on => :create
       before_create :set_confirm_info
@@ -18,20 +18,20 @@ module EmailConfirmable
       scope :confirmed, :conditions => {:confirmed => true}
     end
   end
-  
+
   module InstanceMethods
     def confirm!
       self.confirmed = true
       save!
       after_confirm if self.respond_to?(:after_confirm)
     end
-  
+
     def send_confirmation_email
       Notifier.confirm("default", self).deliver
     end
-  
+
     protected
-    
+
     def set_confirm_info
       # TODO: Should check that this is unique across all objects and if not try again
       self.confirm_id = Digest::MD5.hexdigest(rand.to_s + Time.now.to_s)[0...20]
