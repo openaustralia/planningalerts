28e4aab90f655551c360097b9bce0d55d1369b85
Matthew Landauer <matthew@openaustralia.org>
2014-05-20 13:59:39 +1000

    Whitespace changes only

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,11 +2,11 @@ class Comment < ActiveRecord::Base
   belongs_to :application
   has_many :reports
   validates_presence_of :name, :text, :address
-  
+
   acts_as_email_confirmable
   scope :visible, :conditions => {:confirmed => true, :hidden => false}
   scope :in_past_week, where("created_at > ?", 7.days.ago)
-  
+
   # Send the comment to the planning authority
   def after_confirm
     CommentNotifier.notify("default", self).deliver
