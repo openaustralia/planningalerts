43082bd07e80c0f9a9edb5bd0f3d870727b619bb
Matthew Landauer <matthew@openaustralia.org>
2014-05-20 12:36:14 +1000

    Extract setup in test

--- spec/mailers/comment_notifier_spec.rb
+++ spec/mailers/comment_notifier_spec.rb
@@ -8,34 +8,32 @@ describe CommentNotifier do
       @comment = mock_model(Comment, :email => "foo@bar.com", :name => "Matthew", :application => application, :confirm_id => "abcdef", :text => "It's a good thing.\n\nOh yes it is.", :address => "1 Bar Street")
     end
 
-    it "should be sent to the planning authority's feedback email address" do
-      notifier = CommentNotifier.notify(@comment)
-      notifier.to.should == [@comment.application.authority.email]
-    end
+    context "default theme" do
+      let(:notifier) { CommentNotifier.notify(@comment) }
 
-    it "should have the sender as the main planningalerts email address" do
-      notifier = CommentNotifier.notify(@comment)
-      notifier.sender.should == "contact@planningalerts.org.au"
-    end
+      it "should be sent to the planning authority's feedback email address" do
+        notifier.to.should == [@comment.application.authority.email]
+      end
 
-    it "should be from the email address of the person who made the comment" do
-      notifier = CommentNotifier.notify(@comment)
-      notifier.from.should == [@comment.email]
-    end
+      it "should have the sender as the main planningalerts email address" do
+        notifier.sender.should == "contact@planningalerts.org.au"
+      end
 
-    it "should say in the subject line it is a comment on a development application" do
-      notifier = CommentNotifier.notify(@comment)
-      notifier.subject.should == "Comment on application X/001"
-    end
+      it "should be from the email address of the person who made the comment" do
+        notifier.from.should == [@comment.email]
+      end
 
-    it "should have specific information in the body of the email" do
-      notifier = CommentNotifier.notify(@comment)
-      notifier.text_part.body.to_s.should == Rails.root.join("spec/mailers/regression/comment_notifier/email1.txt").read
-    end
+      it "should say in the subject line it is a comment on a development application" do
+        notifier.subject.should == "Comment on application X/001"
+      end
 
-    it "should format paragraphs correctly in the html version of the email" do
-      notifier = CommentNotifier.notify(@comment)
-      notifier.html_part.body.to_s.should == Rails.root.join("spec/mailers/regression/comment_notifier/email1.html").read
+      it "should have specific information in the body of the email" do
+        notifier.text_part.body.to_s.should == Rails.root.join("spec/mailers/regression/comment_notifier/email1.txt").read
+      end
+
+      it "should format paragraphs correctly in the html version of the email" do
+        notifier.html_part.body.to_s.should == Rails.root.join("spec/mailers/regression/comment_notifier/email1.html").read
+      end
     end
   end
 end
