f4df63d985ee1999b9acda077d5e278eb4b3adb0
Matthew Landauer <matthew@openaustralia.org>
2014-05-20 13:12:55 +1000

    Don't do the from address directly in the theming

--- app/mailers/action_mailer_themer.rb
+++ app/mailers/action_mailer_themer.rb
@@ -9,16 +9,14 @@ module ActionMailerThemer
     if theme == "default"
       template_path = mailer_name
       @host = ActionMailer::Base.default_url_options[:host]
-      from = "#{::Configuration::EMAIL_FROM_NAME} <#{::Configuration::EMAIL_FROM_ADDRESS}>"
     elsif theme == "nsw"
       template_path = ["../../lib/themes/#{theme}/views/#{mailer_name}", mailer_name]
       self.prepend_view_path "lib/themes/#{theme}/views"
       @host = "planningalerts.nsw.gov.au"
-      from = "NSW PlanningAlerts <contact@planningalerts.nsw.gov.au>"
     else
       raise "Unknown theme #{theme}"
     end
 
-    mail(params.merge(from: from, template_path: template_path))
+    mail(params.merge(template_path: template_path))
   end
 end
--- app/mailers/alert_notifier.rb
+++ app/mailers/alert_notifier.rb
@@ -3,9 +3,17 @@ class AlertNotifier < ActionMailer::Base
   helper :application, :applications
 
   def alert(theme, alert, applications, comments = [])
+    if theme == "default"
+      from = "#{::Configuration::EMAIL_FROM_NAME} <#{::Configuration::EMAIL_FROM_ADDRESS}>"
+    elsif theme == "nsw"
+      from = "NSW PlanningAlerts <contact@planningalerts.nsw.gov.au>"
+    else
+      raise "Unknown theme #{theme}"
+    end
+
     @alert, @applications, @comments = alert, applications, comments
 
-    themed_mail(:theme => theme, :to => alert.email,
+    themed_mail(:theme => theme, :from => from, :to => alert.email,
       :subject => render_to_string(:partial => "subject",
         :locals => {:applications => applications, :comments => comments, :alert => alert}).strip,
       "return-path" => ::Configuration::BOUNCE_EMAIL_ADDRESS)
