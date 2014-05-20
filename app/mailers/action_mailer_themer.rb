47a8b116b353cedf0ef974015a575b070082435b
Matthew Landauer <matthew@openaustralia.org>
2014-05-20 18:01:05 +1000

    Extract class

--- app/mailers/action_mailer_themer.rb
+++ app/mailers/action_mailer_themer.rb
@@ -1,3 +1,31 @@
+class ThemeChooser
+  attr_reader :theme
+
+  def initialize(theme)
+    @theme = theme
+  end
+
+  def host
+    if theme == "default"
+      ActionMailer::Base.default_url_options[:host]
+    elsif theme == "nsw"
+      "planningalerts.nsw.gov.au"
+    else
+      raise "Unknown theme #{theme}"
+    end
+  end
+
+  def email_from
+    if theme == "default"
+      "#{::Configuration::EMAIL_FROM_NAME} <#{::Configuration::EMAIL_FROM_ADDRESS}>"
+    elsif theme == "nsw"
+      "NSW PlanningAlerts <contact@planningalerts.nsw.gov.au>"
+    else
+      raise "Unknown theme #{theme}"
+    end
+  end
+end
+
 module ActionMailerThemer
 
   private
@@ -8,25 +36,19 @@ module ActionMailerThemer
     # TODO Extract this into the theme
     if theme == "default"
       template_path = mailer_name
-      @host = ActionMailer::Base.default_url_options[:host]
     elsif theme == "nsw"
       template_path = ["../../lib/themes/#{theme}/views/#{mailer_name}", mailer_name]
       self.prepend_view_path "lib/themes/#{theme}/views"
-      @host = "planningalerts.nsw.gov.au"
     else
       raise "Unknown theme #{theme}"
     end
+
+    @host = ThemeChooser.new(theme).host
 
     mail(params.merge(template_path: template_path))
   end
 
   def email_from(theme)
-    if theme == "default"
-      "#{::Configuration::EMAIL_FROM_NAME} <#{::Configuration::EMAIL_FROM_ADDRESS}>"
-    elsif theme == "nsw"
-      "NSW PlanningAlerts <contact@planningalerts.nsw.gov.au>"
-    else
-      raise "Unknown theme #{theme}"
-    end
+    ThemeChooser.new(theme).email_from
   end
 end
