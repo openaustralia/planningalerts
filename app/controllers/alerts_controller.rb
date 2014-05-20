f7f3bdcd726c7233934c3ee9264f56c379eb8f21
Matthew Landauer <matthew@openaustralia.org>
2014-05-20 14:37:50 +1000

    Whitespace changes only

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -5,24 +5,24 @@ class AlertsController < ApplicationController
     @alert = Alert.new(:address => params[:address], :email => params[:email])
     @set_focus_control = params[:address] ? "alert_email" : "alert_address"
   end
-  
+
   def new
     @alert = Alert.new(:address => params[:address], :email => params[:email])
     @set_focus_control = params[:address] ? "alert_email" : "alert_address"
   end
-  
+
   def create
     @alert = Alert.new(:address => params[:alert][:address], :email => params[:alert][:email], :radius_meters => zone_sizes['l'], :theme => @theme)
     if !@alert.save
       render 'new'
     end
   end
-  
+
   def unsubscribe
     @alert = Alert.find_by_confirm_id(params[:id])
     @alert.update_attribute(:unsubscribed, true) if @alert
   end
-  
+
   def area
     @zone_sizes = zone_sizes
     @alert = Alert.find_by_confirm_id!(params[:id])
@@ -34,19 +34,19 @@ class AlertsController < ApplicationController
       render "area_updated"
     end
   end
-  
+
   def statistics
     # Commented out because this page is killing mysql.
     #@no_confirmed_alerts = Alert.confirmed.count
     #@no_alerts = Alert.count
     # Hmmm... Temporary variable because we're calling a very slow method. Not good.
     #@alerts_in_inactive_areas = Alert.alerts_in_inactive_areas
-    #@no_alerts_in_active_areas = @no_alerts - @alerts_in_inactive_areas.count    
+    #@no_alerts_in_active_areas = @no_alerts - @alerts_in_inactive_areas.count
     #@freq = Alert.distribution_of_lgas(@alerts_in_inactive_areas)
   end
-  
+
   private
-  
+
   def zone_sizes
     {'s' => ::Configuration::SMALL_ZONE_SIZE,
       'm' => ::Configuration::MEDIUM_ZONE_SIZE,
