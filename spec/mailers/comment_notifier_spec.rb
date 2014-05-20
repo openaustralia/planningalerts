ac92ce2db8f948c4031cbaa8878a9e1726aab62c
Matthew Landauer <matthew@openaustralia.org>
2014-05-20 12:32:12 +1000

    Whitespace changes only

--- spec/mailers/comment_notifier_spec.rb
+++ spec/mailers/comment_notifier_spec.rb
@@ -7,12 +7,12 @@ describe CommentNotifier do
       application = mock_model(Application, :authority => authority, :address => "12 Foo Rd", :council_reference => "X/001", :description => "Building something", :id => 123)
       @comment = mock_model(Comment, :email => "foo@bar.com", :name => "Matthew", :application => application, :confirm_id => "abcdef", :text => "It's a good thing.\n\nOh yes it is.", :address => "1 Bar Street")
     end
-  
+
     it "should be sent to the planning authority's feedback email address" do
       notifier = CommentNotifier.notify(@comment)
       notifier.to.should == [@comment.application.authority.email]
     end
-    
+
     it "should have the sender as the main planningalerts email address" do
       notifier = CommentNotifier.notify(@comment)
       notifier.sender.should == "contact@planningalerts.org.au"
@@ -27,7 +27,7 @@ describe CommentNotifier do
       notifier = CommentNotifier.notify(@comment)
       notifier.subject.should == "Comment on application X/001"
     end
-    
+
     it "should have specific information in the body of the email" do
       notifier = CommentNotifier.notify(@comment)
       notifier.text_part.body.to_s.should == Rails.root.join("spec/mailers/regression/comment_notifier/email1.txt").read
