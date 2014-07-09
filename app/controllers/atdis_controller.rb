179f38124698ae8a6987431eca6940dc297615f9
Matthew Landauer <matthew@openaustralia.org>
2014-07-09 13:15:04 +1000

    Give a human error if the url is invalid

--- app/controllers/atdis_controller.rb
+++ app/controllers/atdis_controller.rb
@@ -8,6 +8,8 @@ class AtdisController < ApplicationController
         @error = "Could not load data - #{e}"
       rescue RestClient::InternalServerError => e
         @error = "Remote server returned an internal server error (error code 500) accessing #{params[:url]}"
+      rescue URI::InvalidURIError => e
+        @error = "The url appears to be invalid #{params[:url]}"
       end
     else
       @feed = Feed.new
