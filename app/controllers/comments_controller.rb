6d51de5e45fc2eae5cc0406f5e7414ccc81ee30f
Matthew Landauer <matthew@openaustralia.org>
2014-07-15 16:49:57 +1000

    Add hidden text field as a honey trap for spammers in new comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,14 +6,19 @@ class CommentsController < ApplicationController
   
   def create
     @application = Application.find(params[:application_id])
-    # TODO: Put controls on model so that hash assignment below can't be abused
-    @comment = Comment.new(params[:comment])
-    @comment.application_id = @application.id
-    if !@comment.save
-      flash.now[:error] = "Some of the comment wasn't filled out completely. See below."
-      # HACK: Required for new email alert signup form
-      @alert = Alert.new(:address => @application.address)
-      render 'applications/show'
+
+    # First check if the honeypot field has been filled out by a spam bot
+    # If so, make it look like things worked but don't actually do anything
+    if params[:little_sweety].blank?
+      # TODO: Put controls on model so that hash assignment below can't be abused
+      @comment = Comment.new(params[:comment])
+      @comment.application_id = @application.id
+      if !@comment.save
+        flash.now[:error] = "Some of the comment wasn't filled out completely. See below."
+        # HACK: Required for new email alert signup form
+        @alert = Alert.new(:address => @application.address)
+        render 'applications/show'
+      end
     end
   end
   
