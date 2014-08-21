d79a5acd200438f754b521cf537a26a87c53051e
Henare Degan <henare.degan@gmail.com>
2014-08-22 09:39:21 +1000

    Workaround the foundation server being offline. Fixes #589

--- app/models/planning_alerts_rss.rb
+++ app/models/planning_alerts_rss.rb
@@ -7,7 +7,7 @@ class PlanningAlertsRSS
       content = HTTParty.get(URL).body
       feed    = RSS::Parser.parse(content, false)
       feed.channel.items[0...6] # just use the first 6 items
-    rescue SocketError, Errno::ETIMEDOUT
+    rescue SocketError, Errno::ETIMEDOUT, RSS::NotWellFormedError
       []
     end
   end
