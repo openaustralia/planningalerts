342b13b309827579cad6010a55a43974f0344002
Matthew Landauer <matthew@openaustralia.org>
2014-10-28 11:27:06 +1100

    Small refactor

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,12 +3,12 @@ class ReportsController < ApplicationController
     @comment = Comment.visible.find(params[:comment_id])
     @report = Report.new
   end
-  
+
   def create
     @comment = Comment.visible.find(params[:comment_id])
-    @report = @comment.reports.build(:name => params[:report][:name], :email => params[:report][:email], :details => params[:report][:details])
-    if @report.save
-      ReportNotifier.notify(@report).deliver
+    report = @comment.reports.build(:name => params[:report][:name], :email => params[:report][:email], :details => params[:report][:details])
+    if report.save
+      ReportNotifier.notify(report).deliver
     else
       render 'new'
     end
