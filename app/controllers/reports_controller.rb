5dc7d2cae45ac9eab9bc5af487811a0a530424dd
Matthew Landauer <matthew@openaustralia.org>
2014-10-28 11:31:19 +1100

    Add honeypot to stop spammers abusing the abuse form

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,11 +6,16 @@ class ReportsController < ApplicationController
 
   def create
     @comment = Comment.visible.find(params[:comment_id])
-    report = @comment.reports.build(:name => params[:report][:name], :email => params[:report][:email], :details => params[:report][:details])
-    if report.save
-      ReportNotifier.notify(report).deliver
-    else
-      render 'new'
+
+    # First check if the honeypot field has been filled out by a spam bot
+    # If so, make it look like things worked but don't actually do anything
+    if params[:little_sweety].blank?
+      report = @comment.reports.build(:name => params[:report][:name], :email => params[:report][:email], :details => params[:report][:details])
+      if report.save
+        ReportNotifier.notify(report).deliver
+      else
+        render 'new'
+      end
     end
   end
 end
