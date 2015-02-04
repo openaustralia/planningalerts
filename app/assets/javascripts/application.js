a681169736d1d5b24525d5fad225d56ad7cb4829
Luke Bacon <luke@equivalentideas.com>
2015-02-04 18:17:21 +1100

    replace form with button and stripe Checkout.js Remove form markup and styles Add checkout.js configuration to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,31 @@ $("#menu .toggle").click(function(){
     $("#menu ul").toggleClass("hidden").css("display", "");
   });
 });
+
+if ("#button-pro-signup".length) {
+  var handler = StripeCheckout.configure({
+    key: 'pk_test_7waRLEZaqjxyoxtZzlHtliMS',
+    image: '/square-image.png',
+    token: function(token) {
+      // Use the token to create the charge with a server-side script.
+      // You can access the token ID with `token.id`
+    }
+  });
+
+  $('#button-pro-signup').on('click', function(e) {
+    // Open Checkout with further options
+    handler.open({
+      name: 'PlanningAlerts',
+      amount: 9900,
+      currency: 'AUD'
+    });
+    e.preventDefault();
+
+    // TODO: send GA event track
+  });
+
+  // Close Checkout on page navigation
+  $(window).on('popstate', function() {
+    handler.close();
+  });
+}
