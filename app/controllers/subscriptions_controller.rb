66ededc9a8009fb4540ddad1c2cb7e30b3ac0e07
Luke Bacon <luke@equivalentideas.com>
2015-02-05 07:24:44 +1100

    add subscription controller basics from https://stripe.com/docs/checkout/guides/rails

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,2 +1,25 @@
 class SubscriptionsController < ApplicationController
+  def new
+  end
+
+  def create
+    # Amount in cents
+    @amount = 9900
+
+    customer = Stripe::Customer.create(
+      :email => 'example@stripe.com',
+      :card  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'aud'
+    )
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to charges_path
+  end
 end
