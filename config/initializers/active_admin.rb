4314e38f942d48374253f26564907075abb22493
Matthew Landauer <matthew@openaustralia.org>
2015-02-24 12:10:44 +1100

    From admin panel link back to main site

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,12 +5,12 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "PlanningAlerts Admin Panel"
+  config.site_title = "PlanningAlerts"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
-  # config.site_title_link = "/"
+  config.site_title_link = "/"
 
   # Set an optional image to be displayed for the header
   # instead of a string (overrides :site_title)
