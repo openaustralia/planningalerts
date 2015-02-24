28f3f970c93f36880197847794ebf14a32db5d15
Matthew Landauer <matthew@openaustralia.org>
2015-02-24 15:33:57 +1100

    Allow new attributes to actually be updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :rememberable,
     :trackable, :validatable
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :admin
+  attr_accessible :email, :name, :organisation, :password, :password_confirmation,
+    :remember_me, :bulk_api, :admin
 end
