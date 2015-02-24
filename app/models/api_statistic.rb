2f4cbaed94cf9ae00fa70e07548f520955a975c4
Matthew Landauer <matthew@openaustralia.org>
2015-02-24 17:53:14 +1100

    Temporarily disable api logging

--- app/models/api_statistic.rb
+++ app/models/api_statistic.rb
@@ -4,6 +4,7 @@ class ApiStatistic < ActiveRecord::Base
   def self.log(request)
     # Lookup the api key if there is one
     key = ApiKey.find_by_key(request.query_parameters["key"])
-    create!(:ip_address => request.remote_ip, :query => request.fullpath, :user_agent => request.headers["User-Agent"],:query_time => Time.now, api_key: key)
+    # TODO Temporarily don't log api stats while we perform a migration on the table
+    #create!(:ip_address => request.remote_ip, :query => request.fullpath, :user_agent => request.headers["User-Agent"],:query_time => Time.now, api_key: key)
   end
 end
