6a2e8add52ab125c75ec65fd947135e226559fc7
Matthew Landauer <matthew@openaustralia.org>
2015-02-24 16:18:12 +1100

    Set api key for users when they're created

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :name, :organisation, :password, :password_confirmation,
     :remember_me, :bulk_api, :admin
+  before_create :set_api_key
+
+  def set_api_key
+    self.api_key = Digest::MD5.base64digest(id.to_s + rand.to_s + Time.now.to_s)[0...20]
+  end
 end
