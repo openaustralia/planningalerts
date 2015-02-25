fed067d812ec4e144ca80917beda77c5f9abb082
Matthew Landauer <matthew@openaustralia.org>
2015-02-25 15:44:04 +1100

    New fields in users required for devise confirmable

---
+++ db/migrate/20150225042953_add_devise_confirmable_to_users.rb
@@ -0,0 +1,11 @@
+class AddDeviseConfirmableToUsers < ActiveRecord::Migration
+  def change
+    # New columns required for device confirmable
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+    # Only if using reconfirmable
+    add_column :users, :unconfirmed_email, :string
+    add_index :users, :confirmation_token, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150224180014) do
+ActiveRecord::Schema.define(:version => 20150225042953) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -182,9 +182,14 @@ ActiveRecord::Schema.define(:version => 20150224180014) do
     t.string   "name"
     t.string   "organisation"
     t.boolean  "bulk_api",                              :default => false, :null => false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["api_key"], :name => "index_users_on_api_key"
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
