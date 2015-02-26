2b40ac8e3e3e08ea53599e7f2fef45c5e4440612
Matthew Landauer <matthew@openaustralia.org>
2015-02-26 16:55:46 +1100

    For the time being only allow viewing of users

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register User do
+  actions :index, :show
+
   index :download_links => false do
     column :email
     column :name
@@ -7,22 +9,5 @@ ActiveAdmin.register User do
     column :bulk_api
     column :admin
     default_actions
-  end
-
-  form do |f|
-    f.inputs do
-      f.input :email
-      f.input :name
-      f.input :organisation
-      f.input :bulk_api
-      # Only show password fields when creating new user
-      if f.object.new_record?
-        f.input :password
-        f.input :password_confirmation
-      end
-      f.input :admin
-    end
-
-    f.buttons
   end
 end
