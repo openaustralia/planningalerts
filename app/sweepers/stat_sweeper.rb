06a83ebaab7618299b6ea380cde89a36b0443d55
Henare Degan <henare.degan@gmail.com>
2015-03-10 15:12:32 +1100

    Don't expire page since we don't cache it any more

--- app/sweepers/stat_sweeper.rb
+++ app/sweepers/stat_sweeper.rb
@@ -6,6 +6,5 @@ class StatSweeper < StandaloneSweeper
   def after_save(stat)
     expire_page(:controller => 'alerts', :action => 'statistics')
     expire_page(:controller => 'static', :action => %w( about faq get_involved ))
-    expire_page(:controller => 'api', :action => 'howto')
   end
 end
