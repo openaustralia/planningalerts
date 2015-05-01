c8ef7bcd2435b8fcf0294c850ea819586c1b2384
Henare Degan <henare.degan@gmail.com>
2015-05-01 11:16:41 +1000

    Add API usage statistics to admin panel. Fixes #663

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -6,6 +6,12 @@ ActiveAdmin.register User do
     column :name
     column :organisation
     column :api_key
+    column "Total API calls" do |u|
+      u.api_statistics.count
+    end
+    column "API calls in last month" do |u|
+      u.api_statistics.where("query_time >= ?", 1.month.ago).count
+    end
     column :bulk_api
     column :admin
     default_actions
