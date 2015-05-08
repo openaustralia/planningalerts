c7df00e4102bd0bd612aaf1e17725d1f303922f2
Henare Degan <henare.degan@gmail.com>
2015-05-08 16:09:50 +1000

    Make secret_key_base configurable
    So we're not storing the real one in the repository

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-PlanningalertsApp::Application.config.secret_key_base = '39d782b099a06652f92585de84f568c71885a4a129a307f03d2a803c6298ea129e334442ab042dc7fa664adc6e28b41317c3d53291366d0d7b5761c65102f3d4'
+PlanningalertsApp::Application.config.secret_key_base = Configuration::SECRET_KEY_BASE
