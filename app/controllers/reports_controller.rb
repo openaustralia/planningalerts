cf63bcae9655952770e24f55ffc7f4e274629fca
Henare Degan <henare.degan@gmail.com>
2015-05-11 18:07:35 +1000

    Make variable accessible by the view

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,9 +10,9 @@ class ReportsController < ApplicationController
     # First check if the honeypot field has been filled out by a spam bot
     # If so, make it look like things worked but don't actually do anything
     if params[:little_sweety].blank?
-      report = @comment.reports.build(:name => params[:report][:name], :email => params[:report][:email], :details => params[:report][:details])
-      if report.save
-        ReportNotifier.delay.notify(report)
+      @report = @comment.reports.build(:name => params[:report][:name], :email => params[:report][:email], :details => params[:report][:details])
+      if @report.save
+        ReportNotifier.delay.notify(@report)
       else
         render 'new'
       end
