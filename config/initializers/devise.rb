de30da49d60bb5ca1327199e9e34555853e9e801
Henare Degan <henare.degan@gmail.com>
2015-06-30 10:58:57 +1000

    Allow sign out via GET until we upgrade to DELETE

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -227,7 +227,8 @@ Devise.setup do |config|
   # config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+  # TODO: Change to :delete
+  config.sign_out_via = :get
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
