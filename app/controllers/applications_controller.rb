4f75934b852d298307ecc6615144b3587b3814f1
Henare Degan <henare.degan@gmail.com>
2015-07-01 09:03:23 +1000

    Correct template name so search RSS works

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -88,7 +88,7 @@ class ApplicationsController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.rss { render "index", :format => :rss, :layout => false, :content_type => Mime::XML }
+      format.rss { render "api/index", :format => :rss, :layout => false, :content_type => Mime::XML }
     end
   end
 
