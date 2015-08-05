6c945162533d5acf78211d65c9a9b45e53637771
Henare Degan <henare.degan@gmail.com>
2015-08-05 16:14:01 +1000

    Create factory alerts with different addresses
    So that the duplicate alerts aren't automatically deleted by us.

--- spec/factories.rb
+++ spec/factories.rb
@@ -28,7 +28,7 @@ FactoryGirl.define do
 
   factory :alert do
     email "mary@example.org"
-    address "252-254 Illawarra Road Marrickville 2204"
+    sequence(:address) { |s| "#{s} Illawarra Road Marrickville 2204" }
     lat -33.911105
     lng 151.155503
     radius_meters 2000
