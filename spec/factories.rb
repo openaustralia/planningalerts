b403870ad818d522f192023ab78c70bcdba123f8
Henare Degan <henare.degan@gmail.com>
2015-08-05 14:31:43 +1000

    Update syntax of factories definition

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,29 +1,31 @@
-Factory.define :authority do |a|
-  a.sequence(:full_name) { |n| "Acme Local Planning Authority - #{n}" }
-  a.short_name {|b| b.full_name}
-end
+FactoryGirl.define do
+  factory :authority do
+    sequence(:full_name) { |n| "Acme Local Planning Authority - #{n}" }
+    short_name {|b| b.full_name}
+  end
 
-Factory.define :application do |a|
-  a.association :authority
-  a.council_reference "001"
-  a.date_scraped {|b| 10.minutes.ago}
-  a.address "A test address"
-  a.description "pretty"
-  a.info_url "http://foo.com"
-end
+  factory :application do
+    association :authority
+    council_reference "001"
+    date_scraped {|b| 10.minutes.ago}
+    address "A test address"
+    description "pretty"
+    info_url "http://foo.com"
+  end
 
-Factory.define :comment do |c|
-  c.email "matthew@openaustralia.org"
-  c.name "Matthew Landauer"
-  c.text "a comment"
-  c.address "12 Foo Street"
-  c.association :application
-end
+  factory :comment do
+    email "matthew@openaustralia.org"
+    name "Matthew Landauer"
+    text "a comment"
+    address "12 Foo Street"
+    association :application
+  end
 
-Factory.define :user do |u|
-  u.email "foo@bar.com"
-  u.password "foofoo"
-end
+  factory :user do
+    email "foo@bar.com"
+    password "foofoo"
+  end
 
-Factory.define :alert do |a|
+  factory :alert do
+  end
 end
