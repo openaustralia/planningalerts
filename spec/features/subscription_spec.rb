6e0f1526ef56739cfaca13fe763bd94f273f91f6
Henare Degan <henare.degan@gmail.com>
2015-08-06 15:23:47 +1000

    Add test for trial subscription to integration test #693

--- spec/features/subscription_spec.rb
+++ spec/features/subscription_spec.rb
@@ -11,7 +11,7 @@ feature "Subscribing for access to several alerts" do
     create(:alert, email: email, confirmed: true, address: "456 Marrickville Rd, Marrickville")
   end
 
-  scenario "Signing up for a 3rd alert" do
+  scenario "Signing up for a 3rd alert and subscribing" do
     VCR.use_cassette("planningalerts") do
       visit "/alerts/signup"
 
@@ -28,6 +28,7 @@ feature "Subscribing for access to several alerts" do
     click_first_link_in_email
 
     expect(page).to have_content("You now have several email alerts")
+    expect(Subscription.find_by!(email: email).trial_days_remaining).to eql 14
     # We're assuming the user has completed the Stripe form here
     click_button("Subscribe now $49/month")
 
