308c5bfae8535862b95576c231a9b67bdb5d7882
Henare Degan <henare.degan@gmail.com>
2015-08-13 12:29:33 +1000

    Test remaining subscribe flow, edit form so it works with test

--- spec/features/subscription_spec.rb
+++ spec/features/subscription_spec.rb
@@ -68,8 +68,13 @@ feature "Subscribing for access to several alerts" do
       visit_in_email("Subscribe now")
 
       expect(page).to have_content("Subscribe now")
-      # they subscribe
-      # their subscription is confirmed
+      # Fake what the Stripe JS does (i.e. inject the token in the form if successful)
+      # FIXME: This isn't having an effect because we're just setting the plan amout to 0. See comment above.
+      first("input[name='stripeToken']", visible: false).set(stripe_helper.generate_card_token)
+      click_button("Subscribe now $49/month")
+
+      expect(page).to have_content("Your subscription for mary@enterpriserealty.com has been confirmed")
+      expect(Subscription.find_by!(email: email)).to be_paid
     end
   end
 end
