f0674c1cede36c5646c25719273d2f3eac709688
Henare Degan <henare.degan@gmail.com>
2015-08-14 15:27:58 +1000

    Add @emikulic to the list of contributors - thanks!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -82,7 +82,8 @@ module ApplicationHelper
       { name: "Dylan Fogarty-MacDonald", email_md5: "8bb01991a13de24d9620ad71d046301a" },
       { name: "Luke Bacon", email_md5: "0a705a02c7de5b135d90f7c08a343fa9" },
       { name: "Peter Serwylo", email_md5: "c4b310e4388f2d02bc857d0815513f65" },
-      { name: "Chris Nilsson", email_md5: "61aad7aa6e8cff40e22f420015d5aee9" }
+      { name: "Chris Nilsson", email_md5: "61aad7aa6e8cff40e22f420015d5aee9" },
+      { name: "Emil Mikulic", email_md5: "dce73ce6fdc8f512ad557eb19e65480c" }
     ]
   end
 end
