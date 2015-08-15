f20779cea892e8ef58f037988f50cd824171742e
Luke Bacon <luke@equivalentideas.com>
2015-08-15 21:20:08 +1000

    Use contexts to group tests (refactor only)

--- spec/views/alert_notifier/alert_spec.rb
+++ spec/views/alert_notifier/alert_spec.rb
@@ -17,57 +17,66 @@ describe "alert_notifier/alert" do
     rendered.should have_content("Alterations & additions")
   end
 
-  it "should not show the trial banner if there's no subscription" do
-    assign(:alert, mock_model(Alert, address: "Foo Parade",
-      radius_meters: 2000, confirm_id: "1234", subscription: nil))
-    render
-    rendered.should_not have_content("trial subscription")
+  context "when the recipient is not a subscriber" do
+    before :each do
+      assign(:alert, mock_model(Alert, address: "Foo Parade",
+        radius_meters: 2000, confirm_id: "1234", subscription: nil))
+      render
+    end
+
+    it "should not show the trial banner" do
+      rendered.should_not have_content("trial subscription")
+    end
+
+    it "should not show a note saying they are a ‘paid subscriber’" do
+      rendered.should_not have_content("You’re a paid subscriber")
+    end
   end
 
-  it "should show the trial banner to trial subscribers" do
-    subscription = create(:subscription, trial_started_at: Date.today)
-    assign(:alert, mock_model(Alert, address: "Foo Parade",
-      radius_meters: 2000, confirm_id: "1234", subscription: subscription))
-    render
-    rendered.should have_content("trial subscription")
+  context "when the recipient has a trial subscription" do
+    before :each do
+      subscription = create(:subscription, trial_started_at: Date.today)
+      assign(:alert, mock_model(Alert, address: "Foo Parade",
+        radius_meters: 2000, confirm_id: "1234", subscription: subscription))
+      render
+    end
+
+    it "should show the trial banner" do
+      rendered.should have_content("trial subscription")
+    end
+
+    it "should show the number of days remaining in the trial" do
+      rendered.should have_content("7 days remaining")
+    end
+
+    it "should not show a note saying they are a ‘paid subscriber’" do
+      rendered.should_not have_content("You’re a paid subscriber")
+    end
   end
 
-  it "should show the number of days remaining in the trial subscription" do
-    subscription = create(:subscription, trial_started_at: Date.today)
-    assign(:alert, mock_model(Alert, address: "Foo Parade",
-      radius_meters: 2000, confirm_id: "1234", subscription: subscription))
-    render
-    rendered.should have_content("7 days remaining")
+  context "when the recipient has a trial subscription with only one day left" do
+    before :each do
+      subscription = create(:subscription, trial_started_at: 6.days.ago)
+      assign(:alert, mock_model(Alert, address: "Foo Parade",
+        radius_meters: 2000, confirm_id: "1234", subscription: subscription))
+      render
+    end
+
+    it "should not pluralise 'day'" do
+      rendered.should have_content("1 day remaining")
+    end
   end
 
-  it "should not pluralise 'day' when there's only one left" do
-    subscription = create(:subscription, trial_started_at: 6.days.ago)
-    assign(:alert, mock_model(Alert, address: "Foo Parade",
-      radius_meters: 2000, confirm_id: "1234", subscription: subscription))
-    render
-    rendered.should have_content("1 day remaining")
-  end
+  context "when the recipient is a paid subscriber" do
+    before :each do
+      subscription = create(:subscription, stripe_subscription_id: "a_stripe_id")
+      assign(:alert, mock_model(Alert, address: "Foo Parade",
+        radius_meters: 2000, confirm_id: "1234", subscription: subscription))
+      render
+    end
 
-  it "should not show a note to people without a subscription saying they are a ‘paid subscriber’" do
-    assign(:alert, mock_model(Alert, address: "Foo Parade",
-      radius_meters: 2000, confirm_id: "1234", subscription: nil))
-    render
-    rendered.should_not have_content("You’re a paid subscriber")
-  end
-
-  it "should not show a note to trial subscribers saying they are a ‘paid subscriber’" do
-    subscription = create(:subscription, trial_started_at: Date.today)
-    assign(:alert, mock_model(Alert, address: "Foo Parade",
-      radius_meters: 2000, confirm_id: "1234", subscription: subscription))
-    render
-    rendered.should_not have_content("You’re a paid subscriber")
-  end
-
-  it "should show a note to paid subscribers saying they are a ‘paid subscriber’" do
-    subscription = create(:subscription, stripe_subscription_id: "a_stripe_id")
-    assign(:alert, mock_model(Alert, address: "Foo Parade",
-      radius_meters: 2000, confirm_id: "1234", subscription: subscription))
-    render
-    rendered.should have_content("You’re a paid subscriber")
+    it "should show a note saying they are a ‘paid subscriber’" do
+      rendered.should have_content("You’re a paid subscriber")
+    end
   end
 end
