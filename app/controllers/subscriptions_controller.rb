dc0de69546a72a80e44dc3bab11f6574e1a1da63
Henare Degan <henare.degan@gmail.com>
2015-08-18 12:57:06 +1000

    Remove unlikely error checking on Subscriptions#create
    Stripe will error check most of this but in the unlikely event this
    breaks we should fail fast and hard so we find out about the problem.
    
    Closes #731.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -7,20 +7,16 @@ class SubscriptionsController < ApplicationController
     @email = params[:stripeEmail]
     subscription = Subscription.find_or_create_by!(email: @email)
 
-    if @email.blank?
-      redirect_to new_subscription_path, alert: 'Sorry, there&rsquo;s an error in the form. <strong><a href="mailto:contact@planningalerts.org.au?subject=Unable to subscribe">Please contact us</a></strong>.'.html_safe
-    else
-      customer = Stripe::Customer.create(
-        email: @email,
-        card: params[:stripeToken],
-        description: "PlanningAlerts subscriber"
-      )
-      stripe_subscription = customer.subscriptions.create(plan: "planningalerts")
-      subscription.update!(stripe_subscription_id: stripe_subscription.id,  stripe_customer_id: customer.id)
-    end
+    customer = Stripe::Customer.create(
+      email: @email,
+      card: params[:stripeToken],
+      description: "PlanningAlerts subscriber"
+    )
+    stripe_subscription = customer.subscriptions.create(plan: "planningalerts")
+    subscription.update!(stripe_subscription_id: stripe_subscription.id,  stripe_customer_id: customer.id)
 
-  # TODO: rescue and redirect to new on attempt to reload the create page
-  # which tries to reuse the token again and errors.
+    # TODO: rescue and redirect to new on attempt to reload the create page
+    # which tries to reuse the token again and errors.
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
