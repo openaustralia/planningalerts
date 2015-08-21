b6422eba69463bef7608aaa43b7bd30252234726
Henare Degan <henare.degan@gmail.com>
2015-08-21 16:38:42 +1000

    Move API logging flag out of configuration
    You change this so rarely I think it's best to have it committed if
    you do change it. It also hopefully means you remember to change it
    back after your migration.

--- app/models/api_statistic.rb
+++ app/models/api_statistic.rb
@@ -1,9 +1,15 @@
 class ApiStatistic < ActiveRecord::Base
   belongs_to :user
 
+  # If you're deploying big database changes you can flip this and commit
+  # so that API calls aren't blocked by your migration
+  LOGGING_ENABLED = true
+
   def self.log(request)
-    # Lookup the api key if there is one
-    user = User.find_by_api_key(request.query_parameters["key"]) unless request.query_parameters["key"].blank?
-    create!(ip_address: request.remote_ip, query: request.fullpath, user_agent: request.headers["User-Agent"], query_time: Time.now, user: user)
+    if LOGGING_ENABLED
+      # Lookup the api key if there is one
+      user = User.find_by_api_key(request.query_parameters["key"]) unless request.query_parameters["key"].blank?
+      create!(ip_address: request.remote_ip, query: request.fullpath, user_agent: request.headers["User-Agent"], query_time: Time.now, user: user)
+    end
   end
 end
--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -1,5 +1,3 @@
 # Stores application configuration settings
 module Configuration
-  # It can be useful to temporarily stops API calls being logged
-  DISABLE_API_LOGGING = false
 end
