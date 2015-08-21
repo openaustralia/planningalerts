5fec8a716f5f04f4f326b6b903fa07e9c1ed6e80
Henare Degan <henare.degan@gmail.com>
2015-08-21 12:51:50 +1000

    Move Honeybadger API key to .env

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -40,8 +40,6 @@ module Configuration
 
   THEME_NSW_GOOGLE_ANALYTICS_KEY = "UA-3107958-12"
 
-  HONEYBADGER_API_KEY = 'xxxxxxxx'
-
   # Stripe is used to process cards and customers
   # for subscriptions. See app/controllers/subscriptions_controller.rb
   STRIPE_PUBLISHABLE_KEY = 'xxxxxxxxxxxxxxxxx'
--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,3 +1,3 @@
 Honeybadger.configure do |config|
-  config.api_key = Configuration::HONEYBADGER_API_KEY
+  config.api_key = ENV["HONEYBADGER_API_KEY"]
 end
