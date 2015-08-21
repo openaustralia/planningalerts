53fa415c3e9f4c72048064a965ddf69f71d3bfa9
Henare Degan <henare.degan@gmail.com>
2015-08-21 16:31:21 +1000

    Move secret key configuration to .env

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -1,9 +1,5 @@
 # Stores application configuration settings
 module Configuration
-  DEVISE_SECRET_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-
-  SECRET_KEY_BASE = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-
   # It can be useful to temporarily stops API calls being logged
   DISABLE_API_LOGGING = false
 end
