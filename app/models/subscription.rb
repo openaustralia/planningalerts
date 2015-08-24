640988b04ec8bbcda921f243a01c0230383bfd2b
Henare Degan <henare.degan@gmail.com>
2015-08-24 15:30:27 +1000

    Add validation to ensure correct Stripe plan IDs are used

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,7 @@
 class Subscription < ActiveRecord::Base
   has_many :alerts, foreign_key: :email, primary_key: :email
   validates :email, uniqueness: true, presence: true
+  validates :stripe_plan_id, inclusion: %w(planningalerts-15 planningalerts-34)
 
   FEATURE_ENABLED = !Rails.env.production?
 
