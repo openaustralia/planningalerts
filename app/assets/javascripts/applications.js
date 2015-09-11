f589f83b5780ff67c5dea19c5aeb815e4b26cdfd
Luke Bacon <luke@equivalentideas.com>
2015-09-11 16:02:04 +1000

    Track comment confirmation
    This will give us more information about what people do before
    and after they comment on an application.
    
    This sends a Google Analytics tracking event when the comment
    confirmation notice is displayed.

--- app/assets/javascripts/applications.js
+++ app/assets/javascripts/applications.js
@@ -3,3 +3,13 @@ $('#comment_address_input a').click(function(e) {
   e.preventDefault();
   $('#faq_commenting_address').slideToggle('fast');
 });
+
+// GA Tracking of comment process
+$( document ).ready(function() {
+  // check if the Google Analytics function is available
+  if (typeof ga == 'function') {
+    if ($('.notice-comment-confirmed').length) {
+      ga('send', 'event', 'comments', 'comment confirm message displayed');
+    }
+  }
+});
