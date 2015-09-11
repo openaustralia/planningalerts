be39ee3eb561a8b065b89da2abe1f8412a96d08d
Luke Bacon <luke@equivalentideas.com>
2015-09-11 11:05:01 +1000

    Make code more readable by avoiding parallel assignment
    See https://github.com/bbatsov/ruby-style-guide#parallel-assignment

--- app/mailers/alert_notifier.rb
+++ app/mailers/alert_notifier.rb
@@ -3,7 +3,11 @@ class AlertNotifier < ActionMailer::Base
   helper :application, :applications
 
   def alert(theme, alert, applications, comments = [])
-    @alert, @applications, @comments, @theme = alert, applications, comments, theme
+    @theme = theme
+    @alert = alert
+    @applications = applications
+    @comments = comments
+
     themed_mail(theme: theme, from: email_from(theme), to: alert.email,
       subject: render_to_string(partial: "subject",
         locals: {applications: applications, comments: comments, alert: alert}).strip,
