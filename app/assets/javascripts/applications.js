d1d81a8bff88446c97215d9aa78f99fac26edade
Luke Bacon <luke@equivalentideas.com>
2015-09-14 11:32:16 +1000

    Track clicks to submit a new comment
    Add js function to send a Google Analytics tracking event when
    someone clicks on the comment form submit button.

--- app/assets/javascripts/applications.js
+++ app/assets/javascripts/applications.js
@@ -8,6 +8,10 @@ $('#comment_address_input a').click(function(e) {
 $( document ).ready(function() {
   // check if the Google Analytics function is available
   if (typeof ga == 'function') {
+    $('#comment_submit_action input[type="submit"]').click(function(e) {
+      ga('send', 'event', 'comments', 'click submit new comment');
+    });
+
     if ($('.notice-comment-confirmed').length) {
       ga('send', 'event', 'comments', 'comment confirm message displayed');
     }
