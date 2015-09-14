7218bb5dbc2ce2944b1180211a5d22097ac37e0d
Luke Bacon <luke@equivalentideas.com>
2015-09-14 14:33:12 +1000

    Add tracking on link to comment form
    This will help us see if this link is actually helpful. Do people
    use this link to navigate, then make a comment?

--- app/assets/javascripts/applications.js
+++ app/assets/javascripts/applications.js
@@ -8,6 +8,10 @@ $('#comment_address_input a').click(function(e) {
 $( document ).ready(function() {
   // check if the Google Analytics function is available
   if (typeof ga == 'function') {
+    $('.link-to-comment-form').click(function(e) {
+      ga('send', 'event', 'comments', 'click link to go to comment form');
+    });
+
     $('#comment_submit_action input[type="submit"]').click(function(e) {
       ga('send', 'event', 'comments', 'click submit new comment');
     });
