e568b08ba776ba065cc5e93e766fa1c13f2d7c2a
Luke Bacon <luke@equivalentideas.com>
2015-09-16 17:21:41 +1000

    Add pseudo code for sending a message to a councilor test

---
+++ spec/features/write_to_councillors_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+feature "Send a message to a councilor" do
+  scenario "Sending a message" do
+    # visit an application page
+    # there should be a short text introduction to writing to your councillors
+    # there should be an expanation that this wont necessarily impact the decision about this application,
+    #   encourage people to use the official process for that.
+    # there should be a list of councilors to select from
+    # select the councillor you would like to write to
+    # fill out your name
+    # write your message
+    # post your message
+    # the message appears on the page
+    # the message is sent off to the councillor
+  end
+end
