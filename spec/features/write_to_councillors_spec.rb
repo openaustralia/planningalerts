9eea4adfe4eef8091c9e914e739903614d4a17a6
Luke Bacon <luke@equivalentideas.com>
2015-09-17 14:41:36 +1000

    Add test background to visit application page as admin or not

--- spec/features/write_to_councillors_spec.rb
+++ spec/features/write_to_councillors_spec.rb
@@ -7,16 +7,40 @@ feature "Send a message to a councilor" do
   # and find out where they stand on this development I care about
 
   context "when not logged in" do
+    background do
+      authority = create(:authority, full_name: "Foo")
+      VCR.use_cassette('planningalerts') do
+        application = create(:application, id: "1", authority_id: authority.id, comment_url: 'mailto:foo@bar.com')
+        visit application_path(application)
+      end
+    end
+
     scenario "can’t see councilor messages sections" do
-      # visit an application page
       # there should not be an introduction to writing to your councillors
       # and you should not be able to write and submit a message.
     end
   end
 
   context "when logged in as admin" do
+    background do
+      authority = create(:authority, full_name: "Foo")
+      admin = create(:admin)
+
+      visit new_user_session_path
+      within("#new_user") do
+        fill_in "Email", with: admin.email
+        fill_in "Password", with: admin.password
+      end
+      click_button "Sign in"
+      expect(page).to have_content "Signed in successfully"
+
+      VCR.use_cassette('planningalerts') do
+        application = create(:application, id: "1", authority_id: authority.id, comment_url: 'mailto:foo@bar.com')
+        visit application_path(application)
+      end
+    end
+
     scenario "sending a message" do
-      # visit an application page
       # there should be a short text introduction to writing to your councillors
       # there should be an expanation that this wont necessarily impact the decision about this application,
       #   encourage people to use the official process for that.
