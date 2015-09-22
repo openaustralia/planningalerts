3063740a92f8cb4a2fba4ff7ca04366d4865d975
Luke Bacon <luke@equivalentideas.com>
2015-09-23 07:24:53 +1000

    Make method modular by moving to scope and counting separately (refactor)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -15,12 +15,13 @@ class Comment < ActiveRecord::Base
     .select {|c| where("email = ? AND created_at < ?", c.email, c.created_at.to_date).empty? }
   }
 
+  scope :by_returning_commenters_for_date, ->(date) {
+    visible_with_unique_emails_for_date(date)
+    .select {|c| where("email = ? AND created_at < ?", c.email, c.created_at.to_date).any? }
+  }
+
   # Send the comment to the planning authority
   def after_confirm
     CommentNotifier.delay.notify("default", self)
-  end
-
-  def self.count_of_returning_commenters_for_date(date)
-    visible_with_unique_emails_for_date(date).count - by_first_time_commenters_for_date(date).count
   end
 end
