719531cff9d1c669c0dbba3f1f8234c64075021c
Luke Bacon <luke@equivalentideas.com>
2015-09-23 07:35:56 +1000

    Make method spacing consistent (whitespace only)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,10 +6,12 @@ class Comment < ActiveRecord::Base
   acts_as_email_confirmable
   scope :visible, -> { where(confirmed: true, hidden: false) }
   scope :in_past_week, -> { where("created_at > ?", 7.days.ago) }
+
   scope :visible_with_unique_emails_for_date, ->(date) {
     visible.where("date(created_at) = ?", date).to_a
     .uniq {|c| c.email}
   }
+
   scope :by_first_time_commenters_for_date, ->(date) {
     visible_with_unique_emails_for_date(date)
     .select {|c| where("email = ? AND created_at < ?", c.email, c.created_at.to_date).empty? }
