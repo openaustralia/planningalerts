082ab4b9abe06e6cad81a9a3d58e76aa3c18ca10
Luke Bacon <luke@equivalentideas.com>
2015-09-25 12:39:28 +1000

    Move metrics date span into controller, simplifies view

--- app/controllers/performance_controller.rb
+++ app/controllers/performance_controller.rb
@@ -1,5 +1,6 @@
 class PerformanceController < ApplicationController
   def index
     @comments = Comment.all if Comment.any?
+    @comments_metrics_timespan = (3.months.ago.to_date..Date.today).to_a.reverse
   end
 end
