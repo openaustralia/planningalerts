5d149751eed7ee671117639cab015ccafb84cfa9
Luke Bacon <luke@equivalentideas.com>
2015-10-15 18:07:01 +1100

    Extract messy Ruby code in view into helper (refactor only)

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -6,4 +6,8 @@ module CommentsHelper
   def donation_declaration_notice
     "If you have made a donation to a Councillor and/or gift to a Councillor or Council employee you may #{link_to("need to disclose this", faq_path(:anchor => "disclosure"))}.".html_safe
   end
+
+  def address_input_explanation
+    "We never display your street address. #{link_to("Why do you need my address?", faq_path(anchor: "address"))}".html_safe
+  end
 end
--- spec/helpers/comments_helper_spec.rb
+++ spec/helpers/comments_helper_spec.rb
@@ -5,4 +5,9 @@ describe CommentsHelper do
     it { expect(helper.donation_declaration_notice).to eq "If you have made a donation to a Councillor and/or gift to a Councillor or Council employee you may #{link_to("need to disclose this", faq_path(:anchor => "disclosure"))}." }
     it { expect(helper.donation_declaration_notice).to be_html_safe }
   end
+
+  describe "#address_input_explanation" do
+    it { expect(helper.address_input_explanation).to eq "We never display your street address. #{link_to("Why do you need my address?", faq_path(anchor: "address"))}" }
+    it { expect(helper.address_input_explanation).to be_html_safe }
+  end
 end
