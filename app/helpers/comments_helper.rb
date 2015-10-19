46b44c8d8b2fc3d38bf10967457254b6c472101b
Luke Bacon <luke@equivalentideas.com>
2015-10-19 11:00:23 +1100

    Inline simple helper methods and clean up unused tests/methods

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,12 +2,4 @@ module CommentsHelper
   def comment_as_html(text)
     Sanitize.clean(simple_format(text), Sanitize::Config::BASIC).html_safe
   end
-
-  def donation_declaration_notice
-    "Have you made a donation or gift to a Councillor or Council employee? #{link_to("You may need to disclose this", faq_path(:anchor => "disclosure"))}.".html_safe
-  end
-
-  def address_input_explanation
-    "We never display your street address. #{link_to("Why do you need my address?", faq_path(anchor: "address"))}".html_safe
-  end
 end
--- spec/helpers/comments_helper_spec.rb
+++ spec/helpers/comments_helper_spec.rb
@@ -1,13 +1,4 @@
 require 'spec_helper'
 
 describe CommentsHelper do
-  describe "#donation_declaration_notice" do
-    it { expect(helper.donation_declaration_notice).to eq "Have you made a donation or gift to a Councillor or Council employee? #{link_to("You may need to disclose this", faq_path(:anchor => "disclosure"))}." }
-    it { expect(helper.donation_declaration_notice).to be_html_safe }
-  end
-
-  describe "#address_input_explanation" do
-    it { expect(helper.address_input_explanation).to eq "We never display your street address. #{link_to("Why do you need my address?", faq_path(anchor: "address"))}" }
-    it { expect(helper.address_input_explanation).to be_html_safe }
-  end
 end
