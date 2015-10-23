117f0acd061ebaa3ea4c5e2fd29fc04a21e80c5d
Henare Degan <henare.degan@gmail.com>
2015-10-23 16:55:54 +1100

    Add a prefix to the subject if you've got an expired subscription

--- spec/views/alert_notifier/_subject.text.erb_spec.rb
+++ spec/views/alert_notifier/_subject.text.erb_spec.rb
@@ -23,4 +23,27 @@ describe "alert_notifier/_subject.text.erb" do
     subject { render "alert_notifier/subject", alert: alert, applications: [application], comments: [comment] }
     it { should eql "1 new comment and 1 new planning application near 123 Sample St\n" }
   end
+
+  context "with an expired subscription" do
+    let(:alert) do
+      subscription = create(:subscription, email: "foo@example.org", trial_started_at: 7.days.ago)
+      2.times { create(:alert, email: "foo@example.org", confirmed: true) }
+      create(:alert, email: "foo@example.org", address: "123 Sample St", confirmed: true, subscription: subscription)
+    end
+
+    context "with an application" do
+      subject { render "alert_notifier/subject", alert: alert, applications: [application], comments: [] }
+      it { should eql "You're missing out on: 1 new planning application near 123 Sample St\n" }
+    end
+
+    context "with a comment" do
+      subject { render "alert_notifier/subject", alert: alert, applications: [], comments: [comment] }
+      it { should eql "You're missing out on: 1 new comment on planning applications near 123 Sample St\n" }
+    end
+
+    context "with an application and a comment" do
+      subject { render "alert_notifier/subject", alert: alert, applications: [application], comments: [comment] }
+      it { should eql "You're missing out on: 1 new comment and 1 new planning application near 123 Sample St\n" }
+    end
+  end
 end
