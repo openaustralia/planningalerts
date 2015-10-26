da97139eb4a6d6e99d07836d8976a882fed24876
Luke Bacon <luke@equivalentideas.com>
2015-10-27 06:38:09 +1100

    Use factory so we only have to create data we need for test (refactor only)

--- spec/views/comments/comment_spec.rb
+++ spec/views/comments/comment_spec.rb
@@ -12,8 +12,9 @@ describe "comments/_comment" do
   end
 
   it "should add rel='no-follow' to links in the comment text" do
-    comment = mock_model(Comment, name: "Matthew", updated_at: Time.now, application: application,
-      text: 'This is a link to <a href="http://openaustralia.org">openaustralia.org</a>')
+    comment = create(:comment,
+                     text: 'This is a link to <a href="http://openaustralia.org">openaustralia.org</a>',
+                     application: application)
     expected_html = "<blockquote class='comment-text'><p>This is a link to <a href=\"http://openaustralia.org\" rel=\"nofollow\">openaustralia.org</a></p></blockquote>"
 
     render comment, in_list: false
@@ -22,8 +23,9 @@ describe "comments/_comment" do
   end
 
   it "should format simple text in separate paragraphs with p tags" do
-    comment = mock_model(Comment, name: "Matthew", updated_at: Time.now, application: application,
-      text: "This is the first paragraph\nAnd the next line\n\nThis is a new paragraph")
+    comment = create(:comment,
+                     text: "This is the first paragraph\nAnd the next line\n\nThis is a new paragraph",
+                     application: application)
     expected_html = "<blockquote class='comment-text'><p>This is the first paragraph
 <br>And the next line</p>
 
@@ -35,8 +37,9 @@ describe "comments/_comment" do
   end
 
   it "should get rid of nasty javascript and strip out images" do
-    comment = mock_model(Comment, name: "Matthew", updated_at: Time.now, application: application,
-      text: "<a href=\"javascript:document.location='http://www.google.com/'\">A nasty link</a><img src=\"http://foo.co\">")
+    comment = create(:comment,
+                     text: "<a href=\"javascript:document.location='http://www.google.com/'\">A nasty link</a><img src=\"http://foo.co\">",
+                     application: application)
     expected_html = "<blockquote class='comment-text'><p><a rel=\"nofollow\">A nasty link</a></p></blockquote>"
 
     render comment, in_list: false
