0086cb01bf2e6ae5db5c248ce5b9785ef0e37951
Luke Bacon <luke@equivalentideas.com>
2015-10-27 06:30:31 +1100

    Use factory to create all needed data for test
    This refactors ahead of a change to the comment partial, to ask
    for the authority of the application. Our factory creates all the
    data we need.
    
    This also uses 'let' to reduce repetition in this spec file.

--- spec/views/comments/comment_spec.rb
+++ spec/views/comments/comment_spec.rb
@@ -5,12 +5,13 @@ describe "comments/_comment" do
     Timecop.freeze(Time.local(2015, 1, 26, 10, 5, 0))
   end
 
+  let(:application) { VCR.use_cassette('planningalerts') { create(:application) } }
+
   after do
     Timecop.return
   end
 
   it "should add rel='no-follow' to links in the comment text" do
-    application = mock_model(Application)
     comment = mock_model(Comment, name: "Matthew", updated_at: Time.now, application: application,
       text: 'This is a link to <a href="http://openaustralia.org">openaustralia.org</a>')
     expected_html = "<blockquote class='comment-text'><p>This is a link to <a href=\"http://openaustralia.org\" rel=\"nofollow\">openaustralia.org</a></p></blockquote>"
@@ -21,7 +22,6 @@ describe "comments/_comment" do
   end
 
   it "should format simple text in separate paragraphs with p tags" do
-    application = mock_model(Application)
     comment = mock_model(Comment, name: "Matthew", updated_at: Time.now, application: application,
       text: "This is the first paragraph\nAnd the next line\n\nThis is a new paragraph")
     expected_html = "<blockquote class='comment-text'><p>This is the first paragraph
@@ -35,7 +35,6 @@ describe "comments/_comment" do
   end
 
   it "should get rid of nasty javascript and strip out images" do
-    application = mock_model(Application)
     comment = mock_model(Comment, name: "Matthew", updated_at: Time.now, application: application,
       text: "<a href=\"javascript:document.location='http://www.google.com/'\">A nasty link</a><img src=\"http://foo.co\">")
     expected_html = "<blockquote class='comment-text'><p><a rel=\"nofollow\">A nasty link</a></p></blockquote>"
