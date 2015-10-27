6f5abb4c3601d2155456ebd64f4b6f3bd3983d09
Henare Degan <henare.degan@gmail.com>
2015-10-27 12:09:23 +1100

    Make indenting tidy only

--- spec/helpers/alert_notifier_helper_spec.rb
+++ spec/helpers/alert_notifier_helper_spec.rb
@@ -152,8 +152,9 @@ describe AlertNotifierHelper do
     let(:alert) { create(:alert, address: "123 Sample St") }
     let(:application) do
       mock_model(Application, address: "Bar Street",
-      description: "Alterations & additions", council_reference: "007",
-      location: double("Location", lat: 1.0, lng: 2.0))
+                              description: "Alterations & additions",
+                              council_reference: "007",
+                              location: double("Location", lat: 1.0, lng: 2.0))
     end
     let(:comment) { create(:comment, application: application) }
 
