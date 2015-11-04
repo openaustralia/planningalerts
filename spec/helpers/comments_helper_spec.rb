a029b5e618166aa2dbe5075290d02233e9836a5b
Luke Bacon <luke@equivalentideas.com>
2015-11-04 11:58:45 +1100

    Test a string, rather than generating the expected output
    This test could pass if 'comment.id' wasn’t returning what we want.
    
    This changes the test to be more concrete and generates the data that
    we need for it.
    
    Thanks @henare

--- spec/helpers/comments_helper_spec.rb
+++ spec/helpers/comments_helper_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 describe CommentsHelper do
   describe "#comment_path" do
-    let(:comment) { VCR.use_cassette('planningalerts') { create(:confirmed_comment) } }
+    let(:application) { VCR.use_cassette('planningalerts') { create(:application, id: 1) } }
+    let(:comment) { create(:confirmed_comment, id: 1, application: application) }
 
     it "returns the path for the application with an anchor for the comment" do
-      expect(helper.comment_path(comment)).to eq application_path(comment.application, anchor: "comment#{comment.id}")
+      expect(helper.comment_path(comment)).to eq "/applications/1#comment1"
     end
   end
 end
