44af80765190c4739454d0eff679e4fc6329ca68
Luke Bacon <luke@equivalentideas.com>
2015-11-04 12:01:43 +1100

    Improve test wording (refactor only)

--- spec/helpers/comments_helper_spec.rb
+++ spec/helpers/comments_helper_spec.rb
@@ -5,7 +5,7 @@ describe CommentsHelper do
     let(:application) { VCR.use_cassette('planningalerts') { create(:application, id: 1) } }
     let(:comment) { create(:confirmed_comment, id: 1, application: application) }
 
-    it "returns the path for the application with an anchor for the comment" do
+    it "returns the path for the application with an anchor with the comment id" do
       expect(helper.comment_path(comment)).to eq "/applications/1#comment1"
     end
   end
