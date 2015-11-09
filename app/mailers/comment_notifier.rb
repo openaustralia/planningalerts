c31671fd4315504742922ea4bbc2ba6c555bfc5e
Henare Degan <henare.degan@gmail.com>
2015-11-09 12:23:54 +1100

    Have replies to councillor messages go to a special address
    Admins will check this address and manually upload any replies they
    find. This Wizard of Oz experiment will allow us to learn how the
    feature us used until we have time to set up WriteIt, etc.
    
    See #812 for more details.

--- app/mailers/comment_notifier.rb
+++ app/mailers/comment_notifier.rb
@@ -15,12 +15,11 @@ class CommentNotifier < ActionMailer::Base
 
   def notify_councillor(theme, comment)
     @comment = comment
+    from_address = ENV["EMAIL_COUNCILLOR_REPLIES_TO"]
 
-    themed_mail(theme: theme, from: "#{comment.name} <#{comment.email}>",
+    themed_mail(theme: theme, from: "#{comment.name} <#{from_address}>",
       sender: email_from(theme),
-      # Setting reply-to to ensure that we don't get the replies for email clients that are not
-      # respecting the from, sender headers that we've set.
-      reply_to: "#{comment.name} <#{comment.email}>",
+      reply_to: "#{comment.name} <#{from_address}>",
       to: comment.councillor.email, subject: "Planning application at #{comment.application.address}")
   end
 end
