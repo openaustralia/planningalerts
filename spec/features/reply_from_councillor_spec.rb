8f40fd5e0e09148b1fb1d3cf441d497fd8a51e62
Henare Degan <henare.degan@gmail.com>
2015-11-09 14:29:06 +1100

    Add the rest of the integration test

--- spec/features/reply_from_councillor_spec.rb
+++ spec/features/reply_from_councillor_spec.rb
@@ -2,15 +2,17 @@ require "spec_helper"
 
 feature "Councillor replies to a message sent to them" do
   given (:councillor) { create(:councillor, name: "Louise Councillor", email: "louise@council.nsw.gov.au") }
+  given (:application) { create(:application, id: 8, address: "24 Bruce Road Glenbrook", description: "A lovely house") }
   given (:comment) do
     VCR.use_cassette('planningalerts') do
-      application = create(:application, id: 8, address: "24 Bruce Road Glenbrook", description: "A lovely house")
-      create(:comment, application: application,
-                                 name: "Matthew Landauer",
-                                 councillor: councillor,
-                                 text: "I think this is a really good idea")
+      create(:comment, id: 5,
+                       application: application,
+                       name: "Matthew Landauer",
+                       councillor: councillor,
+                       text: "I think this is a really good idea")
     end
   end
+  given (:admin) { create(:admin) }
 
   background { comment.confirm! }
 
@@ -21,8 +23,21 @@ feature "Councillor replies to a message sent to them" do
     # It's sent to replies@planningalerts.org.au
     # An admin checks this mailbox
 
-    # Action: An admin enters the reply in the admin backend
+    ## The admin enters the reply into the admin backend
+    # Sign in as an admin
+    visit new_user_session_path
+    fill_in "Email", with: admin.email
+    fill_in "Password", with: admin.password
+    click_button "Sign in"
+    # Find the original comment from the reply and visit it
+    visit application_path application
+    within("#comment5") { click_link "add reply" }
+    # Enter the reply
+    select "Louise Councillor", from: "Councillor"
+    fill_in "Text", with: "I'm glad you think it's a good idea. I do too."
+    click_button "Create Reply"
 
-    # Result: It's shown on the application page
+    visit application_path application
+    expect(page).to have_content "I'm glad you think it's a good idea. I do too."
   end
 end
