20e5afd4c937fa34241bc93ddd33648967733f29
Henare Degan <henare.degan@gmail.com>
2015-11-10 10:54:20 +1100

    Show comment association as number field on admin reply form
    This is so the field isn't massive on production. It's prepopulated
    when admins use the "add reply" link anyway.
    
    Fixes #851

--- app/admin/replies.rb
+++ app/admin/replies.rb
@@ -3,5 +3,15 @@ ActiveAdmin.register Reply do
 
   remove_filter :comment
 
+  form do |f|
+    inputs do
+      input :comment_id, as: :number, label: "Comment ID"
+      input :councillor
+      input :text
+      input :received_at
+    end
+    actions
+  end
+
   permit_params :text, :received_at, :comment_id, :councillor_id
 end
