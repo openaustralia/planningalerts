748bda090ff0a26834cb530ac6fa5ff57ef1c2ba
Henare Degan <henare.degan@gmail.com>
2015-12-10 15:18:03 +1100

    Add tests for Councillor validation

--- spec/models/councillor_spec.rb
+++ spec/models/councillor_spec.rb
@@ -4,4 +4,9 @@ describe Councillor do
   describe "#prefixed_name" do
     it { expect(create(:councillor, name: "Steve").prefixed_name).to eq "local councillor Steve" }
   end
+
+  describe "validations" do
+    it { expect(Councillor.new).to_not be_valid }
+    it { expect(Councillor.new(authority: create(:authority))).to be_valid }
+  end
 end
