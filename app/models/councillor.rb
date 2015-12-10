9711ac441c4fb8ffd7c28d4997b6bb2f4bae4e93
Henare Degan <henare.degan@gmail.com>
2015-12-10 15:18:20 +1100

    Ensure the image URL is HTTPS. Closes #854

--- app/models/councillor.rb
+++ app/models/councillor.rb
@@ -4,6 +4,7 @@ class Councillor < ActiveRecord::Base
   belongs_to :authority
 
   validates :authority, presence: true
+  validates :image_url, format: { with: /\Ahttps/, message: "must be HTTPS" }, allow_blank: true
 
   def prefixed_name
     "local councillor #{name}"
--- spec/models/councillor_spec.rb
+++ spec/models/councillor_spec.rb
@@ -8,5 +8,7 @@ describe Councillor do
   describe "validations" do
     it { expect(Councillor.new).to_not be_valid }
     it { expect(Councillor.new(authority: create(:authority))).to be_valid }
+    it { expect(Councillor.new(authority: create(:authority), image_url: "https://foobar.com")).to be_valid }
+    it { expect(Councillor.new(authority: create(:authority), image_url: "http://foobar.com")).to_not be_valid }
   end
 end
