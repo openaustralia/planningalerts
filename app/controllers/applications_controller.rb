2164f4c8884a608ba1d424cd8e5fd75e28904272
Luke Bacon <luke@equivalentideas.com>
2015-12-15 12:59:47 +1100

    Remove prototype councillor messages
    We've got the real thing working so don't need these examples any more.

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -111,22 +111,6 @@ class ApplicationsController < ApplicationController
       @councillors = @application.councillors_for_authority
     end
 
-    if params[:with_councillor_message] == "true" || params[:with_councillor_reply] == "true"
-      @message_to_councillor = Comment.new(name: "Laurie Burdekin",
-                                           text: "Hi Melissa, this development needs to be rethought out to suit the area and the community. This land was purchased from the original home owners to be used for a bypass and should never have been rezoned to accommodate a development such as this. We need you to reject this application at the council meeting.",
-                                           updated_at: 5.hours.ago,
-                                           id: "99")
-
-      if params[:with_councillor_reply] == "true"
-        @reply_from_councillor = { councillor: { name: "Melissa Brooks", party: "The Greens" },
-                                   parent_comment_id: @message_to_councillor.id,
-                                   text: "Thanks for your message Laurie. I’ll be sure to let the council know about your views at the upcoming council meeting on 12th November. Best wishes, Melissa Brooks",
-                                   updated_at: 1.hours.ago,
-                                   id: "100",
-                                   authority_id: 1}
-      end
-    end
-
     respond_to do |format|
       format.html
     end
--- spec/features/view_message_to_councillor_spec.rb
+++ spec/features/view_message_to_councillor_spec.rb
@@ -1,27 +1,5 @@
 require 'spec_helper'
 feature "View a message sent to a councillor" do
-  # TODO: Remove this when we remove the prototype
-  context "when with_councillor_message param is not true" do
-    given(:application) { VCR.use_cassette('planningalerts') { create(:application, id: "1", comment_url: 'mailto:foo@bar.com') } }
-
-    scenario "can’t see messages to councillors" do
-      visit application_path(application)
-
-      expect(page).to_not have_content("wrote to local councillor")
-    end
-  end
-
-  # TODO: Remove this when we remove the prototype
-  context "when with_councillor_message param is true" do
-    given(:application) { VCR.use_cassette('planningalerts') { create(:application, id: "1", comment_url: 'mailto:foo@bar.com') } }
-
-    scenario "can see messages to councillors" do
-      visit application_path(application, with_councillor_message: "true")
-
-      expect(page).to have_content("wrote to local councillor")
-    end
-  end
-
   given(:comment) do
     VCR.use_cassette('planningalerts') do
       create(
