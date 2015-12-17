6f4cabaf60afcc3c957eda82dbe7cc4154667d11
Luke Bacon <luke@equivalentideas.com>
2015-12-17 13:26:39 +1100

    Add intergration test for hiding an existing comment
    Realised that admins updating comments wasn't covered by tests when
    working on validations of comments model.

---
+++ spec/features/admin_hides_comment_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+feature "Admin hides comment" do
+  background do
+    VCR.use_cassette('planningalerts') do
+      create(:confirmed_comment)
+    end
+
+    admin = create(:admin)
+
+    visit new_user_session_path
+    within("#new_user") do
+      fill_in "Email", with: admin.email
+      fill_in "Password", with: admin.password
+    end
+    click_button "Sign in"
+    expect(page).to have_content "Signed in successfully"
+  end
+
+  scenario "hides existing comment" do
+    visit admin_root_path
+    click_link "Comments"
+
+    within("#comment_1") do
+      click_link "Edit"
+    end
+
+    check "Hidden"
+    click_button "Update Comment"
+
+    expect(page).to have_content("Comment was successfully updated")
+    expect(page).to have_content("Hidden true")
+  end
+end
