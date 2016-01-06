195344f8304134a0e9b9c6119518d6c70f8feb22
Luke Bacon <luke@equivalentideas.com>
2016-01-06 14:50:44 +1100

    Extract method

--- app/controllers/performance_controller.rb
+++ app/controllers/performance_controller.rb
@@ -1,6 +1,12 @@
 class PerformanceController < ApplicationController
   def index
-    @daily_comment_metrics = (3.months.ago.to_date..Date.today).to_a.reverse.map do |day|
+    @daily_comment_metrics = comment_performance_data
+  end
+
+  private
+
+  def comment_performance_data
+    (3.months.ago.to_date..Date.today).to_a.reverse.map do |day|
       {
         date: day,
         first_time_commenters: Comment.by_first_time_commenters_for_date(day).count,
