2ebcbf8865e0d2ba41793762d2220bad83c04a2d
Luke Bacon <luke@equivalentideas.com>
2016-01-25 18:07:16 +1100

    Add test to check that reply text doesn't include any sneaky javascript
    Since we're rendering this stuff to html so that users get nice
    formatting, I thought it was a good idea to make sure we sanitize
    reply.text of any javascript that might slip in.
    
    Currently administrators are entering the replies manually, but that
    will hopefully not be the case soon.

--- spec/views/replies/reply_spec.rb
+++ spec/views/replies/reply_spec.rb
@@ -20,4 +20,23 @@ describe "replies/_reply" do
 <br />Louise Councillor</p>"
     )
   end
+
+  it "doesn’t include any nasty javascript" do
+    reply = VCR.use_cassette('planningalerts') do
+      create(
+        :reply,
+        text: "
+          <a href=\"javascript:document.location='http://www.google.com/'\">A nasty link</a>
+          <script type='text/javascript'>alert('danger danger');</script>
+        "
+      )
+    end
+
+    render reply
+
+    expect(rendered).to include("<a>A nasty link</a>")
+    expect(rendered).to_not include(
+      "<script type='text/javascript'>alert('danger danger');</script>"
+    )
+  end
 end
