openaustralia/planningalerts

commit e78b71d07fbf616bb2bff813332982b211eb1717

Set radius to float as expected by meters_in_words helper method.

app/controllers/applications_controller.rb
@@ -41,7 +41,7 @@ class ApplicationsController < ApplicationController
 
   def address
     @q = params[:q]
-    @radius = params[:radius] || 2000
+    @radius = params[:radius].to_f || 2000
     @sort = params[:sort] || 'time'
     per_page = 30
     @page = params[:page]
@@ -56,9 +56,9 @@ class ApplicationsController < ApplicationController
         @other_addresses = location.all[1..-1].map{|l| l.full_address}
         @applications = case @sort
                         when 'distance'
-                          Application.near([location.lat, location.lng], @radius.to_f / 1000, units: :km).reorder('distance').paginate(page: params[:page], per_page: per_page)
+                          Application.near([location.lat, location.lng], @radius / 1000, units: :km).reorder('distance').paginate(page: params[:page], per_page: per_page)
                         else # date_scraped
-                          Application.near([location.lat, location.lng], @radius.to_f / 1000, units: :km).paginate(page: params[:page], per_page: per_page)
+                          Application.near([location.lat, location.lng], @radius / 1000, units: :km).paginate(page: params[:page], per_page: per_page)
                         end
         @rss = applications_path(format: 'rss', address: @q, radius: @radius)
       end

commit 19356f0ffdbbfb81e5871367632bbc6ac8be1a9a

Add feature spec for searching for applications
After introducing the bad regression with e78b71d0, I realised that we
don't have integration tests for the basic use path of hitting the home
page, entering an address, and seeing some applications near by.

See the bug introduced by e78b71d0 in issue #907
https://github.com/openaustralia/planningalerts/issues/907)

This commit adds a basic spec for searching for applications near an
address. This will help us avoid adding this bug again in the future.

spec/features/search_for_applications_spec.rb
@@ -1,0 +1,29 @@
+require "spec_helper"
+
+feature "Searching for development application near an address" do
+  around do |scenario|
+    VCR.use_cassette('planningalerts') do
+      scenario.run
+    end
+  end
+
+  background do
+    create(:application,
+            address: "24 Bruce Road Glenbrook",
+            description: "A lovely house")
+  end
+
+  scenario "successfully" do
+    visit root_path
+
+    fill_in "Enter a street address", with: "24 Bruce Road, Glenbrook"
+    click_button "Search"
+
+    expect(page).to have_content "Applications within 2 km of 24 Bruce Road, Glenbrook NSW 2773"
+
+    within "ol.applications" do
+      expect(page).to have_content "24 Bruce Road"
+      expect(page).to have_content "A lovely house"
+    end
+  end
+end
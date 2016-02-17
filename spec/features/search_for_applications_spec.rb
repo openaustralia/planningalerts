19356f0ffdbbfb81e5871367632bbc6ac8be1a9a
Luke Bacon <luke@equivalentideas.com>
2016-02-17 11:33:58 +1100

    Add feature spec for searching for applications
    After introducing the bad regression with e78b71d0, I realised that we
    don't have integration tests for the basic use path of hitting the home
    page, entering an address, and seeing some applications near by.
    
    See the bug introduced by e78b71d0 in issue #907
    https://github.com/openaustralia/planningalerts/issues/907)
    
    This commit adds a basic spec for searching for applications near an
    address. This will help us avoid adding this bug again in the future.

---
+++ spec/features/search_for_applications_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+feature "Searching for development application near an address" do
+  around do |scenario|
+    VCR.use_cassette('planningalerts') do
+      scenario.run
+    end
+  end
+
+  background do
+    create(:application,
+            address: "24 Bruce Road Glenbrook",
+            description: "A lovely house")
+  end
+
+  scenario "successfully" do
+    visit root_path
+
+    fill_in "Enter a street address", with: "24 Bruce Road, Glenbrook"
+    click_button "Search"
+
+    expect(page).to have_content "Applications within 2 km of 24 Bruce Road, Glenbrook NSW 2773"
+
+    within "ol.applications" do
+      expect(page).to have_content "24 Bruce Road"
+      expect(page).to have_content "A lovely house"
+    end
+  end
+end
