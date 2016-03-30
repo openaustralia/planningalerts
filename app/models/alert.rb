3f3121609bfc865c143a4922f6c172ce192ab2ca
Luke Bacon <luke@equivalentideas.com>
2016-03-30 18:07:25 +1100

    Reformat statement for readability (refactor only)
    It's really hard to follow this long line at the moment.
    Format #applications_with_new_comments with methods chained on new
    lines, like #applications_with_new_replies.

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -113,7 +113,11 @@ class Alert < ActiveRecord::Base
 
   # Applications in the area of interest which have new comments made since we were last alerted
   def applications_with_new_comments
-    Application.near([location.lat, location.lng], radius_km, units: :km).joins(:comments).where('comments.updated_at > ?', cutoff_time).where('comments.confirmed' => true).where('comments.hidden' => false).uniq
+    Application.near([location.lat, location.lng], radius_km, units: :km)
+               .joins(:comments)
+               .where('comments.updated_at > ?', cutoff_time)
+               .where('comments.confirmed' => true)
+               .where('comments.hidden' => false).uniq
   end
 
   def applications_with_new_replies
