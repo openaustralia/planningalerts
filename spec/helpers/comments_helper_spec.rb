88dd0c68e3c0c497073a5d65f1873266d40e1acc
Luke Bacon <luke@equivalentideas.com>
2016-04-13 11:40:46 +1000

    Add basic test coverage for #comment_as_html helper

--- spec/helpers/comments_helper_spec.rb
+++ spec/helpers/comments_helper_spec.rb
@@ -1,6 +1,18 @@
 require 'spec_helper'
 
 describe CommentsHelper do
+  describe "#comment_as_html" do
+    it "wraps lines of text in HTML paragraphs" do
+      expect(helper.comment_as_html("This is a paragraph\n\nThis is another paragraph"))
+      .to eql "<p>This is a paragraph</p>\n\n<p>This is another paragraph</p>"
+    end
+
+    it "removes scary scripts" do
+      expect(helper.comment_as_html("watch out <script>alert('danger');</script>"))
+      .to eql "<p>watch out </p>"
+    end
+  end
+
   describe "#comment_path" do
     let(:application) { VCR.use_cassette('planningalerts') { create(:application, id: 1) } }
     let(:comment) { create(:confirmed_comment, id: 1, application: application) }
