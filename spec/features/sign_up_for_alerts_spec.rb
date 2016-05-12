ca708b6b5b8edfe4ab3521957f295781f063028c
Luke Bacon <luke@equivalentideas.com>
2016-05-12 14:59:26 +1000

    Add integration test for signing up via application page
    This is the way most people sign up so it's pretty darn important.

--- spec/features/sign_up_for_alerts_spec.rb
+++ spec/features/sign_up_for_alerts_spec.rb
@@ -26,6 +26,35 @@ feature "Sign up for alerts" do
     Alert.active.find_by(address: "24 Bruce Road, Glenbrook NSW 2773", radius_meters: "2000", email: current_email_address).should_not be_nil
   end
 
+  context "via an application page" do
+    around do |example|
+      VCR.use_cassette('planningalerts') do
+        example.run
+      end
+    end
+
+    given(:application) do
+      create(:application, address: "24 Bruce Rd, Glenbrook NSW 2773")
+    end
+
+    scenario "successfully" do
+      visit application_path(application)
+
+      within "#new_alert" do
+        fill_in("Enter your email address", with: "example@example.com")
+        click_button("Create Alert")
+      end
+
+      page.should have_content("Now check your email")
+
+      open_email("example@example.com")
+      current_email.should have_subject("Please confirm your planning alert")
+      click_first_link_in_email
+
+      page.should have_content("your alert has been activated")
+    end
+  end
+
   context "via the homepage" do
     around do |example|
       VCR.use_cassette('planningalerts') do
