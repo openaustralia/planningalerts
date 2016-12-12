7782dc6cec1cf85cd25c205dabe1b4fb3bdbf19e
Luke Bacon <luke@equivalentideas.com>
2016-12-13 10:10:11 +1100

    Make the back amount a round number
    While 3.50 is a nice amount to donate, a round number is simpler
    conceptually and easier to change. It also means we can use the much
    better input modes on mobiles for numbers.
    
    This is also much easier to implement with stripe and make flexible.
    
    Add a todo about adding minimum values and input restrictions to numbers
    only to make this as easy as possible.

--- spec/features/user_becomes_backer_spec.rb
+++ spec/features/user_becomes_backer_spec.rb
@@ -4,7 +4,7 @@ feature "User signs up to donate monthly" do
   it "successfully" do
     visit new_backer_path
 
-    click_button "Donate $3.50 each month"
+    click_button "Donate $4 each month"
 
     expect(page).to have_content "Thank you for backing PlanningAlerts"
   end
