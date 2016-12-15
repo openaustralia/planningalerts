778a4412a77922e90dd3f7fe5a314f25213c9367
Luke Bacon <luke@equivalentideas.com>
2016-12-15 17:27:18 +1100

    Make the tests more stable by mocking the stripe checkout submission
    We don't need to test that checkout.js does what it says, just that the
    inputs we control are there for users and submission leads to the
    correct processing. These tests currently make an actual call to stripe
    which is really slow and difficult to debug here.
    
    This changes the test to have the user put in there details, but we mock
    out the actual form submission using a token supplied by stripe-mock.
    
    This is faster and easier to manage.

--- spec/features/user_becomes_backer_spec.rb
+++ spec/features/user_becomes_backer_spec.rb
@@ -4,9 +4,7 @@ feature "Subscribing to donate monthly" do
   let(:stripe_helper) { StripeMock.create_test_helper }
   before do
     StripeMock.start
-    # When plan is set to 0 StripeMock doesn't check for the card number when creating the customer
-    # FIXME: StripeMock should create a customer when only a token is supplied
-    stripe_helper.create_plan(id: "planningalerts-backers-test-1", amount: 0)
+    stripe_helper.create_plan(id: "planningalerts-backers-test-1", amount: 1)
   end
 
   after { StripeMock.stop }
@@ -37,37 +35,41 @@ feature "Subscribing to donate monthly" do
 
       click_button "Donate $4 each month"
 
-      page.within_frame find("iframe.stripe_checkout_app") do
-        fill_in "Email", with: email
-        fill_in "Card number", with: "4242424242424242"
-        fill_in "MM / YY", with: "1222"
-        fill_in "CVC", with: "123"
-        click_button "Subscribe AUD $4.00/mo"
-      end
-      sleep(3) # let the form process
+      fill_out_and_submit_stripe_card_form_with_email(email)
 
       expect(page).to have_content "Thank you for backing PlanningAlerts"
       expect(Subscription.find_by!(email: email)).to be_paid
     end
+
+    it "successfully at a higher rate", js: true do
+      visit new_subscription_path
+
+      fill_in "Choose your contribution", with: 10
+      click_button "Donate $10 each month"
+
+      fill_out_and_submit_stripe_card_form_with_email(email)
+
+      expect(page).to have_content "Thank you for backing PlanningAlerts"
+      expect(Subscription.find_by!(email: email)).to be_paid
+
+      stripe_customer = Stripe::Customer.retrieve(Subscription.find_by!(email: email).stripe_customer_id)
+      subscription_quantity_on_stripe = stripe_customer.subscriptions.data.first.quantity
+
+      expect(subscription_quantity_on_stripe).to eq "10"
+    end
+  end
+end
+
+def fill_out_and_submit_stripe_card_form_with_email(email)
+  page.within_frame find("iframe.stripe_checkout_app") do
+    fill_in "Email", with: email
+    fill_in "Card number", with: "4242424242424242"
+    fill_in "MM / YY", with: "1222"
+    fill_in "CVC", with: "123"
   end
 
-  it "successfully at a higher rate" do
-    visit new_subscription_path(email: email)
-
-    fill_in "Choose your contribution", with: 10
-
-    # Fake what the Stripe JS does (i.e. inject the token in the form if successful)
-    # FIXME: This isn't having an effect because we're just setting the plan amout to 0. See comment above.
-    first("input[name='stripeToken']", visible: false).set(stripe_helper.generate_card_token)
-
-    click_button "Donate each month"
-
-    expect(page).to have_content "Thank you for backing PlanningAlerts"
-    expect(Subscription.find_by!(email: email)).to be_paid
-
-    stripe_customer = Stripe::Customer.retrieve(Subscription.find_by!(email: email).stripe_customer_id)
-    subscription_quantity_on_stripe = stripe_customer.subscriptions.data.first.quantity
-
-    expect(subscription_quantity_on_stripe).to eq "10"
-  end
+  token = StripeMock.create_test_helper.generate_card_token
+  page.execute_script("document.querySelector('input[name=\"stripeToken\"]').setAttribute(\"value\", \"#{token}\");")
+  page.execute_script("document.querySelector('input[name=\"stripeEmail\"]').setAttribute(\"value\", \"#{email}\");")
+  page.execute_script("$('#subscription-payment-form').submit();")
 end
