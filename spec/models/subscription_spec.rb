430bd19df30f7e4b40f091738cd3989b28d57ec6
Luke Bacon <luke@equivalentideas.com>
2016-12-19 17:57:21 +1100

    Extract shared test logic

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -10,76 +10,59 @@ describe Subscription do
     it { expect(Subscription.new(stripe_subscription_id: nil)).to_not be_paid }
   end
 
-  describe "#send_customer_to_stripe" do
+  context do
     let(:stripe_helper) { StripeMock.create_test_helper }
-    before { StripeMock.start }
-    after { StripeMock.stop }
-
-    it "creates a stripe customer with the subscriptions attributes" do
-      subscription = create(:subscription, email: "jenny@local.org")
-      valid_stripe_token = stripe_helper.generate_card_token
-
-      stripe_customer = subscription.create_stripe_customer(valid_stripe_token)
-
-      expect(stripe_customer.email).to eq "jenny@local.org"
-    end
-
-    it "returns a stripe customer object" do
-      subscription = create(:subscription, email: "jenny@local.org")
-      valid_stripe_token = stripe_helper.generate_card_token
-
-      stripe_customer = subscription.create_stripe_customer(valid_stripe_token)
-
-      expect(stripe_customer.class).to eq Stripe::Customer
-    end
-  end
-
-  describe "#create_stripe_subscription" do
-    let(:stripe_helper) { StripeMock.create_test_helper }
-    before { StripeMock.start }
-    after { StripeMock.stop }
-
-    it "creates a stripe subscription with the correct quantity" do
+    let(:valid_stripe_token) { stripe_helper.generate_card_token }
+    let(:subscription) { create(:subscription, email: "jenny@local.org",
+                                               stripe_subscription_id: nil,
+                                               stripe_customer_id: nil) }
+    around :all do |test|
+      StripeMock.start
       stripe_helper.create_plan(id: "planningalerts-backers-test-1", amount: 1)
-      subscription = create(:subscription, email: "jenny@local.org")
-      valid_stripe_token = stripe_helper.generate_card_token
-      stripe_customer = Stripe::Customer.create( email: subscription.email,
-                                                 source: valid_stripe_token)
-
-      stripe_subscription = subscription.create_stripe_subscription(stripe_customer, 4)
-
-      expect(stripe_subscription.quantity).to eq 4
+      test.run
+      StripeMock.stop
     end
 
-    it "returns a stripe subscription object" do
-      stripe_helper.create_plan(id: "planningalerts-backers-test-1", amount: 1)
-      subscription = create(:subscription, email: "jenny@local.org")
-      valid_stripe_token = stripe_helper.generate_card_token
-      stripe_customer = Stripe::Customer.create( email: subscription.email,
-                                                 source: valid_stripe_token)
+    describe "#send_customer_to_stripe" do
+      it "creates a stripe customer with the subscription’s attributes" do
+        stripe_customer = subscription.create_stripe_customer(valid_stripe_token)
 
-      stripe_subscription = subscription.create_stripe_subscription(stripe_customer, 4)
+        expect(stripe_customer.email).to eql "jenny@local.org"
+      end
 
-      expect(stripe_subscription.class).to eq Stripe::Subscription
+      it "returns a stripe customer object" do
+        stripe_customer = subscription.create_stripe_customer(valid_stripe_token)
+
+        expect(stripe_customer.class).to eql Stripe::Customer
+      end
     end
-  end
 
-  describe "#send_subscription_to_stripe_and_store_ids" do
-    let(:stripe_helper) { StripeMock.create_test_helper }
-    before { StripeMock.start }
-    after { StripeMock.stop }
+    describe "#create_stripe_subscription" do
+      let(:stripe_customer) do
+        Stripe::Customer.create(email: subscription.email,
+                                source: valid_stripe_token)
+      end
 
-    it "updates the subscription with the stripe ids" do
-      stripe_helper.create_plan(id: "planningalerts-backers-test-1", amount: 1)
-      subscription = create(:subscription, email: "jenny@local.org",
-                                           stripe_subscription_id: nil,
-                                           stripe_customer_id: nil)
-      valid_stripe_token = stripe_helper.generate_card_token
+      it "creates a stripe subscription with the correct quantity" do
+        stripe_subscription = subscription.create_stripe_subscription(stripe_customer, 4)
 
-      subscription.send_subscription_to_stripe_and_store_ids(valid_stripe_token, 4)
+        expect(stripe_subscription.quantity).to eql 4
+      end
 
-      expect(subscription.stripe_subscription_id).to_not be nil
-      expect(subscription.stripe_customer_id).to_not be nil
+      it "returns a stripe subscription object" do
+        stripe_subscription = subscription.create_stripe_subscription(stripe_customer, 4)
+
+        expect(stripe_subscription.class).to eql Stripe::Subscription
+      end
+    end
+
+    describe "#send_subscription_to_stripe_and_store_ids" do
+      it "updates the subscription with the stripe ids" do
+        subscription.send_subscription_to_stripe_and_store_ids(valid_stripe_token, 4)
+
+        expect(subscription.stripe_subscription_id).to_not eql nil
+        expect(subscription.stripe_customer_id).to_not eql nil
+      end
     end
   end
 end
