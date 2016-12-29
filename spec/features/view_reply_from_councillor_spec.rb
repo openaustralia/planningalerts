88ac25d639005d1b3246b11ac58376b4774f0f19
Luke Bacon <luke@equivalentideas.com>
2016-12-29 22:31:33 +1100

    Confirm that people can see old replies from councillors who have since left the council

---
+++ spec/features/view_reply_from_councillor_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+feature "View a reply from a councillor" do
+  given(:comment) do
+    VCR.use_cassette('planningalerts') do
+      authority = create(:authority, full_name: "City of Sydney")
+      create(
+        :comment_to_councillor,
+        :confirmed,
+        name: "Richard Pope",
+        councillor: create(:councillor, name: "Louise Councillor", authority: authority),
+        application: create(:application, authority: authority)
+      )
+    end
+  end
+
+  before do
+    create(:reply, comment: comment, councillor: comment.councillor)
+  end
+
+  scenario "on the application page" do
+    visit application_path(comment.application)
+
+    expect(page).to have_content("Richard Pope wrote to local councillor Louise Councillor")
+    expect(page).to have_content("Louise Councillor local councillor for City of Sydney replied to Richard Pope")
+  end
+
+  context "when they are not one of the authority's current councillors" do
+    before do
+      comment.councillor.update(current: false)
+    end
+
+    scenario "on the application page" do
+      visit application_path(comment.application)
+
+      expect(page).to have_content("Richard Pope wrote to local councillor Louise Councillor")
+      expect(page).to have_content("Louise Councillor local councillor for City of Sydney replied to Richard Pope")
+    end
+  end
+end
