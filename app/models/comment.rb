fc12794831eb14ad6adcf9076b2623557862d11a
Luke Bacon <luke@equivalentideas.com>
2017-01-04 15:42:13 +1100

    Use Acitve Job for sending comments to WriteIt
    Sending these immediately was a regression introduced in the Rails
    upgrade, see https://github.com/openaustralia/planningalerts/issues/1047
    
    This uses the correct method for using ActiveJob:
    http://guides.rubyonrails.org/active_job_basics.html#action-mailer
    
    Fixes #1047

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -36,7 +36,7 @@ class Comment < ActiveRecord::Base
 
   def send_comment!
     if to_councillor? && ENV["WRITEIT_BASE_URL"]
-      CommentNotifier.send_comment_via_writeit!(self).deliver_now
+      CommentNotifier.send_comment_via_writeit!(self).deliver_later
     elsif to_councillor?
       CommentNotifier.notify_councillor("default", self).deliver_later
     else
