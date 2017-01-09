9f7627605b55ca53b84b1db6fcaf8d4b53606316
Luke Bacon <luke@equivalentideas.com>
2017-01-09 17:02:48 +1100

    Extract method

--- app/models/new_alert_parser.rb
+++ app/models/new_alert_parser.rb
@@ -8,24 +8,24 @@ class NewAlertParser
   def parse
     alert.geocode_from_address
 
-    if preexisting_matching_alert
-      if preexisting_matching_alert.confirmed?
-        if preexisting_matching_alert.unsubscribed?
-          alert
-        else
-          nil
-        end
-      else
-        preexisting_matching_alert.send_confirmation_email
-
-        nil
-      end
-    else
-      alert
-    end
+    preexisting_matching_alert ? parse_for_preexisting_alert_states : alert
   end
 
   private
+
+  def parse_for_preexisting_alert_states
+    if preexisting_matching_alert.confirmed?
+      if preexisting_matching_alert.unsubscribed?
+        alert
+      else
+        nil
+      end
+    else
+      preexisting_matching_alert.send_confirmation_email
+
+      nil
+    end
+  end
 
   def preexisting_matching_alert
     Alert.find_by(email: alert.email, address: alert.address)
