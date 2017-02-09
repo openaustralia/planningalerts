openaustralia/planningalerts

commit 95b8c5ca0cecbdf09cea0de241d93a6668880008

simplify the autocomplete code as per google docs

app/assets/javascripts/address_autocomplete.js
@@ -1,46 +1,11 @@
-var service = new google.maps.places.AutocompleteService();
+function initAutoComplete() {
+  var input = document.getElementById('q');
+  var options = {
+    componentRestrictions: {country: "au"},
+    types: ["address"]
+  };
 
-$("#alert_address,#q").autocomplete({
-    html: true,
-    source: function(request, response) {
-        service.getPlacePredictions({
-            input: request.term,
-            componentRestrictions: {country: "au"},
-            types: ["address"]
-        }, function(predictions, status){
-            response($.map(predictions, function(prediction){
-                // Just highlight the first matched substring
-                var length = prediction.matched_substrings[0].length;
-                var offset = prediction.matched_substrings[0].offset;
-                // Remove Australia from the list to make a slightly shorter version of the text
-                textArray = $.map(prediction.terms.slice(0,-1), function(term){
-                    return(term.value);
-                });
-                // Translate name of state to shorter version
-                state = textArray[textArray.length - 1];
-                switch(state) {
-                    case "Australian Capital Territory": state = "ACT"; break;
-                    case "New South Wales": state = "NSW"; break;
-                    case "Northern Territory": state = "NT"; break;
-                    case "Queensland": state = "QLD"; break;
-                    case "South Australia": state = "SA"; break;
-                    case "Tasmania": state = "TAS"; break;
-                    case "Victoria": state = "VIC"; break;
-                    case "Western Australia": state = "WA"; break;
-                }
-                // Don't use comma to separate state from rest of address to behave the same as the geocoder
-                text = textArray.slice(0,-1).join(", ") + " " + state
-                var html = text.slice(0, offset) +
-                    "<strong>" + text.slice(offset, offset+length) + "</strong>" +
-                    text.slice(offset+length);
+  var autocomplete = new google.maps.places.Autocomplete(input, options);
+}
 
-                return({label: html, value: text});
-            }));
-        });
-    },
-    select: function( event, ui ) {
-        $(this).val(ui.item.value);
-        // If we're on the home page (which has a form with one field) make selecting an address submit the form
-        $("form.one-field").submit();
-    }
-});
+initAutoComplete()

commit a4098741cf46df65d01c57a8c629c64354f65c49

Provide address autocomplete on the alert signup forms
Due to a regression in 95b8c5ca0cecbdf09cea0de241d93a6668880008
autocomplete for addresses hasn't been working anywhere except the
address search on the homepage.

This is because the autocomplete input everywhere else, e.g. on the
/alerts/sign_up page and on the authorities applications sign up form,
required a different selector.

This commit adds tests to confirm that autocomplete is working the
sign up forms.

It also simplifies the autocomplete code by introducing one standard
HTML class name for targeting input fields for autocomplete
functionality.

app/assets/javascripts/address_autocomplete.js
@@ -1,5 +1,5 @@
 function initAutoComplete() {
-  var input = document.getElementById('q');
+  var input = document.querySelector(".address-autocomplete-input");
   var options = {
     componentRestrictions: {country: "au"},
     types: ["address"]

app/views/alerts/_signup_form.html.haml
@@ -6,7 +6,7 @@
     - else
       = f.input :address, label: "Enter a street address",
                           placeholder: "e.g. #{alert.address_for_placeholder}",
-                          input_html: { required: "required" }
+                          input_html: { required: "required", class: "address-autocomplete-input" }
     = f.input :email, label: "Enter your email address",
                       placeholder: "your@email.com",
                       input_html: { required: "required" }

app/views/applications/address.html.haml
@@ -7,7 +7,7 @@
     %ol
       %li#q_input.string{class: ("error" if @error)}
         = label_tag(:q, "Enter a street address")
-        = text_field_tag(:q, @q, placeholder: "e.g. 1 Sowerby St, Goulburn, NSW 2580", required: "required")
+        = text_field_tag(:q, @q, placeholder: "e.g. 1 Sowerby St, Goulburn, NSW 2580", required: "required", class: "address-autocomplete-input")
         - if @error
           %p.inline-errors= @error
         = link_to "#", class: "inline-hints", id: "geolocate" do

spec/features/search_for_applications_spec.rb
@@ -40,7 +40,7 @@ feature "Searching for development application near an address" do
       fill_in "Enter a street address", with: "24 Bruce R"
 
       # this simulates focusing on the input field, which triggers the autocomplete search
-      page.execute_script("el = document.querySelector('#q');
+      page.execute_script("el = document.querySelector('.address-autocomplete-input');
                            event = document.createEvent('HTMLEvents');
                            event.initEvent('focus', false, true);
                            el.dispatchEvent(event);")

spec/features/sign_up_for_alerts_spec.rb
@@ -102,6 +102,33 @@ feature "Sign up for alerts" do
 
       expect(page).to have_content("your alert has been activated")
     end
+
+    context "with javascript" do
+      before do
+        # This is a hack to get around the ssl_required? method in
+        # the application controller which redirects poltergeist to https.
+        allow(Rails.env).to receive(:development?).and_return true
+      end
+
+      scenario "autocomplete results are displayed", js: true do
+        visit applications_path(authority_id: "glenbrook")
+
+        fill_in "Enter a street address", with: "24 Bruce Road Glenb"
+
+        # this simulates focusing on the input field, which triggers the autocomplete search
+        page.execute_script("el = document.querySelector('.address-autocomplete-input');
+                            event = document.createEvent('HTMLEvents');
+                            event.initEvent('focus', false, true);
+                            el.dispatchEvent(event);")
+
+        # Confirm that the suggested addresses appear.
+        within ".pac-container" do
+          expect(page).to have_content "Bruce Road, Glenbrook, New South Wales"
+        end
+
+        # TODO: Actually test clicking the suggestion and seeing results
+      end
+    end
   end
 
   context "when there is already an unconfirmed alert for the address" do
@@ -180,6 +207,33 @@ feature "Sign up for alerts" do
         expect(page).to have_content("your alert has been activated")
         expect(page).to have_content("24 Bruce Rd, Glenbrook NSW 2773")
       end
+    end
+  end
+
+  context "with javascript" do
+    before do
+      # This is a hack to get around the ssl_required? method in
+      # the application controller which redirects poltergeist to https.
+      allow(Rails.env).to receive(:development?).and_return true
+    end
+
+    scenario "autocomplete results are displayed", js: true do
+      visit '/alerts/signup'
+
+      fill_in "Enter a street address", with: "24 Bruce Road Glenb"
+
+      # this simulates focusing on the input field, which triggers the autocomplete search
+      page.execute_script("el = document.querySelector('.address-autocomplete-input');
+                           event = document.createEvent('HTMLEvents');
+                           event.initEvent('focus', false, true);
+                           el.dispatchEvent(event);")
+
+      # Confirm that the suggested addresses appear.
+      within ".pac-container" do
+        expect(page).to have_content "Bruce Road, Glenbrook, New South Wales"
+      end
+
+      # TODO: Actually test clicking the suggestion and seeing results
     end
   end
 