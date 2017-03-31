8aa793e9f814046c0bba2072ecafb668b507c04b
Luke Bacon <luke@equivalentideas.com>
2017-03-31 16:33:03 +1100

    Use newer Ruby hash style consistent with the app

--- app/helpers/replies_helper.rb
+++ app/helpers/replies_helper.rb
@@ -1,5 +1,5 @@
 module RepliesHelper
   def reply_path(reply)
-    application_path(reply.comment.application,:anchor => "reply#{reply.id}")
+    application_path(reply.comment.application, anchor: "reply#{reply.id}")
   end
 end
