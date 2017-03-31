b4fa7ca4c884b5b2a42475da268c4ccd89fe82ae
Luke Bacon <luke@equivalentideas.com>
2017-03-31 16:27:39 +1100

    Indentation should be 2 spaces not 1

--- spec/helpers/replies_helper_spec.rb
+++ spec/helpers/replies_helper_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe RepliesHelper do
   describe "#reply_path" do
-   let(:application) { VCR.use_cassette('planningalerts') { create(:application, id: 1) } }
-   let(:reply) { create(:reply, id: 1, comment: create(:confirmed_comment, application: application)) }
+    let(:application) { VCR.use_cassette('planningalerts') { create(:application, id: 1) } }
+    let(:reply) { create(:reply, id: 1, comment: create(:confirmed_comment, application: application)) }
 
-   it "returns the path for the application with an anchor with the reply id" do
-     expect(helper.reply_path(reply)).to eq "/applications/1#reply1"
-   end
+    it "returns the path for the application with an anchor with the reply id" do
+      expect(helper.reply_path(reply)).to eq "/applications/1#reply1"
+    end
   end
 end
