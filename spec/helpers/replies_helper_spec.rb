aa6a2dbdb7ca6f60a424fec1ed00a943520d48b5
Luke Bacon <luke@equivalentideas.com>
2017-03-31 16:25:50 +1100

    Inline unused variable
    The comment is a detail that we don't care about in the test outcome.
    As long as it doesn't make the line too long and complex, I think it's
    better to inline it.

--- spec/helpers/replies_helper_spec.rb
+++ spec/helpers/replies_helper_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 describe RepliesHelper do
   describe "#reply_path" do
    let(:application) { VCR.use_cassette('planningalerts') { create(:application, id: 1) } }
-   let(:comment) { create(:confirmed_comment, application: application) }
-   let(:reply) { create(:reply, id: 1, comment: comment) }
+   let(:reply) { create(:reply, id: 1, comment: create(:confirmed_comment, application: application)) }
 
    it "returns the path for the application with an anchor with the reply id" do
      expect(helper.reply_path(reply)).to eq "/applications/1#reply1"
