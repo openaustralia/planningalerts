362d7429631c3abb577717342220c8ab342b37ab
Henare Degan <henare.degan@gmail.com>
2017-05-05 16:54:00 +1000

    Switch from antispam honeypot that's stopped working to reCAPTCHA

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,16 +6,12 @@ class ReportsController < ApplicationController
 
   def create
     @comment = Comment.visible.find(params[:comment_id])
+    @report = @comment.reports.build(name: params[:report][:name], email: params[:report][:email], details: params[:report][:details])
 
-    # First check if the honeypot field has been filled out by a spam bot
-    # If so, make it look like things worked but don't actually do anything
-    if params[:little_sweety].blank?
-      @report = @comment.reports.build(name: params[:report][:name], email: params[:report][:email], details: params[:report][:details])
-      if @report.save
-        ReportNotifier.notify(@report).deliver_later
-      else
-        render 'new'
-      end
+    if verify_recaptcha && @report.save
+      ReportNotifier.notify(@report).deliver_later
+    else
+      render 'new'
     end
   end
 end
