63d5c7e6d1670e5e5c7f6578d1b914fdb8c7c5d2
Henare Degan <henare.degan@gmail.com>
2017-05-06 17:36:36 +1000

    Make logic simpler by checking built-in reCAPTCHA flash message

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,14 +8,13 @@ class ReportsController < ApplicationController
     @comment = Comment.visible.find(params[:comment_id])
     @report = @comment.reports.build(name: params[:report][:name], email: params[:report][:email], details: params[:report][:details])
 
-    if verify_recaptcha
-      if @report.save
-        ReportNotifier.notify(@report).deliver_later
-      else
-        render 'new'
-      end
+    if verify_recaptcha && @report.save
+      ReportNotifier.notify(@report).deliver_later
     else
-      flash[:error] = "Sorry, we couldn't verify that you're not a robot. Make sure you click the <em>I'm not a robot</em> box below and try again.".html_safe
+      if flash[:recaptcha_error]
+        flash[:error] = "Sorry, we couldn't verify that you're not a robot. Make sure you click the <em>I'm not a robot</em> box below and try again.".html_safe
+      end
+
       render 'new'
     end
   end
