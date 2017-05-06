e922d4044a278514b4505746f54cd442fde92cc4
Henare Degan <henare.degan@gmail.com>
2017-05-06 17:22:07 +1000

    Add an error message if reCAPTCHA verification didn't happen

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,10 +8,14 @@ class ReportsController < ApplicationController
     @comment = Comment.visible.find(params[:comment_id])
     @report = @comment.reports.build(name: params[:report][:name], email: params[:report][:email], details: params[:report][:details])
 
-    if verify_recaptcha && @report.save
-      ReportNotifier.notify(@report).deliver_later
+    if verify_recaptcha
+      if @report.save
+        ReportNotifier.notify(@report).deliver_later
+      else
+        render 'new'
+      end
     else
-      # TODO: Show an error message because the CAPTCHA probably wasn't verified
+      flash[:error] = "Sorry, we couldn't verify that you're not a robot. Make sure you click the <em>I'm not a robot</em> box below and try again.".html_safe
       render 'new'
     end
   end
