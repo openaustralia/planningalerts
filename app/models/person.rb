b0d5442217be3b1872bf14c8219cadf5e55b3879
Luke Bacon <luke@equivalentideas.com>
2017-05-26 16:58:07 +1000

    Add method for getting people that signed up a week ago

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,21 @@
 class Person
+  attr_reader :email
+
   def initialize(email: nil)
     @email = email
   end
 
-  attr_reader :email
+  def self.subscribed_one_week_ago
+    date = 1.week.ago.to_date
+    alerts = Alert.where(confirmed: true).where("date(created_at) = ?", date).group(:email)
+    #
+    # Remove people who signed up before that date
+    alerts = alerts.reject do |alert|
+      Alert.where(email: alert.email).where("created_at < ?", alert.created_at).any?
+    end
+
+    alerts.collect do |alert|
+      Person.new(email: alert.email)
+    end
+  end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -4,4 +4,26 @@ RSpec.describe Person, type: :model do
   it "they have an email" do
     expect(Person.new(email: "elzia@example.org").email).to eql "elzia@example.org"
   end
+
+  describe ".subscribed_one_week_ago" do
+    before :each do
+      Timecop.freeze(Time.utc(2017, 5, 27))
+
+      create(:confirmed_alert, email: "jane@example.org", created_at: Time.utc(2017, 5, 20, 5, 32))
+      create(:confirmed_alert, email: "eliza@example.org", created_at: Time.utc(2017, 5, 20, 15, 16))
+      create(:confirmed_alert, email: "old_subscribed@example.org", created_at: Time.utc(2017, 5, 20, 11, 02))
+      create(:confirmed_alert, email: "old_subscribed@example.org", created_at: Time.utc(2017, 4, 29))
+    end
+
+    after :each do
+      Timecop.return
+    end
+
+    it "returns the people who subscribed one week ago" do
+      expect(Person.subscribed_one_week_ago.map(&:email)).to eq [
+        "eliza@example.org",
+        "jane@example.org"
+      ]
+    end
+  end
 end
