fb383140c00c1d2cfe7ec91c82a294d28ac70476
Luke Bacon <luke@equivalentideas.com>
2017-05-26 17:22:12 +1000

    Remove typo comment line

--- app/models/person.rb
+++ app/models/person.rb
@@ -8,7 +8,7 @@ class Person
   def self.subscribed_one_week_ago
     date = 1.week.ago.to_date
     alerts = Alert.active.where("date(created_at) = ?", date).group(:email)
-    #
+
     # Remove people who signed up before that date
     alerts = alerts.reject do |alert|
       Alert.where(email: alert.email).where("created_at < ?", alert.created_at).any?
