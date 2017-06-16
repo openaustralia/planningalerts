d29938f4d969d48ab0a9ea782674cb2f63b6b269
Luke Bacon <luke@equivalentideas.com>
2017-06-16 16:45:17 +1000

    Create AlertSubscriber records for all existing alerts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529023305) do
+ActiveRecord::Schema.define(version: 20170616063045) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "resource_id",   limit: 255,   null: false
