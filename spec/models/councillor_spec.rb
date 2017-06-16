817aa39070ff81d930d21c309ffa49ee4463ae48
Luke Bacon <luke@equivalentideas.com>
2017-06-16 14:54:52 +1000

    Add test for double quotes

--- spec/models/councillor_spec.rb
+++ spec/models/councillor_spec.rb
@@ -55,4 +55,10 @@ describe Councillor do
       expect(councillor.cached_image_available?).to be false
     end
   end
+
+  describe "#test_method_for_hound" do
+    it "should not book me for using double quotes here" do
+      councillor = build(:councillor, popolo_id: "authority/foo_bar")
+    end
+  end
 end
