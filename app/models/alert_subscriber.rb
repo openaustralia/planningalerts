144dd44600865addddd279dac475395c4d185cd0
Luke Bacon <luke@equivalentideas.com>
2017-06-16 16:25:04 +1000

    Remove unused method

--- app/models/alert_subscriber.rb
+++ app/models/alert_subscriber.rb
@@ -1,18 +1,3 @@
 class AlertSubscriber < ActiveRecord::Base
   has_many :alerts
-
-  # TODO: Extract generic new_subscribers_for_date
-  def self.subscribed_one_week_ago
-    date = 1.week.ago.to_date
-    alerts = Alert.active.where("date(created_at) = ?", date).group(:email)
-
-    # Remove people who signed up before that date
-    alerts = alerts.reject do |alert|
-      Alert.where(email: alert.email).where("created_at < ?", alert.created_at).any?
-    end
-
-    alerts.collect do |alert|
-      AlertSubscriber.new(email: alert.email)
-    end
-  end
 end
