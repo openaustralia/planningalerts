6bcaf4851d35482f80a1bc11451c890699ef5e6a
hisayohorie <hisayohorie@gmail.com>
2017-06-19 12:30:09 -0400

    fixed the typos in the suggested_councilor_spec.rb

--- spec/models/suggested_councillor_spec.rb
+++ spec/models/suggested_councillor_spec.rb
@@ -3,27 +3,27 @@ require 'spec_helper'
 RSpec.describe SuggestedCouncillor, type: :model do
 
   describe "is invalid without an email" do
-    subject { SuggestedCouncillor.new(name: "Milla", email: nil. authority_id: 1) }
-    it { is_expected.to_not_be_valid }
+    subject { SuggestedCouncillor.new(name: "Milla", email: nil, authority_id: 1) }
+    it { is_expected.to_not be_valid }
   end
 
   describe "is invalid with a blank email" do
-    subject { SuggestedCouncillor.new(name: "Milla", email: "". authority_id: 1) }
-    it { is_expected.to_not_be_valid }
+    subject { SuggestedCouncillor.new(name: "Milla", email: "", authority_id: 1) }
+    it { is_expected.to_not be_valid }
   end
 
   describe "is invalid without an name" do
-    subject { SuggestedCouncillor.new(name: nil, email: "test@test.com". authority_id: 1) }
-    it { is_expected.to_not_be_valid }
+    subject { SuggestedCouncillor.new(name: nil, email: "test@test.com", authority_id: 1) }
+    it { is_expected.to_not be_valid }
   end
 
   describe "is invalid without a blank name" do
-    subject { SuggestedCouncillor.new(name: "", email: "test@test.com". authority_id: 1) }
-    it { is_expected.to_not_be_valid }
+    subject { SuggestedCouncillor.new(name: "", email: "test@test.com", authority_id: 1) }
+    it { is_expected.to_not be_valid }
   end
 
   describe "is invalid without an authority_id" do
-    subject { SuggestedCouncillor.new(name: "Milla", email: "test@test.com". authority_id: nil) }
-    it { is_expected.to_not_be_valid }
+    subject { SuggestedCouncillor.new(name: "Milla", email: "test@test.com", authority_id: nil) }
+    it { is_expected.to_not be_valid }
   end
 end
