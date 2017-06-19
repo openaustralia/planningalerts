969ef04ba6d4c75fbae5ae4bbdbcfc523f9bc636
hisayohorie <hisayohorie@gmail.com>
2017-06-18 22:49:09 -0400

    changed the syntax to Ruby 1.9

--- spec/models/suggested_councillor_spec.rb
+++ spec/models/suggested_councillor_spec.rb
@@ -1,21 +1,20 @@
 require 'spec_helper'
 
 RSpec.describe SuggestedCouncillor, type: :model do
-
   it "should require a name" do
-    user = SuggestedCouncillor.create(:name => "")
+    user = SuggestedCouncillor.create( name: "")
     user.valid?
     user.errors.should have_key(:name)
   end
 
   it "should require an email" do
-    user = SuggestedCouncillor.create(:email => "")
+    user = SuggestedCouncillor.create(email: "")
     user.valid?
     user.errors.should have_key(:email)
   end
 
   it "should require an authority_id" do
-    user = SuggestedCouncillor.create(:authority_id => "")
+    user = SuggestedCouncillor.create(authority_id: "")
     user.valid?
     user.errors.should have_key(:authority_id)
   end
