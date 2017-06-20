8bfa81541e94eab28788c2b6967f9ed2c1975b06
Luke Bacon <luke@equivalentideas.com>
2017-06-20 18:21:56 +1000

    If the feature flag is off, return a 404 page
    Start of #1176

--- spec/features/user_contributes_new_councillor_spec.rb
+++ spec/features/user_contributes_new_councillor_spec.rb
@@ -3,15 +3,31 @@ require "spec_helper"
 feature "Contributing a new councillor for an authority" do
   let(:authority) { create(:authority, full_name: "Casey City Council") }
 
-  it "successfully" do
-    visit new_authority_suggested_councillor_path(authority.short_name_encoded)
+  context "when the feature flag is off" do
+    it "isn't available" do
+      visit new_authority_suggested_councillor_path(authority.short_name_encoded)
 
-    within_fieldset "Add a councillor" do
-      fill_in "Name", with: "Mila Gilic"
-      fill_in "Email", with: "mgilic@casey.vic.gov.au"
+      expect(page.status_code).to eq 404
     end
-    click_button "Submit"
+  end
 
-    expect(page).to have_content "Thank you"
+  context "when the feature flag is on" do
+    around do |test|
+      with_modified_env CONTRIBUTE_COUNCILLORS_ENABLED: "true" do
+        test.run
+      end
+    end
+
+    it "successfully" do
+      visit new_authority_suggested_councillor_path(authority.short_name_encoded)
+
+      within_fieldset "Add a councillor" do
+        fill_in "Name", with: "Mila Gilic"
+        fill_in "Email", with: "mgilic@casey.vic.gov.au"
+      end
+      click_button "Submit"
+
+      expect(page).to have_content "Thank you"
+    end
   end
 end
