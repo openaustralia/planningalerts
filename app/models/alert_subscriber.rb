6610254f577eb9f73205a16df1dec73a20e7b55b
Luke Bacon <luke@equivalentideas.com>
2017-06-22 17:53:41 +1000

    Add validations to AlertSubscriber

--- app/models/alert_subscriber.rb
+++ app/models/alert_subscriber.rb
@@ -1,3 +1,5 @@
 class AlertSubscriber < ActiveRecord::Base
   has_many :alerts
+
+  validates :email, presence: true, uniqueness: true
 end
--- spec/models/alert_subscriber_spec.rb
+++ spec/models/alert_subscriber_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe AlertSubscriber, type: :model do
   it "they have an email" do
     expect(AlertSubscriber.create(email: "elzia@example.org").email).to eql "elzia@example.org"
   end
+
+  it { expect(AlertSubscriber.create(email: nil)).to_not be_valid }
+
+  it "has a unique email" do
+    AlertSubscriber.create(email: "foo@bar.org")
+
+    expect(AlertSubscriber.create(email: "foo@bar.org")).to_not be_valid
+  end
 end
