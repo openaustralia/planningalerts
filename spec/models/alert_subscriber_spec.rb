5128365e57e55ee413becfaf167619cf16ada0c5
Luke Bacon <luke@equivalentideas.com>
2017-06-22 17:54:24 +1000

    Remove redundant test

--- spec/models/alert_subscriber_spec.rb
+++ spec/models/alert_subscriber_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 RSpec.describe AlertSubscriber, type: :model do
-  it "they have an email" do
-    expect(AlertSubscriber.create(email: "elzia@example.org").email).to eql "elzia@example.org"
-  end
-
   it { expect(AlertSubscriber.create(email: nil)).to_not be_valid }
 
   it "has a unique email" do
