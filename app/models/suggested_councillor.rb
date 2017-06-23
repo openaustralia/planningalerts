e3c3f52896b0de3d70cae49339f7771c97b5f6ef
hisayohorie <hisayohorie@gmail.com>
2017-06-22 22:34:55 -0400

    Added association and nested attribute for contributor in suggested_councillor.rb
    to add contributor form nested in the suggested councillor form.

--- app/models/suggested_councillor.rb
+++ app/models/suggested_councillor.rb
@@ -1,4 +1,6 @@
 class SuggestedCouncillor < ActiveRecord::Base
   belongs_to :authority
+  belongs_to :contributor
+  accepts_nested_attributes_for :contributor
   validates :authority_id, :name, :email, presence: true
 end
