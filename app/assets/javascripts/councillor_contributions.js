1a37155936e005162912da6df9a6c4069a1e1013
hisayohorie <hisayohorie@gmail.com>
2017-07-07 12:19:50 -0400

    Fixed extra lines and class names
    For styling.

--- app/assets/javascripts/councillor_contributions.js
+++ app/assets/javascripts/councillor_contributions.js
@@ -1,14 +1,11 @@
 $(document).ready(function(){
   $('.remove_fields').live('click',function(){
     var num = $(".nested-fields").length - 1;
-    document.getElementById("counts").value = "Submit " + (num) + " new councillors"
+    document.getElementById("counts").value = "Submit " + (num) + " new councillors";
   });
 
-  $('#add_button').live('click',function(){
+  $('#add-button').live('click',function(){
     var num = $(".nested-fields").length;
-    document.getElementById("counts").value = "Submit " + (num) + " new councillors"
+    document.getElementById("counts").value = "Submit " + (num) + " new councillors";
   });
-
-
-
 });
