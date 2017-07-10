5a75f5ced1500af90854c46bdb009da5befa4251
hisayohorie <hisayohorie@gmail.com>
2017-07-10 12:42:19 -0400

    Updated the validation and association for suggested_councillor and councillor_contribution models
    To save suggested_councillor though councillor_contribution form.

--- app/models/councillor_contribution.rb
+++ app/models/councillor_contribution.rb
@@ -1,6 +1,6 @@
 class CouncillorContribution < ActiveRecord::Base
   belongs_to :contributor
   belongs_to :authority
-  has_many :suggested_councillors
+  has_many :suggested_councillors, inverse_of: :councillor_contribution
   accepts_nested_attributes_for :suggested_councillors
 end
--- app/models/suggested_councillor.rb
+++ app/models/suggested_councillor.rb
@@ -1,5 +1,5 @@
 class SuggestedCouncillor < ActiveRecord::Base
   has_one :authority, through: :councillor_contribution
   belongs_to :councillor_contribution
-  validates :councillor_contribution_id, :name, :email, presence: true
+  validates :councillor_contribution, :name, :email, presence: true
 end
