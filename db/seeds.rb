a4d2553c5f5110495f5dd7bb4153d45dc00add64
hisayohorie <hisayohorie@gmail.com>
2017-07-19 22:30:37 -0400

    Pupulated Authority data
    To develop council directory.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-User.create! email: 'admin@example.com', password: 'password'
-Authority.create! full_name: 'Marrickville Council',
-                  short_name: 'Marrickville',
-                  state: 'NSW',
-                  email: 'council@marrickville.nsw.gov.au',
+Authority.create! full_name: 'Blbury City Council',
+                  short_name: 'BlburyCity',
+                  state: 'WA',
+                  email: 'council@blbury.wa.gov.au',
                   population_2011: '81489',
-                  morph_name: 'planningalerts-scrapers/marrickville'
+                  morph_name: 'planningalerts-scrapers/blbury'
+
+Authority.create! full_name: 'Newcastles Council',
+                  short_name: 'Newcastles',
+                  state: 'NSW',
+                  email: 'council@newcastles.nsw.gov.au',
+                  population_2011: '81489',
+                  morph_name: 'planningalerts-scrapers/newcastles'
+
+Authority.create! full_name: 'Shire of Kents Council',
+                  short_name: 'KentsShire',
+                  state: 'WA',
+                  email: 'council@Kents.wa.gov.au',
+                  population_2011: '81489',
+                  morph_name: 'planningalerts-scrapers/kents'
+
+Authority.create! full_name: 'Dorsets Council',
+                  short_name: 'Dorsets',
+                  state: 'TAS',
+                  email: 'council@dorsets.tas.gov.au',
+                  population_2011: '81489',
+                  morph_name: 'planningalerts-scrapers/dorsets'
