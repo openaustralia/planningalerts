8c12c0551a9a5163d0a3eaa4bc985e2809e5e808
hisayohorie <hisayohorie@gmail.com>
2017-07-19 23:04:33 -0400

    Added website_url column in authority table seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,25 +3,28 @@ Authority.create! full_name: 'Blbury City Council',
                   state: 'WA',
                   email: 'council@blbury.wa.gov.au',
                   population_2011: '81489',
-                  morph_name: 'planningalerts-scrapers/blbury'
+                  morph_name: 'planningalerts-scrapers/blbury',
+                  website_url: 'http://www.alburycity.nsw.gov.au'
 
 Authority.create! full_name: 'Newcastles Council',
                   short_name: 'Newcastles',
                   state: 'NSW',
                   email: 'council@newcastles.nsw.gov.au',
                   population_2011: '81489',
-                  morph_name: 'planningalerts-scrapers/newcastles'
-
+                  morph_name: 'planningalerts-scrapers/newcastle',
+                  website_url: 'http://www.newcastle.nsw.gov.au'
 Authority.create! full_name: 'Shire of Kents Council',
                   short_name: 'KentsShire',
                   state: 'WA',
                   email: 'council@Kents.wa.gov.au',
                   population_2011: '81489',
-                  morph_name: 'planningalerts-scrapers/kents'
+                  morph_name: 'planningalerts-scrapers/kents',
+                  website_url: 'http://www.kentwa.gov/'
 
 Authority.create! full_name: 'Dorsets Council',
                   short_name: 'Dorsets',
                   state: 'TAS',
                   email: 'council@dorsets.tas.gov.au',
                   population_2011: '81489',
-                  morph_name: 'planningalerts-scrapers/dorsets'
+                  morph_name: 'planningalerts-scrapers/dorsets',
+                  website_url: 'http://www.dorset.tas.gov.au/'
