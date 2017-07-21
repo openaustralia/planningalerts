e276198a6a82dce0a32985435b3a0f2b16a4d3ed
hisayohorie <hisayohorie@gmail.com>
2017-07-21 15:08:02 -0400

    Nested the unit test in the context "feature flag is on"

--- spec/features/council_contribution_tutorial_spec.rb
+++ spec/features/council_contribution_tutorial_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 
 feature "tutuorial page shows directory of council website" do
-  let(:authority) { create(:authority, full_name: "Casey City Council", website_url: "https//caseycity.nsw.gov.au") }
-
-  scenario "on the contribution tutorial page" do
-    visit contribution_tutorial_path
-    expect(page).to have_button("Search")
+  background do
+    create(:authority, full_name: "Casey City Council", website_url: "https//caseycity.nsw.gov.au")
   end
 
+  context "when the feature flag is off" do
+    it "isn't available" do
+      visit contribution_tutorial_path
+      expect(page.status_code).to eq 404
+    end
+  context "when the feature flag is on" do
+    around do |test|
+      with_modified_env CONTRIBUTE_COUNCILLORS_ENABLED: "true" do
+        test.run
+      end
+    end
+
+      scenario "on the contribution tutorial page" do
+        visit contribution_tutorial_path
+        expect(page).to have_content("Casey City Council")
+      end
+    end
+  end
 end
