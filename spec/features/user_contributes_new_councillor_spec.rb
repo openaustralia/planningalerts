ed9c1120fe921df90ce9b3c947b08905949cb195
Luke Bacon <luke@equivalentideas.com>
2017-08-01 13:05:33 +1000

    Remove legend with redundant label
    The heading of the page explains that this is for adding councillors,
    so I don't think we need to restate that here.
    
    This commit also changes the tests to use css to target the fieldset,
    rather than the text of the legend (now missing).

--- spec/features/user_contributes_new_councillor_spec.rb
+++ spec/features/user_contributes_new_councillor_spec.rb
@@ -27,7 +27,7 @@ feature "Contributing new councillors for an authority" do
     it "works successfully when the contributor provides their information" do
       visit new_authority_councillor_contribution_path(authority.short_name_encoded)
 
-      within_fieldset "Add a councillor" do
+      within ".councillor-contribution-councillors fieldset" do
         fill_in "Full name", with: "Mila Gilic"
         fill_in "Email", with: "mgilic@casey.vic.gov.au"
       end
@@ -47,7 +47,7 @@ feature "Contributing new councillors for an authority" do
     it "works successfully without contributor information" do
       visit new_authority_councillor_contribution_path(authority.short_name_encoded)
 
-      within_fieldset "Add a councillor" do
+      within ".councillor-contribution-councillors fieldset" do
         fill_in "Full name", with: "Mila Gilic"
         fill_in "Email", with: "mgilic@casey.vic.gov.au"
       end
@@ -57,7 +57,7 @@ feature "Contributing new councillors for an authority" do
       expect(page).to have_content "Mila Gilic"
       expect(page).to have_content "mgilic@casey.vic.gov.au"
 
-      within_fieldset "Add a councillor" do
+      within ".councillor-contribution-councillors fieldset" do
         fill_in "Full name", with: "Rosalie Crestani"
         fill_in "Email", with: "rcrestani@casey.vic.gov.au"
       end
@@ -70,21 +70,21 @@ feature "Contributing new councillors for an authority" do
     it "successfully with three councillors and one blank councillor" do
       visit new_authority_councillor_contribution_path(authority.short_name_encoded)
 
-      within_fieldset "Add a councillor" do
+      within ".councillor-contribution-councillors fieldset" do
         fill_in "Full name", with: "Mila Gilic"
         fill_in "Email", with: "mgilic@casey.vic.gov.au"
       end
 
       click_button "Add another councillor"
 
-      within_fieldset "Add a councillor" do
+      within ".councillor-contribution-councillors fieldset" do
         fill_in "Full name", with: "Rosalie Crestani"
         fill_in "Email", with: "rcrestani@casey.vic.gov.au"
       end
 
       click_button "Add another councillor"
 
-      within_fieldset "Add a councillor" do
+      within ".councillor-contribution-councillors fieldset" do
         fill_in "Full name", with: "Rosalie Crestani"
         fill_in "Email", with: "rcrestani@casey.vic.gov.au"
       end
@@ -100,21 +100,21 @@ feature "Contributing new councillors for an authority" do
     it "successfully with three councillors" do
       visit new_authority_councillor_contribution_path(authority.short_name_encoded)
 
-      within_fieldset "Add a councillor" do
+      within ".councillor-contribution-councillors fieldset" do
         fill_in "Full name", with: "Mila Gilic"
         fill_in "Email", with: "mgilic@casey.vic.gov.au"
       end
 
       click_button "Add another councillor"
 
-      within_fieldset "Add a councillor" do
+      within ".councillor-contribution-councillors fieldset" do
         fill_in "Full name", with: "Rosalie Crestani"
         fill_in "Email", with: "rcrestani@casey.vic.gov.au"
       end
 
       click_button "Add another councillor"
 
-      within_fieldset "Add a councillor" do
+      within ".councillor-contribution-councillors fieldset" do
         fill_in "Full name", with: "Rosalie Crestani"
         fill_in "Email", with: "rcrestani@casey.vic.gov.au"
       end
