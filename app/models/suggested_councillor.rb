306567f9f9902b7fc77ba7f48a24009d49c5cc96
hisayohorie <hisayohorie@gmail.com>
2017-08-03 13:07:21 -0400

    Added councillor_contribution validation back
    It was accidentaly removed.

--- app/models/suggested_councillor.rb
+++ app/models/suggested_councillor.rb
@@ -1,6 +1,7 @@
 class SuggestedCouncillor < ActiveRecord::Base
   has_one :authority, through: :councillor_contribution
   belongs_to :councillor_contribution
+  validates :councillor_contribution, :name, :email, presence: true
   validates :email, :name, presence: true
   validates_email_format_of :email
 end
