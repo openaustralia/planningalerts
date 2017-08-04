4d6551b9c451e0189e4fd7e0bee72d8ae4f755fb
hisayohorie <hisayohorie@gmail.com>
2017-08-04 13:28:19 -0400

    Added Proc.new in validation of email format
    To display different error messages when the format is invalid and when email is blank

--- app/models/suggested_councillor.rb
+++ app/models/suggested_councillor.rb
@@ -2,5 +2,6 @@ class SuggestedCouncillor < ActiveRecord::Base
   has_one :authority, through: :councillor_contribution
   belongs_to :councillor_contribution
   validates :councillor_contribution, :name, :email, presence: true
-  validates_email_format_of :email
+  validates_email_format_of :email, :message => "format seems not right.
+  Does it follow the format of example@email.com?", :unless => Proc.new{|c| c.email.blank?}
 end
