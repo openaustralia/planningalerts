66c63060eb4fd9548723f43d2b196238659112ad
hisayohorie <hisayohorie@gmail.com>
2017-08-07 12:51:58 -0400

    Updated old ruby syntax to new version

--- app/models/suggested_councillor.rb
+++ app/models/suggested_councillor.rb
@@ -2,6 +2,6 @@ class SuggestedCouncillor < ActiveRecord::Base
   has_one :authority, through: :councillor_contribution
   belongs_to :councillor_contribution
   validates :councillor_contribution, :name, :email, presence: true
-  validates_email_format_of :email, :message => "format seems not right.
-  Does it follow the format of example@email.com?", :unless => Proc.new{|c| c.email.blank?}
+  validates_email_format_of :email, message: "format seems not right.
+  Does it follow the format of example@email.com?", unless: Proc.new{|c| c.email.blank?}
 end
