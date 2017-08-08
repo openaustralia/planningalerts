2415111f2e691b7eacb80fd673d3171bb662be4b
hisayohorie <hisayohorie@gmail.com>
2017-08-07 23:56:57 -0400

    Updated the error message for invalid email format

--- app/models/suggested_councillor.rb
+++ app/models/suggested_councillor.rb
@@ -2,6 +2,5 @@ class SuggestedCouncillor < ActiveRecord::Base
   has_one :authority, through: :councillor_contribution
   belongs_to :councillor_contribution
   validates :councillor_contribution, :name, :email, presence: true
-  validates_email_format_of :email, message: "format seems not right.
-  Does it follow the format of example@email.com?", unless: Proc.new{|c| c.email.blank?}
+  validates_email_format_of :email, message: "Email must be a valid email address, e.g. jane@example.com", unless: Proc.new{|c| c.email.blank?}
 end
