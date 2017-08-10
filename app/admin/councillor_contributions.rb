38344fef6dba3be1735012905cf4b6d2a355261c
hisayohorie <hisayohorie@gmail.com>
2017-08-10 11:55:39 -0400

    Added index table

--- app/admin/councillor_contributions.rb
+++ app/admin/councillor_contributions.rb
@@ -1,5 +1,10 @@
 ActiveAdmin.register CouncillorContribution do
   actions :index, :show
 
+  index do
+    column "Councillor Contribution id", :id
+    column :authority
+    actions
+  end
 
 end
