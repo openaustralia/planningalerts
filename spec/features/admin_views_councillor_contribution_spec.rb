b6248113f1fe4af3a067361aed985fb299634f62
hisayohorie <hisayohorie@gmail.com>
2017-08-11 16:47:39 -0400

    Set up data for feature tests for admin councillor contribution page

--- spec/features/admin_views_councillor_contribution_spec.rb
+++ spec/features/admin_views_councillor_contribution_spec.rb
@@ -18,3 +18,34 @@ feature "Admin views a suggested councillor" do
     expect(page).to have_content "Casey City Council Mila Gilic mgilic@casey.vic.gov.au"
   end
 end
+
+feature "Admin views councillor contributions" do
+  before do
+    authority = create(:authority, full_name: "Casey City Council")
+    contributor = create(:contributor, name: "Felix Chaung", email: "example@gmail.com")
+    councillor_contribution = create(:councillor_contribution, contributor: contributor)
+
+    create(
+      :suggested_councillor,
+      name: "Mila Gilic",
+      email: "mgilic@casey.vic.gov.au",
+      authority: authority,
+      councillor_contribution: councillor_contribution
+    )
+    create(
+      :suggested_councillor,
+      name: "Susan Serey",
+      email: "sserey@.vic.gov.au",
+      authority: authority,
+      councillor_contribution: councillor_contribution
+    )
+    create(
+      :suggested_councillor,
+      name: "Rosalie Crestani",
+      email: "rcrestani@.vic.gov.au",
+      authority: authority,
+      councillor_contribution: councillor_contribution
+    )
+  end
+
+end
