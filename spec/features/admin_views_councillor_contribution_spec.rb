e90530e1dab8d731ef03e7b87b397004459b7427
hisayohorie <hisayohorie@gmail.com>
2017-08-11 17:51:30 -0400

    Moved authority in the councillor contribution dataset from suggested councillor
    To accurately reflect the association

--- spec/features/admin_views_councillor_contribution_spec.rb
+++ spec/features/admin_views_councillor_contribution_spec.rb
@@ -25,14 +25,13 @@ feature "Admin views councillor contributions" do
 
       authority = create(:authority, full_name: "Casey City Council")
       contributor = create(:contributor, name: "Felix Chaung", email: "example@gmail.com")
-      councillor_contribution = create(:councillor_contribution, contributor: contributor)
+      councillor_contribution = create(:councillor_contribution, contributor: contributor, authority: authority)
       creation_time = "2017-08-01 00:00:00 -0400"
 
       create(
         :suggested_councillor,
         name: "Mila Gilic",
         email: "mgilic@casey.vic.gov.au",
-        authority: authority,
         councillor_contribution: councillor_contribution,
         created_at: creation_time
       )
@@ -40,7 +39,6 @@ feature "Admin views councillor contributions" do
         :suggested_councillor,
         name: "Susan Serey",
         email: "sserey@.vic.gov.au",
-        authority: authority,
         councillor_contribution: councillor_contribution,
         created_at: creation_time
       )
@@ -48,7 +46,6 @@ feature "Admin views councillor contributions" do
         :suggested_councillor,
         name: "Rosalie Crestani",
         email: "rcrestani@.vic.gov.au",
-        authority: authority,
         councillor_contribution: councillor_contribution,
         created_at: creation_time
       )
