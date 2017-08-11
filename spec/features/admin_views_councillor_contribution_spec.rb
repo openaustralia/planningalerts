994bcc4a743c67ee29f2a709598f3dbc69b28e83
hisayohorie <hisayohorie@gmail.com>
2017-08-11 17:38:27 -0400

    Moved the dataset for the test inside of before :each do block
    To enable timecop.

--- spec/features/admin_views_councillor_contribution_spec.rb
+++ spec/features/admin_views_councillor_contribution_spec.rb
@@ -23,27 +23,35 @@ feature "Admin views councillor contributions" do
   before :each do
       Timecop.freeze(Time.local(2017, 8, 1))
 
-    create(
-      :suggested_councillor,
-      name: "Mila Gilic",
-      email: "mgilic@casey.vic.gov.au",
-      authority: authority,
-      councillor_contribution: councillor_contribution
-    )
-    create(
-      :suggested_councillor,
-      name: "Susan Serey",
-      email: "sserey@.vic.gov.au",
-      authority: authority,
-      councillor_contribution: councillor_contribution
-    )
-    create(
-      :suggested_councillor,
-      name: "Rosalie Crestani",
-      email: "rcrestani@.vic.gov.au",
-      authority: authority,
-      councillor_contribution: councillor_contribution
-    )
+      authority = create(:authority, full_name: "Casey City Council")
+      contributor = create(:contributor, name: "Felix Chaung", email: "example@gmail.com")
+      councillor_contribution = create(:councillor_contribution, contributor: contributor)
+      creation_time = "2017-08-01 00:00:00 -0400"
+
+      create(
+        :suggested_councillor,
+        name: "Mila Gilic",
+        email: "mgilic@casey.vic.gov.au",
+        authority: authority,
+        councillor_contribution: councillor_contribution,
+        created_at: creation_time
+      )
+      create(
+        :suggested_councillor,
+        name: "Susan Serey",
+        email: "sserey@.vic.gov.au",
+        authority: authority,
+        councillor_contribution: councillor_contribution,
+        created_at: creation_time
+      )
+      create(
+        :suggested_councillor,
+        name: "Rosalie Crestani",
+        email: "rcrestani@.vic.gov.au",
+        authority: authority,
+        councillor_contribution: councillor_contribution,
+        created_at: creation_time
+      )
   end
 
 end
