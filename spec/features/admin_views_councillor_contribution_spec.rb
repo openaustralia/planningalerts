a7a4e92f1b63690304881acaa5b86cdeb89d6759
Luke Bacon <luke@equivalentideas.com>
2017-08-15 23:13:38 +1000

    Add test for what the admin sees on the contribution show page

--- spec/features/admin_views_councillor_contribution_spec.rb
+++ spec/features/admin_views_councillor_contribution_spec.rb
@@ -55,11 +55,25 @@ feature "Admin views councillor contributions" do
     Timecop.return
   end
 
-  it "successfully" do
+  it "successfully on the index page" do
     sign_in_as_admin
 
     click_link "Councillor Contributions"
 
     expect(page).to have_content "Felix Chaung #{Time.current.strftime('%B %d, %Y %H:%M')} Casey City Council"
+  end
+
+  it "successfully with suggested councillor on the show page" do
+    sign_in_as_admin
+
+    click_link "Councillor Contributions"
+
+    click_link "View"
+
+    expect(page).to have_content "Contributor Felix Chaung"
+    expect(page).to have_content "Authority Casey City Council"
+    expect(page).to have_content "Mila Gilic mgilic@casey.vic.gov.au"
+    expect(page).to have_content "Susan Serey sserey@casey.vic.gov.au"
+    expect(page).to have_content "Rosalie Crestani rcrestani@casey.vic.gov.au"
   end
 end
