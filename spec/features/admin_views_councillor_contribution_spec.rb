dfc4a5a439e994344640bbaf8ddc1093ed240e4b
hisayohorie <hisayohorie@gmail.com>
2017-08-18 16:35:16 -0400

    Updated the email address in the data set to a complete email format
    For accuracy.

--- spec/features/admin_views_councillor_contribution_spec.rb
+++ spec/features/admin_views_councillor_contribution_spec.rb
@@ -52,7 +52,7 @@ feature "Admin views councillor contributions" do
     click_link "View"
 
     expect(page).to have_content "Felix Chaung"
-    expect(page).to have_content "example@gmail"
+    expect(page).to have_content "example@gmail.com"
     expect(page).to have_content "Casey City Council"
     expect(page).to have_content "Mila Gilic mgilic@casey.vic.gov.au"
     expect(page).to have_content "Susan Serey sserey@casey.vic.gov.au"
