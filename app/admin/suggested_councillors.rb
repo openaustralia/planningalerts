b49ab90560e4211187c10e6da8a26fc2bc4d3cb5
hisayohorie <hisayohorie@gmail.com>
2017-08-25 11:49:13 -0400

    Added columns in suggested_councillor.csv to match with popolo_csv format

--- app/admin/suggested_councillors.rb
+++ app/admin/suggested_councillors.rb
@@ -4,6 +4,17 @@ ActiveAdmin.register SuggestedCouncillor do
  filter :councillor_contribution, :collection => proc { @councillor_contribution.suggested_councillors}
   csv do
     column :name
+    column (:start_date){nil}
+    column (:end_date){nil}
+    column (:exective){nil}
+    column (:council){@councillor_contribution.authority.full_name}
+    column (:council_website){nil}
+    column (:id){nil}
     column :email
+    column (:image){nil}
+    column (:party){nil}
+    column (:source){nil}
+    column (:ward){nil}
+    column (:phone_mobile){nil}
   end
 end
