26f1ae66cae3ef41be5ddb61cac507354a54da8e
hisayohorie <hisayohorie@gmail.com>
2017-08-28 13:52:16 -0400

    Prepopulated the dataset of councillor_contribution

--- spec/controllers/councillor_contributions_controller_spec.rb
+++ spec/controllers/councillor_contributions_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe CouncillorContributionsController do
-  
+    let(:authority){create(:authority)}
+    let(:councillor_contribution){create(:councillor_contribution, authority: authority)}
+    let(:suggested_councillors){create(:suggested_councillor, councillor_contribution: councillor_contribution)}
+
+    
 end
