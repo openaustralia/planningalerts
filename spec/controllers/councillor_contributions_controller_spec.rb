ac3ee6eddbcd2626cc3b4d3f59911dc4dd588664
hisayohorie <hisayohorie@gmail.com>
2017-08-28 13:53:14 -0400

    Added the test block for csv download response

--- spec/controllers/councillor_contributions_controller_spec.rb
+++ spec/controllers/councillor_contributions_controller_spec.rb
@@ -5,5 +5,19 @@ describe CouncillorContributionsController do
     let(:councillor_contribution){create(:councillor_contribution, authority: authority)}
     let(:suggested_councillors){create(:suggested_councillor, councillor_contribution: councillor_contribution)}
 
-    
+    context "when the feature flag is on" do
+      around do |test|
+        with_modified_env CONTRIBUTE_COUNCILLORS_ENABLED: "true" do
+          test.run
+        end
+      end
+
+    describe "#show" do
+      it "download csv file" do
+        get :show, authority_id: authority.id, id: councillor_contribution.id, format: "csv"
+        expect(response.header['Content-Type']).to include'text/csv'
+        expect(response.body).to include "name,start_date,end_date,exective,council,council_website,id,email,image,party,source,ward,phone_mobile"
+      end
+    end
+  end
 end
