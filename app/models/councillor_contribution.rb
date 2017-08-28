4ae68325a8d69aedbc64d4747388fe6b71e198c8
hisayohorie <hisayohorie@gmail.com>
2017-08-28 11:35:32 -0400

    Added to_csv method in councillor contribution model
    To move the csv download logic of councillor contribution to the model
    from active admin

--- app/models/councillor_contribution.rb
+++ app/models/councillor_contribution.rb
@@ -16,4 +16,17 @@ class CouncillorContribution < ActiveRecord::Base
       "Anonymous"
     end
   end
+
+  def to_csv
+    attributes = %w{name start_date end_date exective council council_website
+      id email image party source ward phone_mobile}
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      suggested_councillors.each do |s|
+        csv << [s.name, nil, nil, nil, authority.full_name, nil, s.csv_councillor_id, s.email, nil,
+        nil, nil, nil, nil, nil]
+      end
+    end
+  end
 end
