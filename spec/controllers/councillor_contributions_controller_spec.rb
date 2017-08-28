586e48efb29107f5a23711b63bbc830408211039
hisayohorie <hisayohorie@gmail.com>
2017-08-28 16:01:15 -0400

    Added "feature flag is on" block in councillor_contributions_controller_spec.rb

--- spec/controllers/councillor_contributions_controller_spec.rb
+++ spec/controllers/councillor_contributions_controller_spec.rb
@@ -5,12 +5,12 @@ describe CouncillorContributionsController do
     let(:councillor_contribution){create(:councillor_contribution, authority: authority)}
     let(:suggested_councillors){create(:suggested_councillor, councillor_contribution: councillor_contribution)}
 
-    context "when the feature flag is on" do
-      around do |test|
-        with_modified_env CONTRIBUTE_COUNCILLORS_ENABLED: "true" do
-          test.run
-        end
+  context "when the feature flag is on" do
+    around do |test|
+      with_modified_env CONTRIBUTE_COUNCILLORS_ENABLED: "true" do
+        test.run
       end
+    end
 
     describe "#show" do
       it "download csv file" do
