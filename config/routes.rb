4742a2515a1909a093c191d201e87ff4caa78f0c
hisayohorie <hisayohorie@gmail.com>
2017-08-29 15:44:57 -0400

    Added route for council directory

--- config/routes.rb
+++ config/routes.rb
@@ -109,6 +109,7 @@ PlanningalertsApp::Application.routes.draw do
   end
 
   post "/authorities/:authority_id/councillor_contributions/new", to: "councillor_contributions#new"
+  get '/council_directory' => 'councillor_contributions#council_directory'
 
   resources :contributors, only:[:new, :create]
     get "/contributors/no_info", to: "contributors#no_contributor_info"
