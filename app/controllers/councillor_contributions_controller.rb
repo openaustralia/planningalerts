b8951be86ba803172b24a9a333f82df2331ff78f
Luke Bacon <luke@equivalentideas.com>
2017-08-30 14:00:46 +1000

    Make filename more useful
    This format with the date then authority in short-name form
    is more sortable and more succinct so it's easier to manage for the admin.

--- app/controllers/councillor_contributions_controller.rb
+++ app/controllers/councillor_contributions_controller.rb
@@ -37,7 +37,7 @@ class CouncillorContributionsController < ApplicationController
       format.csv do
         send_data(
           @councillor_contribution.to_csv,
-          filename: "councillor_contribution for #{@councillor_contribution.authority.full_name},#{@councillor_contribution.created_at}.csv",
+          filename: "#{@councillor_contribution.created_at.to_date.to_s}_#{@councillor_contribution.authority.short_name.downcase}_councillor_contribution.csv",
           content_type: Mime[:csv]
         )
       end
