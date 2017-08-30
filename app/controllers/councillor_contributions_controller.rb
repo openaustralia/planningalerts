a1602a1d2ba65961cf52f9c725988d07a7ab422a
Luke Bacon <luke@equivalentideas.com>
2017-08-30 14:14:46 +1000

    Add the contribution id to the filename for uniqueness
    I can imagine circumstances where contributions for the same council get
    made on the same day, as the admin you'd want these to have separate
    filenames so you can download and manage them cleanly.

--- app/controllers/councillor_contributions_controller.rb
+++ app/controllers/councillor_contributions_controller.rb
@@ -37,7 +37,7 @@ class CouncillorContributionsController < ApplicationController
       format.csv do
         send_data(
           @councillor_contribution.to_csv,
-          filename: "#{@councillor_contribution.created_at.to_date.to_s}_#{@councillor_contribution.authority.short_name.downcase}_councillor_contribution.csv",
+          filename: "#{@councillor_contribution.created_at.to_date.to_s}_#{@councillor_contribution.authority.short_name.downcase}_councillor_contribution_#{@councillor_contribution.id}.csv",
           content_type: Mime[:csv]
         )
       end
