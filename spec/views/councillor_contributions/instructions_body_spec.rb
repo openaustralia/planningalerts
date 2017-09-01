5789a9313450f5368c3121b707851cfe4807c749
Luke Bacon <luke@equivalentideas.com>
2017-09-01 14:55:47 +1000

    Extract the test setup to the matching context
    More consistent with how we write tests.

--- spec/views/councillor_contributions/instructions_body_spec.rb
+++ spec/views/councillor_contributions/instructions_body_spec.rb
@@ -2,12 +2,14 @@ require 'spec_helper'
 
 describe "councillor_contributions/_instructions_body" do
   context "when the authority has a website url" do
-    it "render a link to the website" do
+    before do
       assign(
         :authority,
         create(:authority, full_name: "Example Council", website_url: "https://example.nsw.gov.au")
       )
+    end
 
+    it "render a link to the website" do
       render "instructions_body"
 
       expect(rendered).to include "<a href=\"https://example.nsw.gov.au\">Example Council</a>"
@@ -15,12 +17,14 @@ describe "councillor_contributions/_instructions_body" do
   end
 
   context "when the authority does not have a website url" do
-    it "render it's name with no link" do
+    before do
       assign(
         :authority,
         create(:authority, full_name: "Example Council", website_url: nil)
       )
+    end
 
+    it "render it's name with no link" do
       render "instructions_body"
 
       expect(rendered).to_not include "<a href='https://example.nsw.gov.au'>Example Council</a>"
