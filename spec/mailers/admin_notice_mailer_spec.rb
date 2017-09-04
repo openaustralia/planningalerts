37afeeb3e7c77097ea69429fd3e13922da608441
hisayohorie <hisayohorie@gmail.com>
2017-09-04 18:46:59 -0400

    Created admin_notice_maier_spec.rb

---
+++ spec/mailers/admin_notice_mailer_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe AdminNoticeMailer do
+  it "should come from the moderator's email address" do
+  end
+
+  it "should go to the moderator email address" do
+  end
+
+  it "should contain the authority name of the councillor contribution" do
+  end
+
+  it "should contain the link to the admin show page of the councillor contribition" do
+  end
+end
