5ee2e02aceb71becb146e8523b5ec54d069d0966
hisayohorie <hisayohorie@gmail.com>
2017-09-05 12:24:06 -0400

    Changed the name of the mailer file to be more explicit

--- app/mailers/admin_notice_mailer.rb
+++
@@ -1,9 +0,0 @@
-class AdminNoticeMailer < ActionMailer::Base
-  def notice_for_councillor_contribution(councillor_contribution)
-    @councillor_contribution = councillor_contribution
-      mail(
-      to: ENV["EMAIL_MODERATOR"],
-      subject: "New councillor contribution",
-      from: ENV["EMAIL_MODERATOR"])
-    end
-end
