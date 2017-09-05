9848364917b27746a511462116311a2f2a30ecdd
hisayohorie <hisayohorie@gmail.com>
2017-09-05 12:06:20 -0400

    Refactored unit tests @mailer to mailer to reflect change in the dataset pattern

--- spec/mailers/admin_notice_mailer_spec.rb
+++ spec/mailers/admin_notice_mailer_spec.rb
@@ -6,19 +6,24 @@ describe AdminNoticeMailer do
       let(:councillor_contribution) { create(:councillor_contribution, authority: authority, id: 1) }
       let(:mailer) { AdminNoticeMailer.notice_for_councillor_contribution(councillor_contribution) }
 
-  it "should go to the moderator email address" do
-    expect(@mailer.to).to eq(["moderator@planningalerts.org.au"])
-  end
+    it "should come from the moderator's email address" do
+      expect(mailer.from).to eq(["moderator@planningalerts.org.au"])
+    end
 
-  it "should tell the moderator what the email is about" do
-    expect(@mailer.subject).to eq("New councillor contribution")
-  end
+    it "should go to the moderator email address" do
+      expect(mailer.to).to eq(["moderator@planningalerts.org.au"])
+    end
 
-  it "should contain the authority name of the councillor contribution" do
-    expect(@mailer.body.to_s).to include("Casey City Council")
-  end
+    it "should tell the moderator what the email is about" do
+      expect(mailer.subject).to eq("New councillor contribution")
+    end
 
-  it "should contain the link to the admin show page of the councillor contribition" do
-    expect(@mailer.body.to_s).to have_link("link.", href: "http://dev.planningalerts.org.au/admin/councillor_contributions/1")
+    it "should contain the authority name of the councillor contribution" do
+      expect(mailer.body.to_s).to include("Casey City Council")
+    end
+
+    it "should contain the link to the admin show page of the councillor contribition" do
+      expect(mailer.body.to_s).to have_link("link.", href: "http://dev.planningalerts.org.au/admin/councillor_contributions/1")
+    end
   end
 end
