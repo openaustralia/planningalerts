0079d21393d0f325acb3bbbf55e1ae8831e4b060
hisayohorie <hisayohorie@gmail.com>
2017-09-05 21:36:48 -0400

    Fixed indentation

--- app/mailers/councillor_contribution_notifier.rb
+++ app/mailers/councillor_contribution_notifier.rb
@@ -1,9 +1,10 @@
 class CouncillorContributionNotifier < ActionMailer::Base
   def notify(councillor_contribution)
     @councillor_contribution = councillor_contribution
-      mail(
+    mail(
       to: ENV["EMAIL_MODERATOR"],
       subject: "PlanningAlerts: New councillor contribution",
-      from: ENV["EMAIL_MODERATOR"])
-    end
+      from: ENV["EMAIL_MODERATOR"]
+    )
+  end
 end
