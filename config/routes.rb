3cd951cd30c24d884050eea17b23d6995f22d0f4
hisayohorie <hisayohorie@gmail.com>
2017-09-07 12:18:38 -0400

    Updated the route for contributor from get to post with new url

--- config/routes.rb
+++ config/routes.rb
@@ -109,7 +109,7 @@ PlanningalertsApp::Application.routes.draw do
   end
 
   post "/authorities/:authority_id/councillor_contributions/new", to: "councillor_contributions#new"
-  get "/contributors/:id/thank_you", to: "councillor_contributions#thank_you", as: :contributors_thank_you
+  post "/auhtorities/:authority_id/councillor_contributions/add_contributor", to: "councillor_contributions#add_contributor", as: :add_contributor_authority_councillor_contribution
   get "councillor_contributions/:id/contributor/new", to: "councillor_contributions#new_contributor", as: :new_contributor
   post "councillor_contributions/:id/contributor/create", to: "councillor_contributions#create_contributor", as: :create_contributor
 
