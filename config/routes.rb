eb0fba994b369b99eb126bcf2d029d307c2962ce
hisayohorie <hisayohorie@gmail.com>
2017-09-08 00:02:51 -0400

    Replaced councillor_contribution#update route to #thank_you route

--- config/routes.rb
+++ config/routes.rb
@@ -102,7 +102,7 @@ PlanningalertsApp::Application.routes.draw do
         get :per_week
       end
     end
-    resources :councillor_contributions, only:[:new, :update, :show]
+    resources :councillor_contributions, only:[:new, :show]
     collection do
       get :test_feed
     end
@@ -110,6 +110,7 @@ PlanningalertsApp::Application.routes.draw do
 
   post "/authorities/:authority_id/councillor_contributions/new", to: "councillor_contributions#new"
   post "/auhtorities/:authority_id/councillor_contributions/add_contributor", to: "councillor_contributions#add_contributor", as: :add_contributor_authority_councillor_contribution
+  post "/authorities/:authority_id/councillor_contributions/:id/thank_you", to: "councillor_contributions#thank_you", as: :authority_councillor_contribution_thank_you
 
   namespace :atdis do
     get :test
