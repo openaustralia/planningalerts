fd9d59883f142f2677dfd41a4b7c462f3e0107a8
hisayohorie <hisayohorie@gmail.com>
2017-09-14 12:18:55 -0400

    Deleted :edit and :update action from admin/councillor_contribution

--- app/admin/councillor_contributions.rb
+++ app/admin/councillor_contributions.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register CouncillorContribution do
-  actions :index, :show, :edit, :update
+  actions :index, :show
 
   index do
     column(:contributor) { |contribution| contribution.attribution }
