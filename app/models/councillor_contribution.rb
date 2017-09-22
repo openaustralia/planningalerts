4b0e97b9ecb62a6094a7f9dc3d31ba6a7031b448
Luke Bacon <luke@equivalentideas.com>
2017-09-22 13:12:26 +1000

    Include the council website and source in the CSV
    Fixes #1251

--- app/models/councillor_contribution.rb
+++ app/models/councillor_contribution.rb
@@ -30,8 +30,8 @@ class CouncillorContribution < ActiveRecord::Base
 
       suggested_councillors.each do |s|
         csv << [
-          s.name, nil, nil, nil, authority.full_name, nil,
-          s.popolo_id, s.email, nil, nil, nil, nil, nil, nil
+          s.name, nil, nil, nil, authority.full_name, authority.website_url,
+          s.popolo_id, s.email, nil, nil, nil, source, nil, nil
         ]
       end
     end
