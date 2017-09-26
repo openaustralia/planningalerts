fdee68f4a0c505e9c46bef8bbe67c31e22019fb4
hisayohorie <hisayohorie@gmail.com>
2017-09-25 23:10:15 -0400

    Fixed indentation

--- app/admin/councillor_contributions.rb
+++ app/admin/councillor_contributions.rb
@@ -11,10 +11,10 @@ ActiveAdmin.register CouncillorContribution do
   end
 
   show title: proc {|resource| "Councillor Contribution for #{resource.authority.full_name}, #{resource.created_at.strftime('%B %d, %Y')}"} do
-    attributes_table do
-      row(:contributor) { |contribution| contribution.attribution(with_email: true) }
-      row :source
-    end
+  attributes_table do
+    row(:contributor) { |contribution| contribution.attribution(with_email: true) }
+    row :source
+  end
 
   h3 "Suggested Councillors"
   table_for resource.suggested_councillors, class: "index_table" do
