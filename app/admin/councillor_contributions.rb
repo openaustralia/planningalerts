da831225197304d1e980dcf999763c9213f55beb
Luke Bacon <luke@equivalentideas.com>
2017-09-28 11:08:57 +1000

    Fix indentation

--- app/admin/councillor_contributions.rb
+++ app/admin/councillor_contributions.rb
@@ -11,17 +11,17 @@ ActiveAdmin.register CouncillorContribution do
   end
 
   show title: proc {|resource| "Councillor Contribution for #{resource.authority.full_name}, #{resource.created_at.strftime('%B %d, %Y')}"} do
-  attributes_table do
-    row(:contributor) { |contribution| contribution.attribution(with_email: true) }
-    row :source
-  end
+    attributes_table do
+      row(:contributor) { |contribution| contribution.attribution(with_email: true) }
+      row :source
+    end
 
-  h3 "Suggested Councillors"
-  table_for resource.suggested_councillors, class: "index_table" do
-    column :name
-    column :email
+    h3 "Suggested Councillors"
+    table_for resource.suggested_councillors, class: "index_table" do
+      column :name
+      column :email
+    end
   end
-end
 
   action_item :download, only: [:show] do
     link_to "Download the suggested councillors CSV", authority_councillor_contribution_path(resource.authority, resource.id, format: :csv)
