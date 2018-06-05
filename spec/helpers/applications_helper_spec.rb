4c4099c1b0160c5932ac26d7e3b3ff1d4e63a396
Matthew Landauer <mlandauer@gmail.com>
2018-06-05 17:28:40 +1000

    Fix up tests - we don't want to set google api credentials in the test environment because everything will want to use those then

--- spec/helpers/applications_helper_spec.rb
+++ spec/helpers/applications_helper_spec.rb
@@ -200,9 +200,12 @@ describe ApplicationsHelper do
     describe "static maps" do
       before :each do
         allow(@application).to receive(:address).and_return("Foo Road, NSW")
+        allow(ENV).to receive(:[]).with("GOOGLE_MAPS_CLIENT_ID").and_return("abc")
+        allow(ENV).to receive(:[]).with("GOOGLE_MAPS_CRYPTOGRAPHIC_KEY").and_return("123456789012345678901234567=")
       end
 
       it "should generate a static google map api image" do
+
         expect(helper.google_static_map(@application, size: "350x200", zoom: 16)).to eq(
           "<img alt=\"Map of Foo Road, NSW\" src=\"https://maps.googleapis.com/maps/api/staticmap?zoom=16&size=350x200&maptype=roadmap&markers=color:red%7C1.0,2.0&client=abc&signature=UT70D_1Q-u5dLxy5pENnThfA6cI=\" width=\"350\" height=\"200\" />"
         )
@@ -215,6 +218,8 @@ describe ApplicationsHelper do
     describe "static streetview" do
       before :each do
         allow(@application).to receive(:address).and_return("Foo Road, NSW")
+        allow(ENV).to receive(:[]).with("GOOGLE_MAPS_CLIENT_ID").and_return("abc")
+        allow(ENV).to receive(:[]).with("GOOGLE_MAPS_CRYPTOGRAPHIC_KEY").and_return("123456789012345678901234567=")
       end
 
       it "should generate a static google streetview image" do
