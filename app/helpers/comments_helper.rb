144f3c658381321b8000fc818173b06532fd163b
Matthew Landauer <mlandauer@gmail.com>
2018-11-24 18:02:18 +1100

    Add comment about html safety and add pragma for rubocop to ignore

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -3,7 +3,13 @@
 module CommentsHelper
   def comment_as_html(text)
     # duplicate string to avoid "can't modify frozen String"
-    Sanitize.clean(simple_format(auto_link(text.dup)), Sanitize::Config::BASIC).html_safe
+    cleaned = Sanitize.clean(simple_format(auto_link(text.dup)), Sanitize::Config::BASIC)
+    # We're trusting that the sanitize library does the right thing here. We
+    # kind of have to. It's returning some allowed html. So, we have to mark
+    # it as safe
+    # rubocop:disable Rails/OutputSafety
+    cleaned.html_safe
+    # rubocop:enable Rails/OutputSafety
   end
 
   def comment_path(comment)
