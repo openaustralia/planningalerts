f329f4f0211d2b89fc45f9a7490af72db26f5759
Matthew Landauer <mlandauer@gmail.com>
2018-12-05 09:58:02 +1100

    Inline a couple of files for simplicity

--- app/assets/javascripts/maps/preview.js
+++
@@ -1,8 +0,0 @@
-function preview(centre_lat, centre_lng, radius_in_metres) {
-  map.removeAllPolylines();
-  r = new mxn.Radius(new mxn.LatLonPoint(centre_lat, centre_lng), 10);
-  p = r.getPolyline(radius_in_metres / 1000, "#FF0000");
-  p.setWidth(0);
-  p.setOpacity(0.2);
-  map.addPolyline(p);
-};
