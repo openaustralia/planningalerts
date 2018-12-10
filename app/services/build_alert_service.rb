c99a9c193c9e136e2c239fe63d5f5a2825fc1b29
Matthew Landauer <mlandauer@gmail.com>
2018-12-11 05:43:40 +1100

    Inline methods for less verbosity

--- app/services/build_alert_service.rb
+++ app/services/build_alert_service.rb
@@ -15,32 +15,18 @@ class BuildAlertService
     # Ensures the address is normalised into a consistent form
     alert.geocode_from_address
 
-    preexisting_matching_alert ? parse_for_preexisting_alert_states : alert
-  end
-
-  private
-
-  def parse_for_preexisting_alert_states
-    if preexisting_alert_is_confirmed_but_unsubscribed?
+    if !preexisting_matching_alert
       alert
-    elsif preexisting_alert_is_confirmed_and_subscribed?
+    elsif preexisting_matching_alert.confirmed? && preexisting_matching_alert.unsubscribed?
+      alert
+    elsif preexisting_matching_alert.confirmed? && !preexisting_matching_alert.unsubscribed?
       send_notice_to_existing_active_alert_owner_and_return
-    elsif preexisting_alert_is_unconfirmed?
+    elsif !preexisting_matching_alert.confirmed?
       resend_original_confirmation_email_and_return
     end
   end
 
-  def preexisting_alert_is_confirmed_but_unsubscribed?
-    preexisting_matching_alert.confirmed? && preexisting_matching_alert.unsubscribed?
-  end
-
-  def preexisting_alert_is_confirmed_and_subscribed?
-    preexisting_matching_alert.confirmed? && !preexisting_matching_alert.unsubscribed?
-  end
-
-  def preexisting_alert_is_unconfirmed?
-    !preexisting_matching_alert.confirmed?
-  end
+  private
 
   def preexisting_matching_alert
     Alert.find_by(email: alert.email, address: alert.address)
