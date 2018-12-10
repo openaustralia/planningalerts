615a6207f18dfcb0921f651dccd3834eefc29245
Matthew Landauer <mlandauer@gmail.com>
2018-12-11 05:44:55 +1100

    Rename method

--- app/services/build_alert_service.rb
+++ app/services/build_alert_service.rb
@@ -15,33 +15,33 @@ class BuildAlertService
     # Ensures the address is normalised into a consistent form
     alert.geocode_from_address
 
-    if !preexisting_matching_alert
+    if !preexisting_alert
       alert
-    elsif preexisting_matching_alert.confirmed? && preexisting_matching_alert.unsubscribed?
+    elsif preexisting_alert.confirmed? && preexisting_alert.unsubscribed?
       alert
-    elsif preexisting_matching_alert.confirmed? && !preexisting_matching_alert.unsubscribed?
+    elsif preexisting_alert.confirmed? && !preexisting_alert.unsubscribed?
       send_notice_to_existing_active_alert_owner_and_return
-    elsif !preexisting_matching_alert.confirmed?
+    elsif !preexisting_alert.confirmed?
       resend_original_confirmation_email_and_return
     end
   end
 
   private
 
-  def preexisting_matching_alert
+  def preexisting_alert
     Alert.find_by(email: alert.email, address: alert.address)
   end
 
   def send_notice_to_existing_active_alert_owner_and_return
     AlertNotifier.new_signup_attempt_notice(
-      preexisting_matching_alert
+      preexisting_alert
     ).deliver_later
 
     nil
   end
 
   def resend_original_confirmation_email_and_return
-    preexisting_matching_alert.send_confirmation_email
+    preexisting_alert.send_confirmation_email
 
     nil
   end
