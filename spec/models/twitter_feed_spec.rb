f1e940bbf060174d96c1cf5d9e3805429d7e7c86
Matthew Landauer <mlandauer@gmail.com>
2018-12-17 08:07:17 +1100

    And test that something is logged

--- spec/models/twitter_feed_spec.rb
+++ spec/models/twitter_feed_spec.rb
@@ -4,6 +4,12 @@ require "spec_helper"
 
 describe TwitterFeed do
   describe "#items" do
+    let(:silent_logger) do
+      logger = double
+      allow(logger).to receive(:error)
+      logger
+    end
+
     context "with no twitter credentials" do
       it "should return nothing" do
         expect(TwitterFeed.new("planningalerts").items).to be_empty
@@ -22,24 +28,40 @@ describe TwitterFeed do
         end
       end
 
-      it "should return the last 2 tweets" do
-        items = VCR.use_cassette("twitter") do
-          TwitterFeed.new("planningalerts").items
+      context "for a valid twitter user" do
+        let(:twitter_user) { "planningalerts" }
+
+        it "should return the last 2 tweets" do
+          items = VCR.use_cassette("twitter") do
+            TwitterFeed.new(twitter_user).items
+          end
+          expect(items.count).to eq 2
+          expect(items[0].title).to eq "@wheelyweb definitely agree that things would be improved by showing a selection of trending applications in differ… https://t.co/z8XDZOz7kK"
+          expect(items[0].date).to eq Time.zone.parse("2018-11-21 23:51:45 UTC")
+          expect(items[0].link).to eq "https://twitter.com/planningalerts/status/1065392354255749120"
+          expect(items[1].title).to eq "@wheelyweb the focus of PlanningAlerts is still very much on finding out what is happening locally near you. So, we… https://t.co/kXoTcuBAIy"
+          expect(items[1].date).to eq Time.zone.parse("2018-11-21 23:46:57 UTC")
+          expect(items[1].link).to eq "https://twitter.com/planningalerts/status/1065391145876828160"
         end
-        expect(items.count).to eq 2
-        expect(items[0].title).to eq "@wheelyweb definitely agree that things would be improved by showing a selection of trending applications in differ… https://t.co/z8XDZOz7kK"
-        expect(items[0].date).to eq Time.zone.parse("2018-11-21 23:51:45 UTC")
-        expect(items[0].link).to eq "https://twitter.com/planningalerts/status/1065392354255749120"
-        expect(items[1].title).to eq "@wheelyweb the focus of PlanningAlerts is still very much on finding out what is happening locally near you. So, we… https://t.co/kXoTcuBAIy"
-        expect(items[1].date).to eq Time.zone.parse("2018-11-21 23:46:57 UTC")
-        expect(items[1].link).to eq "https://twitter.com/planningalerts/status/1065391145876828160"
       end
 
-      it "should return nothing for an invalid user" do
-        items = VCR.use_cassette("twitter") do
-          TwitterFeed.new("abc768a").items
+      context "for an invalid twitter user" do
+        let(:twitter_user) { "abc768a" }
+
+        it "should return nothing" do
+          items = VCR.use_cassette("twitter") do
+            TwitterFeed.new(twitter_user, silent_logger).items
+          end
+          expect(items).to be_empty
         end
-        expect(items).to be_empty
+
+        it "should log an error" do
+          logger = double
+          expect(logger).to receive(:error).with("while accessing twitter API: Sorry, that page does not exist.")
+          VCR.use_cassette("twitter") do
+            TwitterFeed.new(twitter_user, logger).items
+          end
+        end
       end
     end
   end
