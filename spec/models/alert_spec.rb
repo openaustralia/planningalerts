d400f58a65e407f23f86be24a8403b38bea38f13
Matthew Landauer <mlandauer@gmail.com>
2018-12-18 07:16:54 +1100

    Add a test for the queueing up of alerts

--- spec/models/alert_spec.rb
+++ spec/models/alert_spec.rb
@@ -672,4 +672,46 @@ describe Alert do
       end
     end
   end
+
+  describe ".queue_up_alerts_for_next_day" do
+    context "with no active alerts" do
+      it "should log some useful messages" do
+        logger = double
+        expect(logger).to receive(:info).with("Checking 0 active alerts")
+        expect(logger).to receive(:info).with("Splitting mailing for the next 24 hours into batches of size 100 roughly every 1440 minutes")
+        expect(logger).to receive(:info).with("Mailing jobs for the next 24 hours queued")
+        Alert.queue_up_alerts_for_next_day(logger)
+      end
+    end
+
+    context "with two confirmed alerts" do
+      let(:alert1) { create(:confirmed_alert) }
+      let(:alert2) { create(:confirmed_alert) }
+      before(:each) do
+        alert1
+        alert2
+      end
+
+      it "should log some messages" do
+        logger = double
+        expect(logger).to receive(:info).with("Checking 2 active alerts")
+        # TODO: The batch period is not optimal right now
+        expect(logger).to receive(:info).with("Splitting mailing for the next 24 hours into batches of size 1 roughly every 480 minutes")
+        expect(logger).to receive(:info).with("Mailing jobs for the next 24 hours queued")
+        Alert.queue_up_alerts_for_next_day(logger, 1)
+      end
+
+      it "should queue up batches" do
+        # Silent logger
+        logger = double
+        allow(logger).to receive(:info)
+
+        alert = double
+        expect(Alert).to receive(:delay).and_return(alert).twice
+        expect(alert).to receive(:process_alerts).with([alert1.id])
+        expect(alert).to receive(:process_alerts).with([alert2.id])
+        Alert.queue_up_alerts_for_next_day(logger, 1)
+      end
+    end
+  end
 end
