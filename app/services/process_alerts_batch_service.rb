c6d8f9b1a4908c6944b05055bfb6fa28bf42990c
Matthew Landauer <mlandauer@gmail.com>
2018-12-19 08:56:41 +1100

    Also record no_replies in email batch

--- app/services/process_alerts_batch_service.rb
+++ app/services/process_alerts_batch_service.rb
@@ -12,20 +12,26 @@ class ProcessAlertsBatchService < ApplicationService
     total_no_emails = 0
     total_no_applications = 0
     total_no_comments = 0
+    total_no_replies = 0
     Alert.find(alert_ids).each do |alert|
-      no_emails, no_applications, no_comments = ProcessAlertService.call(alert: alert)
+      no_emails, no_applications, no_comments, no_replies = ProcessAlertService.call(alert: alert)
 
       total_no_emails += no_emails
       total_no_applications += no_applications
       total_no_comments += no_comments
+      total_no_replies += no_replies
     end
 
     # Update statistics. Updating the Stat at the end of each mail run has the advantage of not continiously invalidating
     # page caches during mail runs.
     Stat.emails_sent += total_no_emails
     Stat.applications_sent += total_no_applications
-    EmailBatch.create!(no_emails: total_no_emails, no_applications: total_no_applications,
-                       no_comments: total_no_comments)
+    EmailBatch.create!(
+      no_emails: total_no_emails,
+      no_applications: total_no_applications,
+      no_comments: total_no_comments,
+      no_replies: total_no_replies
+    )
   end
 
   private
--- spec/services/process_alerts_batch_service_spec.rb
+++ spec/services/process_alerts_batch_service_spec.rb
@@ -9,13 +9,13 @@ describe ProcessAlertsBatchService do
     let(:alert3) { create(:confirmed_alert) }
 
     before(:each) do
-      allow(ProcessAlertService).to receive(:call).with(alert: alert1).and_return([1, 5, 1, 0])
+      allow(ProcessAlertService).to receive(:call).with(alert: alert1).and_return([1, 5, 1, 1])
       allow(ProcessAlertService).to receive(:call).with(alert: alert2).and_return([1, 3, 2, 0])
       allow(ProcessAlertService).to receive(:call).with(alert: alert3).and_return([0, 0, 0, 0])
     end
 
     it "should process each individual alert" do
-      expect(ProcessAlertService).to receive(:call).with(alert: alert1).and_return([1, 5, 1, 0])
+      expect(ProcessAlertService).to receive(:call).with(alert: alert1).and_return([1, 5, 1, 1])
       expect(ProcessAlertService).to receive(:call).with(alert: alert2).and_return([1, 3, 2, 0])
       expect(ProcessAlertService).to receive(:call).with(alert: alert3).and_return([0, 0, 0, 0])
       allow(Alert).to receive(:find).with([alert1.id, alert2.id, alert3.id]).and_return([alert1, alert2, alert3])
@@ -31,6 +31,7 @@ describe ProcessAlertsBatchService do
       expect(batch.no_emails).to eq 2
       expect(batch.no_applications).to eq 8
       expect(batch.no_comments).to eq 3
+      expect(batch.no_replies).to eq 1
     end
 
     it "should increment the global stats" do
