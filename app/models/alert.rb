4db57a4c298d220555dceb020341283de86b9ded
Matthew Landauer <mlandauer@gmail.com>
2018-12-18 19:18:05 +1100

    Move method to its own service

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -115,29 +115,8 @@ class Alert < ApplicationRecord
     [applications.size, comments.size, replies.size]
   end
 
-  # TODO: Also include no_replies in stats and return
-  # TODO: Move to its own service class
   def self.process_alerts(alert_ids)
-    # Only send alerts to confirmed users
-    total_no_emails = 0
-    total_no_applications = 0
-    total_no_comments = 0
-    Alert.find(alert_ids).each do |alert|
-      no_applications, no_comments = alert.process!
-      next if no_applications.zero? && no_comments.zero?
-
-      total_no_applications += no_applications
-      total_no_comments += no_comments
-      total_no_emails += 1
-    end
-
-    # Update statistics. Updating the Stat at the end of each mail run has the advantage of not continiously invalidating
-    # page caches during mail runs.
-    Stat.emails_sent += total_no_emails
-    Stat.applications_sent += total_no_applications
-    EmailBatch.create!(no_emails: total_no_emails, no_applications: total_no_applications,
-                       no_comments: total_no_comments)
-    [total_no_emails, total_no_applications, total_no_comments]
+    ProcessAlertsService.new(alert_ids: alert_ids).call
   end
 
   def geocode_from_address
---
+++ app/services/process_alerts_service.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class ProcessAlertsService
+  def initialize(alert_ids:)
+    @alert_ids = alert_ids
+  end
+
+  # TODO: Also include no_replies in stats and return
+  def call
+    # Only send alerts to confirmed users
+    total_no_emails = 0
+    total_no_applications = 0
+    total_no_comments = 0
+    Alert.find(alert_ids).each do |alert|
+      no_applications, no_comments = alert.process!
+      next if no_applications.zero? && no_comments.zero?
+
+      total_no_applications += no_applications
+      total_no_comments += no_comments
+      total_no_emails += 1
+    end
+
+    # Update statistics. Updating the Stat at the end of each mail run has the advantage of not continiously invalidating
+    # page caches during mail runs.
+    Stat.emails_sent += total_no_emails
+    Stat.applications_sent += total_no_applications
+    EmailBatch.create!(no_emails: total_no_emails, no_applications: total_no_applications,
+                       no_comments: total_no_comments)
+    [total_no_emails, total_no_applications, total_no_comments]
+  end
+
+  private
+
+  attr_reader :alert_ids
+end
