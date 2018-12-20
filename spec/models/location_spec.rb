6d0a7908fa027abf35a0473b74c44bfc39da10c6
Matthew Landauer <mlandauer@gmail.com>
2018-12-20 17:01:19 +1100

    Add test for distance_to

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -3,6 +3,21 @@
 require File.dirname(__FILE__) + "/../spec_helper"
 
 describe "Location" do
+  describe "#distance_to" do
+    it "should return results consistent with endpoint method" do
+      loc1 = Location.from_lat_lng(-33.772609, 150.624263)
+      # 500 metres NE
+      loc2 = loc1.endpoint(45, 500)
+      expect(loc1.distance_to(loc2)).to be_within(0.1).of(500)
+    end
+
+    it "should return a known distance" do
+      loc1 = Location.from_lat_lng(-33.772609, 150.624263)
+      loc2 = Location.from_lat_lng(-33.772, 150.624)
+      expect(loc1.distance_to(loc2)).to be_within(0.1).of(72)
+    end
+  end
+
   describe "valid location" do
     before :each do
       @result = double(all: [double(country_code: "AU", lat: -33.772609, lng: 150.624263, accuracy: 6, city: "Glenbrook", state: "NSW", zip: "2773", full_address: "24 Bruce Road, Glenbrook, NSW 2773, Australia")])
