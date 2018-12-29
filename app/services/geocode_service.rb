944fb2e4ad37d0562f0c756bbf22e02836657d0c
Matthew Landauer <mlandauer@gmail.com>
2018-12-29 13:48:58 +1100

    Also record mappify results

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -6,13 +6,25 @@ class GeocodeService < ApplicationService
   end
 
   def call
-    geocode_query = GeocodeQuery.create!(query: address)
     google_result = GoogleGeocodeService.call(address)
+    mappify_result = MappifyGeocodeService.call(address)
+
+    geocode_query = GeocodeQuery.create!(query: address)
     GeocodeResult.create!(
-      geocoder: "google",
-      lat: google_result.top.lat,
-      lng: google_result.top.lng,
-      geocode_query_id: geocode_query.id
+      [
+        {
+          geocoder: "google",
+          lat: google_result.top.lat,
+          lng: google_result.top.lng,
+          geocode_query_id: geocode_query.id
+        },
+        {
+          geocoder: "mappify",
+          lat: mappify_result.top.lat,
+          lng: mappify_result.top.lng,
+          geocode_query_id: geocode_query.id
+        }
+      ]
     )
     google_result
   end
--- spec/services/geocode_service_spec.rb
+++ spec/services/geocode_service_spec.rb
@@ -11,8 +11,16 @@ describe GeocodeService do
       nil
     )
   end
+  let(:mappify_result) do
+    GeocoderResults.new(
+      [double(lat: 1.7, lng: 2.3)],
+      true,
+      nil
+    )
+  end
   before(:each) do
     allow(GoogleGeocodeService).to receive(:call).with(address).and_return(google_result)
+    allow(MappifyGeocodeService).to receive(:call).with(address).and_return(mappify_result)
   end
 
   it "should delegate the result to GoogleGeocodeService" do
@@ -28,11 +36,25 @@ describe GeocodeService do
 
   it "should write the top result of the google geocoder to the database" do
     GeocodeService.call(address)
-    expect(GeocodeResult.count).to eq 1
-    geocode_result = GeocodeResult.first
+    expect(GeocodeResult.where(geocoder: "google").count).to eq 1
+    geocode_result = GeocodeResult.where(geocoder: "google").first
     expect(geocode_result.geocoder).to eq "google"
     expect(geocode_result.lat).to eq google_result.top.lat
     expect(geocode_result.lng).to eq google_result.top.lng
     expect(geocode_result.geocode_query.query).to eq address
   end
+
+  it "should write the top result of the mappify geocoder to the database" do
+    GeocodeService.call(address)
+    expect(GeocodeResult.where(geocoder: "mappify").count).to eq 1
+    geocode_result = GeocodeResult.where(geocoder: "mappify").first
+    expect(geocode_result.geocoder).to eq "mappify"
+    expect(geocode_result.lat).to eq mappify_result.top.lat
+    expect(geocode_result.lng).to eq mappify_result.top.lng
+    expect(geocode_result.geocode_query.query).to eq address
+  end
+
+  pending "should record results even if one of the geocoders errors"
+
+  pending "should only record the results if the geocoders differ by some threshold"
 end
