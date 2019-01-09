2d6a4fa2861c86bf50654743b36521b0f3c51324
Matthew Landauer <mlandauer@gmail.com>
2019-01-10 09:34:36 +1100

    Inline methods

--- app/services/google_geocode_service.rb
+++ app/services/google_geocode_service.rb
@@ -6,12 +6,55 @@ class GoogleGeocodeService < ApplicationService
   end
 
   def call
-    geo_loc = call_api
+    params = {
+      address: address,
+      key: ENV["GOOGLE_MAPS_SERVER_KEY"],
+      region: "au",
+      sensor: false
+    }
+    response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?" + params.to_query)
 
-    all = geo_loc[:all].find_all { |g| g[:country_code] == "AU" }
-    all_converted = all.map { |g| convert_to_geocoded_location(g) }
+    status = response.parsed_response["status"]
+    # TODO: Raise a proper error class here
+    raise "An error #{status}" unless %w[OK ZERO_RESULTS INVALID_REQUEST].include?(status)
 
-    if !geo_loc[:success]
+    success = (status == "OK")
+    results = response.parsed_response["results"]
+    all = []
+    results.each do |result|
+      components = result["address_components"]
+      city = components.find { |c| c["types"].include?("locality") }
+      state = components.find { |c| c["types"].include?("administrative_area_level_1") }
+      zip = components.find { |c| c["types"].include?("postal_code") }
+      country = components.find { |c| c["types"].include?("country") }
+      # Even though we've biased the results towards au by using region: "au",
+      # Google can still return results outside our country of interest. So,
+      # test for this and ignore those
+      next if country.nil? || country["short_name"] != "AU"
+
+      all << {
+        lat: result["geometry"]["location"]["lat"],
+        lng: result["geometry"]["location"]["lng"],
+        city: (city["long_name"] if city),
+        state: (state["short_name"] if state),
+        zip: (zip["long_name"] if zip),
+        full_address: result["formatted_address"],
+        accuracy: ACCURACY[result["geometry"]["location_type"]]
+      }
+    end
+
+    all_converted = all.map do |g|
+      GeocodedLocation.new(
+        lat: g[:lat],
+        lng: g[:lng],
+        suburb: g[:city],
+        state: g[:state],
+        postcode: g[:zip],
+        full_address: g[:full_address].sub(", Australia", "")
+      )
+    end
+
+    if !success
       error = if address == ""
                 "Please enter a street address"
               else
@@ -35,50 +78,4 @@ class GoogleGeocodeService < ApplicationService
     "GEOMETRIC_CENTER" => 5,
     "APPROXIMATE" => 4
   }.freeze
-
-  def call_api
-    params = {
-      address: address,
-      key: ENV["GOOGLE_MAPS_SERVER_KEY"],
-      region: "au",
-      sensor: false
-    }
-    response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?" + params.to_query)
-
-    status = response.parsed_response["status"]
-    # TODO: Raise a proper error class here
-    raise "An error #{status}" unless %w[OK ZERO_RESULTS INVALID_REQUEST].include?(status)
-
-    success = (status == "OK")
-    results = response.parsed_response["results"]
-    all = results.map do |result|
-      components = result["address_components"]
-      city = components.find { |c| c["types"].include?("locality") }
-      state = components.find { |c| c["types"].include?("administrative_area_level_1") }
-      zip = components.find { |c| c["types"].include?("postal_code") }
-      country = components.find { |c| c["types"].include?("country") }
-      {
-        lat: result["geometry"]["location"]["lat"],
-        lng: result["geometry"]["location"]["lng"],
-        city: (city["long_name"] if city),
-        state: (state["short_name"] if state),
-        zip: (zip["long_name"] if zip),
-        full_address: result["formatted_address"],
-        country_code: (country["short_name"] if country),
-        accuracy: ACCURACY[result["geometry"]["location_type"]]
-      }
-    end
-    { all: all, success: success }
-  end
-
-  def convert_to_geocoded_location(geo_loc)
-    GeocodedLocation.new(
-      lat: geo_loc[:lat],
-      lng: geo_loc[:lng],
-      suburb: geo_loc[:city],
-      state: geo_loc[:state],
-      postcode: geo_loc[:zip],
-      full_address: geo_loc[:full_address].sub(", Australia", "")
-    )
-  end
 end
