a424b6b017b1363a907dbcba102216779ee527d6
Matthew Landauer <mlandauer@gmail.com>
2019-01-14 06:15:51 +1100

    Being very long-winded about all of this

--- app/controllers/admin/site_settings_controller.rb
+++ app/controllers/admin/site_settings_controller.rb
@@ -3,8 +3,17 @@
 module Admin
   class SiteSettingsController < ApplicationController
     def update
-      SiteSetting.set(params[:site_setting].permit!)
+      SiteSetting.set(site_setting)
       redirect_to admin_dashboard_url, notice: "Site settings updated"
+    end
+
+    private
+
+    def site_setting
+      params[:site_setting].permit(
+        :streetview_in_emails_enabled,
+        :streetview_in_app_enabled
+      ).to_h.symbolize_keys
     end
   end
 end
