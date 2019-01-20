79980e6608054e76e1a2fd9b80182ae4182928ff
Matthew Landauer <mlandauer@gmail.com>
2019-01-21 10:12:43 +1100

    Use "STAGE" env variable to set redis namespace for sidekiq
    Because both staging and production have the "production" environment. 
    Duh!

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,13 +3,13 @@ require 'sidekiq'
 Sidekiq.configure_server do |config|
   config.redis = {
     url: "redis://localhost:6379/0",
-    namespace: "pa_#{Rails.env}"
+    namespace: "pa_#{ENV['STAGE']}"
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
     url: "redis://localhost:6379/0",
-    namespace: "pa_#{Rails.env}"
+    namespace: "pa_#{ENV['STAGE']}"
   }
 end
