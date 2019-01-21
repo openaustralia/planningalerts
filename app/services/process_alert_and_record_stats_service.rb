8258852aedd629f12039088a591b8c5f66f53b81
Matthew Landauer <mlandauer@gmail.com>
2019-01-21 11:34:48 +1100

    Add TODOs

--- app/services/process_alert_and_record_stats_service.rb
+++ app/services/process_alert_and_record_stats_service.rb
@@ -12,10 +12,12 @@ class ProcessAlertAndRecordStatsService < ApplicationService
     alert = Alert.find(alert_id)
     no_emails, no_applications, no_comments, no_replies = ProcessAlertService.call(alert: alert)
 
-    # Update statistics. Updating the Stat at the end of each mail run has the advantage of not continiously invalidating
-    # page caches during mail runs.
+    # Update statistics
+    # TODO: Deal with concurrency issues with the way the stats are beign updated
+    # TODO: Figure out the impact on caching updating this so regularly now
     Stat.emails_sent += no_emails
     Stat.applications_sent += no_applications
+    # TODO: Rename EmailBatch as we're not using batches anymore
     EmailBatch.create!(
       no_emails: no_emails,
       no_applications: no_applications,
