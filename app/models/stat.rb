cb9eee0ee764740ee87ee3dc0df16bf29409c662
Matthew Landauer <mlandauer@gmail.com>
2019-01-21 12:02:06 +1100

    Add new methods for increment stats directly

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -9,12 +9,20 @@ class Stat < ApplicationRecord
     set_value_for_key("applications_sent", value)
   end
 
+  def self.increment_applications_sent(value)
+    increment_value_for_key("applications_sent", value)
+  end
+
   def self.emails_sent
     get_value_for_key("emails_sent")
   end
 
   def self.emails_sent=(value)
     set_value_for_key("emails_sent", value)
+  end
+
+  def self.increment_emails_sent(value)
+    increment_value_for_key("emails_sent", value)
   end
 
   def self.set_value_for_key(key, value)
@@ -25,6 +33,12 @@ class Stat < ApplicationRecord
 
   def self.get_value_for_key(key)
     record_for_key(key).value
+  end
+
+  def self.increment_value_for_key(key, value)
+    # rubocop:disable Rails/SkipsModelValidations
+    Stat.update_counters(record_for_key(key).id, value: value)
+    # rubocop:enable Rails/SkipsModelValidations
   end
 
   def self.record_for_key(key)
--- spec/models/stat_spec.rb
+++ spec/models/stat_spec.rb
@@ -19,6 +19,24 @@ describe Stat do
     end
   end
 
+  describe ".increment_applications_sent" do
+    it "should increment the stat atomically" do
+      Stat.delete_all
+      Stat.create!(key: "applications_sent", value: 4)
+      Stat.increment_applications_sent(3)
+      expect(Stat.applications_sent).to eq 7
+    end
+  end
+
+  describe ".increment_emails_sent" do
+    it "should increment the stat atomically" do
+      Stat.delete_all
+      Stat.create!(key: "emails_sent", value: 14)
+      Stat.increment_emails_sent(10)
+      expect(Stat.emails_sent).to eq 24
+    end
+  end
+
   describe ".emails_sent" do
     it "should read in the number of emails that have been sent" do
       Stat.delete_all
