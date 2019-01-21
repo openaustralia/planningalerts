28f3ee9877fec54421d46aa0a54ceb7d0a1cf0c2
Matthew Landauer <mlandauer@gmail.com>
2019-01-21 11:17:32 +1100

    Change tests so that we're only ever calling with one alert

--- spec/services/process_alert_and_record_stats_service_spec.rb
+++ spec/services/process_alert_and_record_stats_service_spec.rb
@@ -4,33 +4,27 @@ require "spec_helper"
 
 describe ProcessAlertAndRecordStatsService do
   context "with two confirmed alerts" do
-    let(:alert1) { create(:confirmed_alert) }
-    let(:alert2) { create(:confirmed_alert) }
-    let(:alert3) { create(:confirmed_alert) }
+    let(:alert) { create(:confirmed_alert) }
 
     before(:each) do
-      allow(ProcessAlertService).to receive(:call).with(alert: alert1).and_return([1, 5, 1, 1])
-      allow(ProcessAlertService).to receive(:call).with(alert: alert2).and_return([1, 3, 2, 0])
-      allow(ProcessAlertService).to receive(:call).with(alert: alert3).and_return([0, 0, 0, 0])
+      allow(ProcessAlertService).to receive(:call).with(alert: alert).and_return([1, 5, 1, 1])
     end
 
-    it "should process each individual alert" do
-      expect(ProcessAlertService).to receive(:call).with(alert: alert1).and_return([1, 5, 1, 1])
-      expect(ProcessAlertService).to receive(:call).with(alert: alert2).and_return([1, 3, 2, 0])
-      expect(ProcessAlertService).to receive(:call).with(alert: alert3).and_return([0, 0, 0, 0])
-      allow(Alert).to receive(:find).with([alert1.id, alert2.id, alert3.id]).and_return([alert1, alert2, alert3])
-      ProcessAlertAndRecordStatsService.call(alert_ids: [alert1.id, alert2.id, alert3.id])
+    it "should process the individual alert" do
+      expect(ProcessAlertService).to receive(:call).with(alert: alert).and_return([1, 5, 1, 1])
+      allow(Alert).to receive(:find).with([alert.id]).and_return([alert])
+      ProcessAlertAndRecordStatsService.call(alert_ids: [alert.id])
     end
 
     it "should create a record of the batch of sent email alerts" do
-      allow(Alert).to receive(:find).with([alert1.id, alert2.id, alert3.id]).and_return([alert1, alert2, alert3])
+      allow(Alert).to receive(:find).with([alert.id]).and_return([alert])
 
-      ProcessAlertAndRecordStatsService.call(alert_ids: [alert1.id, alert2.id, alert3.id])
+      ProcessAlertAndRecordStatsService.call(alert_ids: [alert.id])
       expect(EmailBatch.count).to eq 1
       batch = EmailBatch.first
-      expect(batch.no_emails).to eq 2
-      expect(batch.no_applications).to eq 8
-      expect(batch.no_comments).to eq 3
+      expect(batch.no_emails).to eq 1
+      expect(batch.no_applications).to eq 5
+      expect(batch.no_comments).to eq 1
       expect(batch.no_replies).to eq 1
     end
 
@@ -39,11 +33,11 @@ describe ProcessAlertAndRecordStatsService do
       Stat.emails_sent = 5
       Stat.applications_sent = 10
 
-      allow(Alert).to receive(:find).with([alert1.id, alert2.id, alert3.id]).and_return([alert1, alert2, alert3])
+      allow(Alert).to receive(:find).with([alert.id]).and_return([alert])
 
-      ProcessAlertAndRecordStatsService.call(alert_ids: [alert1.id, alert2.id, alert3.id])
-      expect(Stat.emails_sent).to eq 7
-      expect(Stat.applications_sent).to eq 18
+      ProcessAlertAndRecordStatsService.call(alert_ids: [alert.id])
+      expect(Stat.emails_sent).to eq 6
+      expect(Stat.applications_sent).to eq 15
     end
   end
 end
