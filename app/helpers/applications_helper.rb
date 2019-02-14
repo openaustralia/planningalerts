c374dee0eb5387a3f740498cb5f22936b9f1bd36
Matthew Landauer <mlandauer@gmail.com>
2019-02-14 13:11:02 +1100

    distance_in_words helper was unnecessary - as there is already one - metres_in_words

--- app/helpers/applications_helper.rb
+++ app/helpers/applications_helper.rb
@@ -126,16 +126,6 @@ module ApplicationsHelper
     image_tag(google_static_streetview_url(application, size: size, fov: fov, key: key), size: size, alt: "Streetview of #{application.address}")
   end
 
-  # Given a distance in metres returns some words describing the distance
-  # in human terms
-  def distance_in_words(distance)
-    if distance >= 1000
-      "#{(distance / 1000).round(1)}km"
-    else
-      "#{distance.round}m"
-    end
-  end
-
   HEADING_SECTOR_NAMES = %w[N NE E SE S SW W NW].freeze
 
   def heading_in_words(degrees)
