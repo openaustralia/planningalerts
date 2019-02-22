0d63fc88ea30eea0321db849967901b65c5e766f
Matthew Landauer <mlandauer@gmail.com>
2019-02-22 19:42:26 +1100

    Don't redefine constant

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,14 +1,13 @@
 # TODO: Store this in a more sensibly named/namespaced variable
-ElasticSearchClient = nil
 
-if ENV["ELASTICSEARCH_HOST"].present?
-  ElasticSearchClient = Elasticsearch::Client.new hosts: [
-    {
-      host: ENV["ELASTICSEARCH_HOST"],
-      port: ENV["ELASTICSEARCH_PORT"],
-      user: ENV["ELASTICSEARCH_USER"],
-      password: ENV["ELASTICSEARCH_PASSWORD"],
-      scheme: ENV["ELASTICSEARCH_SCHEME"]
-    }
-  ]
-end
+ElasticSearchClient = if ENV["ELASTICSEARCH_HOST"].present?
+                        Elasticsearch::Client.new hosts: [
+                          {
+                            host: ENV["ELASTICSEARCH_HOST"],
+                            port: ENV["ELASTICSEARCH_PORT"],
+                            user: ENV["ELASTICSEARCH_USER"],
+                            password: ENV["ELASTICSEARCH_PASSWORD"],
+                            scheme: ENV["ELASTICSEARCH_SCHEME"]
+                          }
+                        ]
+                      end
