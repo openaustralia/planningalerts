36654f8158219a9295296554555157a41a4cc47a
Matthew Landauer <mlandauer@gmail.com>
2019-02-27 12:42:41 +1100

    Remove last remaining bits of api_statistics

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -8,19 +8,11 @@ ActiveAdmin.register User do
     column :name
     column :organisation
     column :api_key
-    column "Total API calls" do |u|
-      u.api_statistics.count
-    end
-    column "API calls in last month" do |u|
-      u.api_statistics.where("query_time >= ?", 1.month.ago).count
-    end
     column :api_disabled
     column :bulk_api
     column :admin
     actions
   end
-
-  remove_filter :api_statistics
 
   form do |_f|
     inputs "Details" do
