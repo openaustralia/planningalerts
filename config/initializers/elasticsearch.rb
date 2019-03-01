bd29a469b4d855512e9fc2ec1b7bb8ba2be3d35f
Matthew Landauer <mlandauer@gmail.com>
2019-03-02 06:26:08 +1100

    Now use a single env variable for elasticsearch config

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,13 +1,5 @@
 # TODO: Store this in a more sensibly named/namespaced variable
 
-ElasticSearchClient = if ENV["ELASTICSEARCH_HOST"].present?
-                        Elasticsearch::Client.new hosts: [
-                          {
-                            host: ENV["ELASTICSEARCH_HOST"],
-                            port: ENV["ELASTICSEARCH_PORT"],
-                            user: ENV["ELASTICSEARCH_USER"],
-                            password: ENV["ELASTICSEARCH_PASSWORD"],
-                            scheme: ENV["ELASTICSEARCH_SCHEME"]
-                          }
-                        ]
+ElasticSearchClient = if ENV["ELASTICSEARCH_URL"].present?
+                        Elasticsearch::Client.new url: ENV["ELASTICSEARCH_URL"]
                       end
