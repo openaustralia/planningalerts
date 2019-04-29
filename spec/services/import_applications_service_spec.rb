85c44fc008a9e2dc81773e625631e107993f7173
Matthew Landauer <mlandauer@gmail.com>
2019-04-29 14:27:19 +1000

    Modernise tests a little

--- spec/services/import_applications_service_spec.rb
+++ spec/services/import_applications_service_spec.rb
@@ -3,9 +3,34 @@
 require "spec_helper"
 
 describe ImportApplicationsService do
+  let(:auth) { create(:authority, full_name: "Fiddlesticks", state: "NSW", short_name: "Fiddle") }
+  let(:date) { Date.new(2009, 1, 1) }
+  let(:feed) do
+    <<-JSON
+    [
+      {
+        "date_scraped": "2012-08-24",
+        "council_reference": "R1",
+        "address": "1 Smith Street, Fiddleville",
+        "description": "Knocking a house down",
+        "info_url": "http://fiddle.gov.au/info/R1",
+        "comment_url": "http://fiddle.gov.au/comment/R1",
+        "date_received": "2009-01-01",
+        "on_notice_from": "2009-01-05",
+        "on_notice_to": "2009-01-19"
+      },
+      {
+        "council_reference": "R2",
+        "address": "2 Smith Street, Fiddleville",
+        "description": "Putting a house up",
+        "info_url": "http://fiddle.gov.au/info/R2",
+        "comment_url": "http://fiddle.gov.au/comment/R2"
+      }
+    ]
+    JSON
+  end
+
   before :each do
-    Authority.delete_all
-    @auth = create(:authority, full_name: "Fiddlesticks", state: "NSW", short_name: "Fiddle")
     # Stub out the geocoder to return some arbitrary coordinates so that the tests can run quickly
     allow(GeocodeService).to receive(:call).and_return(
       GeocoderResults.new(
@@ -22,81 +47,52 @@ describe ImportApplicationsService do
         nil
       )
     )
+    allow(ImportApplicationsService).to receive(:open_url_safe).and_return(feed)
   end
 
-  describe "importing applications from the scraper web service urls" do
-    before :each do
-      feed = <<-JSON
-      [
-        {
-          "date_scraped": "2012-08-24",
-          "council_reference": "R1",
-          "address": "1 Smith Street, Fiddleville",
-          "description": "Knocking a house down",
-          "info_url": "http://fiddle.gov.au/info/R1",
-          "comment_url": "http://fiddle.gov.au/comment/R1",
-          "date_received": "2009-01-01",
-          "on_notice_from": "2009-01-05",
-          "on_notice_to": "2009-01-19"
-        },
-        {
-          "council_reference": "R2",
-          "address": "2 Smith Street, Fiddleville",
-          "description": "Putting a house up",
-          "info_url": "http://fiddle.gov.au/info/R2",
-          "comment_url": "http://fiddle.gov.au/comment/R2"
-        }
-      ]
-      JSON
-      @date = Date.new(2009, 1, 1)
-      Application.delete_all
-      allow(ImportApplicationsService).to receive(:open_url_safe).and_return(feed)
+  it "should import the correct applications" do
+    logger = double
+    expect(logger).to receive(:info).with("2 new applications found for Fiddlesticks, NSW with date from 2009-01-01 to 2009-01-01")
+    expect(logger).to receive(:info).with("Took 0 s to import applications from Fiddlesticks, NSW")
+    Timecop.freeze(date) do
+      ImportApplicationsService.call(authority: auth, scrape_delay: 0, logger: logger, morph_api_key: "123")
     end
+    expect(Application.count).to eq(2)
+    r1 = Application.find_by(council_reference: "R1")
+    expect(r1.date_scraped).to eq(date)
+    expect(r1.authority).to eq(auth)
+    expect(r1.address).to eq("1 Smith Street, Fiddleville")
+    expect(r1.description).to eq("Knocking a house down")
+    expect(r1.info_url).to eq("http://fiddle.gov.au/info/R1")
+    expect(r1.comment_url).to eq("http://fiddle.gov.au/comment/R1")
+    expect(r1.date_received).to eq(date)
+    expect(r1.on_notice_from).to eq(Date.new(2009, 1, 5))
+    expect(r1.on_notice_to).to eq(Date.new(2009, 1, 19))
+  end
 
-    it "should import the correct applications" do
-      logger = double
-      expect(logger).to receive(:info).with("2 new applications found for Fiddlesticks, NSW with date from 2009-01-01 to 2009-01-01")
-      expect(logger).to receive(:info).with("Took 0 s to import applications from Fiddlesticks, NSW")
-      Timecop.freeze(@date) do
-        ImportApplicationsService.call(authority: @auth, scrape_delay: 0, logger: logger, morph_api_key: "123")
-      end
-      expect(Application.count).to eq(2)
-      r1 = Application.find_by(council_reference: "R1")
-      expect(r1.date_scraped).to eq(@date)
-      expect(r1.authority).to eq(@auth)
-      expect(r1.address).to eq("1 Smith Street, Fiddleville")
-      expect(r1.description).to eq("Knocking a house down")
-      expect(r1.info_url).to eq("http://fiddle.gov.au/info/R1")
-      expect(r1.comment_url).to eq("http://fiddle.gov.au/comment/R1")
-      expect(r1.date_received).to eq(@date)
-      expect(r1.on_notice_from).to eq(Date.new(2009, 1, 5))
-      expect(r1.on_notice_to).to eq(Date.new(2009, 1, 19))
+  it "should not create new applications when they already exist" do
+    logger = double
+    expect(logger).to receive(:info).with("2 new applications found for Fiddlesticks, NSW with date from 2009-01-01 to 2009-01-01")
+    expect(logger).to receive(:info).with("0 new applications found for Fiddlesticks, NSW with date from 2009-01-01 to 2009-01-01")
+    expect(logger).to receive(:info).twice.with("Took 0 s to import applications from Fiddlesticks, NSW")
+
+    # Getting the feed twice with the same content
+    Timecop.freeze(date) do
+      ImportApplicationsService.call(authority: auth, scrape_delay: 0, logger: logger, morph_api_key: "123")
+      ImportApplicationsService.call(authority: auth, scrape_delay: 0, logger: logger, morph_api_key: "123")
     end
+    expect(Application.count).to eq(2)
+  end
 
-    it "should not create new applications when they already exist" do
-      logger = double
-      expect(logger).to receive(:info).with("2 new applications found for Fiddlesticks, NSW with date from 2009-01-01 to 2009-01-01")
-      expect(logger).to receive(:info).with("0 new applications found for Fiddlesticks, NSW with date from 2009-01-01 to 2009-01-01")
-      expect(logger).to receive(:info).twice.with("Took 0 s to import applications from Fiddlesticks, NSW")
-
-      # Getting the feed twice with the same content
-      Timecop.freeze(@date) do
-        ImportApplicationsService.call(authority: @auth, scrape_delay: 0, logger: logger, morph_api_key: "123")
-        ImportApplicationsService.call(authority: @auth, scrape_delay: 0, logger: logger, morph_api_key: "123")
-      end
-      expect(Application.count).to eq(2)
-    end
-
-    it "should escape the morph api key and the sql query" do
-      logger = double
-      allow(logger).to receive(:info)
-      expect(ImportApplicationsService).to receive(:open_url_safe).with(
-        "https://api.morph.io//data.json?key=12%2F&query=select+%2A+from+%60data%60+where+%60date_scraped%60+%3E%3D+%272009-01-01%27+and+%60date_scraped%60+%3C%3D+%272009-01-01%27",
-        logger
-      )
-      Timecop.freeze(@date) do
-        ImportApplicationsService.call(authority: @auth, scrape_delay: 0, logger: logger, morph_api_key: "12/")
-      end
+  it "should escape the morph api key and the sql query" do
+    logger = double
+    allow(logger).to receive(:info)
+    expect(ImportApplicationsService).to receive(:open_url_safe).with(
+      "https://api.morph.io//data.json?key=12%2F&query=select+%2A+from+%60data%60+where+%60date_scraped%60+%3E%3D+%272009-01-01%27+and+%60date_scraped%60+%3C%3D+%272009-01-01%27",
+      logger
+    )
+    Timecop.freeze(date) do
+      ImportApplicationsService.call(authority: auth, scrape_delay: 0, logger: logger, morph_api_key: "12/")
     end
   end
 end
