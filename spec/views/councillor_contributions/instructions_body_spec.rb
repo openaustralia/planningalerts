4ddf9e39040a315567bcd57a319880b185ba398b
Matthew Landauer <mlandauer@gmail.com>
2019-04-30 09:49:42 +1000

    Avoid using instance variable in view partial

--- spec/views/councillor_contributions/instructions_body_spec.rb
+++ spec/views/councillor_contributions/instructions_body_spec.rb
@@ -4,30 +4,20 @@ require "spec_helper"
 
 describe "councillor_contributions/_instructions_body" do
   context "when the authority has a website url" do
-    before do
-      assign(
-        :authority,
-        create(:authority, full_name: "Example Council", website_url: "https://example.nsw.gov.au")
-      )
-    end
+    let(:authority) { create(:authority, full_name: "Example Council", website_url: "https://example.nsw.gov.au") }
 
     it "render a link to the website" do
-      render "instructions_body"
+      render "instructions_body", authority: authority
 
       expect(rendered).to include "<a title=\"Go to the Example Council website.\" target=\"_blank\" href=\"https://example.nsw.gov.au\">Example Council website</a>"
     end
   end
 
   context "when the authority does not have a website url" do
-    before do
-      assign(
-        :authority,
-        create(:authority, full_name: "Example Council", website_url: nil)
-      )
-    end
+    let(:authority) { create(:authority, full_name: "Example Council", website_url: nil) }
 
     it "render its name with no link" do
-      render "instructions_body"
+      render "instructions_body", authority: authority
 
       expect(rendered).to_not include "<a title=\"Go to the Example Council website.\" target=\"_blank\" href=\"https://example.nsw.gov.au\">Example Council</a>"
       expect(rendered).to include "Example Council"
