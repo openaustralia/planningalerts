f8da5a4502eae7f9c07fa7c0ee2e8701c1a982aa
Matthew Landauer <mlandauer@gmail.com>
2020-07-02 14:58:20 +1000

    Fix tiny bug with help of typing

--- app/models/authority_logger.rb
+++ app/models/authority_logger.rb
@@ -1,4 +1,4 @@
-# typed: false
+# typed: true
 # frozen_string_literal: true
 
 class AuthorityLogger < Logger
@@ -12,7 +12,7 @@ class AuthorityLogger < Logger
   def add(severity, message = nil, progname = nil)
     @other_logger.add(severity, message, progname)
     # Put a maximum limit on how long the log can get
-    e = Authority.find(@authority_id).last_scraper_run_log + progname + "\n"
+    e = (Authority.find(@authority_id).last_scraper_run_log || "") + progname + "\n"
     return if e.size >= 5000
 
     # We want this log to be written even if the rest of the authority
