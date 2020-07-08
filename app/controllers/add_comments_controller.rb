58fa7d66d25f886b016929ef0855d893aaa76a63
Matthew Landauer <mlandauer@gmail.com>
2020-07-09 06:13:16 +1000

    Start of type checking controllers

--- app/controllers/add_comments_controller.rb
+++ app/controllers/add_comments_controller.rb
@@ -1,18 +1,34 @@
-# typed: false
+# typed: true
 # frozen_string_literal: true
 
 class AddCommentsController < ApplicationController
   respond_to :html
 
+  class AddCommentParams < T::Struct
+    const :name, String
+    const :text, String
+    const :address, String
+    const :email, String
+    const :comment_for, T.nilable(String)
+  end
+
+  class CreateParams < T::Struct
+    const :application_id, Integer
+    const :little_sweety, T.nilable(String)
+    const :add_comment, AddCommentParams
+    const :councillors_list_toggler, T.nilable(String)
+  end
+
   def create
-    @application = Application.find(params[:application_id])
+    typed_params = TypedParams[CreateParams].new.extract!(params)
+    @application = Application.find(typed_params.application_id)
 
     # First check if the honeypot field has been filled out by a spam bot
     # If so, make it look like things worked but don't actually do anything
-    return if params[:little_sweety].present?
+    return if typed_params.little_sweety.present?
 
     @add_comment = AddComment.new(
-      add_comment_params.merge(
+      typed_params.add_comment.serialize.merge(
         application: @application
       )
     )
@@ -24,23 +40,11 @@ class AddCommentsController < ApplicationController
     # TODO: This seems to have a lot repeated from Application#show
     flash.now[:error] = t(".not_filled_out")
 
-    @councillor_list_open = true if params[:councillors_list_toggler] == "open"
+    @councillor_list_open = true if typed_params.councillors_list_toggler == "open"
 
     # HACK: Required for new email alert signup form
     @alert = Alert.new(address: @application.address)
 
     render "applications/show"
-  end
-
-  private
-
-  def add_comment_params
-    params.require(:add_comment).permit(
-      :name,
-      :text,
-      :address,
-      :email,
-      :comment_for
-    )
   end
 end
