89bc5fbca3a7e628970c397cd384de1d0dd361e1
Matthew Landauer <mlandauer@gmail.com>
2020-07-08 17:01:51 +1000

    A little more type checking

---
+++ sorbet/rbi/email_confirmable.rb
@@ -0,0 +1,18 @@
+# typed: true
+
+# This is a horrible hack
+module EmailConfirmable
+  def self.scope(*args, &block); end
+
+  def self.where(*args); end
+
+  def self.after_commit(*args, &block); end
+
+  def self.before_create(*args, &block); end
+
+  def self.validates_email_format_of(*attr_names); end
+
+  def self.validates(*attributes); end
+
+  def confirm_id=(value); end
+end
