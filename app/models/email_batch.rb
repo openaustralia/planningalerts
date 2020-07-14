75dfe6d55a12c6f450eb3792a008caab3699ea68
Matthew Landauer <mlandauer@gmail.com>
2020-07-14 12:55:20 +1000

    Strict type checking for EmailBatch

--- app/models/email_batch.rb
+++ app/models/email_batch.rb
@@ -1,9 +1,12 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 class EmailBatch < ApplicationRecord
+  extend T::Sig
+
   scope(:in_past_week, -> { where("created_at > ?", 7.days.ago) })
 
+  sig { returns(Integer) }
   def self.total_sent_in_past_week
     in_past_week.sum(:no_emails)
   end
