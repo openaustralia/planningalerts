e2e0c565805d1c70cfdb6515d840cf22dc303888
Matthew Landauer <mlandauer@gmail.com>
2020-07-15 16:46:42 +1000

    Strict type checking for AlertMailer

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -1,15 +1,25 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 class AlertMailer < ApplicationMailer
+  extend T::Sig
+
   include EmailFrom
   helper :application, :applications, :comments
 
+  sig do
+    params(
+      alert: Alert,
+      applications: T::Array[Application],
+      comments: T::Array[Comment],
+      replies: T::Array[Reply]
+    ).returns(Mail::Message)
+  end
   def alert(alert, applications, comments = [], replies = [])
-    @alert = alert
-    @applications = applications
-    @comments = comments
-    @replies = replies
+    @alert = T.let(alert, T.nilable(Alert))
+    @applications = T.let(applications, T.nilable(T::Array[Application]))
+    @comments = T.let(comments, T.nilable(T::Array[Comment]))
+    @replies = T.let(replies, T.nilable(T::Array[Reply]))
 
     mail(
       from: email_from, to: alert.email,
@@ -21,6 +31,7 @@ class AlertMailer < ApplicationMailer
     )
   end
 
+  sig { params(alert: Alert).returns(Mail::Message) }
   def new_signup_attempt_notice(alert)
     @alert = alert
 
