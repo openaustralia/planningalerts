b517f6f5ef859b8c089aea4004a85ad6c45af291
Matthew Landauer <mlandauer@gmail.com>
2020-07-16 12:45:14 +1000

    Stricter type checking for ConfirmationMailer

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,10 +1,14 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 class ConfirmationMailer < ApplicationMailer
+  extend T::Sig
+
   include EmailFrom
   helper :comments
 
+  # TODO: Don't like the typing here
+  sig { params(object: T.untyped).returns(Mail::Message) }
   def confirm(object)
     class_name = object.class.name.underscore
     # This seems a bit long-winded. Is there a better way?
