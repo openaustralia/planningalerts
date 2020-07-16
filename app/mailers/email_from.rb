7910286cf610c75a05d030367c5bde826ff736d6
Matthew Landauer <mlandauer@gmail.com>
2020-07-16 12:53:44 +1000

    Strict type checking for EmailFrom

--- app/mailers/email_from.rb
+++ app/mailers/email_from.rb
@@ -1,9 +1,12 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 module EmailFrom
+  extend T::Sig
+
   private
 
+  sig { returns(String) }
   def email_from
     "#{ENV['EMAIL_FROM_NAME']} <#{ENV['EMAIL_FROM_ADDRESS']}>"
   end
