98686412f8c0ea49b4f3008bd0c02ec438bb1493
Matthew Landauer <mlandauer@gmail.com>
2020-07-16 13:03:35 +1000

    Strict type checking for ReplyMailer

--- app/mailers/reply_mailer.rb
+++ app/mailers/reply_mailer.rb
@@ -1,13 +1,16 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 class ReplyMailer < ApplicationMailer
+  extend T::Sig
+
   include EmailFrom
   helper :comments
 
+  sig { params(reply: Reply).returns(Mail::Message) }
   def notify_comment_author(reply)
-    @reply = reply
-    @comment = @reply.comment
+    @reply = T.let(reply, T.nilable(Reply))
+    @comment = T.let(reply.comment, T.nilable(Comment))
 
     mail(to: reply.comment.email,
          sender: email_from,
