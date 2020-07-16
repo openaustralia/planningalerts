c442dfcebcee686ff122f9075226c851a7129d77
Matthew Landauer <mlandauer@gmail.com>
2020-07-16 14:11:23 +1000

    Strict type checking for AlertsController

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,7 +1,9 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 class AlertsController < ApplicationController
+  extend T::Sig
+
   caches_page :statistics
 
   class NewParams < T::Struct
@@ -32,30 +34,38 @@ class AlertsController < ApplicationController
     const :size, T.nilable(String)
   end
 
+  sig { void }
   def new
     typed_params = TypedParams[NewParams].new.extract!(params)
-    @alert = Alert.new(typed_params.serialize)
-    @set_focus_control = typed_params.address ? "alert_email" : "alert_address"
+    @alert = T.let(Alert.new(typed_params.serialize), T.nilable(Alert))
+    @set_focus_control = T.let(typed_params.address ? "alert_email" : "alert_address", T.nilable(String))
   end
 
+  sig { void }
   def create
     typed_params = TypedParams[CreateParams].new.extract!(params)
-    @address = typed_params.alert.address
-    @alert = BuildAlertService.call(
-      email: typed_params.alert.email,
-      address: @address,
-      radius_meters: zone_sizes["l"]
+    address = typed_params.alert.address
+    @address = T.let(address, T.nilable(String))
+    @alert = T.let(
+      BuildAlertService.call(
+        email: typed_params.alert.email,
+        address: address,
+        radius_meters: T.must(zone_sizes["l"])
+      ),
+      T.nilable(Alert)
     )
 
     render "new" if @alert.present? && !@alert.save
   end
 
+  sig { void }
   def confirmed
     typed_params = TypedParams[ConfirmedParams].new.extract!(params)
     @alert = Alert.find_by!(confirm_id: typed_params.id)
     @alert.confirm!
   end
 
+  sig { void }
   def unsubscribe
     typed_params = TypedParams[UnsubscribeParams].new.extract!(params)
     @alert = Alert.find_by(confirm_id: typed_params.id)
@@ -63,21 +73,25 @@ class AlertsController < ApplicationController
   end
 
   # TODO: Split this into two actions
+  sig { void }
   def area
     typed_params = TypedParams[AreaParams].new.extract!(params)
-    @zone_sizes = zone_sizes
-    @alert = Alert.find_by!(confirm_id: typed_params.id)
+    @zone_sizes = T.let(zone_sizes, T.nilable(T::Hash[String, Integer]))
+    alert = Alert.find_by!(confirm_id: typed_params.id)
+    @alert = T.let(alert, T.nilable(Alert))
     if request.get? || request.head?
-      @size = @zone_sizes.invert[@alert.radius_meters]
+      @size = T.let(zone_sizes.invert[alert.radius_meters], T.nilable(String))
     else
-      @alert.radius_meters = @zone_sizes[typed_params.size]
-      @alert.save!
+      # TODO: If we seperate this action into two then we won't need to use T.must here
+      alert.radius_meters = T.must(zone_sizes[T.must(typed_params.size)])
+      alert.save!
       render "area_updated"
     end
   end
 
   private
 
+  sig { returns(T::Hash[String, Integer]) }
   def zone_sizes
     {
       "s" => Rails.application.config.planningalerts_small_zone_size,
