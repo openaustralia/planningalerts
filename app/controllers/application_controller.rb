826e0949f0de2c581af9861d73bee22a88bb2c96
Matthew Landauer <mlandauer@gmail.com>
2020-07-16 16:05:06 +1000

    Strict type checking for ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  extend T::Sig
   extend ThemesOnRails::ControllerAdditions::ClassMethods
 
   theme :theme_resolver
@@ -20,6 +21,7 @@ class ApplicationController < ActionController::Base
   before_action :set_header_variable, :validate_page_param
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  sig { void }
   def authenticate_active_admin_user!
     authenticate_user!
     render plain: "Not authorised", status: :forbidden unless current_user.admin?
@@ -27,6 +29,7 @@ class ApplicationController < ActionController::Base
 
   private
 
+  sig { returns(String) }
   def theme_resolver
     # Only show a different theme if the user is an admin
     if session[:theme] && current_user&.admin?
@@ -36,20 +39,24 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  sig { void }
   def set_header_variable
-    @alert_count = Stat.applications_sent
+    @alert_count = T.let(Stat.applications_sent, T.nilable(Integer))
   end
 
+  sig { returns(T::Boolean) }
   def ssl_required?
     Rails.env.production?
   end
 
+  sig { void }
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name organisation])
   end
 
   # this method is to respond to the will_paginate bug of invalid page number leading to error being thrown.
   # see discussion here https://github.com/mislav/will_paginate/issues/271
+  sig { void }
   def validate_page_param
     params[:page] = (params[:page].to_i if params[:page].present? && params[:page].to_i.positive?)
   end
