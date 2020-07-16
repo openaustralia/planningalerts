184ddf1f20e7d90814330f0b9cca713e0a15becc
Matthew Landauer <mlandauer@gmail.com>
2020-07-16 13:52:14 +1000

    Strict type checking for AddCommentsController

--- app/controllers/add_comments_controller.rb
+++ app/controllers/add_comments_controller.rb
@@ -1,7 +1,9 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 class AddCommentsController < ApplicationController
+  extend T::Sig
+
   respond_to :html
 
   class AddCommentParams < T::Struct
@@ -19,31 +21,34 @@ class AddCommentsController < ApplicationController
     const :councillors_list_toggler, T.nilable(String)
   end
 
+  sig { void }
   def create
     typed_params = TypedParams[CreateParams].new.extract!(params)
-    @application = Application.find(typed_params.application_id)
+    application = Application.find(typed_params.application_id)
+    @application = T.let(application, T.nilable(Application))
 
     # First check if the honeypot field has been filled out by a spam bot
     # If so, make it look like things worked but don't actually do anything
     return if typed_params.little_sweety.present?
 
-    @add_comment = AddComment.new(
+    add_comment = AddComment.new(
       typed_params.add_comment.serialize.merge(
         application: @application
       )
     )
+    @add_comment = T.let(add_comment, T.nilable(AddComment))
 
-    @comment = @add_comment.save_comment
+    @comment = T.let(add_comment.save_comment, T.nilable(Comment))
 
     return if @comment
 
     # TODO: This seems to have a lot repeated from Application#show
     flash.now[:error] = t(".not_filled_out")
 
-    @councillor_list_open = true if typed_params.councillors_list_toggler == "open"
+    @councillor_list_open = T.let(true, T.nilable(T::Boolean)) if typed_params.councillors_list_toggler == "open"
 
     # HACK: Required for new email alert signup form
-    @alert = Alert.new(address: @application.address)
+    @alert = T.let(Alert.new(address: application.address), T.nilable(Alert))
 
     render "applications/show"
   end
