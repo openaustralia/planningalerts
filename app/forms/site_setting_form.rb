820d13c982c346f2f7278fdb795fa372aacde491
Matthew Landauer <mlandauer@gmail.com>
2020-07-18 04:05:01 +1000

    Strict type checking for SIteSettingForm

--- app/forms/site_setting_form.rb
+++ app/forms/site_setting_form.rb
@@ -1,7 +1,9 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 class SiteSettingForm
+  extend T::Sig
+
   include ActiveModel::Model
   # See https://sorbet.org/docs/error-reference#4002
   T.unsafe(self).include Virtus.model
@@ -10,14 +12,17 @@ class SiteSettingForm
   attribute :streetview_in_app_enabled, Virtus::Attribute::Boolean
 
   # If called without parameters will initialise with the current site settings
+  sig { params(params: T.nilable(T::Hash[Symbol, T.untyped])).void }
   def initialize(params = nil)
     super(params || SiteSetting.settings_with_defaults)
   end
 
+  sig { void }
   def persist
     SiteSetting.set(attributes)
   end
 
+  sig { returns(T::Boolean) }
   def persisted?
     true
   end
