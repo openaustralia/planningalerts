51671eab1e82254dd9edaddab950a5538d7100fc
Matthew Landauer <mlandauer@gmail.com>
2020-07-23 15:26:36 +1000

    Can now figure out whether issue is closed or not

--- app/models/github_issue.rb
+++ app/models/github_issue.rb
@@ -1,3 +1,25 @@
+# typed: strict
+# frozen_string_literal: true
+
 class GithubIssue < ApplicationRecord
+  extend T::Sig
   belongs_to :authority
+
+  # Use the GitHub API to get the issue state
+  sig { params(client: Octokit::Client).returns(String) }
+  def state(client)
+    github_issue(client).state
+  end
+
+  sig { params(client: Octokit::Client).returns(T::Boolean) }
+  def closed?(client)
+    state(client) == "closed"
+  end
+
+  private
+
+  sig { params(client: Octokit::Client).returns(T.untyped) }
+  def github_issue(client)
+    client.issue(github_repo, github_number)
+  end
 end
