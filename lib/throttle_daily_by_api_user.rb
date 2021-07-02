7495237ad8eb2f46f2e854b9b8212b676799b66c
Matthew Landauer <mlandauer@gmail.com>
2021-07-02 11:10:59 +1000

    Make logic more clear by using the negative

--- lib/throttle_daily_by_api_user.rb
+++ lib/throttle_daily_by_api_user.rb
@@ -15,15 +15,15 @@ class ThrottleDailyByApiUser < Rack::Throttle::Daily
     end
     # rubocop:enable Lint/SuppressedException
 
-    if path_info.nil? || path_info[:controller] != "api" || path_info[:action] == "howto"
-      true
-    else
+    if path_info && path_info[:controller] == "api" && path_info[:action] != "howto"
       # Doing a database lookup in rack middleware - not ideal but
       # we're only doing it if it's an api request and this allows
       # us to manage things in the admin panel without needing to deploy
       # or edit files
       user = User.find_by(api_key: client_identifier(request))
       !user.nil? && user.unlimited_api_usage
+    else
+      true
     end
   end
 
