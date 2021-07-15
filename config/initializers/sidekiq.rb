56cecde388a1b6d084c6a89812c15effc9b24651
Matthew Landauer <mlandauer@gmail.com>
2021-07-15 15:24:59 +1000

    Use REDIS_URL env variable if it's present

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,16 +1,22 @@
 # typed: strict
 require 'sidekiq'
 
+redis_url = if ENV["REDIS_URL"].present?
+  ENV["REDIS_URL"]
+else
+  "redis://localhost:6379/0"
+end
+
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: "redis://localhost:6379/0",
+    url: redis_url,
     namespace: "pa_#{ENV['STAGE']}"
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: "redis://localhost:6379/0",
+    url: redis_url,
     namespace: "pa_#{ENV['STAGE']}"
   }
 end
