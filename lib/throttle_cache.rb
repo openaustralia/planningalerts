65eb43357101733ef91c8b9c5c6b0c352406b934
Matthew Landauer <mlandauer@gmail.com>
2021-07-19 12:38:14 +1000

    Add type checking and keep rubocop happy

--- lib/throttle_cache.rb
+++ lib/throttle_cache.rb
@@ -1,13 +1,21 @@
+# typed: strict
+# frozen_string_literal: true
+
 class ThrottleCache
+  extend T::Sig
+
+  sig { returns(T.any(Redis, Redis::Namespace)) }
   attr_reader :redis
 
+  sig { params(redis_config: T.untyped).void }
   def initialize(redis_config)
-    @redis = Redis.new(redis_config)
-    if redis_config[:namespace]
-      @redis = Redis::Namespace.new(redis_config[:namespace], redis: @redis)
-    end
+    @redis = T.let(Redis.new(redis_config), T.any(Redis, Redis::Namespace))
+    return if redis_config[:namespace].nil?
+
+    @redis = Redis::Namespace.new(redis_config[:namespace], redis: @redis)
   end
 
+  sig { params(key: String, value: Integer).void }
   def set(key, value)
     redis.set(key, value)
     # Keep keys around for a year so we could potentially use the redis cache
@@ -19,7 +27,10 @@ class ThrottleCache
     redis.expire(key, 1.year)
   end
 
+  sig { params(key: String).returns(Integer) }
+  # rubocop:disable Rails/Delegate
   def get(key)
     redis.get(key)
   end
+  # rubocop:enable Rails/Delegate
 end
