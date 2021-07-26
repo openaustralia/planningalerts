ee8ff741dd94f67009a81d8fe6d26829942b33f2
Matthew Landauer <mlandauer@gmail.com>
2021-07-26 16:01:20 +1000

    Deploy to second PA server in development

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,8 @@ elsif stage == "test"
   set :app_name, "planningalerts-test"
   set :honeybadger_env, "staging"
 elsif stage == "development"
-  server "planningalerts.org.au.test", :app, :web, :db, primary: true
+  server "web1.planningalerts.org.au.test", :app, :web, :db, primary: true
+  server "web2.planningalerts.org.au.test", :app, :web
   set :deploy_to, "/srv/www/production"
   set :app_name, "planningalerts"
 else
