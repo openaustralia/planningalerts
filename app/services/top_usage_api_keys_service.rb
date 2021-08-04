e1447dc73775389ba55aca8c64fa09dc4bfc9e6e
Matthew Landauer <mlandauer@gmail.com>
2021-08-04 16:41:51 +1000

    Extract method

--- app/services/top_usage_api_keys_service.rb
+++ app/services/top_usage_api_keys_service.rb
@@ -12,10 +12,14 @@ class TopUsageAPIKeysService < ApplicationService
     # Definitely not the most efficient way to do things. It would be more
     # efficient in redis land to be using a hash but we're depending currently
     # on the way rack-throttle stores stuff
-    keys = redis.scan_each(match: "throttle:*:#{date}").to_a.uniq
+    keys = all_keys_for_date(redis: redis, date: date)
     api_keys = keys.map { |k| k.split(":")[1] }
     users = api_keys.map { |k| User.find_by(api_key: k) }
     values = redis.mget(keys).map(&:to_i)
     values.zip(users).map { |a| { requests: a[0], user: a[1] } }
+  end
+
+  def self.all_keys_for_date(redis:, date:)
+    redis.scan_each(match: "throttle:*:#{date}").to_a.uniq
   end
 end
