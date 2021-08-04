69cae0093fc4c1762ecb20b3b85a93105cb49b1a
Matthew Landauer <mlandauer@gmail.com>
2021-08-04 16:56:07 +1000

    Change method signatures only

--- app/services/top_usage_api_keys_service.rb
+++ app/services/top_usage_api_keys_service.rb
@@ -15,21 +15,21 @@ class TopUsageAPIKeysService < ApplicationService
   end
 
   def call(date:)
-    all_usage_on_date(date: date).sort { |a, b| b[:requests] <=> a[:requests] }
+    all_usage_on_date(date).sort { |a, b| b[:requests] <=> a[:requests] }
   end
 
-  def all_usage_on_date(date:)
+  def all_usage_on_date(date)
     # Definitely not the most efficient way to do things. It would be more
     # efficient in redis land to be using a hash but we're depending currently
     # on the way rack-throttle stores stuff
-    keys = all_keys_for_date(date: date)
+    keys = all_keys_for_date(date)
     api_keys = keys.map { |k| k.split(":")[1] }
     users = api_keys.map { |k| User.find_by(api_key: k) }
     values = redis.mget(keys).map(&:to_i)
     values.zip(users).map { |a| { requests: a[0], user: a[1] } }
   end
 
-  def all_keys_for_date(date:)
+  def all_keys_for_date(date)
     redis.scan_each(match: "throttle:*:#{date}").to_a.uniq
   end
 end
