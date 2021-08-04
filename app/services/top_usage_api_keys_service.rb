461c019e19d9489c5fd7727837248e12e52a77ed
Matthew Landauer <mlandauer@gmail.com>
2021-08-04 16:47:54 +1000

    Extract redis

--- app/services/top_usage_api_keys_service.rb
+++ app/services/top_usage_api_keys_service.rb
@@ -4,26 +4,32 @@
 class TopUsageAPIKeysService < ApplicationService
   extend T::Sig
 
+  attr_reader :redis
+
   def self.call(redis:, date:)
-    new.call(redis: redis, date: date)
+    new(redis).call(date: date)
   end
 
-  def call(redis:, date:)
-    all_usage_on_date(redis: redis, date: date).sort { |a, b| b[:requests] <=> a[:requests] }
+  def initialize(redis)
+    @redis = redis
   end
 
-  def all_usage_on_date(redis:, date:)
+  def call(date:)
+    all_usage_on_date(date: date).sort { |a, b| b[:requests] <=> a[:requests] }
+  end
+
+  def all_usage_on_date(date:)
     # Definitely not the most efficient way to do things. It would be more
     # efficient in redis land to be using a hash but we're depending currently
     # on the way rack-throttle stores stuff
-    keys = all_keys_for_date(redis: redis, date: date)
+    keys = all_keys_for_date(date: date)
     api_keys = keys.map { |k| k.split(":")[1] }
     users = api_keys.map { |k| User.find_by(api_key: k) }
     values = redis.mget(keys).map(&:to_i)
     values.zip(users).map { |a| { requests: a[0], user: a[1] } }
   end
 
-  def all_keys_for_date(redis:, date:)
+  def all_keys_for_date(date:)
     redis.scan_each(match: "throttle:*:#{date}").to_a.uniq
   end
 end
