2dee178698bd6b7f3dde87840e7165322be473a0
Matthew Landauer <mlandauer@gmail.com>
2021-08-05 14:26:08 +1000

    Add some type signatures to new code

--- app/services/top_usage_api_users_service.rb
+++ app/services/top_usage_api_users_service.rb
@@ -4,16 +4,22 @@
 class TopUsageAPIUsersService < ApplicationService
   extend T::Sig
 
+  sig { returns(Redis) }
   attr_reader :redis
 
+  # TODO: Return struct instead of hash
+  sig { params(redis: Redis, date_from: Date, date_to: Date, number: Integer).returns(T::Array[T::Hash[Symbol, T.untyped]]) }
   def self.call(redis:, date_from:, date_to:, number:)
     new(redis).call(date_from: date_from, date_to: date_to, number: number)
   end
 
+  sig { params(redis: Redis).void }
   def initialize(redis)
     @redis = redis
   end
 
+  # TODO: Return struct instead of hash
+  sig { params(date_from: Date, date_to: Date, number: Integer).returns(T::Array[T::Hash[Symbol, T.untyped]]) }
   def call(date_from:, date_to:, number:)
     top_total_usage_by_api_key_in_date_range(date_from, date_to, number).map do |h|
       user = User.find_by(api_key: h[:api_key])
@@ -21,10 +27,13 @@ class TopUsageAPIUsersService < ApplicationService
     end
   end
 
+  sig { params(date: Date).returns(T::Array[String]) }
   def all_keys_for_date(date)
     redis.scan_each(match: "throttle:*:#{date}").to_a.uniq
   end
 
+  # TODO: Return struct instead of hash
+  sig { params(date_from: Date, date_to: Date).returns(T::Array[T::Hash[Symbol, T.untyped]]) }
   def all_usage_by_api_key_in_date_range(date_from, date_to)
     keys = []
     (date_from..date_to).each do |date|
@@ -35,6 +44,8 @@ class TopUsageAPIUsersService < ApplicationService
     values.zip(api_keys).map { |a| { requests: a[0], api_key: a[1] } }
   end
 
+  # TODO: Return struct instead of hash
+  sig { params(date_from: Date, date_to: Date).returns(T::Array[T::Hash[Symbol, T.untyped]]) }
   def total_usage_by_api_key_in_date_range(date_from, date_to)
     # Use 0 as a default value for the hash members
     r = Hash.new(0)
@@ -49,10 +60,12 @@ class TopUsageAPIUsersService < ApplicationService
     end
   end
 
+  # TODO: Return struct instead of hash
+  sig { params(date_from: Date, date_to: Date, number: Integer).returns(T::Array[T::Hash[Symbol, T.untyped]]) }
   def top_total_usage_by_api_key_in_date_range(date_from, date_to, number)
     # Put it in array form
     array = total_usage_by_api_key_in_date_range(date_from, date_to)
     array = array.sort { |a, b| b[:requests] <=> a[:requests] }
-    array[0..(number - 1)]
+    array[0..(number - 1)] || []
   end
 end
