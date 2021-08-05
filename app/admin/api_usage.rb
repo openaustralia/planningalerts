16993f8eae6b48f1d10a4db114603eb7b5692828
Matthew Landauer <mlandauer@gmail.com>
2021-08-05 16:55:12 +1000

    Show basic admin page with top api users

--- app/admin/api_usage.rb
+++ app/admin/api_usage.rb
@@ -8,6 +8,18 @@ ActiveAdmin.register_page "API usage" do
     para do
       link_to "View dashboard in Kibana", "https://d2ba5a791b81410f8d2365c8a6d59905.ap-southeast-2.aws.found.io:9243/app/kibana#/dashboard/8e1285f0-3582-11e9-8f74-c54feae3d9a2?_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-1y%2Cmode%3Aquick%2Cto%3Anow))"
     end
-    iframe src: "https://d2ba5a791b81410f8d2365c8a6d59905.ap-southeast-2.aws.found.io:9243/app/kibana#/dashboard/8e1285f0-3582-11e9-8f74-c54feae3d9a2?embed=true&_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cmode%3Aquick%2Cto%3Anow))", height: "600", width: "100%"
+
+    redis = Redis.new
+    date_to = Date.today
+    date_from = date_to - 30
+    result = TopUsageAPIUsersService.call(redis: redis, date_from: date_from,
+      date_to: date_to, number: 10)
+
+    h2 "Top users of the API (by number of requests) over the last 30 days"
+
+    table_for result do
+      column(:email) { |usage| link_to usage.user.email, admin_user_path(usage.user) }
+      column :requests
+    end
   end
 end
