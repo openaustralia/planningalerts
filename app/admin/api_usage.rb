c51b5b31b76e99d53d5a87500f9ed1a8a67f00d2
Matthew Landauer <mlandauer@gmail.com>
2021-08-10 15:28:36 +1000

    Can now look at API usage for different number of days

--- app/admin/api_usage.rb
+++ app/admin/api_usage.rb
@@ -2,6 +2,15 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register_page "API usage" do
+  controller do
+    def index
+      if params[:period].nil?
+        redirect_to period: 30
+      end
+      @period = params[:period].to_i
+    end
+  end
+
   content do
     # These URLs require you to login to our Kibana instance with a username and password.
     # So they're relatively safe to include here.
@@ -13,18 +22,33 @@ ActiveAdmin.register_page "API usage" do
     redis = Redis.new(Rails.configuration.redis)
     redis = Redis::Namespace.new(Rails.configuration.redis[:namespace], redis: redis)
 
+    period = @arbre_context.assigns[:period]
     date_to = Time.zone.today
-    date_from = date_to - 30
+    # If period is 1 then we just want today's data roughly. If period is 2
+    # we want today and yesterday
+    date_from = date_to - (period - 1)
     result = TopUsageAPIUsersService.call(redis: redis, date_from: date_from,
                                           date_to: date_to, number: 20)
 
-    h2 "Top 20 users of the API (by number of requests) over the last 30 days"
+    h2 "Top 20 users of the API (by number of requests) over the last #{pluralize(period, 'day')}"
 
     para do
       "Note that these numbers are measured before any authorization is done. " \
       "So, for instance if an API key has been disabled but a user is still " \
       "making requests with that key, which are then getting denied, those " \
       "requests will still show up in the list here."
+    end
+
+    para do
+      link_to_unless_current "last 30 days", period: 30
+    end
+
+    para do
+      link_to_unless_current "last 7 days", period: 7
+    end
+
+    para do
+      link_to_unless_current "last 1 day", period: 1
     end
 
     table_for result do
