899c3fb59542cedceed596c6c2c50baa88dd2f53
Matthew Landauer <mlandauer@gmail.com>
2021-08-25 04:23:06 +1000

    Set value on new api key automatically

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,6 +1,17 @@
-# typed: false
+# typed: strict
 # frozen_string_literal: true
 
 class ApiKey < ApplicationRecord
+  extend T::Sig
+
   belongs_to :user
+
+  before_create :set_value
+
+  private
+
+  sig { void }
+  def set_value
+    self.value = SecureRandom.base58(20)
+  end
 end
