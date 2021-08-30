ebe207727df7e408780acac652e67dc58b6d3c8f
Matthew Landauer <mlandauer@gmail.com>
2021-08-31 05:42:22 +1000

    Inline parent class

--- lib/throttle_daily_by_api_user.rb
+++ lib/throttle_daily_by_api_user.rb
@@ -3,17 +3,24 @@
 
 require "sorbet-runtime"
 
-class ThrottleDailyByApiUser < Rack::Throttle::Daily
+class ThrottleDailyByApiUser < Rack::Throttle::TimeWindow
   extend T::Sig
 
   # By default we allow up to 1000 API requests per day per API key
   DEFAULT_MAX_PER_DAY = 1000
+
+  sig { params(app: T.untyped, options: T::Hash[Symbol, T.untyped]).void }
+  def initialize(app, options = {})
+    super
+  end
 
   sig { params(request: Rack::Request).returns(Integer) }
   def max_per_day(request)
     key = ApiKey.find_by(value: client_identifier(request))
     key&.daily_limit || DEFAULT_MAX_PER_DAY
   end
+
+  alias max_per_window max_per_day
 
   sig { params(request: Rack::Request).returns(T::Boolean) }
   def whitelisted?(request)
@@ -35,5 +42,12 @@ class ThrottleDailyByApiUser < Rack::Throttle::Daily
   sig { params(request: T.untyped).returns(T.untyped) }
   def client_identifier(request)
     request.params["key"]
+  end
+
+  protected
+
+  sig { params(request: Rack::Request).returns(String) }
+  def cache_key(request)
+    [super, Time.zone.now.strftime("%Y-%m-%d")].join(":")
   end
 end
