18d1613c33c32a775c7473ed3c6e51c24145044f
Matthew Landauer <mlandauer@gmail.com>
2021-08-31 15:20:15 +1000

    Add example application to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,28 @@
 # frozen_string_literal: true
 
 User.create! email: "admin@example.com", password: "password"
-Authority.create! full_name: "Marrickville Council",
-                  short_name: "Marrickville",
-                  state: "NSW",
-                  email: "council@marrickville.nsw.gov.au",
-                  population_2017: "81489",
-                  morph_name: "planningalerts-scrapers/marrickville",
-                  website_url: "http://www.marrickville.nsw.gov.au",
-                  disabled: false
+authority = Authority.create! full_name: "Marrickville Council",
+                              short_name: "Marrickville",
+                              state: "NSW",
+                              email: "council@marrickville.nsw.gov.au",
+                              population_2017: "81489",
+                              morph_name: "planningalerts-scrapers/marrickville",
+                              website_url: "http://www.marrickville.nsw.gov.au",
+                              disabled: false
+
+CreateOrUpdateApplicationService.call(
+  authority: authority,
+  council_reference: "DA21/0642",
+  attributes: {
+    address: "28 Grey Street, Emu Plains NSW 2750",
+    description: "Demolition of swimming pool",
+    date_scraped: Time.zone.now,
+    info_url: "http://bizsearch.penrithcity.nsw.gov.au/ePlanning/Pages/XC.Track/SearchApplication.aspx?id=489056",
+    # Values that would normally be the result of geocoding
+    suburb: "Emu Plains",
+    state: "NSW",
+    postcode: "2750",
+    lat: -33.7543286,
+    lng: 150.6486731
+  }
+)
