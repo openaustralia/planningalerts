84a116a9d42319b406e59002010ff2bae6d69871
Matthew Landauer <mlandauer@gmail.com>
2021-09-16 16:48:42 +1000

    Turn on all rails 5.2 default configs

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ Bundler.require(*Rails.groups)
 module PlanningalertsApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
