aa1cb43a00c3a1c0aadca4efb6ea945a9bd75ecc
Matthew Landauer <mlandauer@gmail.com>
2021-09-23 14:14:15 +1000

    Whitespace change only

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
       end
 
       collection do
-        get "export_active_emails"        
+        get "export_active_emails"
       end
     end
     resources :background_jobs, only: :index
