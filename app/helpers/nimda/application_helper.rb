a01a9a14cde1db1fc48f1970e979e209ef63e278
Matthew Landauer <mlandauer@gmail.com>
2021-09-27 15:06:33 +1000

    Keep rubocop happy

--- app/helpers/nimda/application_helper.rb
+++ app/helpers/nimda/application_helper.rb
@@ -1,9 +1,16 @@
+# typed: strict
+# frozen_string_literal: true
+
 module Nimda
   module ApplicationHelper
+    extend T::Sig
+
+    sig { params(tag: String, search_term: String).returns(String) }
     def add_tag_to_search(tag, search_term)
       tag + ": " + search_term
     end
 
+    sig { params(tag: String, search_term: String).returns(T::Boolean) }
     def include_search_tag?(tag, search_term)
       search_term.split(" ").include?(tag + ":")
     end
