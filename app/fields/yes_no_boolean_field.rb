92adf287fedebb6afad3c4c45680b3ef06f9640a
Matthew Landauer <mlandauer@gmail.com>
2021-09-30 13:29:34 +1000

    Keep rubocop happy

--- app/fields/yes_no_boolean_field.rb
+++ app/fields/yes_no_boolean_field.rb
@@ -1,6 +1,12 @@
+# typed: strict
+# frozen_string_literal: true
+
 require "administrate/field/base"
 
 class YesNoBooleanField < Administrate::Field::Base
+  extend T::Sig
+
+  sig { returns(String) }
   def to_s
     if data.nil?
       "-"
