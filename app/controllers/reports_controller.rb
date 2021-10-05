31baa610ff3f84660fca9b124196cddce3fec3f5
Matthew Landauer <mlandauer@gmail.com>
2021-10-05 17:01:03 +1100

    A bit more controller typing

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,35 +1,20 @@
-# typed: ignore
+# typed: true
 # frozen_string_literal: true
 
 class ReportsController < ApplicationController
-  class NewParams < T::Struct
-    const :comment_id, T.nilable(Integer)
-  end
-
   def new
-    typed_params = TypedParams[NewParams].new.extract!(params)
-    @comment = Comment.visible.find(typed_params.comment_id)
+    # typed_params = TypedParams[NewParams].new.extract!(params)
+    @comment = Comment.visible.find(params[:comment_id])
     @report = Report.new
   end
 
-  class ReportParams < T::Struct
-    const :name, String
-    const :email, String
-    const :details, String
-  end
-
-  class CreateParams < T::Struct
-    const :comment_id, Integer
-    const :report, ReportParams
-  end
-
   def create
-    typed_params = TypedParams[CreateParams].new.extract!(params)
-    @comment = Comment.visible.find(typed_params.comment_id)
+    # typed_params = TypedParams[CreateParams].new.extract!(params)
+    @comment = Comment.visible.find(params[:comment_id])
     @report = @comment.reports.build(
-      name: typed_params.report.name,
-      email: typed_params.report.email,
-      details: typed_params.report.details
+      name: params[:report][:name],
+      email: params[:report][:email],
+      details: params[:report][:details]
     )
 
     if verify_recaptcha && @report.save
