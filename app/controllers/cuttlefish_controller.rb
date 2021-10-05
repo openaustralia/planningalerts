c6bd8cf66be9cd651d2876cb8cced526c81124ed
Matthew Landauer <mlandauer@gmail.com>
2021-10-05 17:26:17 +1100

    A bit more controller typing

--- app/controllers/cuttlefish_controller.rb
+++ app/controllers/cuttlefish_controller.rb
@@ -1,4 +1,4 @@
-# typed: ignore
+# typed: true
 # frozen_string_literal: true
 
 class CuttlefishController < ApplicationController
@@ -35,17 +35,17 @@ class CuttlefishController < ApplicationController
   end
 
   def event
-    typed_params = TypedParams[EventParams].new.extract!(params)
+    # typed_params = TypedParams[EventParams].new.extract!(params)
 
     # First check that key is what we expect. Otherwise ignore this request
-    if typed_params.key != ENV["CUTTLEFISH_WEBHOOK_KEY"]
+    if params[:key] != ENV["CUTTLEFISH_WEBHOOK_KEY"]
       head :forbidden
       return
     end
 
-    delivery_event = typed_params.delivery_event
+    delivery_event = params[:delivery_event]
     if delivery_event
-      status = delivery_event.status
+      status = delivery_event[:status]
       # We're not interested in soft bounces. So, just accept them and move on...
       if status == "soft_bounce"
         head :ok
@@ -54,35 +54,35 @@ class CuttlefishController < ApplicationController
       success = status == "delivered"
 
       # Check if this is from a comment
-      comment_id = delivery_event.email.meta_values["comment-id"]
+      comment_id = delivery_event[:email][:meta_values]["comment-id"]
       if comment_id
         comment = Comment.find(comment_id)
         comment.update!(
-          last_delivered_at: delivery_event.time,
+          last_delivered_at: delivery_event[:time],
           last_delivered_successfully: success
         )
         unless success
           NotifySlackCommentDeliveryService.call(
             comment: comment,
-            to: delivery_event.email.to,
+            to: delivery_event[:email][:to],
             status: status,
-            extended_status: delivery_event.extended_status,
-            email_id: delivery_event.email.id
+            extended_status: delivery_event[:extended_status],
+            email_id: delivery_event[:email][:id].to_i
           )
         end
       end
 
       # Check if this is from an alert
-      alert_id = delivery_event.email.meta_values["alert-id"]
+      alert_id = delivery_event[:email][:meta_values]["alert-id"]
       if alert_id
         alert = Alert.find(alert_id)
         alert.update!(
-          last_delivered_at: delivery_event.time,
+          last_delivered_at: delivery_event[:time],
           last_delivered_successfully: success
         )
         # Check if there was a hard bounce because of a bad email address
         # If that is the case we will automatically unsubscribe them here
-        alert.unsubscribe_by_bounce! if ["5.1.1", "5.1.10", "5.4.1", "5.4.4"].include?(delivery_event.dsn)
+        alert.unsubscribe_by_bounce! if ["5.1.1", "5.1.10", "5.4.1", "5.4.4"].include?(delivery_event[:dsn])
       end
     end
 
