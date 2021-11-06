babd963693f6e942ba587cca5ff83147d46f062b
Matthew Landauer <mlandauer@gmail.com>
2021-11-07 04:06:51 +1100

    bundle exec tapioca require

--- sorbet/tapioca/require.rb
+++ sorbet/tapioca/require.rb
@@ -1,10 +1,9 @@
 # typed: strict
 # frozen_string_literal: true
 
-# This file was first generated automatically by running "tapioca require"
-# then updated manually
-
 require "active_support/core_ext/integer/time"
+require "active_support/core_ext/securerandom"
+require "active_support/testing/stream"
 require "administrate/base_dashboard"
 require "administrate/custom_dashboard"
 require "administrate/field/base"
@@ -12,8 +11,10 @@ require "bootsnap/setup"
 require "bundler"
 require "bundler/setup"
 require "devise/orm/active_record"
+require "geocoder/stores/active_record"
 require "rack/throttle"
 require "rails/all"
+require "rails_autolink/helpers"
 require "rest-client"
 require "rss/1.0"
 require "rss/2.0"
@@ -21,7 +22,3 @@ require "rubygems"
 require "sidekiq/web"
 require "sorbet-runtime"
 require "zlib"
-require "active_support/core_ext/securerandom"
-require "geocoder/stores/active_record"
-require "rails_autolink/helpers"
-require "active_support/testing/stream"
