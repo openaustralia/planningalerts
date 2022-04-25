8e2712042e25df979c4acb1552e89001c7f1509a
David Black <dblack@atlassian.com>
2022-04-25 21:52:03 +1000

    Seed a Parramatta City Council application for local dev purposes.
    Signed-off-by: David Black <dblack@atlassian.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,15 @@ authority = Authority.create! full_name: "Marrickville Council",
                               website_url: "http://www.marrickville.nsw.gov.au",
                               disabled: false
 
+authorityTwo = Authority.create! full_name: "Parramatta City Council",
+                              short_name: "Parramatta",
+                              state: "NSW",
+                              email: "council@cityofparramatta.nsw.gov.au",
+                              population_2017: "243276",
+                              morph_name: "planningalerts-scrapers/multiple_epathway_scraper",
+                              website_url: "https://www.cityofparramatta.nsw.gov.au",
+                              disabled: false
+
 CreateOrUpdateApplicationService.call(
   authority: authority,
   council_reference: "DA21/0642",
@@ -24,5 +33,22 @@ CreateOrUpdateApplicationService.call(
     postcode: "2750",
     lat: -33.7543286,
     lng: 150.6486731
+  }
+)
+
+CreateOrUpdateApplicationService.call(
+  authority: authorityTwo,
+  council_reference: "DA/321/2022",
+  attributes: {
+    address: "110 Wetherill Street North, SILVERWATER NSW 2128",
+    description: "Change of use to a vehicle dismantling premises and metal car part storage facility.",
+    date_scraped: Time.zone.now,
+    info_url: "https://onlineservices.cityofparramatta.nsw.gov.au/ePathway/Prod/Web/GeneralEnquiry/EnquiryDetailView.aspx?Id=725692",
+    # Values that would normally be the result of geocoding
+    suburb: "Silverwater",
+    state: "NSW",
+    postcode: "2128",
+    lat: -33.83568859225864,
+    lng: 151.04994362636435
   }
 )
