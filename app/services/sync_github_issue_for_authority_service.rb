0603d275c573bea57389e5bdafd8cd849d85d8c4
Matthew Landauer <mlandauer@gmail.com>
2022-07-19 15:47:21 +1000

    Attaches new issue to project

--- app/services/sync_github_issue_for_authority_service.rb
+++ app/services/sync_github_issue_for_authority_service.rb
@@ -1,6 +1,9 @@
 # typed: strict
 # frozen_string_literal: true
 
+require "graphql/client"
+require "graphql/client/http"
+
 # For a broken authority create a Github issue. When the authority is working
 # again tag the Github issue as "probably fixed"
 class SyncGithubIssueForAuthorityService
@@ -10,6 +13,54 @@ class SyncGithubIssueForAuthorityService
   # See https://sorbet.org/docs/error-reference#4002
   T.unsafe(self).include Rails.application.routes.url_helpers
   include AuthoritiesHelper
+
+  # First setup all the graphql stuff
+  Http = GraphQL::Client::HTTP.new("https://api.github.com/graphql") do
+    def headers(context)
+      { "Authorization": "bearer #{ENV['GITHUB_PERSONAL_ACCESS_TOKEN']}" }
+    end
+  end
+  
+  # TODO: Put the schema file in a sensible place
+  SchemaPath = "schema.json"
+  Schema = if File.exist?(SchemaPath)
+              GraphQL::Client.load_schema(SchemaPath)
+            else 
+              schema = GraphQL::Client.load_schema(Http)
+              GraphQL::Client.dump_schema(Http, SchemaPath)
+              schema
+            end
+  
+  Client = GraphQL::Client.new(schema: Schema, execute: Http)
+
+  # First find the project we want to attach the issue to
+  ShowProjectQuery = Client.parse <<-GRAPHQL
+    query($login: String!, $number: Int!) {
+      organization(login: $login) {
+        projectV2(number: $number) {
+          id
+          fields(first: 10) {
+            nodes {
+              ... on ProjectV2FieldCommon {
+                name
+              }
+              ... on ProjectV2SingleSelectField {
+                options {
+                  name
+                }      
+              }
+            }
+          }
+        }  
+      }
+    }
+  GRAPHQL
+
+  AddIssueToProjectMutation = Client.parse <<-GRAPHQL
+    mutation($input: AddProjectV2ItemByIdInput!) {
+      addProjectV2ItemById(input: $input)
+    }
+  GRAPHQL
 
   # The repository in which we want the issues created
   REPO = T.let(
@@ -39,10 +90,23 @@ class SyncGithubIssueForAuthorityService
         github_repo: REPO,
         github_number: issue.number
       )
+      # We also want to attach the issue to a project with some custom fields which makes it
+      # easier to order / prioritise the work of fixing the scrapers
+      attach_issue_to_project(issue.node_id)
     elsif !authority.broken? && issue && !issue.closed?(client)
       logger.info "Authority #{authority.full_name} is fixed but github issue is still open. So labelling."
       issue.add_label!(client, PROBABLY_FIXED_LABEL_NAME)
     end
+  end
+
+  def attach_issue_to_project(issue_id)
+    # TODO: Make this different for development and production
+    result = Client.query(ShowProjectQuery, variables: {login: "planningalerts-scrapers", number: 4})
+    project_id = result.data.organization.project_v2.id
+
+    # Now add the issue to the project
+    result = Client.query(AddIssueToProjectMutation, variables: {input: {projectId: project_id, contentId: issue_id}})
+    # TODO: Check for errors
   end
 
   sig { params(authority: Authority, latest_date: Time).returns(String) }
