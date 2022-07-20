b4f6455dcd7f578f0795fec019ae34846d63e6b6
Matthew Landauer <mlandauer@gmail.com>
2022-07-20 12:03:46 +1000

    Simplify text in issue now that we have more information in custom fields

--- app/services/sync_github_issue_for_authority_service.rb
+++ app/services/sync_github_issue_for_authority_service.rb
@@ -105,7 +105,7 @@ class SyncGithubIssueForAuthorityService
     # not yet scraped anything
     if authority.broken? && latest_date && (issue.nil? || issue.closed?(client))
       logger.info "Creating GitHub issue for broken authority #{authority.full_name}"
-      issue = client.create_issue(REPO, title(authority, latest_date), body(authority))
+      issue = client.create_issue(REPO, title(authority), body)
       authority.create_github_issue!(
         github_repo: REPO,
         github_number: issue.number
@@ -149,18 +149,14 @@ class SyncGithubIssueForAuthorityService
     # TODO: Check for errors
   end
 
-  sig { params(authority: Authority, latest_date: Time).returns(String) }
-  def title(authority, latest_date)
-    "#{authority.full_name}: No data received since #{latest_date.strftime('%e %b %Y')}"
+  sig { params(authority: Authority).returns(String) }
+  def title(authority)
+    authority.full_name
   end
 
-  sig { params(authority: Authority).returns(String) }
-  def body(authority)
-    "This issue has been **automatically** created by PlanningAlerts for " \
-      "[#{authority.full_name}](#{authority_url(authority.short_name_encoded, host: ENV['HOST'])})\n\n" \
-      "It uses the scraper [#{authority.morph_name}](#{morph_url(authority)})\n\n" \
-      "Only close this issue once the authority is working again on PlanningAlerts. " \
-      "Otherwise a new issue will just automatically created. Also, if there is " \
-      "a duplicate issue close the other one in favour of this one."
+  sig { returns(String) }
+  def body
+    "This issue has been **automatically** created by PlanningAlerts. " \
+      "Only close this issue once the authority is working again on PlanningAlerts."
   end
 end
