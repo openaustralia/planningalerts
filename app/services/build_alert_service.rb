7f96a24b2a4e6d2c225ddbd28cfecd6def4bf896
Matthew Landauer <mlandauer@gmail.com>
2022-10-29 14:43:42 +1100

    Small refactor in preperation service handling creation of user

--- app/services/build_alert_service.rb
+++ app/services/build_alert_service.rb
@@ -4,9 +4,6 @@
 class BuildAlertService
   extend T::Sig
 
-  sig { returns(Alert) }
-  attr_reader :alert
-
   sig { params(user: User, address: String, radius_meters: Integer).returns(T.nilable(Alert)) }
   def self.call(user:, address:, radius_meters:)
     new(user: user, address: address, radius_meters: radius_meters).call
@@ -14,28 +11,30 @@ class BuildAlertService
 
   sig { params(user: User, address: String, radius_meters: Integer).void }
   def initialize(user:, address:, radius_meters:)
-    @alert = T.let(
-      Alert.new(
-        email: user.email,
-        user: user,
-        address: address,
-        radius_meters: radius_meters
-      ),
-      Alert
-    )
+    @email = T.let(user.email, String)
+    @user = user
+    @address = address
+    @radius_meters = radius_meters
   end
 
   sig { returns(T.nilable(Alert)) }
   def call
+    alert = Alert.new(
+      email: @email,
+      user: @user,
+      address: @address,
+      radius_meters: @radius_meters
+    )
+
     # Ensures the address is normalised into a consistent form
     alert.geocode_from_address
 
-    a = preexisting_alert
+    a = preexisting_alert(alert)
     if a
       if a.confirmed?
-        send_notice_to_existing_active_alert_owner
+        send_notice_to_existing_active_alert_owner(alert)
       else
-        resend_original_confirmation_email
+        resend_original_confirmation_email(alert)
       end
       nil
     else
@@ -47,8 +46,8 @@ class BuildAlertService
 
   # Matching alerts that have been made before including those that haven't
   # been confirmed yet
-  sig { returns(T.nilable(Alert)) }
-  def preexisting_alert
+  sig { params(alert: Alert).returns(T.nilable(Alert)) }
+  def preexisting_alert(alert)
     Alert.find_by(
       email: alert.email,
       address: alert.address,
@@ -56,15 +55,15 @@ class BuildAlertService
     )
   end
 
-  sig { void }
-  def send_notice_to_existing_active_alert_owner
+  sig { params(alert: Alert).void }
+  def send_notice_to_existing_active_alert_owner(alert)
     AlertMailer.new_signup_attempt_notice(
-      T.must(preexisting_alert)
+      T.must(preexisting_alert(alert))
     ).deliver_later
   end
 
-  sig { void }
-  def resend_original_confirmation_email
-    T.must(preexisting_alert).send_confirmation_email
+  sig { params(alert: Alert).void }
+  def resend_original_confirmation_email(alert)
+    T.must(preexisting_alert(alert)).send_confirmation_email
   end
 end
