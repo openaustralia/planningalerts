2d4a7c3aadac2165bfa07e7de2dc36cf0613910d
Matthew Landauer <mlandauer@gmail.com>
2022-10-31 12:41:55 +1100

    Also remove ability to edit email for alert in admin interface

--- app/dashboards/alert_dashboard.rb
+++ app/dashboards/alert_dashboard.rb
@@ -68,7 +68,6 @@ class AlertDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = T.let(%i[
-    email
     address
     last_sent
     lat
