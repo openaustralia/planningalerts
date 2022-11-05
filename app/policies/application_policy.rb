234eb6899d33ac65e18fc77b7ed0b89a5d369f4a
Matthew Landauer <mlandauer@gmail.com>
2022-11-05 13:22:53 +1100

    Add types to ApplicationPolicy

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -1,53 +1,78 @@
+# typed: strict
 # frozen_string_literal: true
 
 class ApplicationPolicy
-  attr_reader :user, :record
+  extend T::Sig
 
+  sig { returns(User) }
+  attr_reader :user
+
+  sig { returns(T.untyped) }
+  attr_reader :record
+
+  sig { params(user: User, record: T.untyped).void }
   def initialize(user, record)
     @user = user
     @record = record
   end
 
+  sig { returns(T::Boolean) }
   def index?
     false
   end
 
+  sig { returns(T::Boolean) }
   def show?
     false
   end
 
+  sig { returns(T::Boolean) }
   def create?
     false
   end
 
+  sig { returns(T::Boolean) }
   def new?
     create?
   end
 
+  sig { returns(T::Boolean) }
   def update?
     false
   end
 
+  sig { returns(T::Boolean) }
   def edit?
     update?
   end
 
+  sig { returns(T::Boolean) }
   def destroy?
     false
   end
 
   class Scope
+    extend T::Sig
+
+    sig { params(user: User, scope: T.untyped).void }
     def initialize(user, scope)
       @user = user
       @scope = scope
     end
 
+    # TODO: Fix the return type on this
+    sig { void }
     def resolve
       raise NotImplementedError, "You must define #resolve in #{self.class}"
     end
 
     private
 
-    attr_reader :user, :scope
+    sig { returns(User) }
+    attr_reader :user
+
+    # TODO: Fix the return type on this
+    sig { returns(T.untyped) }
+    attr_reader :scope
   end
 end
