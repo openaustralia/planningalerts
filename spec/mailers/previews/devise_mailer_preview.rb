6cd9533e4b2398a509e5c3187180e813f4f735c4
Matthew Landauer <mlandauer@gmail.com>
2022-11-17 14:41:59 +1100

    Add previews of devise emails as well

---
+++ spec/mailers/previews/devise_mailer_preview.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# You can preview the mails by browsing to http://localhost:3000/rails/mailers
+
+require "factory_bot_rails"
+
+class DeviseMailerPreview < ActionMailer::Preview
+  # It shows a different email address in the "to" and the body of the email.
+  # TODO: Figure out what's going on and fix it
+  def confirmation_instructions
+    user = FactoryBot.build_stubbed(:user)
+    Devise::Mailer.confirmation_instructions(user, "faketoken")
+  end
+
+  # It shows a different email address in the "to" and the body of the email.
+  # TODO: Figure out what's going on and fix it
+  def reset_password_instructions
+    user = FactoryBot.build_stubbed(:user)
+    Devise::Mailer.reset_password_instructions(user, "faketoken")
+  end
+end
