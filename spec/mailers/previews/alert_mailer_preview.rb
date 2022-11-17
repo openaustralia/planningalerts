6c9f440334696ca864b5f0e77017fdd9a2a031b3
Matthew Landauer <mlandauer@gmail.com>
2022-11-17 13:49:43 +1100

    Fix alert mailer preview and don't make it depend on data in the database

--- spec/mailers/previews/alert_mailer_preview.rb
+++ spec/mailers/previews/alert_mailer_preview.rb
@@ -4,20 +4,35 @@
 
 class AlertMailerPreview < ActionMailer::Preview
   def alert
-    # Not using Factory here because it seems to be causing problems
-    # with the module reloading
-    alert = Alert.create!(
+    alert = Alert.new(
       lat: -33.902723,
       lng: 151.163362,
       radius_meters: 1000,
-      email: "mary@example.com",
-      address: "1 Illawarra Road Marrickville 2204"
+      user: User.new(email: "mary@example.com"),
+      address: "1 Illawarra Road Marrickville 2204",
+      confirm_id: "1234"
     )
-    # This needs to have an application and a comment loaded for this to work
-    applications = [Application.first]
-    comments = [Comment.first]
-    mail = AlertMailer.alert(alert, applications, comments)
-    alert.destroy
-    mail
+    application = Application.new(
+      id: 1,
+      current_version: ApplicationVersion.new(
+        address: "50 Illawarra Road Marrickville 2204",
+        lat: -33.904130,
+        lng: 151.161630,
+        description: "Something is happening here"
+      )
+    )
+    comment = Comment.new(
+      application: Application.new(
+        id: 2,
+        current_version: ApplicationVersion.new(
+          description: "Erection of a bouncy castle",
+          address: "20 Illawarra Road Marrickville 2204"
+        )
+      ),
+      text: "I really don't like inflatable things",
+      name: "Martha"
+    )
+
+    AlertMailer.alert(alert, [application], [comment])
   end
 end
