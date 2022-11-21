806f70b93fa4995a1d2ad865689d675e2bd58b01
Matthew Landauer <mlandauer@gmail.com>
2022-11-21 13:34:55 +1100

    Don't use minimal layout when editing your profile

--- app/controllers/my_base_devise_controller.rb
+++ app/controllers/my_base_devise_controller.rb
@@ -2,5 +2,19 @@
 # frozen_string_literal: true
 
 class MyBaseDeviseController < ApplicationController
-  layout "minimal"
+  extend T::Sig
+
+  layout :devise_layout
+
+  private
+
+  sig { returns(String) }
+  def devise_layout
+    # For one particular devise action (editing a user profile) we don't want to use the "minimal" layout
+    if controller_name == "registrations" && action_name == "edit"
+      "standard"
+    else
+      "minimal"
+    end
+  end
 end
