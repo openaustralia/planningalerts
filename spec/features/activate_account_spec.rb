1032b3b2d3bfdb81bdc0073782ca081b5ef44119
Matthew Landauer <mlandauer@gmail.com>
2022-11-24 10:11:28 +1100

    Rejig tests for partial new activation flow

--- spec/features/activate_account_spec.rb
+++ spec/features/activate_account_spec.rb
@@ -3,30 +3,43 @@
 require "spec_helper"
 
 describe "Activate account" do
-  it "Successfully does a normal password reset on account" do
-    create(:confirmed_user, email: "matthew@oaf.org.au")
-    encrypted_password = User.find_by(email: "matthew@oaf.org.au").encrypted_password
+  context "with a confirmed user that has not been activated" do
+    before do
+      u = User.new(email: "matthew@oaf.org.au", from_alert_or_comment: true, confirmed_at: Time.zone.now)
+      u.skip_confirmation_notification!
+      u.save!(validate: false)
+    end
 
-    visit "/users/password/new"
-    fill_in "Email", with: "matthew@oaf.org.au"
-    click_button "Send me reset password instructions"
+    it "Successfully does an account activation" do
+      visit "/users/activation/new"
+      fill_in "Email", with: "matthew@oaf.org.au"
+      click_button "Send me account activation instructions"
 
-    expect(page).to have_content "If your email address exists in our database, you will receive a password recovery link"
+      expect(page).to have_content "If your email address exists in our database, you will receive an account activation link"
 
-    expect(unread_emails_for("matthew@oaf.org.au").size).to eq 1
-    open_email("matthew@oaf.org.au")
+      expect(unread_emails_for("matthew@oaf.org.au").size).to eq 1
+      open_email("matthew@oaf.org.au")
 
-    expect(current_email).to have_subject("Reset password instructions")
-    expect(current_email.default_part_body.to_s).to include("Someone has requested a link to change your password. You can do this through the link below.")
+      expect(current_email).to have_subject("Account activation instructions")
+      expect(current_email.default_part_body.to_s).to include("Someone has requested a link to activate your account. You can do this through the link below.")
 
-    visit_in_email("Change my password")
+      # visit_in_email("Activate my account")
 
-    fill_in "New password", with: "my new password"
-    click_button "Change my password"
+      # fill_in "New password", with: "my new password"
+      # click_button "Change my password"
 
-    expect(page).to have_content "Your password has been changed successfully. You are now signed in"
+      # expect(page).to have_content "Your password has been changed successfully. You are now signed in"
 
-    # The password should have changed
-    expect(User.find_by(email: "matthew@oaf.org.au").encrypted_password).not_to eq encrypted_password
+      # # The password should have changed
+      # expect(User.find_by(email: "matthew@oaf.org.au").encrypted_password).not_to eq encrypted_password
+    end
+  end
+
+  context "with an already activated user" do
+    before do
+      create(:confirmed_user, email: "matthew@oaf.org.au")
+    end
+
+    pending "should get a different email"
   end
 end
