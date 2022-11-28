853d726bf4754a77ba0248990e0ba3655a1137bd
Matthew Landauer <mlandauer@gmail.com>
2022-11-28 14:36:48 +1100

    Redirect back to the last page when signing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,9 @@ class ApplicationController < ActionController::Base
 
   before_action :set_header_variable
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # This stores the location on every request so that we can always redirect back after logging in
+  # See https://github.com/heartcombo/devise/wiki/How-To:-%5BRedirect-back-to-current-page-after-sign-in,-sign-out,-sign-up,-update%5D
+  before_action :store_user_location!, if: :storable_location?
 
   sig { void }
   def authenticate_active_admin_user!
@@ -50,5 +53,27 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
+
+  # Its important that the location is NOT stored if:
+  # - The request method is not GET (non idempotent)
+  # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an
+  #    infinite redirect loop.
+  # - The request is an Ajax request as this can lead to very unexpected behaviour.
+  # - The request is not a Turbo Frame request ([turbo-rails](https://github.com/hotwired/turbo-rails/blob/main/app/controllers/turbo/frames/frame_request.rb))
+  sig { returns(T::Boolean) }
+  def storable_location?
+    request.get? &&
+      is_navigational_format? &&
+      !devise_controller? &&
+      controller_name != "activations" &&
+      !request.xhr?
+    # &&
+    # !turbo_frame_request?
+  end
+
+  sig { void }
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
   end
 end
