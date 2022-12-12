ab00ccfa1e761f1883a2826b8f12243b91e4a19e
Matthew Landauer <mlandauer@gmail.com>
2022-12-13 09:49:42 +1100

    Remove duplication in javascript for disclosures in commenting

--- app/assets/javascripts/applications.js
+++ app/assets/javascripts/applications.js
@@ -1,16 +1,7 @@
 // Javascript used on applications pages
-$('#comment_address_input a').click(function(e) {
-  e.preventDefault();
-  $('#faq_commenting_address').slideToggle('fast');
-});
-
-$('#comment_text_input a').click(function(e) {
-  e.preventDefault();
-  $('#disclosure_explanation').slideToggle('fast');
-});
 
 $('a.hideable').click(function(e) {
   e.preventDefault();
-  target = $(e.target).attr("data-target");
+  var target = $(e.target).attr("data-target");
   $(target).slideToggle('fast');
 });
