57da99cc9973699f0fd2521fcfb0c630c3a9739a
Matthew Landauer <mlandauer@gmail.com>
2022-12-14 03:28:46 +1100

    Fixup view of geocode queries

--- app/controllers/geocode_queries_controller.rb
+++ app/controllers/geocode_queries_controller.rb
@@ -1,20 +1,24 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 class GeocodeQueriesController < ApplicationController
+  extend T::Sig
+
+  sig { void }
   def index
     q = GeocodeQuery.all.order(created_at: :desc).includes(:geocode_results)
     respond_to do |format|
       format.html do
-        @geocode_queries = q.paginate(page: params[:page], per_page: 50)
+        @geocode_queries = T.let(q.page(params[:page]).per(50), T.nilable(ActiveRecord::Relation))
       end
       format.csv do
-        send_data q.to_csv, filename: "geocode-queries-#{Time.zone.today}.csv"
+        send_data GeocodeQuery.to_csv, filename: "geocode-queries-#{Time.zone.today}.csv"
       end
     end
   end
 
+  sig { void }
   def show
-    @geocode_query = GeocodeQuery.find(params[:id])
+    @geocode_query = T.let(GeocodeQuery.find(params[:id]), T.nilable(GeocodeQuery))
   end
 end
