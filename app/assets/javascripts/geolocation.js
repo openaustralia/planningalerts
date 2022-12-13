bbf53b81137114bb7cd260b5e756fc9ce370f4ce
Matthew Landauer <mlandauer@gmail.com>
2022-12-14 04:11:42 +1100

    Use plain javascript instead of jquery

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -1,37 +1,40 @@
 // This depends on there being a link with the id #geolocate with a
 // .spinner inside of it that is hidden by default in the css
 
-$(function() {
-  if (navigator.geolocation) {
-    $("#geolocate").css('visibility', 'visible');
-  }
+window.addEventListener("DOMContentLoaded", function() {
+  var geolocate = this.document.getElementById("geolocate");
 
-  $("#geolocate").click(function(e){
-    var link = $(this);
-    e.preventDefault();
-    link.find(".spinner").css('visibility', 'visible');
-    navigator.geolocation.getCurrentPosition(function(pos) {
-      var latlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
-      geocoder = new google.maps.Geocoder();
-      geocoder.geocode({'latLng': latlng}, function(results, status){
-        if (status == google.maps.GeocoderStatus.OK) {
-          location.href = '/?q=' + encodeURIComponent(results[0].formatted_address);
-        } else {
-          link.find(".spinner").css('visibility', 'hidden');
-          link.html("Address lookup failed: " + status);
+  if (geolocate && navigator.geolocation) {
+    geolocate.style.visibility = "visible";
+  
+    geolocate.addEventListener("click", function(e) {
+      var link = this;
+      var spinner = this.querySelector(".spinner");
+      e.preventDefault();
+      spinner.style.visibility = "visible";
+      navigator.geolocation.getCurrentPosition(function(pos) {
+        var latlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+        geocoder = new google.maps.Geocoder();
+        geocoder.geocode({'latLng': latlng}, function(results, status){
+          if (status == google.maps.GeocoderStatus.OK) {
+            location.href = '/?q=' + encodeURIComponent(results[0].formatted_address);
+          } else {
+            spinner.style.visibility = "hidden";
+            link.innerHTML = "Address lookup failed: " + status;
+          }
+        });
+      }, function(err) {
+        spinner.style.visibility = "hidden";
+        if (err.code == 1) { // User said no
+          link.innerHTML = "You declined; please fill in the box above";
+        } else if (err.code == 2) { // No position
+          link.innerHTML = "Could not look up location";
+        } else if (err.code == 3) { // Too long
+          link.innerHTML = "No result returned";
+        } else { // Unknown
+          link.innerHTML = "Unknown error";
         }
-      });
-    }, function(err) {
-      link.find(".spinner").css('visibility', 'hidden');
-      if (err.code == 1) { // User said no
-          link.html("You declined; please fill in the box above");
-      } else if (err.code == 2) { // No position
-          link.html("Could not look up location");
-      } else if (err.code == 3) { // Too long
-          link.html("No result returned");
-      } else { // Unknown
-          link.html("Unknown error");
-      }
-    }, {enableHighAccuracy: true, timeout: 10000});
-  });
+      }, {enableHighAccuracy: true, timeout: 10000});
+    });
+  }
 });
