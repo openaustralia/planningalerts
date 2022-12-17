618772a07e885546d3058c33da787ab1a327690f
Matthew Landauer <mlandauer@gmail.com>
2022-12-18 04:32:45 +1100

    Remove unused code and inline method

--- app/services/top_usage_api_users_service.rb
+++ app/services/top_usage_api_users_service.rb
@@ -4,11 +4,6 @@
 class TopUsageApiUsersService
   extend T::Sig
 
-  class ApiKeyRequests < T::Struct
-    const :api_key, String
-    const :requests, Integer
-  end
-
   class ApiKeyObjectRequests < T::Struct
     const :api_key_object, ApiKey
     const :requests, Integer
@@ -16,11 +11,6 @@ class TopUsageApiUsersService
 
   sig { params(date_from: Date, date_to: Date, number: Integer).returns(T::Array[ApiKeyObjectRequests]) }
   def self.call(date_from:, date_to:, number:)
-    new.call(date_from: date_from, date_to: date_to, number: number)
-  end
-
-  sig { params(date_from: Date, date_to: Date, number: Integer).returns(T::Array[ApiKeyObjectRequests]) }
-  def call(date_from:, date_to:, number:)
     DailyApiUsage.where(date: date_from..date_to).group(:api_key_id).order("SUM(count) DESC").limit(number).sum(:count).map do |api_key_id, sum|
       ApiKeyObjectRequests.new(requests: sum, api_key_object: ApiKey.find(api_key_id))
     end
