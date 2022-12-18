0951d7f8eaf5bd4124c5d61cbf4a98cbd42c8b14
Matthew Landauer <mlandauer@gmail.com>
2022-12-19 02:24:31 +1100

    Show average daily requests
    Easier to make sense of when comparing to the daily limit

--- app/models/daily_api_usage.rb
+++ app/models/daily_api_usage.rb
@@ -11,4 +11,10 @@ class DailyApiUsage < ApplicationRecord
     count = where(date: date_from..date_to).group(:api_key_id).order("SUM(count) DESC").limit(number).sum(:count)
     count.transform_keys { |id| ApiKey.find(id) }
   end
+
+  sig { params(date_from: Date, date_to: Date, number: Integer).returns(T::Hash[ApiKey, Float]) }
+  def self.top_average_usage_in_date_range(date_from:, date_to:, number:)
+    r = top_usage_in_date_range(date_from: date_from, date_to: date_to, number: number)
+    r.transform_values { |v| v.to_f / (date_to - date_from + 1) }
+  end
 end
