e2c7bb404cdc1fd0cd8443c9cdf1cd8b26cf06db
Matthew Landauer <mlandauer@gmail.com>
2023-01-04 00:00:37 +0000

    Skip over background job if the alert has been deleted

--- app/jobs/process_alert_job.rb
+++ app/jobs/process_alert_job.rb
@@ -8,7 +8,11 @@ class ProcessAlertJob < ApplicationJob
 
   sig { params(id: Integer).void }
   def perform(id)
-    alert = Alert.find(id)
+    alert = Alert.find_by(id:)
+    # If the alert has been deleted, for example by a user removing their account
+    # then just skip over and don't cause any kind of error
+    return if alert.nil?
+
     ProcessAlertAndRecordStatsService.call(alert:)
   end
 end
