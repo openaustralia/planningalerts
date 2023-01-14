680a7816ceffc05cf88db5df475a01a86fc4df44
Matthew Landauer <mlandauer@gmail.com>
2023-01-14 21:21:47 +0000

    Don't allow the queue up jobs to retry

--- app/jobs/queue_up_import_applications_job.rb
+++ app/jobs/queue_up_import_applications_job.rb
@@ -3,10 +3,16 @@
 
 class QueueUpImportApplicationsJob < ApplicationJob
   extend T::Sig
+  include Sidekiq::Job
+
+  # Hopefully it's unlikely that this job will fail, since it only queues up other jobs
+  # But if it were to fail we don't want it to retry automatically in case we end with
+  # a bazillion queued jobs. Also, this job gets run once per day anyway so it will run
+  # again
+  sidekiq_options retry: 0
 
   sig { void }
   def perform
-    # TODO: Need some kind of safety mechanism to handle failures on this so that we don't get a bazillion queued jobs
     QueueUpJobsOverTimeService.call(ImportApplicationsJob, 24.hours, Authority.active.all.to_a)
   end
 end
--- app/jobs/queue_up_process_alerts_job.rb
+++ app/jobs/queue_up_process_alerts_job.rb
@@ -3,10 +3,16 @@
 
 class QueueUpProcessAlertsJob < ApplicationJob
   extend T::Sig
+  include Sidekiq::Job
+
+  # Hopefully it's unlikely that this job will fail, since it only queues up other jobs
+  # But if it were to fail we don't want it to retry automatically in case we end with
+  # a bazillion queued jobs. Also, this job gets run once per day anyway so it will run
+  # again
+  sidekiq_options retry: 0
 
   sig { void }
   def perform
-    # TODO: Need some kind of safety mechanism to handle failures on this so that we don't get a bazillion queued jobs
     QueueUpJobsOverTimeService.call(ProcessAlertJob, 24.hours, Alert.active.pluck(:id).shuffle)
   end
 end
