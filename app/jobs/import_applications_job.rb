6a12bb28b2901ab744b4501207c55300414eb437
Matthew Landauer <mlandauer@gmail.com>
2023-01-15 17:55:11 +0000

    Add TODOs

--- app/jobs/import_applications_job.rb
+++ app/jobs/import_applications_job.rb
@@ -1,6 +1,8 @@
 # typed: strict
 # frozen_string_literal: true
 
+# TODO: Move over to using a sidekiq job directly (rather than active job)
+# TODO: use sidekiq-unique-jobs to ensure that we can't have more than one ImportApplicationsJob running at a time
 class ImportApplicationsJob < ApplicationJob
   extend T::Sig
 
