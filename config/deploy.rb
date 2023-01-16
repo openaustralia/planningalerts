cf5d942bdee4dfb855b5550c87b472d50f485cae
Matthew Landauer <mlandauer@gmail.com>
2023-01-16 22:59:35 +0000

    We're not actually using new relic here anymore

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :repo_url, "https://github.com/openaustralia/planningalerts.git"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", 'config/master.key'
-append :linked_files, "config/database.yml", "config/newrelic.yml", ".env.production", "public/sitemap.xml"
+append :linked_files, "config/database.yml", ".env.production", "public/sitemap.xml"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
