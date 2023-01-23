bbb8c17320ab6431513a9cebd7a504fd6892bcb8
Matthew Landauer <mlandauer@gmail.com>
2023-01-23 01:36:43 +0000

    Switch to fira sans as default font

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Fira Sans', ...defaultTheme.fontFamily.sans]
       },
     },
     colors: {
