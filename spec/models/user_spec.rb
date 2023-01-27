db3ce54b530a7c6e9fca28e79e30f75c1d13fcba
Matthew Landauer <mlandauer@gmail.com>
2023-01-27 17:09:45 +0000

    Add some simple tests to check devise configuration

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,14 @@ describe User do
       expect(user).not_to be_valid
     end
   end
+
+  it "strips whitespace on save for email" do
+    user = create(:user, email: "  foo@bar.com ")
+    expect(user.email).to eq "foo@bar.com"
+  end
+
+  it "always stores emails in lowercase" do
+    user = create(:user, email: "FOO@BAR.COM")
+    expect(user.email).to eq "foo@bar.com"
+  end
 end
