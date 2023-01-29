bdc8a83a7bf45afb2283d517452a5b57e6e4efff
Matthew Landauer <mlandauer@gmail.com>
2023-01-29 22:49:38 +0000

    Make a crude first version of a drop down menu for sort order
    The idea is to make it clearer what is actually being sorted currently and that there are options

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,13 @@ document.querySelectorAll(".hideable").forEach(function(e) {
     document.querySelector(target).classList.toggle("hideable-target-show");  
   })
 })
+
+document.querySelectorAll(".dropdown-toggle").forEach(function(e) {
+  e.addEventListener("click", function(e) {
+    e.preventDefault();
+    console.log("Clicked");
+    console.log(e.target.parentElement);
+    // TODO: Should really check that parent has dropdown class as we would expect
+    e.target.parentElement.classList.toggle("open");
+  })
+})
