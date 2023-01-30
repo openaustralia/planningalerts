4cceb6a023982443cbfe9bd058cfca18f258798e
Matthew Landauer <mlandauer@gmail.com>
2023-01-30 01:15:02 +0000

    Use alpine.js to add javascript sprinkles on dropdown instead

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,7 +4,6 @@
 //= require maps
 // Need ujs for confirmation on buttons
 //= require rails-ujs
-//= require dropdown
 
 document.querySelectorAll(".hideable").forEach(function(e) {
   e.addEventListener("click", function(e) {
--- app/assets/javascripts/dropdown.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".dropdown-toggle").forEach(function(e) {
-  e.addEventListener("click", function(e) {
-    e.preventDefault();
-    e.target.closest(".dropdown").classList.toggle("open");
-  })
-})
