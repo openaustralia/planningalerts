23175ed0f6c295c0ef7626f45142dd011677f2ae
Matthew Landauer <mlandauer@gmail.com>
2023-01-30 02:53:31 +0000

    extract promise based version of geolocate api

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -1,6 +1,12 @@
 // This depends on there being a link with the id #geolocate with a
 // .spinner inside of it that is hidden by default in the css
 
+function getPosition(options) {
+  return new Promise((resolve, reject) =>
+    navigator.geolocation.getCurrentPosition(resolve, reject, options)
+  );
+}
+
 window.addEventListener("DOMContentLoaded", function() {
   var geolocate = this.document.getElementById("geolocate");
 
@@ -12,29 +18,31 @@ window.addEventListener("DOMContentLoaded", function() {
       var spinner = this.querySelector(".spinner");
       e.preventDefault();
       spinner.style.visibility = "visible";
-      navigator.geolocation.getCurrentPosition(function(pos) {
-        var latlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
-        geocoder = new google.maps.Geocoder();
-        geocoder.geocode({'latLng': latlng}, function(results, status){
-          if (status == google.maps.GeocoderStatus.OK) {
-            location.href = '/?q=' + encodeURIComponent(results[0].formatted_address);
-          } else {
-            spinner.style.visibility = "hidden";
-            link.innerHTML = "Address lookup failed: " + status;
-          }
+      getPosition({enableHighAccuracy: true, timeout: 10000})
+        .then((pos) => {
+          var latlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+          geocoder = new google.maps.Geocoder();
+          geocoder.geocode({'latLng': latlng}, function(results, status){
+            if (status == google.maps.GeocoderStatus.OK) {
+              location.href = '/?q=' + encodeURIComponent(results[0].formatted_address);
+            } else {
+              spinner.style.visibility = "hidden";
+              link.innerHTML = "Address lookup failed: " + status;
+            }
+          });  
+        })
+        .catch((err) => {
+          spinner.style.visibility = "hidden";
+          if (err.code == 1) { // User said no
+            link.innerHTML = "You declined; please fill in the box above";
+          } else if (err.code == 2) { // No position
+            link.innerHTML = "Could not look up location";
+          } else if (err.code == 3) { // Too long
+            link.innerHTML = "No result returned";
+          } else { // Unknown
+            link.innerHTML = "Unknown error";
+          }  
         });
-      }, function(err) {
-        spinner.style.visibility = "hidden";
-        if (err.code == 1) { // User said no
-          link.innerHTML = "You declined; please fill in the box above";
-        } else if (err.code == 2) { // No position
-          link.innerHTML = "Could not look up location";
-        } else if (err.code == 3) { // Too long
-          link.innerHTML = "No result returned";
-        } else { // Unknown
-          link.innerHTML = "Unknown error";
-        }
-      }, {enableHighAccuracy: true, timeout: 10000});
     });
   }
 });
