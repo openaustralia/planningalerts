0ce0356bef03ff4b43b73134f70b3106cc442434
Matthew Landauer <mlandauer@gmail.com>
2023-01-31 22:48:12 +0000

    Add extra flipper option for new address search options

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -4,7 +4,8 @@ Flipper::UI.configure do |config|
   config.descriptions_source = lambda do |_keys|
     # This should be a complete list of all features being currently used in the codebase
     {
-      "switch_themes" => "Can switch to new in progress design for the site"
+      "switch_themes" => "Can switch to new in progress design for the site",
+      "extra_options_on_address_search" => "Add extra options to filter by time/space when searching address"
     }
   end
 
