36ae4af1949f0451a6cbec62ceaf754bfafd5649
Matthew Landauer <mlandauer@gmail.com>
2023-02-01 06:00:39 +0000

    Make this one component - hide the other two

--- app/components/dropdown_component.rb
+++ app/components/dropdown_component.rb
@@ -3,5 +3,19 @@
 
 class DropdownComponent < ViewComponent::Base
   renders_one :button
-  renders_one :menu, DropdownMenuComponent
+  renders_one :menu, "DropdownMenuComponent"
+
+  class DropdownMenuComponent < ViewComponent::Base
+    renders_many :items, "DropdownMenuItemComponent"
+
+    class DropdownMenuItemComponent < ViewComponent::Base
+      extend T::Sig
+
+      sig { params(disabled: T::Boolean).void }
+      def initialize(disabled:)
+        super
+        @disabled = disabled
+      end
+    end
+  end
 end
--- app/components/dropdown_menu_component.rb
+++
@@ -1,6 +0,0 @@
-# typed: strict
-# frozen_string_literal: true
-
-class DropdownMenuComponent < ViewComponent::Base
-  renders_many :items, DropdownMenuItemComponent
-end
