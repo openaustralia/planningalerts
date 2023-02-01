f8b2c9ece897c971e18478da3979369092829c39
Matthew Landauer <mlandauer@gmail.com>
2023-02-01 05:21:58 +0000

    Extracting our first view component

--- app/components/dropdown_menu_item_component.rb
+++ app/components/dropdown_menu_item_component.rb
@@ -2,7 +2,11 @@
 # frozen_string_literal: true
 
 class DropdownMenuItemComponent < ViewComponent::Base
+  extend T::Sig
+
+  sig { params(disabled: T::Boolean).void }
   def initialize(disabled:)
+    super
     @disabled = disabled
   end
 end
