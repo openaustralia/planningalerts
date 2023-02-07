ca5c8509114b9e5efa1effd55033e4f8d5d00dee
Matthew Landauer <mlandauer@gmail.com>
2023-02-07 22:39:00 +0000

    Make sidekiq logging less verbose

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,6 +3,10 @@ require 'sidekiq'
 
 Sidekiq.configure_server do |config|
   config.redis = Rails.configuration.redis
+  # The default log level of INFO was too verbose in production and was causing
+  # the syslog to get too big if we were running a job that updates the applications
+  # and so reindexes and so causes sidekiq INFO messages to appear by their hundreds
+  config.logger_level = Logger::WARN
 end
 
 Sidekiq.configure_client do |config|
