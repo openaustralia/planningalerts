94cfb6f14bef491ac91c706620c1cddd3bf16879
Matthew Landauer <mlandauer@gmail.com>
2023-02-07 22:40:20 +0000

    Fix typo

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,7 +6,7 @@ Sidekiq.configure_server do |config|
   # The default log level of INFO was too verbose in production and was causing
   # the syslog to get too big if we were running a job that updates the applications
   # and so reindexes and so causes sidekiq INFO messages to appear by their hundreds
-  config.logger_level = Logger::WARN
+  config.logger.level = Logger::WARN
 end
 
 Sidekiq.configure_client do |config|
