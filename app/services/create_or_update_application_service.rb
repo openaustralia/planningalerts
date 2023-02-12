438648e52a998a7e07651a27f61894b795af3ce4
Matthew Landauer <mlandauer@gmail.com>
2023-02-12 18:12:28 +0000

    Remove extraneous gumph around the edges

--- app/services/create_or_update_application_service.rb
+++ app/services/create_or_update_application_service.rb
@@ -5,10 +5,11 @@ class CreateOrUpdateApplicationService
   extend T::Sig
 
   VALID_ATTRIBUTES = T.let(
-    %w[address description info_url date_received on_notice_from on_notice_to date_scraped lat lng suburb state postcode comment_email comment_authority].freeze,
-    T::Array[String]
+    %i[address description info_url date_received on_notice_from on_notice_to date_scraped lat lng suburb state postcode comment_email comment_authority].freeze,
+    T::Array[Symbol]
   )
 
+  # Returns created or updated application
   sig do
     params(
       authority: Authority,
@@ -17,63 +18,33 @@ class CreateOrUpdateApplicationService
     ).returns(Application)
   end
   def self.call(authority:, council_reference:, attributes:)
-    new(
-      authority:,
-      council_reference:,
-      attributes:
-    ).call
-  end
-
-  sig { params(authority: Authority, council_reference: String, attributes: T::Hash[Symbol, T.untyped]).void }
-  def initialize(
-    authority:, council_reference:, attributes:
-  )
-    @authority = authority
-    @council_reference = council_reference
-    @attributes = T.let(attributes.stringify_keys, T::Hash[String, T.untyped])
-
-    @attributes.each_key do |key|
+    attributes.each_key do |key|
       raise "Invalid attribute #{key}" unless VALID_ATTRIBUTES.include?(key)
     end
-  end
 
-  # Returns created or updated application
-  sig { returns(Application) }
-  def call
     Application.transaction do
       # First check if record already exists or create a new one if it doesn't
-      application = Application.find_by(authority:, council_reference:) || Application.new(authority:, council_reference:, first_date_scraped: attributes["date_scraped"])
-      application.address = attributes["address"] if attributes.key?("address")
+      application = Application.find_by(authority:, council_reference:) || Application.new(authority:, council_reference:, first_date_scraped: attributes[:date_scraped])
+      application.address = attributes[:address] if attributes.key?(:address)
 
       # Geocode if address has changed and it hasn't already been geocoded (by passing in lat and lng from the outside)
-      @attributes = attributes.merge(ApplicationVersion.geocode_attributes(attributes["address"]).stringify_keys) if application.address_changed? && !(attributes["lat"] && attributes["lng"] && attributes["suburb"] && attributes["state"] && attributes["postcode"])
+      attributes = attributes.merge(ApplicationVersion.geocode_attributes(attributes[:address])) if application.address_changed? && !(attributes[:lat] && attributes[:lng] && attributes[:suburb] && attributes[:state] && attributes[:postcode])
 
       application.assign_attributes(attributes)
       application.save!
-      create_version(application)
+      create_version(application, attributes)
       application.reindex
       application
     end
   end
 
-  private
-
-  sig { returns(Authority) }
-  attr_reader :authority
-
-  sig { returns(String) }
-  attr_reader :council_reference
-
-  sig { returns(T::Hash[String, T.untyped]) }
-  attr_reader :attributes
-
-  sig { params(application: Application).void }
-  def create_version(application)
+  sig { params(application: Application, attributes: T::Hash[Symbol, T.untyped]).void }
+  def self.create_version(application, attributes)
     previous_version = application.current_version
     new_version = ApplicationVersion.build_version(
       application_id: T.must(application.id),
       previous_version:,
-      attributes:
+      attributes: attributes.stringify_keys
     )
 
     # If none of the data has changed don't save the new version
