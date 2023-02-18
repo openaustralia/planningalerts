02fb0b36ee488a5de1b0158f538dba404b4d5326
Matthew Landauer <mlandauer@gmail.com>
2023-02-18 19:22:16 +0000

    Revert "Massively speed up list of authorities page"
    This reverts commit 71872cd4118dd4c37626ca67c196ee7e1e71791f.

--- app/controllers/authorities_controller.rb
+++ app/controllers/authorities_controller.rb
@@ -11,8 +11,6 @@ class AuthoritiesController < ApplicationController
     @authorities = T.let(Authority.enabled, T.untyped)
     @authorities = @authorities.order(population_2017: :desc) if params[:order] == "population"
     @authorities = @authorities.order(:state, :full_name)
-    # This is faster than calling broken? on each authority in turn
-    @working_authority_ids = T.let(Authority.enabled.joins(:applications).merge(Application.recent).group(:id).pluck(:id), T.nilable(T::Array[Integer]))
   end
 
   sig { void }
--- spec/views/authorities/index_spec.rb
+++ spec/views/authorities/index_spec.rb
@@ -4,10 +4,9 @@ require "spec_helper"
 
 describe "authorities/index.haml" do
   it "shows a list of the authorities" do
-    a1 = mock_model(Authority, id: 1, full_name: "Wombat District Council", short_name_encoded: "wombat", covered?: false)
-    a2 = mock_model(Authority, id: 2, full_name: "Kangaroo City Council", short_name_encoded: "kangaroo", covered?: false)
+    a1 = mock_model(Authority, full_name: "Wombat District Council", short_name_encoded: "wombat", broken?: false, covered?: false)
+    a2 = mock_model(Authority, full_name: "Kangaroo City Council", short_name_encoded: "kangaroo", broken?: false, covered?: false)
     assign(:authorities, [a1, a2])
-    assign(:working_authority_ids, [1, 2])
     render
     expect(rendered).to have_content(a1.full_name)
     expect(rendered).to have_content(a2.full_name)
