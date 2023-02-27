ac00561c8d3d9bdfc86e74a63369c716a9a34392
Matthew Landauer <mlandauer@gmail.com>
2023-02-27 18:00:45 +0000

    add new wikidata_id column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_190615) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_175836) do
   create_table "active_admin_comments", id: :integer, charset: "utf8mb4", force: :cascade do |t|
     t.string "resource_id", default: "", null: false
     t.string "resource_type", default: "", null: false
@@ -142,6 +142,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_190615) do
     t.string "scraper_authority_label", comment: "For scrapers for multiple authorities filter by this label"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "wikidata_id"
     t.index ["short_name"], name: "short_name_unique", unique: true
   end
 
