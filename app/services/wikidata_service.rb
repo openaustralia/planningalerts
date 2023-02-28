0965d69449607f02bb5907491ad219c38bc26789
Matthew Landauer <mlandauer@gmail.com>
2023-02-28 19:19:52 +0000

    rake task to check whether wikidata ids are pointing to the right place

--- app/services/wikidata_service.rb
+++ app/services/wikidata_service.rb
@@ -4,6 +4,9 @@
 module WikidataService
   extend T::Sig
 
+  # ids for the classes of LGA areas for each state
+  LGA_STATES = T.let(%w[Q1426035 Q55557858 Q55558027 Q55558200 Q55593624 Q55671590 Q55687066].freeze, T::Array[String])
+
   # Given an official website for an LGA return the wikidata ID
   sig { params(url: String).returns(T.nilable(String)) }
   def self.id_from_website(url)
@@ -14,7 +17,7 @@ module WikidataService
     # So instead create the query by hand
     query = sparql.query("SELECT * WHERE { ?item wdt:P31 ?parent { ?item wdt:P856 <http://#{domain}> . } UNION { ?item wdt:P856 <http://#{domain}/> . } UNION { ?item wdt:P856 <https://#{domain}> . } UNION { ?item wdt:P856 <https://#{domain}/> . } }")
     # In the case where we get several results just restrict it to LGAs
-    query.select! { |q| %w[Q1426035 Q55557858 Q55558027 Q55558200 Q55593624 Q55671590 Q55687066].include?(q[:parent].to_s.split("/").last) } if query.count > 1
+    query.select! { |q| LGA_STATES.include?(q[:parent].to_s.split("/").last) } if query.count > 1
 
     if query.count.zero?
       return
@@ -24,5 +27,15 @@ module WikidataService
 
     entity_url = query.first[:item].to_s
     entity_url.split("/").last
+  end
+
+  sig { params(id: String).returns(T::Boolean) }
+  def self.lga?(id)
+    # rubocop:disable Rails/DynamicFindBy
+    item = Wikidata::Item.find_by_id(id)
+    # rubocop:enable Rails/DynamicFindBy
+    # instance of
+    ids = item.claims_for_property_id("P31").map { |claim| claim.mainsnak.value.entity.id }
+    ids.any? { |i| LGA_STATES.include?(i) }
   end
 end
--- lib/tasks/planningalerts.rake
+++ lib/tasks/planningalerts.rake
@@ -16,6 +16,21 @@ namespace :planningalerts do
     end
   end
 
+  desc "check wikidata ids point to the correct place"
+  task check_wikidata_ids: :environment do
+    Authority.active.find_each do |authority|
+      if authority.wikidata_id.nil?
+        puts "#{authority.full_name} does not have wikidata_id set"
+        next
+      end
+
+      unless WikidataService.lga?(authority.wikidata_id)
+        puts "#{authority.full_name} wikidata_id #{authority.wikidata_id} does not point to LGA"
+        next
+      end
+    end
+  end
+
   namespace :emergency do
     desc "Regenerates all the counter caches in case they got out of synch"
     task fixup_counter_caches: :environment do
