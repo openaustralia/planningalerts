227178d3c46864c032a2966669cba6342ab68d66
Matthew Landauer <mlandauer@gmail.com>
2023-03-04 05:57:26 +0000

    Improve data migration

--- database-migrate.sh
+++ database-migrate.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-pgloader mysql://root:password@database/pa_development pgsql://postgres:password@postgres/pa_development
+# See https://gist.github.com/tristanm/a2afa29ac6f37bf92b46 for an idea of how to use this.
+# The main idea is to use rails to generate the postgres schema and then use pgloader to migrate
+# the data. Otherwise we seem to run into problems where the primary key of tables isn't recognised
+# by rails for some reason.
+pgloader --verbose database-migrate-commands
