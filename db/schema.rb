e0d49079a34b5edfd37deb22947993686bc66c7c
Matthew Landauer <mlandauer@gmail.com>
2023-03-05 23:51:19 +0000

    Enable postgis extension

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_164924) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_05_235006) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "postgis"
 
   create_table "active_admin_comments", id: :serial, force: :cascade do |t|
     t.string "resource_id", default: "", null: false
