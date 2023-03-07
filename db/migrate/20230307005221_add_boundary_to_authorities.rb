2361bc52290627909f38787d0d988807137458ec
Matthew Landauer <mlandauer@gmail.com>
2023-03-07 01:14:48 +0000

    Try using the same srid of the boundary data

--- db/migrate/20230307005221_add_boundary_to_authorities.rb
+++ db/migrate/20230307005221_add_boundary_to_authorities.rb
@@ -1,5 +1,5 @@
 class AddBoundaryToAuthorities < ActiveRecord::Migration[7.0]
   def change
-    add_column :authorities, :boundary, :st_polygon, geographic: true
+    add_column :authorities, :boundary, :st_polygon, geographic: true, srid: 4283
   end
 end
