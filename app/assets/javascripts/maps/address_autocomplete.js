c6174125a9e4f103d9ab5d4d1addfb0d86b1d1b6
Matthew Landauer <mlandauer@gmail.com>
2023-03-08 23:22:04 +0000

    A little error checking

--- app/assets/javascripts/maps/address_autocomplete.js
+++ app/assets/javascripts/maps/address_autocomplete.js
@@ -8,6 +8,9 @@ function initAutoComplete() {
   var autocomplete = new google.maps.places.Autocomplete(input, options);
 
   autocomplete.addListener('place_changed', function() {
-    document.querySelector("form.one-field").submit();
+    // The "form.one-field" only exists on the address search on the landing page
+    // We only want the form to be submitted automatically on that page
+    var form = document.querySelector("form.one-field");
+    if (form) form.submit();
   });
 }
