44638ba2a584cada631aa0f873be9952ce35f796
Matthew Landauer <mlandauer@gmail.com>
2023-03-08 19:43:15 +0000

    Move more map related javascript to the same file

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -108,4 +108,45 @@ window.addEventListener("DOMContentLoaded", function() {
 
     drawCircleOnMap(map, lat, lng, radius_meters);
   });
+
+  var map_div = document.getElementById('geocoding-map');
+
+  if (map_div) {
+    var googleLatLng = {lat: Number(map_div.dataset.googleLat), lng: Number(map_div.dataset.googleLng)};
+    var mappifyLatLng = {lat: Number(map_div.dataset.mappifyLat), lng: Number(map_div.dataset.mappifyLng)};
+  
+    var map = new google.maps.Map(map_div, { zoom: 13, center: googleLatLng });
+  
+    // TODO: Generalise to any number of geocoder results
+  
+    var googleInfowindow = new google.maps.InfoWindow({
+      content: map_div.dataset.googleHtml
+    });
+  
+    var mappifyInfowindow = new google.maps.InfoWindow({
+      content: map_div.dataset.mappifyHtml
+    });
+  
+    var googleMarker = new google.maps.Marker({
+      position: googleLatLng,
+      map: map,
+      title: 'Google',
+      label: "G"
+    });
+  
+    var mappifyMarker = new google.maps.Marker({
+      position: mappifyLatLng,
+      map: map,
+      title: 'Mappify',
+      label: "M"
+    });
+  
+    googleMarker.addListener('click', function() {
+      googleInfowindow.open(map, googleMarker);
+    });
+  
+    mappifyMarker.addListener('click', function() {
+      mappifyInfowindow.open(map, mappifyMarker);
+    });  
+  }
 });
