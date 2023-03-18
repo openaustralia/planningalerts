b498f1dc34998d77bee9dcd1c8ad27911eca8b09
Matthew Landauer <mlandauer@gmail.com>
2023-03-18 05:44:13 +0000

    Rename parameter

--- app/components/tailwind/nav_item_component.rb
+++ app/components/tailwind/nav_item_component.rb
@@ -4,10 +4,10 @@ module Tailwind
   class NavItemComponent < ViewComponent::Base
     extend T::Sig
 
-    sig { params(url: String, selected: T::Boolean).void }
-    def initialize(url:, selected: false)
+    sig { params(href: String, selected: T::Boolean).void }
+    def initialize(href:, selected: false)
       super
-      @url = url
+      @href = href
       @selected = selected
     end
   end
