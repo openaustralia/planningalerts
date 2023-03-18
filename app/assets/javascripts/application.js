bc4fb6c9872382b96db6b417de4964292f8c862b
Matthew Landauer <mlandauer@gmail.com>
2023-03-18 10:45:42 +0000

    Fix hamburger menu opening
    This was broken when we rejigged the way the js was being loaded

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,10 +4,13 @@
 // Need ujs for confirmation on buttons
 //= require rails-ujs
 
-document.querySelectorAll(".hideable").forEach(function(e) {
-  e.addEventListener("click", function(e) {
-    e.preventDefault();
-    var target = e.target.getAttribute("data-target");
-    document.querySelector(target).classList.toggle("hideable-target-show");  
+window.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll(".hideable").forEach(function(e) {
+    e.addEventListener("click", function(e) {
+      e.preventDefault();
+      var target = e.target.getAttribute("data-target");
+      document.querySelector(target).classList.toggle("hideable-target-show");  
+    })
   })
-})
+});
+
