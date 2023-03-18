b8e5de79ca8cdc76bd261776161d2a85b894aab7
Matthew Landauer <mlandauer@gmail.com>
2023-03-18 05:14:59 +0000

    Extract component

---
+++ app/components/nav_item_component.rb
@@ -0,0 +1,13 @@
+# typed: strict
+
+class NavItemComponent < ViewComponent::Base
+  extend T::Sig
+
+  sig { params(text: String, url: String, selected: T::Boolean).void }
+  def initialize(text:, url:, selected: false)
+    super
+    @text = text
+    @url = url
+    @selected = selected
+  end
+end
