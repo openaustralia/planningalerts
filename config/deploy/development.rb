a6412125907c9cfbadbe2baa4c6c21694be2fccc
Matthew Landauer <mlandauer@gmail.com>
2023-03-27 01:26:11 +0000

    So we can deploy PA in dev again (to vagrant vm)

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,6 +1,7 @@
 # typed: false
 
-server "web1.planningalerts.org.au.test", roles: %i[app web db]
+# Using a static ip address here because the devcontainer doesn't have the names installed in /etc/hosts on the host machine
+server "192.168.56.11", roles: %i[app web db]
 
 set :deploy_to, "/srv/www/production"
 set :honeybadger_env, "development"
