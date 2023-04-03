d2e85bbf92518a2bf4adc84599ad5ae87560128d
Matthew Landauer <mlandauer@gmail.com>
2023-04-03 10:34:22 +0000

    Move value to model

--- app/controllers/documentation_controller.rb
+++ app/controllers/documentation_controller.rb
@@ -27,20 +27,6 @@ class DocumentationController < ApplicationController
 
   sig { void }
   def contact
-    @reasons = T.let(
-      [
-        "I'm having trouble signing up",
-        "I can't find a development application",
-        "I've stopped receiving alerts",
-        "I have a privacy concern",
-        "I want to change or delete a comment I made",
-        "The address or map location is wrong",
-        "I have a question about how Planning Alerts works",
-        "I'm trying to reach council",
-        "Other, please specify below"
-      ],
-      T.nilable(T::Array[String])
-    )
     @contact_message = T.let(ContactMessage.new, T.nilable(ContactMessage))
   end
 
--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -4,4 +4,16 @@
 class ContactMessage < ApplicationRecord
   belongs_to :user
   validates :email, :reason, :details, presence: true
+
+  REASONS_LONG = T.let([
+    "I'm having trouble signing up",
+    "I can't find a development application",
+    "I've stopped receiving alerts",
+    "I have a privacy concern",
+    "I want to change or delete a comment I made",
+    "The address or map location is wrong",
+    "I have a question about how Planning Alerts works",
+    "I'm trying to reach council",
+    "Other, please specify below"
+  ].freeze, T::Array[String])
 end
