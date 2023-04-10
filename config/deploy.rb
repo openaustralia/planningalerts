e8e96fd48691047123e9aa64025e3c8479a91fa0
Matthew Landauer <mlandauer@gmail.com>
2023-04-10 04:06:34 +0000

    sitemaps are now uploaded to s3 instead

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,11 +22,11 @@ set :repo_url, "https://github.com/openaustralia/planningalerts.git"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", 'config/master.key'
-append :linked_files, "config/database.yml", ".env.production", "public/sitemap.xml"
+append :linked_files, "config/database.yml", ".env.production"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/sitemaps"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
