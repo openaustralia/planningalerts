f012bdab464000dd61fcb1bcf8476cccdf13a87a
Matthew Landauer <mlandauer@gmail.com>
2023-04-13 01:06:09 +0000

    Get secret key base from rails credentials now

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -12,4 +12,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-PlanningalertsApp::Application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
+PlanningalertsApp::Application.config.secret_key_base = Rails.application.credentials[:secret_key_base]
