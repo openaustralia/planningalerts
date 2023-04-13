38d69b3c68d1d89afebc418222c9e7c9c2520cdd
Matthew Landauer <mlandauer@gmail.com>
2023-04-13 02:12:43 +0000

    Link rails master key

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :repo_url, "https://github.com/openaustralia/planningalerts.git"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", 'config/master.key'
-append :linked_files, "config/database.yml", ".env.production"
+append :linked_files, "config/database.yml", ".env.production", "config/credentials/production.key"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
