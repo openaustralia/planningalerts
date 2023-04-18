bd43849aeb80f2cf36cac8ca1091bfa5883830c4
Matthew Landauer <mlandauer@gmail.com>
2023-04-18 08:53:18 +0000

    In production get memcache servers from config file uploaded by capistrano

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
-  config.cache_store = :mem_cache_store
+  config.cache_store = [:mem_cache_store] + YAML.load_file("config/memcache.yml")["servers"]
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
