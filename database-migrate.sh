c6aa1682c04467795576a22e01af83f2720aa714
Matthew Landauer <mlandauer@gmail.com>
2023-04-20 06:00:10 +0000

    A few more words about what to do in production for migration

--- database-migrate.sh
+++ database-migrate.sh
@@ -13,7 +13,11 @@ pgloader --verbose database-migrate-commands
 #
 # To run things in PRODUCTION
 #
-# RAILS_ENV=production bin/rails db:create
 # RAILS_ENV=production bin/rails db:schema:load
 # RAILS_ENV=production bin/rails runner database_migrate_template.rb
 # pgloader --verbose database-migrate-commands.production
+#
+# If the migration fails and you need to restart it make sure to run
+# db:schema:load again. Otherwise it's possible for some of the foreign key constraints
+# not to be set up properly. This is because pgloader removes them and adds them back
+# but if it fails it doesn't seem to add them back. Ugh.
