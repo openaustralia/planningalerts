2860796110b548aeeabecfdea6e757970bc9b33c
Matthew Landauer <mlandauer@gmail.com>
2023-04-24 02:55:03 +0000

    Stop boundary data for authority from getting loaded when we're not using it

--- app/models/authority.rb
+++ app/models/authority.rb
@@ -42,12 +42,7 @@ class Authority < ApplicationRecord
 
   sig { returns(Integer) }
   def self.total_population_2021_covered_by_all_active_authorities
-    sum = 0
-    Authority.active.each do |a|
-      population = a.population_2021
-      sum += population if population
-    end
-    sum
+    Authority.active.sum(:population_2021)
   end
 
   sig { returns(Float) }
