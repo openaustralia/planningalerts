628f75eac34eada83cda6e5cdcf5eaa9b1744eca
Matthew Landauer <mlandauer@gmail.com>
2023-04-26 01:24:18 +0000

    Use protect_from_forgery with: :null_session on cuttlefish api controller

--- app/controllers/cuttlefish_controller.rb
+++ app/controllers/cuttlefish_controller.rb
@@ -5,7 +5,7 @@ class CuttlefishController < ApplicationController
   extend T::Sig
 
   # Because this is an API
-  skip_before_action :verify_authenticity_token
+  protect_from_forgery with: :null_session
 
   sig { void }
   def event
