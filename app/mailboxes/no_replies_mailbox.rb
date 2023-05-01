f2ee2ff0e729a7d0aced0e7ebdc42699c589a2b9
Matthew Landauer <mlandauer@gmail.com>
2023-05-01 06:26:19 +0000

    Don't process an email to reply to when it's automatic

--- app/mailboxes/no_replies_mailbox.rb
+++ app/mailboxes/no_replies_mailbox.rb
@@ -3,9 +3,21 @@
 
 class NoRepliesMailbox < ApplicationMailbox
   extend T::Sig
+  before_processing :require_human
 
   sig { void }
   def process
     ReplyToNoReplyMailer.reply(mail).deliver_now
+  end
+
+  private
+
+  # Checks that this isn't some kind of automatically generated email
+  sig { void }
+  def require_human
+    auto_submitted = mail.header["Auto-Submitted"]
+    return if auto_submitted.nil? || auto_submitted.value == "no"
+
+    bounced!
   end
 end
