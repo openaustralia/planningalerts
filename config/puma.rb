e7ce66eba12965291a7e94a4ebd1e7cdbb6c0b31
Matthew Landauer <mlandauer@gmail.com>
2023-05-15 19:43:35 +0000

    Set directory to the one with the symbolic links

--- config/puma.rb
+++ config/puma.rb
@@ -17,7 +17,7 @@ port 8000
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # For the benefit of phased restarts
-directory File.expand_path("..", __dir__)
+directory "/srv/www/production/current"
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
