4357be02ee1072aa74d3635c1b3bde787987c2ff
Matthew Landauer <mlandauer@gmail.com>
2023-08-17 04:31:18 +0000

    Silly me. MyBaseDeviseController was inheriting from ApplicationController

--- app/controllers/my_base_devise_controller.rb
+++ app/controllers/my_base_devise_controller.rb
@@ -18,17 +18,4 @@ class MyBaseDeviseController < ApplicationController
       "minimal"
     end
   end
-
-  # This method is duplicated from application_controller.rb. Really we should be extracting it into
-  # something like a concern (or a module) but had trouble making things work with sorbet. So, for the
-  # time being just duplicated code. I know.
-  # TODO: Remove duplication with application_controller.rb
-  sig { returns(T::Boolean) }
-  def show_tailwind_theme?
-    # We're intentionally not checking whether the feature flag is enabled here because we want
-    # the new theme to be shown even if you're logged out. The feature flag just enables the button
-    # that allows you to do the switching. Cookies are signed so the value can be manipulated by
-    # users outside of pushing the button
-    cookies.signed[:planningalerts_theme] == "tailwind"
-  end
 end
