ca7469b2f438e01f7e6166b865e06d6989dd7060
Matthew Landauer <mlandauer@gmail.com>
2023-08-22 02:13:50 +0000

    Move more into the form builder

--- app/lib/form_builders/tailwind.rb
+++ app/lib/form_builders/tailwind.rb
@@ -5,8 +5,22 @@ module FormBuilders
   class Tailwind < ActionView::Helpers::FormBuilder
     extend T::Sig
 
+    sig { params(method: Symbol, text: T.any(T.nilable(String), T::Hash[Symbol, String]), options: T::Hash[Symbol, String]).returns(String) }
+    def label(method, text = nil, options = {})
+      if text.is_a?(Hash)
+        options = options.merge(text)
+        text = nil
+      end
+      super(method, text, options.merge(class: "font-bold text-2xl text-navy #{options[:class]}"))
+    end
+
     sig { params(method: Symbol, options: T::Hash[Symbol, String]).returns(String) }
     def text_field(method, options = {})
+      super(method, options.merge(class: "text-2xl text-navy placeholder:text-warm-grey border-light-grey2 #{options[:class]}"))
+    end
+
+    sig { params(method: Symbol, options: T::Hash[Symbol, String]).returns(String) }
+    def password_field(method, options = {})
       super(method, options.merge(class: "text-2xl text-navy placeholder:text-warm-grey border-light-grey2 #{options[:class]}"))
     end
   end
