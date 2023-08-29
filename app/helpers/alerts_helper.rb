d92c105997d0694491e622abbfed97445159faa4
Matthew Landauer <mlandauer@gmail.com>
2023-08-29 19:10:00 +0000

    Switch order of words

--- app/helpers/alerts_helper.rb
+++ app/helpers/alerts_helper.rb
@@ -17,6 +17,18 @@ module AlertsHelper
     end
   end
 
+  # This has the two parts of the sentence swapped
+  # over from size_option_sentence
+  sig { params(size: Integer).returns(String) }
+  def size_option_sentence_alternate(size)
+    w = Alert::RADIUS_DESCRIPTIONS[size]
+    if w
+      "Within #{meters_in_words(size.to_f)} (my #{w})"
+    else
+      "Within #{meters_in_words(size.to_f)}"
+    end
+  end
+
   sig { params(date: T.nilable(Time)).returns(String) }
   def last_sent_in_words(date)
     if date
