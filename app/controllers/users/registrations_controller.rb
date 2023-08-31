de6deae321111e1c2e5dba890426e194fdd80cfc
Matthew Landauer <mlandauer@gmail.com>
2023-08-31 04:50:06 +0000

    Use correct layout on error when editing account

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,7 +9,7 @@ module Users
     include Devise::Controllers::Helpers
     include ActionView::Layouts
 
-    layout "profile", only: :edit
+    layout "profile", only: %i[edit update]
 
     # before_action :configure_sign_up_params, only: [:create]
     # before_action :configure_account_update_params, only: [:update]
