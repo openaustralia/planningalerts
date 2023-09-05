a88bc41500497bdae9dd1ab052c83ea65f6f4159
Matthew Landauer <mlandauer@gmail.com>
2023-09-05 00:35:08 +0000

    Revert "WIP preview page"
    This reverts commit fa1245b142a6a8cc4bf5cae8241bae5f9d7a2780.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,8 +5,6 @@ class ProfilesController < ApplicationController
   extend T::Sig
 
   before_action :authenticate_user!
-  after_action :verify_authorized, except: %i[show comments]
-  # after_action :verify_policy_scoped, only: :comments
 
   layout "profile"
 
@@ -15,18 +13,9 @@ class ProfilesController < ApplicationController
     redirect_to profile_alerts_url if show_tailwind_theme?
   end
 
-  # The actions "comments" and "comment_preview" really belong on a new controller called something like "MyComments"
   sig { void }
   def comments
-    # TODO: We want the user to be able to see their hidden comments too
     comments = T.must(current_user).comments.visible.order(confirmed_at: :desc).page(params[:page])
     @comments = T.let(comments, T.nilable(ActiveRecord::Relation))
-  end
-
-  sig { void }
-  def comment_preview
-    comment = Comment.find(params[:id])
-    @comment = T.let(comment, T.nilable(Comment))
-    authorize @comment, :preview?
   end
 end
