05fc01329bb5f9f6bc47a41e0f64fd2838cdcf30
Matthew Landauer <mlandauer@gmail.com>
2023-09-13 07:12:32 +0000

    Make button green and not rounded

--- maizzle/tailwind.config.js
+++ maizzle/tailwind.config.js
@@ -6,6 +6,28 @@ module.exports = {
       xs: {max: '425px'},
     },
     extend: {
+      colors: {
+        'transparent': 'transparent',
+        'current': 'currentColor',
+        'white': '#ffffff',
+        'off-white': "#eeeeee",
+        'light-grey': '#F3F2F2',
+        'fuchsia': '#CA3F94',
+        'lavender': '#826D9C',
+        'yellow': '#F4BE53',
+        'orange': '#D37708',
+        'navy': '#414860',
+        'green': '#03827A',
+        'darker-orange': '#BA671D',
+        'warm-grey': '#767676',
+        // TODO: Come up with a less bad name than this
+        'light-grey2': '#D2CDC1',
+        // A green colour that you might see on a map. Display this while loading the map
+        // Note that we're using the same colour when initialising the google map so that
+        // everything matches
+        'google-maps-green': '#d1e6d9',
+        'error-red': '#B90000'
+      },
       spacing: {
         screen: '100vw',
         full: '100%',
