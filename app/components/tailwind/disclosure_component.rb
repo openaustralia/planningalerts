ac56e518bc8dc40093ec04c16e56abb2f3a60e0b
Matthew Landauer <mlandauer@gmail.com>
2023-09-30 17:23:43 +0000

    Can vary size of text in disclosure component

--- app/components/tailwind/disclosure_component.rb
+++ app/components/tailwind/disclosure_component.rb
@@ -3,6 +3,14 @@
 
 module Tailwind
   class DisclosureComponent < ViewComponent::Base
+    extend T::Sig
+
     renders_one :summary
+
+    sig { params(size: String).void }
+    def initialize(size:)
+      super
+      @size = size
+    end
   end
 end
