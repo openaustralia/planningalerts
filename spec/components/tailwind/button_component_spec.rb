0f0e06f82cd33c8c39ca77c422e40be16ff86d61
Matthew Landauer <mlandauer@gmail.com>
2023-10-05 03:20:17 +0000

    Add beginnings of tests for button component

---
+++ spec/components/tailwind/button_component_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "rails_helper"
+
+RSpec.describe Tailwind::ButtonComponent, type: :component do
+  it "renders the content" do
+    render_inline(described_class.new(tag: :button, size: "lg", type: :primary)) { "Hello world!" }
+    expect(page).to have_text "Hello world!"
+  end
+
+  it "renders a button tag" do
+    render_inline(described_class.new(tag: :button, size: "lg", type: :primary)) { "Hello world!" }
+    expect(page).to have_button
+  end
+
+  it "renders a large button" do
+    render_inline(described_class.new(tag: :button, size: "lg", type: :primary)) { "Hello world!" }
+    expect(page).to have_button(class: %w[px-4 py-2 text-lg])
+  end
+
+  it "renders an extra large button" do
+    render_inline(described_class.new(tag: :button, size: "2xl", type: :primary)) { "Hello world!" }
+    expect(page).to have_button(class: %w[px-11 sm:px-16 py-3 sm:py-4 text-2xl])
+  end
+
+  it "renders a primary button in white on green" do
+    render_inline(described_class.new(tag: :button, size: "2xl", type: :primary)) { "Hello world!" }
+    expect(page).to have_button(class: %w[text-white bg-green])
+  end
+
+  it "renders a secondary button in white on gray" do
+    render_inline(described_class.new(tag: :button, size: "2xl", type: :secondary)) { "Hello world!" }
+    expect(page).to have_button(class: %w[text-white bg-warm-grey])
+  end
+
+  it "renders the text as semibold" do
+    render_inline(described_class.new(tag: :button, size: "2xl", type: :primary)) { "Hello world!" }
+    expect(page).to have_button(class: "font-semibold")
+  end
+
+  it "renders a trash icon" do
+    render_inline(described_class.new(tag: :button, size: "2xl", type: :primary, icon: :trash)) { "Hello world!" }
+    expect(page).to have_css("img[alt='Trash icon']")
+  end
+
+  it "renders a link tag" do
+    render_inline(described_class.new(tag: :a, size: "lg", type: :primary, href: "/foo")) { "Hello world!" }
+    expect(page).to have_link(href: "/foo")
+  end
+
+  it "renders the link tag as inline-block" do
+    render_inline(described_class.new(tag: :a, size: "lg", type: :primary, href: "/foo")) { "Hello world!" }
+    expect(page).to have_link(class: "inline-block")
+  end
+end
