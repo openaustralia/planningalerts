d05439d828bf08590f104177d750be097e1946bd
Matthew Landauer <mlandauer@gmail.com>
2023-10-13 20:10:59 +0000

    comment counter did not take into account draft comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -32,9 +32,9 @@ class Comment < ApplicationRecord
 
   # TODO: Rename the counter to confirmed_comments_count
   counter_culture :application,
-                  column_name: proc { |comment| comment.confirmed? ? "visible_comments_count" : nil },
+                  column_name: proc { |comment| comment.confirmed? && comment.previewed? ? "visible_comments_count" : nil },
                   column_names: {
-                    "comments.confirmed = true" => "visible_comments_count"
+                    "comments.confirmed = true and comments.previewed = true" => "visible_comments_count"
                   }
 
   # TODO: Change confirmed in schema to be null: false
