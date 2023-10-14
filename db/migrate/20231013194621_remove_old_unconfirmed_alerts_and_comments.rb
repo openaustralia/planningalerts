0cdc3d57f69667419c8ae9f55cca1a84c484d686
Matthew Landauer <mlandauer@gmail.com>
2023-10-14 18:47:13 +0000

    Avoid issue with renamed field in migration

--- db/migrate/20231013194621_remove_old_unconfirmed_alerts_and_comments.rb
+++ db/migrate/20231013194621_remove_old_unconfirmed_alerts_and_comments.rb
@@ -1,7 +1,9 @@
 class RemoveOldUnconfirmedAlertsAndComments < ActiveRecord::Migration[7.0]
   def up
     Alert.where(confirmed: false).destroy_all
-    Comment.where("confirmed IS NULL OR confirmed = false").destroy_all
+    Comment.skip_counter_culture_updates do
+      Comment.where("confirmed IS NULL OR confirmed = false").destroy_all      
+    end
   end
 
   def down
