a8c9c917a1d04056b4f278c329d8448cd53d5424
Matthew Landauer <mlandauer@gmail.com>
2023-10-17 16:40:21 +0000

    Get more content in the email dynamically

--- spec/mailers/previews/alerts_preview.rb
+++ spec/mailers/previews/alerts_preview.rb
@@ -63,10 +63,29 @@ class AlertsPreview < ActionMailer::Preview
     comment = Comment.new(
       application: Application.new(
         id: 2,
-        description: "Erection of a bouncy castle",
-        address: "20 Illawarra Road Marrickville 2204"
+        address: "6 Kahibah Road, Umina Beach, NSW",
+        lat: -33.90413,
+        lng: 151.16163,
+        description: "S4.55 to Modify Approved Dwelling and Garage including Deletion of Clerestory, Addition of Laminated Beam, " \
+                     "Relocation of Laundry, Deletion of Stairs and Expansion of Workshop"
       ),
-      text: "I really don't like inflatable things",
+      text: "It has recently come to my attention that a planning application has been submitted" \
+            "for 813 Hight Street, Reservoir.\n\n" \
+            "My concern is with the application for ground floor shops and nine (9) dwellings," \
+            "above, with a reduction in the car parking requirements.\n\n" \
+            "Currently, there are already parking issues in the area, with insufficient parking" \
+            "bays available. Residents of Wild Street and Henry Street are constantly complaining" \
+            "about cars parked on \"their\" street. We often find abusive notes left on vehicles," \
+            "harassment of staff when in the street, and stupid acts of vandalism. We are a" \
+            "business on High Street and staff already have difficulty finding parking as it" \
+            "is. It appears that the council is not concerned for its local businesses or residents" \
+            "and allowing a new building with reduced parking is irresponsible and inconsiderate." \
+            "I understand that the council and developers want to make as much money as possible but" \
+            "it is extremely unfair to cause such distress and inconvenience to everyone else." \
+            "I strongly oppose such a development and would like further information as to how to" \
+            "make this a formal objection.\n\n" \
+            "Regards\n" \
+            "Louise",
       name: "Martha"
     )
 
