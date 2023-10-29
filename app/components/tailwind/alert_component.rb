8b7c44b592e3de4f3f06bfc4064360097ce46ce4
Matthew Landauer <mlandauer@gmail.com>
2023-10-29 23:23:15 +0000

    Improve wording on alt tag for alerts

--- app/components/tailwind/alert_component.rb
+++ app/components/tailwind/alert_component.rb
@@ -14,11 +14,11 @@ module Tailwind
       when :success
         @bg_class = T.let("bg-lavender", String)
         @image_src = T.let("tailwind/tick.svg", String)
-        @image_alt = T.let("Tick", String)
+        @image_alt = T.let("Success", String)
       when :warning
         @bg_class = "bg-error-red"
         @image_src = "tailwind/warning.svg"
-        @image_alt = "Warning sign"
+        @image_alt = "Warning"
       else
         raise "Invalid value for type: #{type}"
       end
