8410a32addeca94e9dd6b2be510a058780fd8a2c
Matthew Landauer <mlandauer@gmail.com>
2023-10-29 23:53:28 +0000

    Illustrations which are not strictly giving information are marked with an empty alt tag to help screen readers know that they're not important

--- spec/features/documentation_pages_spec.rb
+++ spec/features/documentation_pages_spec.rb
@@ -3,8 +3,35 @@
 require "spec_helper"
 
 describe "Browsing basic documentation pages" do
+  # TODO: Include this in spec/spec_helper.rb instead
+  # See https://github.com/heartcombo/devise#controller-tests
+  include Devise::Test::IntegrationHelpers
+
   it "has an about page" do
     visit about_path
     expect(page).to have_content "The aim of this to enable shared scrutiny of what is being built"
+  end
+
+  describe "in the new design" do
+    before do
+      sign_in create(:confirmed_user, tailwind_theme: true)
+      visit faq_path
+    end
+
+    it "has a help page" do
+      expect(page).to have_content "Help"
+    end
+
+    describe "accessibility tests", js: true do
+      it "main content passes" do
+        # Limiting check to main content to ignore (for the time being) colour contrast issues with the header and footer
+        expect(page).to be_axe_clean.within("main")
+      end
+
+      it "page passes most" do
+        # Also doing check across whole page so we catch issues like h1 not being used
+        expect(page).to be_axe_clean.skipping("color-contrast")
+      end
+    end
   end
 end
