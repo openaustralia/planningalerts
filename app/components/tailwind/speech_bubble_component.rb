f82f9e00539b9b98be7616aa5e188cb8349db875
Matthew Landauer <mlandauer@gmail.com>
2023-11-06 03:33:26 +0000

    Small refactor

--- app/components/tailwind/speech_bubble_component.rb
+++ app/components/tailwind/speech_bubble_component.rb
@@ -9,52 +9,47 @@ module Tailwind
     def initialize(size:, alignment:)
       super
 
-      box_classes = case size
-                    when "4xl"
-                      "px-4 py-2 text-4xl rounded-2xl"
-                    when "2xl"
-                      "px-4 py-2 text-2xl rounded-2xl"
-                    when "lg"
-                      "px-2 text-lg rounded-lg"
-                    else
-                      raise "Not supporting the size #{size} yet"
-                    end
-      @box_classes = T.let(box_classes, String)
-
-      arrow_classes = case size
-                      when "4xl", "2xl"
-                        "border-l-[6px] border-t-[14px] border-r-[6px]"
-                      when "lg"
-                        "border-l-[3px] border-t-[6px] border-r-[3px]"
-                      else
-                        raise "Not supporting the size #{size} yet"
-                      end
-      @arrow_classes = T.let(arrow_classes, String)
-
-      # Doing it this way so that tailwind doesn't compile the class out
-      alignment_class = case alignment
-                        when :left
-                          case size
-
-                          when "4xl", "2xl"
+      # Doing it this way so that tailwind doesn't compile the classes out
+      case size
+      when "4xl"
+        box_classes = "px-4 py-2 text-4xl rounded-2xl"
+        arrow_classes = "border-l-[6px] border-t-[14px] border-r-[6px]"
+        alignment_class = case alignment
+                          when :left
                             "left-4"
-                          when "lg"
-                            "left-2"
-                          else
-                            raise "Not supporting the size #{size} yet"
-                          end
-                        when :right
-                          case size
-                          when "4xl", "2xl"
+                          when :right
                             "right-4"
-                          when "lg"
+                          else
+                            raise "Only :left or :right for alignment"
+                          end
+      when "2xl"
+        box_classes = "px-4 py-2 text-2xl rounded-2xl"
+        arrow_classes = "border-l-[6px] border-t-[14px] border-r-[6px]"
+        alignment_class = case alignment
+                          when :left
+                            "left-4"
+                          when :right
+                            "right-4"
+                          else
+                            raise "Only :left or :right for alignment"
+                          end
+      when "lg"
+        box_classes = "px-2 text-lg rounded-lg"
+        arrow_classes = "border-l-[3px] border-t-[6px] border-r-[3px]"
+        alignment_class = case alignment
+                          when :left
+                            "left-2"
+                          when :right
                             "right-2"
                           else
-                            raise "Not supporting the size #{size} yet"
+                            raise "Only :left or :right for alignment"
                           end
-                        else
-                          raise "Only :left or :right for alignment"
-                        end
+      else
+        raise "Not supporting the size #{size} yet"
+      end
+
+      @box_classes = T.let(box_classes, String)
+      @arrow_classes = T.let(arrow_classes, String)
       @alignment = T.let(alignment_class, String)
     end
   end
