761df05a4a572b1cbbaa71a3a246eab586558954
Matthew Landauer <mlandauer@gmail.com>
2023-11-06 03:28:36 +0000

    Put number of comments next to title on applications index page

--- app/components/tailwind/speech_bubble_component.rb
+++ app/components/tailwind/speech_bubble_component.rb
@@ -8,14 +8,50 @@ module Tailwind
     sig { params(size: String, alignment: Symbol).void }
     def initialize(size:, alignment:)
       super
-      @size = size
+
+      box_classes = case size
+                    when "4xl"
+                      "px-4 py-2 text-4xl rounded-2xl"
+                    when "2xl"
+                      "px-4 py-2 text-2xl rounded-2xl"
+                    when "lg"
+                      "px-2 text-lg rounded-lg"
+                    else
+                      raise "Not supporting the size #{size} yet"
+                    end
+      @box_classes = T.let(box_classes, String)
+
+      arrow_classes = case size
+                      when "4xl", "2xl"
+                        "border-l-[6px] border-t-[14px] border-r-[6px]"
+                      when "lg"
+                        "border-l-[3px] border-t-[6px] border-r-[3px]"
+                      else
+                        raise "Not supporting the size #{size} yet"
+                      end
+      @arrow_classes = T.let(arrow_classes, String)
 
       # Doing it this way so that tailwind doesn't compile the class out
       alignment_class = case alignment
                         when :left
-                          "left-4"
+                          case size
+
+                          when "4xl", "2xl"
+                            "left-4"
+                          when "lg"
+                            "left-2"
+                          else
+                            raise "Not supporting the size #{size} yet"
+                          end
                         when :right
-                          "right-4"
+                          case size
+                          when "4xl", "2xl"
+                            "right-4"
+                          when "lg"
+                            "right-2"
+                          else
+                            raise "Not supporting the size #{size} yet"
+                          end
                         else
                           raise "Only :left or :right for alignment"
                         end
