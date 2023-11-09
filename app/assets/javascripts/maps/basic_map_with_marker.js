dbab6ccf27140f6fc26faa555b73fcd80b284e90
Matthew Landauer <mlandauer@gmail.com>
2023-11-09 01:43:25 +0000

    We don't have to use advanced marker so we can get rid of mapId again.

--- app/assets/javascripts/maps/basic_map_with_marker.js
+++ app/assets/javascripts/maps/basic_map_with_marker.js
@@ -14,7 +14,7 @@ function initialiseBasicMapWithMarker(map_div) {
 
 async function initialiseBasicMapWithMarker2(map_div) {
   const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const { Marker } = await google.maps.importLibrary("marker");
 
   var center = { lat: Number(map_div.dataset.lat), lng: Number(map_div.dataset.lng) };
   var address = map_div.dataset.address;
@@ -22,10 +22,9 @@ async function initialiseBasicMapWithMarker2(map_div) {
 
   var map = new Map(
     map_div,
-    // TODO: Where should we store the mapId? It's public
-    { zoom: zoom, center: center, fullscreenControl: false, streetViewControl: false, draggable: false, backgroundColor: "#d1e6d9", mapId: "f47952046639e724" }
+    { zoom: zoom, center: center, fullscreenControl: false, streetViewControl: false, draggable: false, backgroundColor: "#d1e6d9" }
   );
-  new AdvancedMarkerElement({ position: center, map: map, title: address });
+  new Marker({ position: center, map: map, title: address });
 
   return map;
 }
