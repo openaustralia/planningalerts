b0d6a125544234ed75e8699175ce51bccb6c33a6
Matthew Landauer <mlandauer@gmail.com>
2023-11-09 02:49:48 +0000

    Separate old and new pano code

--- app/assets/javascripts/maps/pano.js
+++ app/assets/javascripts/maps/pano.js
@@ -1,24 +1,4 @@
-function computeAngle(endLatLng, startLatLng) {
-  var DEGREE_PER_RADIAN = 57.2957795;
-  var RADIAN_PER_DEGREE = 0.017453;
-
-  var dlat = endLatLng.lat() - startLatLng.lat();
-  var dlng = endLatLng.lng() - startLatLng.lng();
-  // We multiply dlng with cos(endLat), since the two points are very closeby,
-  // so we assume their cos values are approximately equal.
-  var yaw = Math.atan2(dlng * Math.cos(endLatLng.lat() * RADIAN_PER_DEGREE), dlat)
-    * DEGREE_PER_RADIAN;
-  return wrapAngle(yaw);
-}
-
-function wrapAngle(angle) {
-  if (angle >= 360) {
-    angle -= 360;
-  } else if (angle < 0) {
-    angle += 360;
-  }
-  return angle;
-}
+//= require maps/pano_utils
 
 function initialisePano(elem) {
   var lat = Number(elem.dataset.lat);
@@ -35,26 +15,5 @@ function initialisePano(elem) {
     myPano.setPov({heading:angle, pitch:0, zoom:1});
   });
   var panoMarker = new google.maps.Marker({position: pointToLookAt, title: address});
-  panoMarker.setMap(myPano);
-}
-
-async function initialisePano2(elem) {
-  const { StreetViewPanorama } = await google.maps.importLibrary("streetView");
-  const { Marker } = await google.maps.importLibrary("marker")
-
-  var lat = Number(elem.dataset.lat);
-  var lng = Number(elem.dataset.lng);
-  var address = elem.dataset.address;
-
-  // Can't yet figure out how to make the POV point at the marker
-  var pointToLookAt = new google.maps.LatLng(lat, lng);
-  var myPano = new StreetViewPanorama(elem,
-    {position: pointToLookAt, navigationControl: false, addressControl: false, zoom: 0, scrollwheel: false, fullscreenControl: false, linksControl: false});
-  google.maps.event.addListener(myPano, 'position_changed', function() {
-    // Orient the camera to face the position we're interested in
-    var angle = computeAngle(pointToLookAt, myPano.getPosition());
-    myPano.setPov({heading:angle, pitch:0, zoom:1});
-  });
-  var panoMarker = new Marker({position: pointToLookAt, title: address});
   panoMarker.setMap(myPano);
 }
