aedbfd7bb51202f5f6c55e666782bb2d0636d4ba
Matthew Landauer <mlandauer@gmail.com>
2023-11-09 01:22:42 +0000

    Get one map working with new loading method

--- app/assets/javascripts/maps/basic_map_with_marker.js
+++ app/assets/javascripts/maps/basic_map_with_marker.js
@@ -11,3 +11,21 @@ function initialiseBasicMapWithMarker(map_div) {
 
   return map;
 }
+
+async function initialiseBasicMapWithMarker2(map_div) {
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  var center = { lat: Number(map_div.dataset.lat), lng: Number(map_div.dataset.lng) };
+  var address = map_div.dataset.address;
+  var zoom = Number(map_div.dataset.zoom);
+
+  var map = new Map(
+    map_div,
+    // TODO: Where should we store the mapId? It's public
+    { zoom: zoom, center: center, fullscreenControl: false, streetViewControl: false, draggable: false, backgroundColor: "#d1e6d9", mapId: "f47952046639e724" }
+  );
+  new AdvancedMarkerElement({ position: center, map: map, title: address });
+
+  return map;
+}
--- app/assets/javascripts/tailwind/maps.js
+++ app/assets/javascripts/tailwind/maps.js
@@ -1,10 +1,2 @@
 //= require maps/alert_map
 //= require maps/pano
-
-// This function is called after the google maps api is fully loaded. So, we can safely set things
-// up to use it here
-function initialiseAllMaps() {
-  window.dispatchEvent(new Event('map-loaded'));
-}
-
-window.initialiseAllMaps = initialiseAllMaps;
