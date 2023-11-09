af3629eeace212538f165e5dd3a9624c2551225c
Matthew Landauer <mlandauer@gmail.com>
2023-11-09 01:41:59 +0000

    Get streetview working as well

--- app/assets/javascripts/maps/pano.js
+++ app/assets/javascripts/maps/pano.js
@@ -37,3 +37,24 @@ function initialisePano(elem) {
   var panoMarker = new google.maps.Marker({position: pointToLookAt, title: address});
   panoMarker.setMap(myPano);
 }
+
+async function initialisePano2(elem) {
+  const { StreetViewPanorama } = await google.maps.importLibrary("streetView");
+  const { Marker } = await google.maps.importLibrary("marker")
+
+  var lat = Number(elem.dataset.lat);
+  var lng = Number(elem.dataset.lng);
+  var address = elem.dataset.address;
+
+  // Can't yet figure out how to make the POV point at the marker
+  var pointToLookAt = new google.maps.LatLng(lat, lng);
+  var myPano = new StreetViewPanorama(elem,
+    {position: pointToLookAt, navigationControl: false, addressControl: false, zoom: 0, scrollwheel: false, fullscreenControl: false, linksControl: false});
+  google.maps.event.addListener(myPano, 'position_changed', function() {
+    // Orient the camera to face the position we're interested in
+    var angle = computeAngle(pointToLookAt, myPano.getPosition());
+    myPano.setPov({heading:angle, pitch:0, zoom:1});
+  });
+  var panoMarker = new Marker({position: pointToLookAt, title: address});
+  panoMarker.setMap(myPano);
+}
