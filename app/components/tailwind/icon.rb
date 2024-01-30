b8b45240aba9daf45ed3ef2d7cd97b33042d221e
Matthew Landauer <mlandauer@gmail.com>
2024-01-30 02:03:34 +0000

    Inline svg icons used for buttons in one place

--- app/components/tailwind/icon.rb
+++ app/components/tailwind/icon.rb
@@ -8,23 +8,7 @@ module Tailwind
     sig { params(name: Symbol).void }
     def initialize(name:)
       super
-
-      # Using inlined svg icons so that we can set their colour based on the current text colour
-      case name
-      when :trash
-        icon_path = "application/svg/trash"
-      when :edit
-        icon_path = "application/svg/pencil"
-      when :external
-        icon_path = "application/svg/external"
-      when :share
-        # TODO: Share icon is not visually consistent with external link icon
-        icon_path = "application/svg/share"
-      else
-        raise "Unexpected name #{name}"
-      end
-
-      @icon_path = T.let(icon_path, String)
+      @name = name
     end
   end
 end
