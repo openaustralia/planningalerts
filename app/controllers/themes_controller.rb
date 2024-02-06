f3170e0aec4488d237fc0ff284f5b5aa66d3a42d
Matthew Landauer <mlandauer@gmail.com>
2024-02-06 04:33:39 +0000

    Radically simplify the switching back and forth for the new design. See #1754

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -9,11 +9,6 @@ class ThemesController < ApplicationController
     return unless Flipper.enabled?(:switch_themes, current_user)
 
     update_tailwind_theme(!show_tailwind_theme?)
-    notice = if show_tailwind_theme?
-               "Thank you for being an early tester of the new design. Please remember to report any problems that you encounter."
-             else
-               "We're sad to see you go. Please let us know why you switched back"
-             end
-    redirect_to root_path, notice:
+    redirect_to root_path
   end
 end
