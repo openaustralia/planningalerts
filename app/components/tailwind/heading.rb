54ad10dc686090e583710be343efd4eec7265258
Matthew Landauer <mlandauer@gmail.com>
2024-02-07 23:17:28 +0000

    Allow size override on heading

--- app/components/tailwind/heading.rb
+++ app/components/tailwind/heading.rb
@@ -5,13 +5,24 @@ module Tailwind
   class Heading < ViewComponent::Base
     extend T::Sig
 
-    sig { params(tag: Symbol, extra_classes: String).void }
-    def initialize(tag:, extra_classes: "")
+    # TODO: Perhaps we should allow the size override to work by saying we want an h1 heading but with the styling of an h2?
+    sig { params(tag: Symbol, size: T.nilable(String), extra_classes: String).void }
+    def initialize(tag:, size: nil, extra_classes: "")
       super
 
       raise "Invalid tag" unless tag == :h1
 
       @extra_classes = extra_classes
+
+      case size
+      when "3xl"
+        @size_class = T.let("text-3xl", String)
+      # 4xl is the default size for h1
+      when "4xl", nil
+        @size_class = T.let("text-4xl", String)
+      else
+        raise "Unexpected size #{size}"
+      end
     end
   end
 end
