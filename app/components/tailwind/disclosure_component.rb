2b0aff44d0ca64e1c9d1aca80e4e4b65ffca5f3d
Matthew Landauer <mlandauer@gmail.com>
2024-02-23 00:16:06 +0000

    Don't dynamically generate tailwind classes. Explicitly list them so they don't get optimised away

--- app/components/tailwind/disclosure_component.rb
+++ app/components/tailwind/disclosure_component.rb
@@ -7,10 +7,15 @@ module Tailwind
 
     renders_one :summary
 
+    SIZES = T.let({ "base" => "text-base", "xl" => "text-xl", "2xl" => "text-2xl" }.freeze, T::Hash[String, String])
+
     sig { params(size: String).void }
     def initialize(size:)
       super
-      @size = size
+      size_class = SIZES[size]
+      raise "Unexpected size: #{size}" if size_class.nil?
+
+      @size_class = T.let(size_class, String)
     end
   end
 end
