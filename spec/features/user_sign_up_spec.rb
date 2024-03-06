c28dd0ba7e50fde2216a1f478000aa2fa219434c
Matthew Landauer <mlandauer@gmail.com>
2024-03-06 01:21:59 +0000

    Follow most of the rest password flow

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -44,7 +44,7 @@ describe "Signing up for an API account" do
 
   describe "reset your password in the new design" do
     before do
-      sign_in create(:confirmed_user, tailwind_theme: true)
+      sign_in create(:confirmed_user, tailwind_theme: true, email: "matthew@oaf.org.au")
       visit root_path
       sign_out :user
       # Need to be logged out to visit the reset password page
@@ -60,5 +60,45 @@ describe "Signing up for an API account" do
       page.percy_snapshot("Reset password")
     end
     # rubocop:enable RSpec/NoExpectationExample
+
+    describe "putting in an email address" do
+      before do
+        fill_in "Email", with: "matthew@oaf.org.au"
+        click_button "Send me reset password instructions"
+      end
+
+      it "tells me to check my email" do
+        expect(page).to have_content("you will receive a password recovery link at your email address")
+      end
+
+      it "has sent an email" do
+        expect(unread_emails_for("matthew@oaf.org.au").size).to eq(1)
+      end
+
+      it "tells the user what to do" do
+        open_email("matthew@oaf.org.au")
+        # TODO: The subject line should be changed to "Planning Alerts" in /app/config/locales/devise.en.yml
+        # Do this after we've completely moved over to the new theme
+        expect(current_email).to have_subject("PlanningAlerts: Reset password instructions")
+        expect(current_email.default_part_body.to_s).to include("Please click the button below and follow the instructions")
+      end
+
+      describe "clicking the link in the email" do
+        before do
+          open_email("matthew@oaf.org.au")
+          click_email_link_matching(/reset_password_token/)
+        end
+
+        it "tells the user what to do" do
+          expect(page).to have_content("Change your password")
+        end
+
+        # rubocop:disable RSpec/NoExpectationExample
+        it "renders the page", js: true do
+          page.percy_snapshot("Reset password step 2")
+        end
+        # rubocop:enable RSpec/NoExpectationExample
+      end
+    end
   end
 end
