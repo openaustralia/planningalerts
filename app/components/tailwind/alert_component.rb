3520122acff01339acab7e3f03b9a6a9bf5b8be5
Matthew Landauer <mlandauer@gmail.com>
2024-05-16 00:36:04 +0000

    Inline tick svg

--- app/components/tailwind/alert_component.rb
+++ app/components/tailwind/alert_component.rb
@@ -10,25 +10,20 @@ module Tailwind
     def initialize(type:)
       super
 
+      @type = type
       case type
       when :success
         @bg_class = T.let("bg-lavender", String)
         @alignment_class = T.let("items-center", String)
         @padding_class = T.let("px-4 py-3", String)
-        @image_src = T.let("tailwind/tick.svg", String)
-        @image_alt = T.let("Success", String)
       when :congratulations
         @bg_class = T.let("bg-lavender", String)
         @alignment_class = T.let("items-start", String)
         @padding_class = T.let("px-8 py-8", String)
-        @image_src = T.let("tailwind/clapping.svg", String)
-        @image_alt = T.let("Congratulations", String)
       when :warning
         @bg_class = "bg-error-red"
         @alignment_class = T.let("items-center", String)
         @padding_class = T.let("px-4 py-3", String)
-        @image_src = "tailwind/warning.svg"
-        @image_alt = "Warning"
       else
         raise "Invalid value for type: #{type}"
       end
