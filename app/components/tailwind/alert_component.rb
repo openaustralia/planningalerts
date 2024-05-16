9c38fb024eca254b888c790c5796a16aeb075823
Matthew Landauer <mlandauer@gmail.com>
2024-05-16 01:07:35 +0000

    Small refactor

--- app/components/tailwind/alert_component.rb
+++ app/components/tailwind/alert_component.rb
@@ -19,15 +19,15 @@ module Tailwind
         @icon_name = T.let(:tick, Symbol)
         @icon_class = T.let("text-light-lavender", String)
       when :congratulations
-        @bg_class = T.let("bg-lavender", String)
-        @alignment_class = T.let("items-start", String)
-        @padding_class = T.let("px-8 py-8", String)
+        @bg_class = "bg-lavender"
+        @alignment_class = "items-start"
+        @padding_class = "px-8 py-8"
         @icon_name = :clapping
         @icon_class = "text-light-lavender"
       when :warning
         @bg_class = "bg-error-red"
-        @alignment_class = T.let("items-center", String)
-        @padding_class = T.let("px-4 py-3", String)
+        @alignment_class = "items-center"
+        @padding_class = "px-4 py-3"
         @icon_name = :warning
         @icon_class = "text-white"
       else
