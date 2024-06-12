0138de45ca29fc578c42ddd770651c286f8baa1d
Matthew Landauer <mlandauer@gmail.com>
2024-06-12 02:24:30 +0000

    Use new centre block component

--- app/components/tailwind/centre_block.rb
+++ app/components/tailwind/centre_block.rb
@@ -3,5 +3,13 @@
 
 module Tailwind
   class CentreBlock < ViewComponent::Base
+    extend T::Sig
+
+    sig { params(extra_classes: String).void }
+    def initialize(extra_classes: "")
+      super
+
+      @extra_classes = extra_classes
+    end
   end
 end
