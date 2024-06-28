fec1048545b436e1afa1173fb277403c8e6a1d41
Matthew Landauer <mlandauer@gmail.com>
2024-06-28 06:00:40 +0000

    Remove now unused routes

--- app/controllers/atdis_controller.rb
+++ app/controllers/atdis_controller.rb
@@ -10,39 +10,6 @@ class AtdisController < ApplicationController
     redirect_to get_involved_path
   end
 
-  # The job here is to take ugly posted parameters and redirect to a much simpler url
-  sig { void }
-  def test_redirect
-    params_feed = T.cast(params[:feed], ActionController::Parameters)
-
-    @feed = Feed.new(
-      base_url: params_feed[:base_url],
-      page: params_feed[:page],
-      lodgement_date_start: params_feed[:lodgement_date_start],
-      lodgement_date_end: params_feed[:lodgement_date_end],
-      last_modified_date_start: params_feed[:last_modified_date_start],
-      last_modified_date_end: params_feed[:last_modified_date_end],
-      street: params_feed[:street],
-      suburb: params_feed[:suburb],
-      postcode: params_feed[:postcode]
-    )
-    if @feed.valid?
-      redirect_to atdis_test_url(url: @feed.url)
-    else
-      render "test"
-    end
-  end
-
-  sig { void }
-  def feed
-    file = Feed.example_path(params[:number], params[:page] || 1)
-    if File.exist?(file)
-      render file:, content_type: Mime[:json], layout: false
-    else
-      render plain: "not available", status: :not_found
-    end
-  end
-
   # TODO: Move redirect to routes
   sig { void }
   def specification
--- config/routes.rb
+++ config/routes.rb
@@ -183,8 +183,6 @@ Rails.application.routes.draw do
 
   namespace :atdis do
     get :test
-    post :test, action: "test_redirect"
-    get "feed/:number/atdis/1.0/applications.json", action: "feed", as: :feed
     get :specification
   end
 
