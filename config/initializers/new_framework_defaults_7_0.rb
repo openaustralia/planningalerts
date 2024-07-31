cf0510d563c6ff6d4d8320ad3d211357746d8170
Matthew Landauer <mlandauer@gmail.com>
2024-07-31 02:25:05 +0000

    Switched over to all rails 7.0 defaults

--- config/initializers/new_framework_defaults_7_0.rb
+++ config/initializers/new_framework_defaults_7_0.rb
@@ -101,10 +101,9 @@ Rails.application.config.action_dispatch.default_headers = {
 # 
 # If you're upgrading and haven't set `cookies_serializer` previously, your cookie serializer
 # is `:marshal`. The default for new apps is `:json`.
-#
-# Rails.application.config.action_dispatch.cookies_serializer = :json
-#
-#
+
+Rails.application.config.action_dispatch.cookies_serializer = :json
+
 # To migrate an existing application to the `:json` serializer, use the `:hybrid` option.
 # 
 # Rails transparently deserializes existing (Marshal-serialized) cookies on read and
