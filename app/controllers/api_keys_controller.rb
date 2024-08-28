628bb02d55c4668b2dcf09036a57a290ad7413f7
Matthew Landauer <mlandauer@gmail.com>
2024-08-28 01:51:59 +0000

    Create trial keys

--- app/controllers/api_keys_controller.rb
+++ app/controllers/api_keys_controller.rb
@@ -24,7 +24,13 @@ class ApiKeysController < ApplicationController
 
     # For the time being limit users to only creating one API key
     if user.api_keys.empty?
-      user.api_keys.create!
+      if Flipper.enabled?(:api_keys_in_profile, current_user)
+        # Create a trial key which automatically expires and has a low daily limit
+        # TODO: Extract this
+        user.api_keys.create!(daily_limit: ApiKey.default_daily_limit_trial, expires_at: ApiKey.default_trial_duration_days.days.from_now)
+      else
+        user.api_keys.create!
+      end
       redirect_to redirect_url, notice: t(".success")
     else
       # This is a terrible way of showing an error but it's not likely to happen so I'm not going to worry about that
