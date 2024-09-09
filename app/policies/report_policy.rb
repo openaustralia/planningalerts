080fb3113b23aef130ca55bc5c5bc6b19728ba5c
Matthew Landauer <mlandauer@gmail.com>
2024-09-09 05:03:43 +0000

    reports are now accessible

--- app/policies/report_policy.rb
+++ app/policies/report_policy.rb
@@ -2,4 +2,27 @@
 # frozen_string_literal: true
 
 class ReportPolicy < ApplicationPolicy
+  extend T::Sig
+
+  sig { returns(T::Boolean) }
+  def index?
+    user.admin?
+  end
+
+  sig { returns(T::Boolean) }
+  def show?
+    user.admin?
+  end
+
+  sig { returns(T::Boolean) }
+  def destroy?
+    user.admin?
+  end
+
+  class Scope < ApplicationPolicy::Scope
+    sig { returns(ActiveRecord::Relation) }
+    def resolve
+      user.admin? ? scope.all : scope.none
+    end
+  end
 end
