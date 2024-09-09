ab83c4ecb1c1c77b992e51c6517083dd8f4c2296
Matthew Landauer <mlandauer@gmail.com>
2024-09-09 05:44:08 +0000

    api keys are now accessible

--- app/policies/api_key_policy.rb
+++ app/policies/api_key_policy.rb
@@ -2,4 +2,21 @@
 # frozen_string_literal: true
 
 class ApiKeyPolicy < ApplicationPolicy
+  def index?
+    user.admin?
+  end
+
+  def show?
+    user.admin?
+  end
+
+  def update?
+    user.admin?
+  end
+
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      user.admin? ? scope.all : scope.none
+    end
+  end
 end
