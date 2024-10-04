0f22222bab9d05f169bec956ae7b804be4fb0df0
Matthew Landauer <mlandauer@gmail.com>
2024-10-04 02:23:19 +0000

    Keep rubocop happy

--- spec/support/split_helper.rb
+++ spec/support/split_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module SplitHelper
   # Force a specific experiment alternative to always be returned:
   #   use_ab_test(signup_form: "single_page")
@@ -6,11 +8,13 @@ module SplitHelper
   #   use_ab_test(signup_form: "single_page", pricing: "show_enterprise_prices")
   #
   def use_ab_test(alternatives_by_experiment)
+    # rubocop:disable RSpec/AnyInstance
     allow_any_instance_of(Split::Helper).to receive(:ab_test) do |_receiver, experiment, &block|
       variant = alternatives_by_experiment.fetch(experiment) { |key| raise "Unknown experiment '#{key}'" }
-      block.call(variant) unless block.nil?
+      block&.call(variant)
       variant
     end
+    # rubocop:enable RSpec/AnyInstance
   end
 end
 
