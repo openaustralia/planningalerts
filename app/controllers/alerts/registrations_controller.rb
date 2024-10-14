a71ca4ea8f472e8986b5a1420655dd4283565f1e
Matthew Landauer <mlandauer@gmail.com>
2024-10-14 00:02:55 +0000

    Now sends email after registering

--- app/controllers/alerts/registrations_controller.rb
+++ app/controllers/alerts/registrations_controller.rb
@@ -11,5 +11,28 @@ module Alerts
         @alert = T.let(Alert.new(address: params[:user][:address], radius_meters: params[:user][:radius_meters]), T.nilable(Alert))
       end
     end
+
+    # TODO: Make link in email point to the right place
+    sig { void }
+    def create
+      super do
+        @alert = T.let(Alert.new(address: params[:user][:address], radius_meters: params[:user][:radius_meters]), T.nilable(Alert))
+      end
+    end
+
+    protected
+
+    # This is duplicated from users/registrations_controller
+    # TODO: Get rid of duplication
+    sig { params(_resource: User).returns(String) }
+    def after_inactive_sign_up_path_for(_resource)
+      helpers.check_email_user_registration_path
+    end
+
+    sig { returns(T::Hash[Symbol, String]) }
+    def sign_up_params
+      # TODO: Use strong parameters instead
+      { name: params[:user][:name], email: params[:user][:email], password: params[:user][:password] }
+    end
   end
 end
