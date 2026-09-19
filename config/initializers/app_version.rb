a1de00d5749c223845e3df78d940daf72634c1c0
Ian Heggie (OAF) <ian@oaf.org.au>
2026-09-19 21:54:32 +1000

    Fix RAILS_ENV=production crashing outside of capistrano destination
    Signed-off-by: Ian Heggie (OAF) <ian@oaf.org.au>

--- config/initializers/app_version.rb
+++ config/initializers/app_version.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 unless defined?(APP_VERSION)
-  APP_VERSION = if Rails.env.production?
-                  File.read(File.join(Rails.root, "REVISION"))[0..6]
+  revision_path = File.join(Rails.root, "REVISION")
+  APP_VERSION = if Rails.env.production? && File.size?(revision_path)
+                  File.read(revision_path)[0..6]
                 else
                   `git describe --always`
                 end
